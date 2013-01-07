087c6a2cfb6d27231b35481da12e1cc7f641fb80
Jimmy Sanford <ADS\Jimmy_Sanford>
2013-01-07 17:17:07 -0600

    changed primary menu animation from js to css3

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/navigation/PrimaryMenu.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/navigation/PrimaryMenu.js
@@ -9,18 +9,25 @@ Ext.define('Taco.view.navigation.PrimaryMenu', {
         tag: 'div',
         cls: 'taco-primary-menu-ct'
     },
-    autoShow: false,
+    autoShow: true,
     border: false,
     floating: true,
     header: false,
+    hideMode: 'offsets',
     id: 'primaryMenu',
     mixins: { bindable: 'Ext.util.Bindable' },
     plain: true,
     resizable: false,
     shadow: false,
+    x: 0,
+    y: 49,
     
     initComponent: function () {
         this.callParent(arguments);
+
+        this.on({
+            add: function (menu) { menu.hide(); }
+        });
     },
 
     bindStore: function (store, initial) {
@@ -63,13 +70,19 @@ Ext.define('Taco.view.navigation.PrimaryMenu', {
     showMenu: function () {
         var tBox = this.trigger.getEl().getPageBox();
 
-        this.show(null, function () {
-            this.animate({
-                duration: 400,
-                from: { opacity: 0, x: tBox.left, y: tBox.top },
-                to: { opacity: 1, x: tBox.left, y: tBox.bottom }
-            })
-        }, this);
+        if (Ext.isIE8m) {
+            // if IE8 or less use JS to animate
+            this.show(null, function () {
+                this.animate({
+                    duration: 400,
+                    from: { opacity: 0, x: tBox.left, y: tBox.top },
+                    to: { opacity: 1, x: tBox.left, y: tBox.bottom }
+                })
+            }, this);
+        } else {
+            // else let CSS handle the animation
+            this.show();
+        }
 
         this.trigger.addCls('expanded');
     },
@@ -81,15 +94,21 @@ Ext.define('Taco.view.navigation.PrimaryMenu', {
     hideMenu: function () {
         var tBox = this.trigger.getEl().getPageBox();
 
-        this.animate({
-            duration: 400,
-            from: { opacity: 1, x: tBox.left, y: tBox.bottom },
-            to: { opacity: 0, x: tBox.left, y: tBox.top },
-            callback: function () {
-                this.hide();
-            },
-            scope: this
-        });
+        if (Ext.isIE8m) {
+            // if IE8 or less use JS to animate
+            this.animate({
+                duration: 400,
+                from: { opacity: 1, x: tBox.left, y: tBox.bottom },
+                to: { opacity: 0, x: tBox.left, y: tBox.top },
+                callback: function () {
+                    this.hide();
+                },
+                scope: this
+            });
+        } else {
+            // else let CSS handle the animation
+            this.hide();
+        }
 
         this.trigger.removeCls('expanded');
     }
