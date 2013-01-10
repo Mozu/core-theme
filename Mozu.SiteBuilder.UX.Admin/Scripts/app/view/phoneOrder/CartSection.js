dbdcdf8f060536efe34e054af05135d9541931ef
James Zetlen <ADS\james_zetlen>
2013-01-10 07:28:21 -0600

    cart objects for phoneorder

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/phoneOrder/CartSection.js
@@ -0,0 +1,111 @@
+﻿/**
+ * @class Taco.view.phoneOrder.CartSection
+ * @author James Zetlen
+ */
+Ext.define('Taco.view.phoneOrder.CartSection', {
+    extend: 'Taco.core.ux.form.Form',
+    requires: ['Taco.core.ux.simplegrid.Grid'],
+  
+    title: 'Cart',
+    items: [{
+        xtype: 'simplegrid',
+        store: fakeProductStore,
+        columns: [{
+            dataIndex: 'fakeName',
+            text: 'Product',
+            editable: false
+        }]
+    }],
+    initComponent: function () {
+        var me = this,
+            fakeProductStore;
+
+        Ext.define('fakeProduct', {
+            extend: 'Ext.data.Model',
+            fields: ['fakeName']
+        });
+
+        fakeProductStore = Ext.create('Ext.data.Store', {
+            model: 'fakeProduct',
+            data: [
+                { fakeName: 'one' },
+                { fakeName: 'two' },
+                { fakeName: 'three' }
+            ]
+        });
+
+        this.form = Ext.create('Taco.core.ux.form.Form', {
+            manageHeight: false,
+            defaults: {
+                xtype: 'formeditor2',
+                collapsible: true,
+                frameHeader: false,
+                manageHeight: false,
+                margin: '0 0 14 0',
+                bodyPadding: '0 14 14'
+            },
+            items: [{
+                
+            }, {
+                
+            }, {
+                title: 'Shipping',
+                bodyPadding: '0 0 14',
+                items: [{
+                    xtype: 'formflexbox',
+                    width: 612,
+                    padding: '0 14',
+                    defaults: {
+                        xtype: 'textfield',
+                        labelAlign: 'top',
+                        labelSeparator: ''
+                    },
+                    items: [{
+                        name: 'firstName',
+                        fieldLabel: 'First Name',
+                        width: 280
+                    }, {
+                        name: 'lastName',
+                        fieldLabel: 'Last Name',
+                        width: 280
+                    }, {
+                        name: 'companyOrOrganization',
+                        fieldLabel: 'Company',
+                        width: 280
+                    }, {
+                        name: 'phoneNumbers',
+                        fieldLabel: 'Phone',
+                        width: 280
+                    }, {
+                        name: 'address1',
+                        fieldLabel: 'Address Line 1',
+                        width: 584
+                    }, {
+                        name: 'address2',
+                        fieldLabel: 'Address Line 2',
+                        width: 584
+                    }, {
+                        name: 'cityOrTown',
+                        fieldLabel: 'City',
+                        width: 280
+                    }, {
+                        name: 'stateOrProvince',
+                        fieldLabel: 'State',
+                        width: 116
+                    }, {
+                        name: 'postalOrZipCode',
+                        fieldLabel: 'ZIP Code',
+                        width: 116
+                    }]
+                }]
+            }]
+        });
+
+        Ext.apply(me.body, {
+            layout: 'auto',
+            items: [me.form]
+        });
+
+        this.callParent(arguments);
+    }
+});
