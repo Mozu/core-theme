67f78771124041212bffdf03dc6e7432ebfeb522
Chris Missal <ADS\chris_missal>
2013-01-10 13:32:56 -0600

    renames

--- Mozu.SiteBuilder.Mvc/Extensions/ServiceClientMessageHandler2.cs
+++ Mozu.SiteBuilder.Mvc/Extensions/ServiceClientMessageHandler2.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Mozu.Core;
 using Mozu.Core.Api.Client;
 using Mozu.Core.Settings;
@@ -11,15 +7,9 @@ namespace Mozu.SiteBuilder.Mvc.Extensions
 {
     public class ServiceClientMessageHandler2 : ServiceClientMessageHandler, Mozu.Core.Api.Contracts.Client.IServiceClientMessageHandler
     {
-        private static Lazy<FileSystemSettings> g_settings = new Lazy<FileSystemSettings>(() =>
-            {
-                var x = new FileSystemSettings();
-                x.Init();
-                return x;
+        private static Lazy<FileSystemSettings> g_settings = new Lazy<FileSystemSettings>(() => new FileSystemSettings());
 
-            });
-        public ServiceClientMessageHandler2(IApiContext context)
-            : base(context, g_settings.Value )
+        public ServiceClientMessageHandler2(IApiContext context) : base(context, g_settings.Value )
         {
         }
     }
