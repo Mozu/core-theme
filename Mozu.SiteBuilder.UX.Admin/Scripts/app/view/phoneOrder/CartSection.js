be0196bac5be9fde06738874c6def0efb27e4073
James Zetlen <ADS\james_zetlen>
2013-01-10 14:01:45 -0600

    checking in all pending changes, some of which are phantoms, to clear checkouts for chris

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/phoneOrder/CartSection.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/phoneOrder/CartSection.js
@@ -7,9 +7,38 @@ Ext.define('Taco.view.phoneOrder.CartSection', {
     requires: ['Taco.core.ux.simplegrid.Grid'],
   
     title: 'Cart',
-
+    tbar: [
+        {
+            xtype: 'textfield',
+            fieldLabel: 'SKU',
+            name: 'sku'
+        },
+        {
+            xtype: 'button',
+            text: 'Add',
+            disabled: true
+        },
+        {
+            xtype: 'button',
+            text: 'Configure',
+            disabled: true
+        },
+        {
+            xtype: 'button',
+            text: 'Search'
+        },
+        '->',
+        {
+            xtype: 'button',
+            text: 'Clear'
+        }
+    ],
     initComponent: function () {
         var me = this;
+
+        this.items = [
+            {
+                xtype: 'simplegrid',
 
         Ext.applyIf(this, {
             items: [{
