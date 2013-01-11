146440e9c6f5b258c841d7ea9ae712b35f56f992
Foster Hersey <ADS\foster_hersey>
2013-01-11 16:30:02 -0600

    Added a placeholder for sitemap.xml.

---
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/SitemapController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using Mozu.SiteBuilder.Mvc.Navigation;
+
+namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
+{
+    public class SitemapController : Controller
+    {
+        INavigationRepository _nav;
+
+        public SitemapController(INavigationRepository navigationRepository)
+        {
+            _nav = navigationRepository;
+        }
+
+        // GET: /sitemap.xml
+        public ActionResult Index()
+        {
+            var set = _nav.GetSet();
+
+            return Content("oh hai, im fixin ur sitemaps");
+        }
+
+    }
+}
