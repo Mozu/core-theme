b0ab6ac6e12aa0e43c88512f5cb723793d523174
Michael Elder <ADS\michael_elder>
2013-01-14 13:09:26 -0600

    Added file upload form/field for Google Webmaster Tools.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/generalSettings/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/generalSettings/Index.js
@@ -375,10 +375,68 @@ Ext.define('Taco.view.generalsettings.Index', {
             }
         });
 
-        me.sections = [me.about, me.notifications, me.analytics, me.robots];
+        me.googleWebmasterTools = Ext.create("Ext.form.Panel", {
+            noDirtyState: true,
+            title: 'Google Webmaster Tools',
+            titleCollapse: true,
+            collapsible: true,
+            collapsed: true,
+            collapseMode: 'header',
+            layout: "hbox",
+            header: {
+                border: '0 0 0 0'
+            },
+            defaults: {
+                labelAlign: 'top',
+                width: 350,
+                margin: '0 0 15 15'
+            },
+            items: [{
+                // TODO: Need instructions
+                xtype: "container",
+                items: [{
+                    xtype: "component",
+                    html: "<h2 style='padding: 10px 0'>Upload your file for Google Webmaster Tools</h2>" +
+                        "<p>If you're trying to connect your site to Google Webmaster Tools, you've come to the right place.  Upload a copy of the page you want to connect in the space provided below.</p>"
+                }, {
+                    xtype: 'button',
+                    text: 'Upload file',
+                    margin: "12 0 0 0",
+                    handler: function () {
+                        var form = me.googleWebmasterTools.getForm();
+                        if(form.isValid()) {
+                            //console.log("Attempting to upload your file", form.getFields());
+                            //window.top.fff = form.getFields();
+                            form.submit({
+                                url: "/admin/app/webtools/webmasterTools",
+                                waitMsg: "Uploading your file...",
+                                success: function (form, action) {
+                                    console.log("form/action", form, action);
+                                },
+                                failure: function () {
+                                    console.error("You done fucked up.");
+                                }
+                            })
+                        }
+                    }
+                }]
+            }, {
+                xtype: "filefield",
+                name: "gwtFile",
+                fieldLabel: "Upload your file for Google Webmaster Tools",
+                labelAlign: "top",
+                allowBlank: false,
+                buttonText: "Select file...",
+                margin: "0 0 0 75"
+            }]
+        });
+
+        me.googleWebmasterTools.getForm().timeout = 10;
+
+        me.sections = [me.about, me.notifications, me.analytics, me.robots, me.googleWebmasterTools];
 
         me.body = {
-            items: [me.about, me.ipAddresses, me.notifications, me.analytics, me.robots]
+            items: [me.about, me.ipAddresses, me.notifications, me.analytics, me.robots, me.googleWebmasterTools]
         };
 
         me.callParent(arguments);
@@ -435,7 +493,7 @@ Ext.define('Taco.view.generalsettings.Index', {
         }
 
         Ext.each(me.sections, function (section) {
-            if (section.form && !isDirty) {
+            if (!section.noDirtyState && section.form && !isDirty) {
                 var form = section.getForm();
                 isDirty = form.isDirty();
             }
