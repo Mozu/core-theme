7045e20e0d7d2451a8f43e40319c088a8ce570eb
James Zetlen <ADS\james_zetlen>
2013-01-21 15:31:53 -0600

    jquery upgrade stuff plus first google analytics pass

--- Mozu.SiteBuilder.UX.Themes/Themes/Core3/Resources/Scripts/Modules/loading-bars.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core3/Resources/Scripts/Modules/loading-bars.js
@@ -2,7 +2,7 @@
 
     return $(document).ready(function () {
         // no one can stop me from writing an IE polyfill for keyframes that uses marquee
-        if ($.browser.msie && $.browser.version <= 9) {
+        if (!Modernizr.cssanimations) {
             $('.mz-loading-bar').wrapInner("<marquee direction=\"right\">")
         }
     });
