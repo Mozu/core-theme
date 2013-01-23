2f7fe85bb481577a9f431f332be3eebbfe8f1ff2
Thomas Phipps <ADS\Thomas_Phipps>
2013-01-23 12:26:56 -0600

    fixing nugets

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/context/Site.js
@@ -0,0 +1,12 @@
+﻿Ext.define('Taco.core.context.SiteCollection', {
+    //extend: 'Ext.util.Observable',
+    id: -1,
+    name:'',
+    stagingHost: '',
+    defaultHost: '',
+    siteCollection:null,
+    constructor: function () {
+        var me = this;
+    }
+
+});
---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/context/TaContext.js
@@ -0,0 +1,78 @@
+﻿Ext.define('Taco.core.context.TaContext', {
+    extend: 'Ext.util.Observable',
+    requires: ['Taco.core.AppState'],
+
+    singleton: true,
+    currentSiteId: -1,
+    siteCollections:[],
+    currentSiteCollectionId:-1,
+    constructor: function() {
+        var me = this;
+    },
+    setCurrentSite: function (id) {
+        var me = this, newSite = me.findSite(id), newCol = newSite!= null ? newSite.siteCollection: null;
+        
+        
+        if (me.currentSiteId != id) {
+            if (!me.fireEvent('change', me,  { change: 'site', site: newSite, colection: newCol })) {
+                return false;
+            }
+        }
+        if (newCol != me.getCurrentSiteCollection()) {
+            if (!me.fireEvent('change', me, { change: 'sitecollection', site: newSite, colection: newCol })) {
+                return false;
+            }
+        }
+
+        me.currentSiteId = newSite != null ? newSite.id : null;
+        me.currentSiteCollectionId = newCol != null ? newCol.id : null;
+        return true;
+    },
+    setCurrentSiteCollection: function (id) {
+        var me = this, newCol, curSite = me.getCurrentSite();
+        Ext.forEach(me.siteCollections, function (sc) {
+            if (sc.id === id) {
+                newCol = sc;
+            }
+        });
+        if (newCol != me.getCurrentSiteCollection()) {
+            if (!me.fireEvent('change', me,{ change:'sitecollection', site: null, colection: newCol })) {
+                return false;
+            }
+        }
+        if (curSite != null ) {
+            if (!me.fireEvent('change', me,  { change: 'site', site: null, colection: newCol })) {
+                return false;
+            }
+        }
+        me.currentSiteId = null;
+        me.currentSiteCollectionId = newCol != null ? newCol.id : null;
+        return true;
+    },
+    getCurrentSiteCollection: function () {
+        return this.findSite(this.currentSiteId);
+    },
+    getCurrentSite:function() {
+        return this.findSite(this.currentSiteCollectionId);
+    },
+    findSiteCollection: function (id) {
+        var me=this,foundCol;
+        Ext.forEach(me.siteCollections, function (sc) {
+            if (sc.id === id) {
+                foundCol=sc;
+            }
+        });
+        return foundCol;
+    },
+    findSite: function (id) {
+        var foundSite, me = this;
+        Ext.forEach(me.siteCollections, function (sc) {
+            Ext.forEach(sc.sites, function (site) {
+                if (id === site.id) {
+                    foundSite = site;
+                }
+            });
+        });
+        return foundSite;
+    }
+});
