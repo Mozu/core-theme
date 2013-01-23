0e422d6e47c8e057dd2a49fcff98d85598ef358f
Thomas Phipps <ADS\Thomas_Phipps>
2013-01-23 12:26:56 -0600

    fixing nugets

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/context/SiteCollection.js
@@ -0,0 +1,10 @@
+﻿Ext.define('Taco.core.context.SiteCollection', {
+    //extend: 'Ext.util.Observable',
+    
+    id: -1,
+    sites:[],
+    constructor: function() {
+        var me = this;
+    }
+    
+});
