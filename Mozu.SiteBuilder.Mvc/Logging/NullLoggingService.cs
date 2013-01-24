59de8595d03e28b05b47c0fa21b8f29620e56b84
Foster Hersey <ADS\foster_hersey>
2013-01-24 16:12:10 -0600

    Implemented ILoggingService with a NullLoggingService to suppress the application startup exception.

---
+++ Mozu.SiteBuilder.Mvc/Logging/NullLoggingService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Mozu.Core.Logging;
+
+namespace Mozu.SiteBuilder.Mvc.Logging
+{
+    /// <summary>
+    /// This class provides a <code>NullLogger</code> when requested.
+    /// </summary>
+    public sealed class NullLoggingService : ILoggingService
+    {
+        private ILogger _nullLogger;
+
+        public NullLoggingService()
+        {
+            _nullLogger = new NullLogger();
+        }
+
+        public ILogger LoggerFor(Type type)
+        {
+            return _nullLogger;
+        }
+
+        public ILogger LoggerFor(string loggerName)
+        {
+            return _nullLogger;
+        }
+
+        public ILogger LoggerFor<T>()
+        {
+            return _nullLogger;
+        }
+    }
+}
