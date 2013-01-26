bae271dd9f8c7e27910ce4f228ff96958051841d
Michael Elder <ADS\michael_elder>
2013-01-25 18:09:56 -0600

    Structure and styles for Single Site Admin

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Categories.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Categories.js
@@ -6,17 +6,14 @@
 
 Ext.define('Taco.view.product.subform.Categories', {
     extend: 'Taco.view.product.subform.Subform',
-    defaults: {
-        labelAlign: 'top',
-        xtype: 'textfield'
-    },
 
     title: 'Categories',
 
     initComponent: function () {
         this.items = [{
             xtype: 'component',
-            html: '<div style="overflow: hidden; font-size: 16px; padding: 8px 0"><div style="float: left">You have no categories applied to this product</div><a href="javascript:;" style="float: right">Add category</a></div>'
+            width: '100%',
+            html: '<div style="width: 100%; overflow: hidden; font-size: 16px; padding: 8px 0"><div style="float: left">You have no categories applied to this product</div><a href="javascript:;" style="float: right">Add category</a></div>'
         }];
 
         this.callParent( arguments );
--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Subform.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Subform.js
@@ -6,9 +6,16 @@
 
 Ext.define('Taco.view.product.subform.Subform', {
     extend: 'Taco.core.ux.form.Form',
+
+//    componentCls: Taco.baseCSSPrefix + 'formeditor ,
+    bodyCls: [Taco.baseCSSPrefix + 'flexform ', Taco.baseCSSPrefix + 'product-admin-subform'],
+    bodyStyle: {
+        '-webkit-justify-content': 'flex-start'
+    },
+
     defaults: {
         xtype: 'textfield',
-        width: 400,
+        // width: 400,
         labelAlign: 'top',
         labelSeparator: ''
     }
