a796f8da048f38d82638a99f2c2c153338d31d86
Michael Elder <ADS\michael_elder>
2013-01-29 17:25:53 -0600

    Added allowOverrides property; added UI to enable/disable an override section

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Subform.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Subform.js
@@ -8,17 +8,13 @@ Ext.define('Taco.view.product.subform.Subform', {
     extend: 'Taco.core.ux.form.Form',
     requires: ['Taco.view.product.subform.OverrideContainer'],
 
-//    componentCls: Taco.baseCSSPrefix + 'formeditor ,
     bodyCls: [Taco.baseCSSPrefix + 'flexform ', Taco.baseCSSPrefix + 'product-admin-subform'],
     bodyStyle: {
         '-webkit-justify-content': 'flex-start'
     },
 
-    allowOverrides: false,
-
     defaults: {
         xtype: 'textfield',
-        // width: 400,
         labelAlign: 'top',
         labelSeparator: ''
     }
