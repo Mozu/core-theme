5a7771f6913a942991cdd8f020623ba2a662500a
Travis Johnson <ADS\Travis_Johnson>
2013-01-29 12:50:59 -0600

    Cleaned up the Test Suite

--- Mozu.SiteBuilder.UX.Admin/Tests/MozuTestSuite.js
+++ Mozu.SiteBuilder.UX.Admin/Tests/MozuTestSuite.js
@@ -4,359 +4,149 @@ Harness.configure({
     title: "Mozu Test Suite",
     waitForExtReady: true,
     autoCheckGlobals: false,
-    expectedGlobals: [
-        'Ext',
-        'Taco'
-    ],
+    expectedGlobals: ['Ext', 'Taco'],
 
-    preload: [
-        //'/admin/Scripts/ext/ext-all-debug.js',
-        '/admin/tests/chalupa/core.js',
-        '/admin/Scripts/app/app.js'
-    ],
+    preload: ['/admin/tests/chalupa/core.js', '/admin/Scripts/app/app.js'],
 
     hostPageUrl: '/admin?testHarnessMode=true'
 });
 
-Harness.start(
-    {
-        group: "Admin Application (Integration tests)",
-        expanded: true,
-        items: [
-            {
-                group: 'Dashboard',
-                expanded: false
-                /*items: [
-                    {
-                        url: 'integration/my-pickles-test.t.js',
-                        title: 'Click main navigation'
-                    }
-                ]*/
-            }
-            , {
-                group: 'Website',
-                expanded: false
-                /*items: [
-                    {
-                        url: '',
-                        title: 'Click main navigation'
-                    }
-                ]*/
-            }
-            , {
-                group: 'Products',
-                expanded: false,
-                items: [
-                    //{
-                    //    url: '',
-                    //    title: 'Click main navigation'
-                    //}
-                    {
-                        group: 'Products',
-                        expanded: false,
-                        items: [
-                            {
-                                url: 'integration/product-basic-edit-save.js',
-                                title: 'product-basic-edit-save'
-                            }
-                        ]
-                    }
-                    , {
-                        group: 'Inventory',
-                        expanded: false
-                        /*items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                    , {
-                        group: 'Categories',
-                        expanded: false
-                        /*, items: [
-                            {
-                                url: 'integration/category-is-draggable.t.js',
-                                title: 'Category is Draggable'
-                            }
-                        ]*/
-                    }
-                    , {
-                        group: 'Options',
-                        expanded: false
-                        /*items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                ]
-            }
-            , {
-                group: 'Orders',
-                expanded: false
-                /*items: [
-                    {
-                        url: '',
-                        title: 'Click main navigation'
-                    }
-                ]*/
-            }
-            , {
-                group: 'Customers',
-                expanded: false
-                /*items: [
-                    {
-                        url: '',
-                        title: 'Click main navigation'
-                    }
-                ]*/
-            }
-            , {
-                group: 'Discounts',
-                expanded: false
-                /*items: [
-                    {
-                        url: '',
-                        title: 'Click main navigation'
-                    }
-                ]*/
-            }
-            , {
-                group: 'Settings',
-                expanded: false,
-                items: [
-                    // {
-                    //     url: '',
-                    //     title: 'Click main navigation'
-                    // }
-                    {
-                        group: 'General',
-                        expanded: false
-                        /*items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                    , {
-                        group: 'Email',
-                        expanded: false
-                        /*items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                    , {
-                        group: 'Payment/Checkout',
-                        expanded: false
-                        /*items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                    , {
-                        group: 'Tax',
-                        expanded: false
-                        /*items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                    , {
-                        group: 'Shipping',
-                        expanded: false
-                        /*items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                    , {
-                        group: 'File Management',
-                        expanded: false
-                        /*items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                    , {
-                        group: 'Themes',
-                        expanded: false
-                        /*items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                    , {
-                        group: 'Theme Settings',
-                        expanded: false
-                       /* items: [
-                            {
-                                url: '',
-                                title: 'Click submenu navigation'
-                            }
-                        ]*/
-                    }
-                ]
-            }, {
-                group: 'Account',
-                expanded: true,
-                items: [
-                    {
-                        group: 'Roles',
-                        expanded: true,
-                        items: [{
-                            url: 'unit/roles/edit-form.t.js',
-                            title: 'Form Test?'
-                        }]
-                    }
-                ]
-            }
-        ]
+Harness.start({
+
+
+    /*********** ADMIN COMPONENT INTEGRATION TEST ***********/
+
+    group: "Admin Application (Integration tests)",
+    expanded: true,
+    items: [{
+        group: 'Dashboard',
+        expanded: false
     }, {
-        group: 'Components (Unit tests)',
+        group: 'Website',
+        expanded: false
+    }, {
+        group: 'Products',
+        expanded: false,
+        items: [{
+            group: 'Products',
+            expanded: false,
+            items: [{
+                url: 'integration/product-basic-edit-save.js',
+                title: 'product-basic-edit-save'
+            }]
+        }, {
+            group: 'Inventory',
+            expanded: false
+        }, {
+            group: 'Categories',
+            expanded: false
+        }, {
+            group: 'Options',
+            expanded: false
+        }]
+    }, {
+        group: 'Orders',
+        expanded: false
+    }, {
+        group: 'Customers',
+        expanded: false
+    }, {
+        group: 'Discounts',
+        expanded: false
+    }, {
+        group: 'Settings',
+        expanded: false,
+        items: [{
+            group: 'General',
+            expanded: false
+        }, {
+            group: 'Email',
+            expanded: false
+        }, {
+            group: 'Payment/Checkout',
+            expanded: false
+        }, {
+            group: 'Tax',
+            expanded: false
+        }, {
+            group: 'Shipping',
+            expanded: false
+        }, {
+            group: 'File Management',
+            expanded: false
+        }, {
+            group: 'Themes',
+            expanded: false
+        }, {
+            group: 'Theme Settings',
+            expanded: false
+        }]
+    }, {
+        group: 'Account',
         expanded: true,
-        alsoPreload: [
-            {
-                text: "Taco.showViewPort=false;"
-            }
-        ],
-        items: [
-            {
-                group: 'Grids',
-                expanded: true,
-                items: [
-                    {
-                        url: 'unit/grid-action-buttons.js',
-                        title: 'Inline row buttons are clickable'
-                    }
-                    ,{
-                        url: 'unit/treelist-row-is-draggable.t.js',
-                        title: 'TreeList rows are draggable'
-                    }
-                    /*{
-                        group: 'Product Grid',
-                        expanded: false,
-                        items: [
-                            {
-                                url: '',
-                                title: 'Do something'
-                            }
-                        ]
-                    }, {
-                        group: 'Inventory Grid',
-                        expanded: false,
-                        items: [
-                            {
-                                url: '',
-                                title: 'Do something'
-                            }
-                        ]
-                    }, {
-                        group: 'Categories Grid',
-                        expanded: false,
-                        items: [
-                            {
-                                url: '',
-                                title: 'Do something'
-                            }
-                        ]
-                    }, {
-                        group: 'Options Grid',
-                        expanded: false,
-                        items: [
-                            {
-                                url: '',
-                                title: 'Do something'
-                            }
-                        ]
-                    }, {
-                        group: 'Email Grid',
-                        expanded: false,
-                        items: [
-                            {
-                                url: '',
-                                title: 'Do something'
-                            }
-                        ]
-                    }, {
-                        group: 'Tax Grid',
-                        expanded: false,
-                        items: [
-                            {
-                                url: '',
-                                title: 'Do something'
-                            }
-                        ]
-                    }*/
-                ]
-            }, {
-                group: 'Modals',
-                expanded: false
-                /*items: [
-                    {
-                        url: '',
-                        title: 'Do something'
-                    }
-                ]*/
-            }, {
-                group: 'Buttons',
-                expanded: false
-                /*items: [
-                    {
-                        url: '',
-                        title: 'Do something'
-                    }
-                ]*/
-            }, {
-                group: 'Views',
-                expanded: false,
-                items: [
-                    {
-                        group: 'General Settings',
-                        expanded: false,
-                        items: [
-                            {
-                                url: "unit/disabling-google-analytics-disables-ua-field.js",
-                                waitForAppReady: true,
-                                title: "Disabling Google Analytics Disables UA Field"
-                            },
-                            {
-                                url: "unit/enabling-robots-txt-override-enabled-robots-field.js",
-                                waitForAppReady: true,
-                                title: "Enabling Robots.txt override Enables Robots Override Textarea"
-                            }
-                        ]
-                    }
-                ]
-            }, {
-                url: "unit/app-state-matches-uri.t.js",
+        items: [{
+            group: 'Roles',
+            expanded: true,
+            items: [{
+                url: 'unit/roles/edit-form.t.js',
+                title: 'Form Test?'
+            }]
+        }]
+    }]
+},
+
+
+
+/*********** UNIT TESTS ***********/
+
+{
+    group: 'Components (Unit tests)',
+    expanded: true,
+    alsoPreload: [{
+        text: "Taco.showViewPort=false;"
+    }],
+    items: [{
+        group: 'Grids',
+        expanded: true,
+        items: [{
+            url: 'unit/grid-action-buttons.js',
+            title: 'Inline row buttons are clickable'
+        }, {
+            url: 'unit/treelist-row-is-draggable.t.js',
+            title: 'TreeList rows are draggable'
+        }]
+    }, {
+        group: 'Modals',
+        expanded: false
+    }, {
+        group: 'Buttons',
+        expanded: false
+    }, {
+        group: 'Views',
+        expanded: false,
+        items: [{
+            group: 'General Settings',
+            expanded: false,
+            items: [{
+                url: "unit/disabling-google-analytics-disables-ua-field.js",
                 waitForAppReady: true,
-                // separateContext: true,
-                // hostPageUrl: '/admin',
-                alsoPreload: [],
-                title: "App State Should Match URI"
-            }
-        ]
+                title: "Disabling Google Analytics Disables UA Field"
+            }, {
+                url: "unit/enabling-robots-txt-override-enabled-robots-field.js",
+                waitForAppReady: true,
+                title: "Enabling Robots.txt override Enables Robots Override Textarea"
+            }]
+        }]
     }, {
-        url: "sanity-test.t.js",
-        hostPageUrl: '/admin'
-    }, {
-        url: 'speed-test.t.js',
-        title: "Speed's test"
-    }
-);
+        url: "unit/app-state-matches-uri.t.js",
+        waitForAppReady: true,
+        alsoPreload: [],
+        title: "App State Should Match URI"
+    }]
+}, {
+    url: "sanity-test.t.js",
+    hostPageUrl: '/admin'
+}, {
+    url: 'speed-test.t.js',
+    title: "Speed's test"
+});
