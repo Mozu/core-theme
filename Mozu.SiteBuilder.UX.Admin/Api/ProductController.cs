8285691eaacd9d3491f0662b6f86fc402b21a2a6
Foster Hersey <ADS\foster_hersey>
2013-01-29 13:00:25 -0600

    Added SEO information to Product/ProductInSiteInfo and made ProductController write to HttpRuntimeCache.

--- Mozu.SiteBuilder.UX.Admin/Api/ProductController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ProductController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Caching;
 using System.Web.Http;
 using AutoMapper;
 using Mozu.ProductAdmin.Contracts;
@@ -15,11 +17,32 @@ using Product = Mozu.SiteBuilder.UX.Admin.Api.Models.ProductModels.Product;
 
 namespace Mozu.SiteBuilder.UX.Admin.Api
 {
+    /// <summary>
+    /// Controller for products.
+    /// TODO: The Mozu service for this is not currently available.
+    /// TODO: I am storing all items in HttpRuntimeCache in the meantime.
+    /// TODO: Eventually we need to use the services.
+    /// </summary>
     [ServiceContract]
     public class ProductController : BaseController
     {
+        private const string PRODUCTS_CACHE_STRING = "_products";
         private readonly IProductWebApiClient _productClient;
 
+        /// <summary>
+        /// Static constructor.
+        /// Initializes our product list in the RuntimeCache.
+        /// </summary>
+        static ProductController()
+        {
+            Cache cache = HttpRuntime.Cache;
+            if (cache.Get(PRODUCTS_CACHE_STRING) == null)
+                cache.Add(PRODUCTS_CACHE_STRING, new List<DC.Product>(), null, Cache.NoAbsoluteExpiration, Cache.NoSlidingExpiration, CacheItemPriority.NotRemovable, null);
+        }
+
+        /// <summary>
+        /// Public constructor.
+        /// </summary>
         public ProductController(IProductWebApiClient productClient)
         {
             _productClient = productClient;
@@ -28,38 +51,64 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
         [WebGet(UriTemplate = "list")]
         public Task<Response<List<Product>>> GetProductList([FromUri]PagingParamaters pagingParams, [FromUri]FilterCollection extFilter)
         {
-            if (pagingParams.id != null)
+            // our in-memory product repository
+            List<DC.Product> productRepo = (List<DC.Product>)HttpRuntime.Cache.Get(PRODUCTS_CACHE_STRING);
+
+            
             {
-                DC.Product prod = _productClient.GetProductByProductCode(pagingParams.id, null).Result.ReadAsSync();
-                return List(Mapper.Map<Product>(prod));
+                if (pagingParams.id != null)
+                {
+                    // DC.Product prod = _productClient.GetProductByProductCode(pagingParams.id, null).Result.ReadAsSync();
+                    DC.Product prod;
+                    lock (productRepo)
+                    {
+                        prod = productRepo.FirstOrDefault(p => p.ProductCode == pagingParams.id);
+                    }
+                    return List(Mapper.Map<Product>(prod));
+                }
+
+                string filter = OldProductController.CreateFilter(extFilter);
+                string sort = OldProductController.CreateSort(pagingParams);
+
+                ProductCollection res;
+                // res = _productClient.GetProducts(pagingParams.startIndex, pagingParams.pageSize, sort, null, filter).Result.ReadAsAsync().Result;
+                lock (productRepo)
+                {
+                    res = new ProductCollection { Items = productRepo.ToList() };
+                }
+
+                return List(Mapper.Map<List<Product>>(res.Items), (int)res.TotalCount);
             }
-
-            string filter = OldProductController.CreateFilter(extFilter);
-            string sort = OldProductController.CreateSort(pagingParams);
-
-            ProductCollection res = _productClient.GetProducts(pagingParams.startIndex, pagingParams.pageSize, sort, null, filter).Result.ReadAsAsync().Result;
-
-            return List(Mapper.Map<List<Product>>(res.Items), (int)res.TotalCount);
         }
 
         [WebInvoke(UriTemplate = "create")]
         public Task<Response<List<Product>>> CreateProduct(List<Product> products)
         {
+            // our in-memory product repository
+            List<DC.Product> productRepo = (List<DC.Product>)HttpRuntime.Cache.Get(PRODUCTS_CACHE_STRING);
+
             List<Product> createdProducts = new List<Product>(products.Count);
             foreach (Product p in products)
             {
                 DC.Product dataModel = Mapper.Map<DC.Product>(p);
-
                 DC.Product returned;
-                try
+
+                lock (productRepo)
                 {
-                    returned = _productClient.AddProduct(dataModel).Result.ReadAsAsync().Result;
+                    productRepo.Add(dataModel);
+                    returned = dataModel;
                 }
-                catch
-                {
-                    // TODO: should probably do something about these errors
-                    continue;
-                }
+
+                // try
+                // {
+                //     returned = _productClient.AddProduct(dataModel).Result.ReadAsAsync().Result;
+                // }
+                // catch
+                // {
+                //     // TODO: should probably do something about these errors
+                //     continue;
+                // }
+
                 createdProducts.Add(Mapper.Map<Product>(returned));
             }
 
