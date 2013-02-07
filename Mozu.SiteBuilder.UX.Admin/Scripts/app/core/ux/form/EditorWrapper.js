6f187729bae4fea02345e64930913c5690f66a62
Travis Johnson <ADS\Travis_Johnson>
2013-02-07 12:39:58 -0600

    Product sort of working

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/EditorWrapper.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/EditorWrapper.js
@@ -96,8 +96,8 @@
             },
             savesuccess: function () {
                 console.log('savesuccess');
-                this.dirtybutton.setLoading(false);
-                this.dirtybutton.setDirty(false);
+                //this.dirtybutton.setLoading(false);
+                //this.dirtybutton.setDirty(false);
                 this.onComplete();
                 this.fireEvent('aftersave', this, this.record, this.isEdit());
             },
--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/GlobalForm.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/GlobalForm.js
@@ -30,7 +30,8 @@ Ext.define('Taco.view.product.GlobalForm', {
 
     initComponent: function () {
         var subformCfg = {
-            isGlobal: true
+            isGlobal: true,
+            product: this.record
         };
 
         this.items = [
