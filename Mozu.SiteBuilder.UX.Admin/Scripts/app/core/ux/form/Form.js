007b10569e21b42a791f098a071246ef4a34d950
Jimmy Sanford <ADS\Jimmy_Sanford>
2013-02-07 14:22:46 -0600

    undoing some breaking changes

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/Form.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/Form.js
@@ -7,7 +7,6 @@ Ext.define('Taco.core.ux.form.Form', {
     requires: ['Taco.core.ux.form.Tasks', 'Taco.core.ux.form.StepContainer'],
 
     componentCls: Taco.baseCSSPrefix + 'formform',
-    ui: 'taco',
 
     model: '',
     storeType: '',
