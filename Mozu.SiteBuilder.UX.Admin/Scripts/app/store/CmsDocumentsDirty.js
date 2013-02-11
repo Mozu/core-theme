500d3ec73bee4dc847ee5eaba74622594f0f9e75
Foster Hersey <ADS\foster_hersey>
2013-02-11 16:10:02 -0600

    Added CmsController.

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/store/CmsDocumentsDirty.js
@@ -0,0 +1,15 @@
+﻿/**
+* @class Taco.store.CmsDocumentsStore
+* @author Foster Hersey
+* The CmsDocuments store
+* Hint: It's a CmsDocument with unpublished changes.
+*/
+
+
+    Ext.define('Taco.store.CmsDocumentsDirty', {
+        extend: 'Ext.data.Store',
+        model: 'Taco.model.CmsDocumentDirty',
+        pageSize: 25,
+        remoteSort: true,
+        remoteFilter: true
+    });
