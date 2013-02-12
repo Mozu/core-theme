9cab7ca5733f629621f5cabea7ae5a73964b0fea
Travis Johnson <ADS\Travis_Johnson>
2013-02-11 18:06:08 -0600

    Product tabs seems to be working, mostly

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/tab/Picker.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/tab/Picker.js
@@ -9,7 +9,7 @@ Ext.define('Taco.core.ux.tab.Picker', {
         'Ext.form.field.Checkbox',
         'Ext.form.CheckboxGroup'
     ],
-    componentCls: Taco.baseCSSPrefix + 'available-site-list',
+    componentCls: Taco.baseCSSPrefix + 'tab-picker',
     floating: true,
     shadow: false,
     idTpl: '{id}',
@@ -17,7 +17,6 @@ Ext.define('Taco.core.ux.tab.Picker', {
     hidden: true,
 
     initComponent: function () {
-        this.checkboxes = [];
 
         this.addEvents([
             /**
@@ -29,26 +28,45 @@ Ext.define('Taco.core.ux.tab.Picker', {
         this.idTpl = new Ext.Template(this.idTpl);
         this.displayTpl = new Ext.Template(this.displayTpl);
 
-        Ext.each(this.data, function (record) {
-            var id = this.idTpl.apply(record);
-            this.checkboxes.push({
-                id: id,
-                boxLabel: this.displayTpl.apply(record),
-                checked: Ext.Array.contains(this.checkedItems, id),
-                data: record
-            });
-        }, this);
+        
 
         this.checkboxGroup = Ext.widget({
             xtype: 'checkboxgroup',
             columns: 1,
-            vertical: true,
-            items: this.checkboxes
+            vertical: true
         });
 
         this.items = [this.checkboxGroup];
 
         this.callParent(arguments);
+
+        this.buildCheckBoxes();
+    },
+
+    buildCheckBoxes: function (checkedItems) {
+        var checkboxes = [];
+
+        if (!checkedItems) {
+            checkedItems = this.checkedItems;
+        }
+
+        Ext.suspendLayouts();
+        
+        this.checkboxGroup.removeAll();
+
+        Ext.each(this.data, function (record) {
+            var id = this.idTpl.apply(record);
+            checkboxes.push({
+                id: id,
+                boxLabel: this.displayTpl.apply(record),
+                checked: Ext.Array.contains(checkedItems, id),
+                data: record
+            });
+        }, this);
+
+        this.checkboxGroup.add(checkboxes);
+
+        Ext.resumeLayouts();
     },
 
     getCheckedRecords: function () {
@@ -56,15 +74,38 @@ Ext.define('Taco.core.ux.tab.Picker', {
     },
 
     show: function () {
-        this.currentItems = this.getCheckedRecords()
-        this.getEl().addCls('list-open');
+        var el = this.el || this.protoEl;
+        el.addCls('list-open');
+        this.currentItems = this.getCheckedRecords();
         this.callParent(arguments);
+
+        this.alignTo(this.positionNextTo.getEl(), 'tr-br?');
     },
 
     hide: function () {
+        var newValues = this.getCheckedRecords()
+            oldValues = this.currentItems,
+            noChange = true;
         this.getEl().removeCls('list-open');
         this.callParent(arguments);
-        this.fireEvent('selectionchange', this, this.getCheckedRecords(), this.currentItems);
+        
+        
+        if (newValues.length === oldValues.length) {
+            Ext.each(newValues, function (newValue, index) {
+                if (newValue !== oldValues[index]) {
+                    noChange = false;
+                    return false;
+                }
+            });
+        } else {
+            noChange = false
+        }
+
+        if (noChange) {
+            return;
+        }
+
+        this.fireEvent('selectionchange', this, newValues, oldValues);
     },
 
     toggle: function () {
