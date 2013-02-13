630342f3422f1489c497b6720b56266f602d920a
Jimmy Sanford <ADS\Jimmy_Sanford>
2013-02-13 15:10:45 -0600

    fix to itembrowser, grid.panel

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/grid/Panel.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/grid/Panel.js
@@ -8,24 +8,30 @@ Ext.define('Taco.core.ux.grid.Panel', {
     requires: ['Taco.core.ux.grid.Header', 'Taco.core.ux.grid.RowExpander', 'Taco.core.ux.form.SelectField'],
     alias: 'widget.taco.gridpanel',
 
+    actionColumn: {
+        xtype: 'templatecolumn',
+        text: 'Actions',
+        width: 150,
+        tpl: '<div class="taco-actions-control"></div>'
+    },
+
     initComponent: function () {
         var me = this;
 
-        this.columns = this.initColumns();
+        this.columns = this.initColumns(this.columns);
 
         this.callParent(arguments);
     },
 
-    initColumns: function () {
-        var columns = this.columns || [];
+    /**
+     * Alter the provided columns before the grid is configured with them.
+     * @return {Object[]} The modified column configurations.
+     */
+    initColumns: function (columns) {
+        if (Ext.isEmpty(columns)) columns = [];
 
-        if (this.actions) {
-            columns.push({
-                xtype: 'templatecolumn',
-                text: 'Actions',
-                width: 150,
-                tpl: '<div class="taco-actions-control"></div>'
-            });
+        if (this.actions && !Ext.Array.contains(columns, this.actionColumn)) {
+            columns.push(this.actionColumn);
         }
 
         return columns;
