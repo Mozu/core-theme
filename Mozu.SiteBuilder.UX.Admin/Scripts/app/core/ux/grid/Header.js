e8d6de77eaa53521ac430e533f1a264cc86e63fa
Jimmy Sanford <ADS\Jimmy_Sanford>
2013-02-13 17:27:14 -0600

    column header overridden; documentation

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/grid/Header.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/grid/Header.js
@@ -4,40 +4,41 @@
  * Overrides Ext.grid.header.Container.
  * 
  */
- Ext.define('Taco.core.ux.grid.Header', {
-     override: 'Ext.grid.header.Container',
+Ext.define('Taco.core.ux.grid.Header', {
+    override: 'Ext.grid.header.Container',
+    requires: ['Taco.core.ux.grid.HeaderDropZone'],
 
-     constructor: function () {
-         this.callParent(arguments);
-     },
+    constructor: function () {
+        this.callParent(arguments);
+    },
 
-     getMenuItems: function () {
-         var me = this,
+    getMenuItems: function () {
+        var me = this,
 
-         menuItems = me.enableColumnHide ? me.getColumnMenu(me) : [];
+        menuItems = me.enableColumnHide ? me.getColumnMenu(me) : [];
 
-         return menuItems;
-     },
+        return menuItems;
+    },
 
-     getMenu: function () {
-         var me = this;
+    getMenu: function () {
+        var me = this;
 
-         if (!me.menu) {
-             me.menu = new Ext.menu.Menu({
-                 hideOnParentHide: false,
-                 title: "Customize Columns",
-                 minWidth: 150,
-                 shadow: false,
-                 showSeparator: false,
-                 items: me.getMenuItems(),
-                 listeners: {
-                     deactivate: me.onMenuDeactivate,
-                     scope: me
-                 }
-             });
-             me.updateMenuDisabledState();
-             me.fireEvent('menucreate', me, me.menu);
-         }
-         return me.menu;
-     }
- });
+        if (!me.menu) {
+            me.menu = new Ext.menu.Menu({
+                hideOnParentHide: false,
+                title: "Customize Columns",
+                minWidth: 150,
+                shadow: false,
+                showSeparator: false,
+                items: me.getMenuItems(),
+                listeners: {
+                    deactivate: me.onMenuDeactivate,
+                    scope: me
+                }
+            });
+            me.updateMenuDisabledState();
+            me.fireEvent('menucreate', me, me.menu);
+        }
+        return me.menu;
+    }
+});
--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/grid/Panel.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/grid/Panel.js
@@ -1,6 +1,7 @@
 /**
  * @class Taco.core.ux.grid.Panel
  * @author Jimmy Sanford
+ * A base class for grid panels.
  * 
  */
 Ext.define('Taco.core.ux.grid.Panel', {
@@ -8,11 +9,31 @@ Ext.define('Taco.core.ux.grid.Panel', {
     requires: ['Taco.core.ux.grid.Header', 'Taco.core.ux.grid.RowExpander', 'Taco.core.ux.form.SelectField'],
     alias: 'widget.taco.gridpanel',
 
+    /**
+     * @cfg {Ext.grid.column.Column} actionColumn
+     * A column definition object that defines the model-independent, rightmost column in the grid.
+     */
     actionColumn: {
         xtype: 'templatecolumn',
         text: 'Actions',
         width: 150,
+        draggable: false,
+        hideable: false,
+        resizable: false,
+        sortable: false,
         tpl: '<div class="taco-actions-control"></div>'
+    },
+
+    /**
+     * @cfg {Object} columnDefaults
+     * This option is a means of applying default settings to all added columns. Defaults are applied so as not
+     * to override existing properties (see {@link Ext#applyIf}).
+     */
+    columnDefaults: {
+        draggable: true,
+        hideable: true,
+        resizable: false,
+        sortable: true
     },
 
     initComponent: function () {
@@ -30,9 +51,18 @@ Ext.define('Taco.core.ux.grid.Panel', {
     initColumns: function (columns) {
         if (Ext.isEmpty(columns)) columns = [];
 
-        if (this.actions && !Ext.Array.contains(columns, this.actionColumn)) {
+        if (this.actions && this.actionColumn && !Ext.Array.contains(columns, this.actionColumn)) {
             columns.push(this.actionColumn);
         }
+
+        Ext.Array.each(columns, function (col) {
+            Ext.applyIf(col, this.columnDefaults);
+
+            if (col.draggable === false) {
+                col.cls = col.cls || '';
+                col.cls = 'frozen ' + col.cls;
+            }
+        }, this);
 
         return columns;
     },
