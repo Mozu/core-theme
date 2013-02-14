63ab36daed089e3f3c7965e89c4137c3b3f20d1f
James Zetlen <ADS\james_zetlen>
2013-02-14 11:06:19 -0600

    fixing under-tpt

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/Index.js
@@ -82,7 +82,7 @@ Ext.define('Taco.view.product.Index', {
                     pluginId: 'expander',
                     rowBodyTpl: new Ext.XTemplate(
                         '<tpl for="productInSites"><tr class="x-grid-row-body">',
-                            '<td colspan="2" class="x-grid-cell"><div class="x-grid-cell-inner"></div></td>',
+                            '<td colspan="3" class="x-grid-cell"><div class="x-grid-cell-inner"></div></td>',
                             '<td class="x-grid-cell"><div class="x-grid-cell-inner"><a href="#" class="taco-launch-editor" data-site-id="{siteId}">{parent.productName}</a></div></td>',
                             '<td class="x-grid-cell"><div class="x-grid-cell-inner">{price:this.formatPrice}</div></td>',
                             '<td class="x-grid-cell"><div class="x-grid-cell-inner">{salePrice:this.formatPrice}</div></td>',
