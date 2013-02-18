7ee262b30e84bd8409e3ad583638b6a8f4e19800
Chris Missal <ADS\chris_missal>
2013-02-18 12:13:06 -0600

    removed old product option classes

--- Mozu.SiteBuilder.UX.Admin/Api/Models/Options/ProductProperty.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/Options/ProductProperty.cs
@@ -5,24 +5,24 @@ using System.Web;
 
 namespace Mozu.SiteBuilder.UX.Admin.Api.Models.Options
 {
-    public class ProductProperty
-    {
-        public int? Id
-        { get; set; }
-        public List<ProductPropertyValue> SelectedValues
-        {
-            get;
-            set;
-        }
+    //public class ProductProperty
+    //{
+    //    public int? Id
+    //    { get; set; }
+    //    public List<ProductPropertyValue> SelectedValues
+    //    {
+    //        get;
+    //        set;
+    //    }
 
-        public string productCode { get; set; }
-    }
-    public class ProductPropertyValue
-    {
-        public int? Id
-        { get; set; }
-        public string Value
-        { get; set; }
+    //    public string productCode { get; set; }
+    //}
+    //public class ProductPropertyValue
+    //{
+    //    public int? Id
+    //    { get; set; }
+    //    public string Value
+    //    { get; set; }
        
-    }
+    //}
 }
--- Mozu.SiteBuilder.UX.Admin/Api/ProductVariationController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ProductVariationController.cs
@@ -19,80 +19,80 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
     [ServiceContract]
     public class ProductVariationController : BaseController
     {
-        private readonly IAttributeWebApiClient _attClient;
+        //private readonly IAttributeWebApiClient _attClient;
 
 
-        private readonly IProductWebApiClient _productClient;
+        //private readonly IProductWebApiClient _productClient;
 
-        private readonly IAttributeSetWebApiClient _attSetClient;
+        //private readonly IAttributeSetWebApiClient _attSetClient;
 
-        public ProductVariationController(IAttributeWebApiClient attClient,
-           IAttributeSetWebApiClient attSetClient,
-            IProductWebApiClient productClient)
-        {
-            _attClient = attClient;
-            _attSetClient = attSetClient;
-            _productClient = productClient;
-        }
+        //public ProductVariationController(IAttributeWebApiClient attClient,
+        //   IAttributeSetWebApiClient attSetClient,
+        //    IProductWebApiClient productClient)
+        //{
+        //    _attClient = attClient;
+        //    _attSetClient = attSetClient;
+        //    _productClient = productClient;
+        //}
         
-        [WebInvoke(UriTemplate = "edit")]
-        public Task<Response<List<ProductVariation>>> EditProductVariants(List<ProductVariation> variants)
-        {
-            if (variants.Count > 0)
-            {
-                var dcs = Mapper.Map<List<DC.ProductVariation>>(variants);
+        //[WebInvoke(UriTemplate = "edit")]
+        //public Task<Response<List<ProductVariation>>> EditProductVariants(List<ProductVariation> variants)
+        //{
+        //    if (variants.Count > 0)
+        //    {
+        //        var dcs = Mapper.Map<List<DC.ProductVariation>>(variants);
 
 
 
-                var ret = _productClient.UpdateProductVariations(new DC.ProductVariationCollection() { Items = dcs }, variants[0].ProductCode).Result.ReadAsSync();
-                var productVariations = Mapper.Map<List<ProductVariation>>(ret.Items);
+        //        var ret = _productClient.UpdateProductVariations(new DC.ProductVariationCollection() { Items = dcs }, variants[0].ProductCode).Result.ReadAsSync();
+        //        var productVariations = Mapper.Map<List<ProductVariation>>(ret.Items);
 
-                return List(productVariations);
-            }
+        //        return List(productVariations);
+        //    }
 
-            return EmptyList<ProductVariation>();
-        }
+        //    return EmptyList<ProductVariation>();
+        //}
         
-        [WebGet(UriTemplate = "list")]
-        public Task<Response<List<ProductVariation>, VariantMetaData>> GetProductVariants(PagingParamaters pagingParams, FilterCollection extFilter)
-        {
-            var productCode = extFilter.GetValue<string>("productCode");
+        //[WebGet(UriTemplate = "list")]
+        //public Task<Response<List<ProductVariation>, VariantMetaData>> GetProductVariants(PagingParamaters pagingParams, FilterCollection extFilter)
+        //{
+        //    var productCode = extFilter.GetValue<string>("productCode");
             
-            if (string.IsNullOrEmpty(productCode))
-            {
-                return ListWithMetaData(default(List<ProductVariation>), default(VariantMetaData));
-            }
+        //    if (string.IsNullOrEmpty(productCode))
+        //    {
+        //        return ListWithMetaData(default(List<ProductVariation>), default(VariantMetaData));
+        //    }
             
-            var dcs = _productClient.GetProductVariations(productCode, pagingParams.startIndex, pagingParams.pageSize, null, null ).Result.ReadAsSync();
+        //    var dcs = _productClient.GetProductVariations(productCode, pagingParams.startIndex, pagingParams.pageSize, null, null ).Result.ReadAsSync();
 
-            var metaData = new VariantMetaData()
-            {
-                fields = new List<VariantMetaDataFieldItem>()
-            };
+        //    var metaData = new VariantMetaData()
+        //    {
+        //        fields = new List<VariantMetaDataFieldItem>()
+        //    };
 
-            if ( dcs.Items != null && dcs.Items.Count> 0 )
-            {
-                for ( var i =0 ; i< dcs.Items[0].Options.Count ; i++ )
-                {
-                    metaData.fields.Add(new VariantMetaDataFieldItem()
-                        {
-                            caption = dcs.Items[0].Options [i].AttributeInternalName ,
-                            fieldName = "optionValue" + (i+1),
-                            key = dcs.Items[0].Options [i].AttributeId.ToString (CultureInfo.InvariantCulture)
-                        });
-                };
-            }
+        //    if ( dcs.Items != null && dcs.Items.Count> 0 )
+        //    {
+        //        for ( var i =0 ; i< dcs.Items[0].Options.Count ; i++ )
+        //        {
+        //            metaData.fields.Add(new VariantMetaDataFieldItem()
+        //                {
+        //                    caption = dcs.Items[0].Options [i].AttributeInternalName ,
+        //                    fieldName = "optionValue" + (i+1),
+        //                    key = dcs.Items[0].Options [i].AttributeId.ToString (CultureInfo.InvariantCulture)
+        //                });
+        //        };
+        //    }
 
-            var productVariations = Mapper.Map<List<ProductVariation>>(dcs.Items ?? new List<DC.ProductVariation>()).Select(x => FixUp(x, productCode)).ToList();
+        //    var productVariations = Mapper.Map<List<ProductVariation>>(dcs.Items ?? new List<DC.ProductVariation>()).Select(x => FixUp(x, productCode)).ToList();
 
-            return ListWithMetaData(productVariations, metaData, (int) dcs.TotalCount);
-        }
+        //    return ListWithMetaData(productVariations, metaData, (int) dcs.TotalCount);
+        //}
 
-        static ProductVariation FixUp(ProductVariation val, string productCode)
-        {
-            val.ProductCode = productCode;
-            return val;
-        }
+        //static ProductVariation FixUp(ProductVariation val, string productCode)
+        //{
+        //    val.ProductCode = productCode;
+        //    return val;
+        //}
     }
 
      [DataContract()]
