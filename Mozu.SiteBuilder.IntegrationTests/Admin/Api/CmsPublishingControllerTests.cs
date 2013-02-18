eb0d5df251136f9d93e58739c6e310d734608169
Foster Hersey <ADS\foster_hersey>
2013-02-18 17:53:07 -0600

    Addd more unit tests for CmsPublishingController.

--- Mozu.SiteBuilder.IntegrationTests/Admin/Api/CmsPublishingControllerTests.cs
+++ Mozu.SiteBuilder.IntegrationTests/Admin/Api/CmsPublishingControllerTests.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoMapper;
 using Mozu.Content.Contracts.Clients;
-using Mozu.SiteBuilder.Mvc.Models.CMS.Admin;
 using Mozu.SiteBuilder.UX.Admin.Api;
 using Mozu.SiteBuilder.UX.Admin.Api.Models;
 using Mozu.SiteBuilder.UX.Admin.MockServices;
+using Mozu.SiteBuilder.UX.Models.Admin.CMS;
 using NSubstitute;
 using NUnit.Framework;
 using Should;
@@ -23,11 +24,13 @@ namespace Mozu.SiteBuilder.IntegrationTests.Admin.Api
             Items = new List<DC.Document> {
                 new DC.Document {
                     Id = "12345",
-                    Name = "Test Document 1"
+                    Name = "Test Document 1",
+                    DocumentListName = "Pages1"
                 },
                 new DC.Document {
                     Id = "56789",
-                    Name = "Test Document 2"
+                    Name = "Test Document 2",
+                    DocumentListName = "Pages2"
                 }
             }
         };
@@ -37,31 +40,31 @@ namespace Mozu.SiteBuilder.IntegrationTests.Admin.Api
         {
             _documentClient = Substitute.For<IMoreAwesomeDocumentWebApiClient>();
             _testedController = new CmsPublishingController((IMoreAwesomeDocumentWebApiClient)_documentClient);
-
-            // set up Get mock.
-            _documentClient.Get(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(
-                x => {
-                    string documentId = (string)x[1];
-                    DC.Document doc = _mocks.Items.First(d => d.Id == documentId);
-                    return new TestResponse<DC.Document>(doc).Task;
-                }
-            );
             
             // set up GetDrafts mock.
             _documentClient.GetDrafts(Arg.Any<string>(), Arg.Any<String>(), Arg.Any<int?>(), Arg.Any<int?>()).Returns(
-                x => new TestResponse<DC.PagedCollection<DC.Document>>(_mocks).Task
+                args => new TestResponse<DC.PagedCollection<DC.Document>>(_mocks).Task
+            );
+
+            // set up PublishDocuments mock.
+            _documentClient.PublishDocuments(Arg.Any<string>(), Arg.Any<List<string>>()).Returns(
+                args => new TestResponse<List<string>>((List<string>)args[1]).Task
+            );
+
+            // set up Discard mock.
+            _documentClient.Discard(Arg.Any<string>(), Arg.Any<List<string>>()).Returns(
+                args => new TestResponse<List<string>>((List<string>)args[1]).Task
             );
         }
 
         [Test]
-        public void Get_list_with_an_id_should_give_one_record_back()
+        public void Get_list_with_an_id_should_fail()
         {
             var pagingParams = new PagingParamaters { id = _mocks.Items.Last().Id };
             var extFilter = new FilterCollection();
-            Response<List<Document>> response = _testedController.ListDirtyDocuments(pagingParams, extFilter).Result;
+            Response<List<DocumentDraft>> response = _testedController.ListDirtyDocuments(pagingParams, extFilter).Result;
 
-            response.Items.Count.ShouldEqual(1);
-            response.Items[0].Id.ShouldEqual(pagingParams.id);
+            response.Success.ShouldBeFalse();
         }
 
         [Test]
@@ -69,13 +72,59 @@ namespace Mozu.SiteBuilder.IntegrationTests.Admin.Api
         {
             var pagingParams = new PagingParamaters();
             var extFilter = new FilterCollection();
-            Response<List<Document>> response = _testedController.ListDirtyDocuments(pagingParams, extFilter).Result;
+            Response<List<DocumentDraft>> response = _testedController.ListDirtyDocuments(pagingParams, extFilter).Result;
 
             response.Items.Count.ShouldEqual(_mocks.Items.Count);
             response.Items[0].Id.ShouldEqual(_mocks.Items[0].Id);
             response.Items[0].Name.ShouldEqual(_mocks.Items[0].Name);
         }
 
+        [Test]
+        public void Publish_item_should_work()
+        {
+            DocumentDraft d = Mapper.Map<DocumentDraft>(_mocks.Items[0]);
+
+            List<DocumentDraft> itemlist = new List<DocumentDraft> { d };
+
+            var response = _testedController.Publish(itemlist).Result;
+            _documentClient.Received(1).PublishDocuments(d.DocumentListName, Arg.Is<List<string>>(arg => arg.Count == 1 && arg[0] == d.Id));
+
+            response.Success.ShouldBeTrue();
+            response.Items[0].ShouldEqual(d.Id);
+        }
+
+        [Test]
+        public void Publish_all_should_work()
+        {
+            var response = _testedController.PublishAll().Result;
+
+            response.Success.ShouldBeTrue();
+            response.Total.ShouldEqual(_mocks.Items.Count);
+        }
+
+        [Test]
+        public void Discard_item_should_work()
+        {
+            DocumentDraft d = Mapper.Map<DocumentDraft>(_mocks.Items[0]);
+
+            List<DocumentDraft> itemlist = new List<DocumentDraft> { d };
+
+            var response = _testedController.Discard(itemlist).Result;
+
+            _documentClient.Received(1).Discard(d.DocumentListName, Arg.Is<List<string>>(arg => arg.Count == 1 && arg[0] == d.Id));
+
+            response.Success.ShouldBeTrue();
+            response.Items[0].ShouldEqual(d.Id);
+        }
+
+        [Test]
+        public void Discard_all_should_work()
+        {
+            var response = _testedController.DiscardAll().Result;
+
+            response.Success.ShouldBeTrue();
+            response.Total.ShouldEqual(_mocks.Items.Count);
+        }
         // TODO: some more testing
     }
 }
