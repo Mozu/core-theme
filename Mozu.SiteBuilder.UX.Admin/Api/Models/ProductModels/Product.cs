4bc7d723e59cca3d92182124a8e47fec19cf4136
Thomas Phipps <ADS\Thomas_Phipps>
2013-02-19 17:10:41 -0600

    stock on hand bug fix

--- Mozu.SiteBuilder.UX.Admin/Api/Models/ProductModels/Product.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/ProductModels/Product.cs
@@ -76,6 +76,15 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.Models.ProductModels
         [DataMember(EmitDefaultValue = false, Name = "stockOnHand")]
         public int? StockOnHand { get; set; }
 
+
+
+        [DataMember(EmitDefaultValue = false, Name = "stockOnHandAdjustment")]
+        public StockOnHandAdjustment StockOnHandAdjustment { get; set; }
+
+
+
+        
+
         /// <summary>
         /// Hide when out of stock.
         /// </summary>
