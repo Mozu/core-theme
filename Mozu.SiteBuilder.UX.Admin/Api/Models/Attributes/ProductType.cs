6e0e7d581ca02d5e0acecead949e059ac309be1b
Foster Hersey <ADS\foster_hersey>
2013-02-19 14:20:12 -0600

    Added a test fixture for ProductTypeController.

--- Mozu.SiteBuilder.UX.Admin/Api/Models/Attributes/ProductType.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/Attributes/ProductType.cs
@@ -7,7 +7,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.Models.Attributes
     public class ProductType
     {
         [DataMember(Name = "id")]
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [DataMember(Name = "name")]
         public string Name { get; set; }
