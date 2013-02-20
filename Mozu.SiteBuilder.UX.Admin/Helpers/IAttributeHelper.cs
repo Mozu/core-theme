ffa0c28401cb9b28e7f336ffaa8ca4d1055e4973
Chris Missal <ADS\chris_missal>
2013-02-20 13:54:18 -0600

    better defined Attribute workflow

--- Mozu.SiteBuilder.UX.Admin/Helpers/IAttributeHelper.cs
+++ Mozu.SiteBuilder.UX.Admin/Helpers/IAttributeHelper.cs
@@ -1,6 +1,18 @@
-﻿namespace Mozu.SiteBuilder.UX.Admin.Helpers
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mozu.SiteBuilder.UX.Admin.Api.Models;
+using Mozu.SiteBuilder.UX.Admin.Api.Models.Attributes;
+
+namespace Mozu.SiteBuilder.UX.Admin.Helpers
 {
     public interface IAttributeHelper
     {
+        Task<IEnumerable<Attribute>> GetAttributes(PagingParamaters pagingParams, FilterCollection extFilter);
+
+        Task<IEnumerable<Attribute>> CreateAttributes(List<Attribute> attributes);
+
+        Task<IEnumerable<Attribute>> EditAttributes(List<Attribute> attributes);
+
+        Task<IEnumerable<Attribute>> DeleteAttributes(List<Attribute> attributes);
     }
 }
