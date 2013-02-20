1505fc04cb13080bfcfdec969bdda6ae97aaa620
Chris Missal <ADS\chris_missal>
2013-02-20 13:54:18 -0600

    better defined Attribute workflow

--- Mozu.SiteBuilder.UX.Admin/Helpers/AttributeHelper.cs
+++ Mozu.SiteBuilder.UX.Admin/Helpers/AttributeHelper.cs
@@ -1,14 +1,55 @@
-﻿using Mozu.SiteBuilder.UX.Admin.MockServices;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Mozu.SiteBuilder.UX.Admin.Api.Models;
+using Mozu.SiteBuilder.UX.Admin.MockServices;
+using Attribute = Mozu.SiteBuilder.UX.Admin.Api.Models.Attributes.Attribute;
 
 namespace Mozu.SiteBuilder.UX.Admin.Helpers
 {
+    using Contracts = ProductAdmin.Contracts;
+
     public class AttributeHelper : IAttributeHelper
     {
         private readonly IMoreAwesomeAttributeWebApiClient _attributeWebApiClient;
+        private readonly CollectionTaskUnMapper<Attribute, Contracts.Attribute> _attributeMapper = new CollectionTaskUnMapper<Attribute, Contracts.Attribute>();
 
         public AttributeHelper(IMoreAwesomeAttributeWebApiClient attributeWebApiClient)
         {
             _attributeWebApiClient = attributeWebApiClient;
+        }
+
+        public async Task<IEnumerable<Attribute>> GetAttributes(PagingParamaters pagingParams, FilterCollection extFilter)
+        {
+            string filter = null; // extFilter.ToFilterString();
+            string sort = null; // pagingParams.sort.ToSortString();
+
+            var result = await _attributeWebApiClient.GetAttributes(
+                /*startIndex: */     pagingParams.startIndex,
+                /*pageSize: */       pagingParams.pageSize,
+                /*sortBy: */         sort,
+                /*responseGroups: */ null,
+                /*filter: */         filter
+                );
+            var res = result.ReadAsAsync().Result;
+
+            return res.Items.Select(Mapper.Map<Attribute>);
+        }
+
+        public async Task<IEnumerable<Attribute>> CreateAttributes(List<Attribute> attributes)
+        {
+            return await _attributeMapper.PerformAction(attributes, a => _attributeWebApiClient.AddAttribute(a));
+        }
+
+        public async Task<IEnumerable<Attribute>> EditAttributes(List<Attribute> attributes)
+        {
+            return await _attributeMapper.PerformAction(attributes, a => _attributeWebApiClient.UpdateAttribute(a, a.AttributeFQN));
+        }
+
+        public async Task<IEnumerable<Attribute>> DeleteAttributes(List<Attribute> attributes)
+        {
+            return await _attributeMapper.PerformAction(attributes, a => _attributeWebApiClient.DeleteAttribute(a.AttributeFQN));
         }
     }
 }
---
+++ Mozu.SiteBuilder.UX.Admin/Helpers/CollectionTaskUnMapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Mozu.Core.Api.Contracts.Client;
+
+namespace Mozu.SiteBuilder.UX.Admin.Helpers
+{
+    /// <summary>
+    /// A convenience class to aide in dealing with mapping collections that are contained within Tasks.
+    /// </summary>
+    /// <typeparam name="TClient">The type used in the SiteBuilder API.</typeparam>
+    /// <typeparam name="TServer">The type used by the Mozu services API.</typeparam>
+    public class CollectionTaskUnMapper<TClient, TServer>
+    {
+        /// <summary>
+        /// This method will Map a collection of SiteBuilder (TClient) objects into a collection of Mozu service contract objects (TServer),
+        /// perform an action, then map back to the SiteBuilder (TClient) type. Since a query expression or iterator cannot use await
+        /// (because of blocking), this helps get around that problem.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection">A collection of SiteBuilder objects.</param>
+        /// <param name="action">The action to be performed.</param>
+        /// <returns>A Task of Enumerable objects from the SiteBuilder API.</returns>
+        public async Task<IEnumerable<TClient>> PerformAction<T>(IEnumerable<TClient> collection, Func<TServer, Task<ServiceClientResponse<T>>> action)
+        {
+            return from attr in await Task.WhenAll(
+                from attribute in collection.Select(x => Mapper.Map<TServer>(x))
+                let res = action(attribute)
+                select res.Result.ReadAsAsync())
+                   select Mapper.Map<TClient>(attr);
+        }
+    }
+}
