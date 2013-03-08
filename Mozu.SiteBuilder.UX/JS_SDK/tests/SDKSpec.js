47866590cbc132ad06b3f5cbacbfeba9480a98ba
James Zetlen <ADS\james_zetlen>
2013-03-08 13:45:37 -0600

    url updating fixens

--- Mozu.SiteBuilder.UX/JS_SDK/tests/SDKSpec.js
+++ Mozu.SiteBuilder.UX/JS_SDK/tests/SDKSpec.js
@@ -1,5 +1,15 @@
 ﻿describe('Mozu SDK', function () {
 
+    // current service URLs
+    Mozu.ApiReference.urls = {
+        "ProductService": "http://aus01pdweb001.ads.volusion.com:9090/mozu.ProductRuntime.WebApi/products/",
+        "CartService": "http://aus01pdweb001.ads.volusion.com:9090/mozu.Cart.WebApi/commerce/carts/",
+        "UserService": "http://aus01pdweb001.ads.volusion.com:9090/mozu.User.WebApi/users/",
+        "OrderService": "http://aus01pdweb001.ads.volusion.com:9090/mozu.Order.WebApi/orders/",
+        "SearchService": "http://aus01pdweb001.ads.volusion.com:9090/mozu.ProductRuntime.WebApi/productsearch/",
+        "CmsService": "http://aus01pdweb001.ads.volusion.com:9090/mozu.Content.WebApi/documents/"
+    };
+
     it("should expose a Mozu object", function () {
         expect(Mozu).toBeDefined();
     });
