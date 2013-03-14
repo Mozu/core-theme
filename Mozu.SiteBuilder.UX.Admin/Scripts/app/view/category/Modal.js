5b25c5bd107cb8f35f272a7a9680bf514b0131ee
Jimmy Sanford <ADS\Jimmy_Sanford>
2013-03-14 10:03:42 -0500

    maintain category selections

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Modal.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Modal.js
@@ -22,7 +22,42 @@ Ext.define('Taco.view.category.Modal', {
             rootVisible: false,
             store: this.store,
             displayField: 'name',
-            selModel: this.selModel
+            selModel: this.selModel,
+            selectPath: function (path, field, separator, callback, scope) {
+                // override: set keepExisting to true when calling select()
+                var me = this,
+                    root,
+                    keys,
+                    last;
+
+                field = field || me.getRootNode().idProperty;
+                separator = separator || '/';
+
+                keys = path.split(separator);
+                last = keys.pop();
+                if (keys.length > 1) {
+                    me.expandPath(keys.join(separator), field, separator, function(success, node){
+                        var lastNode = node;
+                        if (success && node) {
+                            node = node.findChild(field, last);
+                            if (node) {
+                                me.getSelectionModel().select(node, true);
+                                Ext.callback(callback, scope || me, [true, node]);
+                                return;
+                            }
+                        }
+                        Ext.callback(callback, scope || me, [false, lastNode]);
+                    }, me);
+                } else {
+                    root = me.getRootNode();
+                    if (root.getId() === last) {
+                        me.getSelectionModel().select(root, true);
+                        Ext.callback(callback, scope || me, [true, root]);
+                    } else {
+                        Ext.callback(callback, scope || me, [false, null]);
+                    }
+                }
+            }
         });
 
         this.content = {
@@ -51,21 +86,31 @@ Ext.define('Taco.view.category.Modal', {
 
         this.callParent(arguments);
 
-        this.preselect();
+        this.tree.getView().on({
+            viewready: this.preselect,
+            scope: this
+        });
+
+        this.selModel.on({
+            selectionchange: function (selModel, selection) { console.log(selection, selModel.getSelectionMode()); },
+            scope: this
+        });
     },
 
     cancel: function () {
         this.hide();
     },
 
-    preselect: function () {
+    preselect: function (view) {
         var preselection = this.preselection,
             tree = this.tree;
 
         Ext.Array.each(preselection, function (record) {
             var path = record.get('path');
+
+            path = '/0/' + path + (path ? '/' : '') + record.getId();
             console.log(path);
-            // tree.selectPath(path);
+            tree.selectPath(path);
         }, this);
     },
 
