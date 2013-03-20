8a9f0e55c88747f273eef09f1fa287ae885988f9
Travis Johnson <ADS\Travis_Johnson>
2013-03-20 17:43:47 -0500

    FIXED IT

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Properties.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Properties.js
@@ -6,15 +6,48 @@
 
 Ext.define('Taco.view.product.subform.Properties', {
     extend: 'Taco.view.product.subform.Subform',
+    alias: 'widget.productpropertiesform',
 
     title: 'Properties',
     
     initComponent: function () {
-        this.items = [{
-            xtype: 'component',
-            html: '<div style="font-size: 16px; padding: 8px">TBD</div>'
-        }];
+        this.productTypeStore = Taco.core.data.StoreManager.getOrCreate('Taco.store.ProductTypes');
 
-        this.callParent( arguments );
+        this.items = [this.getEmptyComponent()];
+
+        this.callParent(arguments);
+    },
+
+    loadByProductTypeId: function (id) {
+        var type = this.productTypeStore.getById(id),
+            properties,
+            items = [];
+
+        if (!type) {
+            return;
+        }
+
+        properties = type.getProperties();
+
+        properties.each(function (property) {
+            items.push(Ext.widget({
+                xtype: 'component',
+                html: property.get('attributeName')
+            }));
+        });
+
+        if (!items.length) {
+            items.push(this.getEmptyComponent());
+        }
+
+        this.removeAll();
+        this.add(items);
+    },
+
+    getEmptyComponent: function () {
+        return {
+            xtype: 'component',
+            html: 'Super sorry, but this product type does not have any properties associated to it....<small style="color: #eee;">    <i>idiot</i></small>'
+        };
     }
 });
