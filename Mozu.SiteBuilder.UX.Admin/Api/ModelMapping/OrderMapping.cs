e61a884fce63c131fe0c327be0dca5d61bd2aa07
Thomas Phipps <ADS\Thomas_Phipps>
2013-03-28 13:00:53 -0500

    fix order mapping and fix toolbar navigation

--- Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/OrderMapping.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/OrderMapping.cs
@@ -3,7 +3,9 @@ using AutoMapper;
 
 namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
 {
-    using Contracts = Mozu.CommerceRuntime.Contracts.Orders;
+    using OrdersDC = Mozu.CommerceRuntime.Contracts.Orders;
+    using DiscountDC = Mozu.CommerceRuntime.Contracts.Discounts;
+    using ProductsDC = Mozu.CommerceRuntime.Contracts.Products ;
     using VM = Mozu.SiteBuilder.UX.Models.Orders;
     public class OrderMapping : Profile
     {
@@ -14,64 +16,64 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
 
         protected override void Configure()
         {
-            //Mapper.CreateMap<VM.Order, Contracts.Order>();
-            //Mapper.CreateMap<Contracts.Order, VM.Order>()
-            //    .ForMember(x => x.BillingFirstName, op => op.Ignore())
-            //    .ForMember(x => x.BillingLastName, op => op.Ignore());
+            Mapper.CreateMap<VM.Order, OrdersDC.Order>();
+            Mapper.CreateMap<OrdersDC.Order, VM.Order>()
+                .ForMember(x => x.BillingFirstName, op => op.Ignore())
+                .ForMember(x => x.BillingLastName, op => op.Ignore());
 
-            //Mapper.CreateMap<VM.AppliedDiscount, Contracts.AppliedDiscount>();
-            //Mapper.CreateMap<Contracts.AppliedDiscount, VM.AppliedDiscount>();
+            Mapper.CreateMap<VM.AppliedDiscount, DiscountDC.AppliedDiscount>();
+            Mapper.CreateMap<DiscountDC.AppliedDiscount, VM.AppliedDiscount>();
 
-            //Mapper.CreateMap<VM.Category, Contracts.Category>();
-            //Mapper.CreateMap<Contracts.Category, VM.Category>();
+            Mapper.CreateMap<VM.Category, ProductsDC.Category>();
+            Mapper.CreateMap<ProductsDC.Category, VM.Category>();
 
-            ////Mapper.CreateMap<VM.Discount, Contracts.Discount>();
-            ////Mapper.CreateMap<Contracts.Discount, VM.Discount>();
+            //Mapper.CreateMap<VM.Discount, OrdersDC.Discount>();
+            //Mapper.CreateMap<OrdersDC.Discount, VM.Discount>();
 
-            //Mapper.CreateMap<VM.Measurement, Mozu.Core.Api.Contracts.Measurement>();
-            //Mapper.CreateMap<Mozu.Core.Api.Contracts.Measurement, VM.Measurement>();
+            Mapper.CreateMap<VM.Measurement, Mozu.Core.Api.Contracts.Measurement>();
+            Mapper.CreateMap<Mozu.Core.Api.Contracts.Measurement, VM.Measurement>();
 
-            //Mapper.CreateMap<VM.OrderItem, Contracts.OrderItem>();
-            //Mapper.CreateMap<Contracts.OrderItem, VM.OrderItem>();
+            Mapper.CreateMap<VM.OrderItem, OrdersDC.OrderItem>();
+            Mapper.CreateMap<OrdersDC.OrderItem, VM.OrderItem>();
 
-            //Mapper.CreateMap<VM.OrderNote, Contracts.OrderNote>();
-            //Mapper.CreateMap<Contracts.OrderNote, VM.OrderNote>();
+            Mapper.CreateMap<VM.OrderNote, OrdersDC.OrderNote>();
+            Mapper.CreateMap<OrdersDC.OrderNote, VM.OrderNote>();
 
-            //Mapper.CreateMap<VM.PackageMeasurements, Contracts.PackageMeasurements>();
-            //Mapper.CreateMap<Contracts.PackageMeasurements, VM.PackageMeasurements>();
+            Mapper.CreateMap<VM.PackageMeasurements, Mozu.CommerceRuntime.Contracts.Commerce.PackageMeasurements>();
+            Mapper.CreateMap<Mozu.CommerceRuntime.Contracts.Commerce.PackageMeasurements, VM.PackageMeasurements>();
 
-            //Mapper.CreateMap<VM.PaymentCardReference, Contracts.PaymentCardReference>();
-            //Mapper.CreateMap<Contracts.PaymentCardReference, VM.PaymentCardReference>();
+            Mapper.CreateMap<VM.PaymentCardReference, OrdersDC.PaymentCardReference>();
+            Mapper.CreateMap<OrdersDC.PaymentCardReference, VM.PaymentCardReference>();
 
-            //Mapper.CreateMap<VM.PaymentReference, Contracts.PaymentReference>();
-            //Mapper.CreateMap<Contracts.PaymentReference, VM.PaymentReference>();
+            Mapper.CreateMap<VM.PaymentReference, OrdersDC.PaymentReference>();
+            Mapper.CreateMap<OrdersDC.PaymentReference, VM.PaymentReference>();
 
-            //Mapper.CreateMap<VM.PaymentTransaction, Contracts.PaymentTransaction>();
-            //Mapper.CreateMap<Contracts.PaymentTransaction, VM.PaymentTransaction>();
+            Mapper.CreateMap<VM.PaymentTransaction, OrdersDC.PaymentTransaction>();
+            Mapper.CreateMap<OrdersDC.PaymentTransaction, VM.PaymentTransaction>();
 
-            //Mapper.CreateMap<VM.PaymentTransactionInteraction, Contracts.PaymentTransactionInteraction>();
-            //Mapper.CreateMap<Contracts.PaymentTransactionInteraction, VM.PaymentTransactionInteraction>();
+            Mapper.CreateMap<VM.PaymentTransactionInteraction, OrdersDC.PaymentTransactionInteraction>();
+            Mapper.CreateMap<OrdersDC.PaymentTransactionInteraction, VM.PaymentTransactionInteraction>();
 
-            //Mapper.CreateMap<VM.Product, Contracts.Product>();
-            //Mapper.CreateMap<Contracts.Product, VM.Product>();
+            Mapper.CreateMap<VM.Product, ProductsDC .Product>();
+            Mapper.CreateMap<ProductsDC.Product, VM.Product>();
 
-            //Mapper.CreateMap<VM.ProductOption, Contracts.ProductOption>();
-            //Mapper.CreateMap<Contracts.ProductOption, VM.ProductOption>();
+            Mapper.CreateMap<VM.ProductOption, ProductsDC.ProductOption>();
+            Mapper.CreateMap<ProductsDC.ProductOption, VM.ProductOption>();
 
-            //Mapper.CreateMap<VM.ProductPrice, Contracts.ProductPrice>();
-            //Mapper.CreateMap<Contracts.ProductPrice, VM.ProductPrice>();
+            Mapper.CreateMap<VM.ProductPrice, ProductsDC.ProductPrice>();
+            Mapper.CreateMap<ProductsDC.ProductPrice, VM.ProductPrice>();
 
-            //Mapper.CreateMap<VM.ProductStock, Contracts.ProductStock>();
-            //Mapper.CreateMap<Contracts.ProductStock, VM.ProductStock>();
+            Mapper.CreateMap<VM.ProductStock, ProductsDC.ProductStock>();
+            Mapper.CreateMap<ProductsDC.ProductStock, VM.ProductStock>();
 
-            //Mapper.CreateMap<VM.Shipment, Contracts.Shipment>();
-            //Mapper.CreateMap<Contracts.Shipment, VM.Shipment>();
+            Mapper.CreateMap<VM.Shipment, OrdersDC.Shipment>();
+            Mapper.CreateMap<OrdersDC.Shipment, VM.Shipment>();
 
-            //Mapper.CreateMap<VM.ShippingPrice, Contracts.ShippingPrice>();
-            //Mapper.CreateMap<Contracts.ShippingPrice, VM.ShippingPrice>();
+            Mapper.CreateMap<VM.ShippingPrice, OrdersDC.ShippingPrice>();
+            Mapper.CreateMap<OrdersDC.ShippingPrice, VM.ShippingPrice>();
 
-            //Mapper.CreateMap<VM.ShopperNotes, Contracts.ShopperNotes>();
-            //Mapper.CreateMap<Contracts.ShopperNotes, VM.ShopperNotes>();
+            Mapper.CreateMap<VM.ShopperNotes, OrdersDC.ShopperNotes>();
+            Mapper.CreateMap<OrdersDC.ShopperNotes, VM.ShopperNotes>();
         }
     }
 }
