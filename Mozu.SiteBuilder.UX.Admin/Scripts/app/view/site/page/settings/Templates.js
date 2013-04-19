888e93a533d5f2e4f09e26ee5eea6a4de725209c
James Zetlen <ADS\james_zetlen>
2013-04-19 10:42:46 -0500

    preventing sad travis disorder

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/site/page/settings/Templates.js
@@ -0,0 +1,9 @@
+/**
+ * @class Taco.view.site.page.settings.Templates
+ */
+Ext.define('Taco.view.site.page.settings.Templates', {
+    extend: 'Taco.view.site.page.PageSettingsPanel',
+    form: {
+        
+    }
+});
