12efcedf25a70be50fd685bfcf3d19942f2b0892
Thomas Phipps <ADS\Thomas_Phipps>
2013-04-22 14:36:10 -0500

    adding pannel config tuples for prod and cat

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/site/page/entityAdapters/CategoryEntityAdapter.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/site/page/entityAdapters/CategoryEntityAdapter.js
@@ -2,23 +2,56 @@
  * @class Taco.view.site.page.entityAdapters.CategoryEntityAdapter
  */
 Ext.define('Taco.view.site.page.entityAdapters.CategoryEntityAdapter', {
-	extend: 'Taco.view.site.page.entityAdapters.BaseEntityAdapter',
-	modelName:'Taco.model.Category',
-	getStore:function(){
-		return this.editor.categories;
-	},
+    extend: 'Taco.view.site.page.entityAdapters.BaseEntityAdapter',
+    modelName:'Taco.model.Category',
+    getStore:function(){
+        return this.editor.categories;
+    },
 	
-	isHidden:function(){
-	    return this.get().get('isHidden');
-	},
-	setHidden: function (hide) {
-	    this.get().set('isHidden', hide);
-	},
+    isHidden:function(){
+        return this.get().get('isHidden');
+    },
+    setHidden: function (hide) {
+        this.get().set('isHidden', hide);
+    },
 	
-	getId: function() {
-		return this.pageProps.pageContext.categoryId;
-	},
+    getId: function() {
+        return this.pageProps.pageContext.categoryId;
+    },
 
-	//settingsPanels: ['Taco.view.site.page.settings.General', 'Taco.view.site.page.settings.Templates', 'Taco.view.site.page.settings.Seo', 'Taco.view.site.page.settings.Facets']
-	//settingsPanels:[]
+
+    constructor:function() {
+        this.settingsPanels = this.createSettingsPannels();
+        this.callParent(arguments);
+    },
+
+    createSettingsPannels: function() {
+        var me = this;
+        return [
+            {
+                panelCls: 'Taco.view.site.page.settings.General',
+                getRecord: function() {
+                    return me.get();
+                }
+            },
+            {
+                panelCls: 'Taco.view.site.page.settings.Templates',
+                getRecord: function() {
+                    return me.get();
+                }
+            }, {
+                panelCls:  'Taco.view.site.page.settings.Seo',
+                getRecord: function() {
+                    return me.get();
+                }
+            }, {
+                panelCls: 'Taco.view.site.page.settings.Facets',
+                getRecord: function() {
+                    return me.get();
+                }
+            }];
+    }
+	
+
+//settingsPanels:[]
 });
--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/site/page/entityAdapters/ProductEntityAdapter.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/site/page/entityAdapters/ProductEntityAdapter.js
@@ -3,24 +3,56 @@
  */
 Ext.define('Taco.view.site.page.entityAdapters.ProductEntityAdapter', {
 
-	extend: 'Taco.view.site.page.entityAdapters.BaseEntityAdapter',
-	modelName: 'Taco.model.Product',
-	getStore: function() {
-		return this.editor.products;
-	},
+    extend: 'Taco.view.site.page.entityAdapters.BaseEntityAdapter',
+    modelName: 'Taco.model.Product',
+    getStore: function () {
+        return this.editor.products;
+    },
 
-	isHidden: function() {
-		return !this.get().get('isActive');
-	},
-	setHidden: function(hide) {
-		this.get().set('isActive', !hide);
-	},
+    isHidden: function () {
+        return !this.get().get('isActive');
+    },
+    setHidden: function (hide) {
+        this.get().set('isActive', !hide);
+    },
 
-	getId: function() {
-		return this.pageProps.pageContext.productCode;
-	},
+    getId: function () {
+        return this.pageProps.pageContext.productCode;
+    },
 
-	doFormView: function() {
-		Taco.core.StateManager.attemptNavigate('products/edit/' + this.get().getId());
-	}
+    doFormView: function () {
+        Taco.core.StateManager.attemptNavigate('products/edit/' + this.get().getId());
+    },
+
+    constructor: function () {
+        this.settingsPanels = this.createSettingsPannels();
+        this.callParent(arguments);
+    },
+
+
+
+    createSettingsPannels: function () {
+        var me = this;
+        return [
+            {
+                panelCls: 'Taco.view.site.page.settings.General',
+                getRecord: function () {
+
+                    return me.get().getProductInSite();
+                }
+            },
+            //{
+            //    panelCls: 'Taco.view.site.page.settings.Templates',
+            //    getRecord: function () {
+            //        return me.get().getProductInSite();
+            //    }
+            //},
+            {
+                panelCls: 'Taco.view.site.page.settings.Seo',
+                getRecord: function () {
+                    return me.get().getProductInSite();
+                }
+            }];
+    }
+
 })
