b4e497693ed5fc9645f678373018c74b1862a6de
James Zetlen <ADS\james_zetlen>
2013-04-24 18:18:36 -0500

    cartmonitor timeout checks itself

--- Mozu.SiteBuilder.UX.Themes/Themes/Core3/Resources/Scripts/Modules/cart-monitor.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core3/Resources/Scripts/Modules/cart-monitor.js
@@ -1,10 +1,28 @@
 ﻿define(['jquery', 'modules/api'], function ($, api) {
     $(document).ready(function () {
-        var $cartCount = $('#mz-cart-count');
-        function updateCartQuantity(apiObject) {
-            if (apiObject.type === "cart") $cartCount.text(apiObject.data.Items.length);
+        var $cartCount = $('#mz-cart-count'), timeout;
+        function waitAndGetCart() {
+            return setTimeout(function() {
+                api.get('cart').then(updateCartDetails);
+            }, 2500);
         }
-        api.on('sync', updateCartQuantity);
-        api.on('spawn', updateCartQuantity);
+        function checkForCartUpdates(apiObject) {
+            switch (apiObject.type) {
+                case "cart":
+                    clearTimeout(timeout);
+                    updateCartDetails(apiObject);
+                    break;
+                case "cartitem":
+                    timeout = waitAndGetCart();
+                    break;
+            }
+        }
+        function updateCartDetails(cartObject) {
+            $cartCount.text(cartObject.data.Items.length);
+
+        }
+        api.on('sync', checkForCartUpdates);
+        api.on('spawn', checkForCartUpdates);
+        timeout = waitAndGetCart();
     });
 });
