091e0a2913b40e302d6cdefeeb5ad54a1306a053
James Zetlen <ADS\james_zetlen>
2013-04-24 12:40:44 -0500

    form is now really good at itemdicts in models, so pagesettings basically works

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/site/page/settings/General.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/site/page/settings/General.js
@@ -21,19 +21,20 @@ Ext.define('Taco.view.site.page.settings.General', {
                 fieldLabel: 'Navigation Link Name',
                 name: 'link_title'
             },
-            //{
-            //    xtype: 'checkbox',
-            //    fieldLabel: 'Show in Navigation',
-            //    name: 'showInNav'
-            //},
-            //{
-            //    xtype: 'checkbox',
-            //    fieldLabel: 'Use this page only to group other pages',
-            //    name: 'isGroupPage'
-            //},
+            {
+                xtype: 'checkbox',
+                fieldLabel: 'Show in Navigation',
+                name: 'show_in_nav'
+            },
+            {
+                xtype: 'checkbox',
+                fieldLabel: 'Use this page only to group other pages',
+                name: 'is_group_page'
+            },
             {
                 xtype: 'checkbox',
                 fieldLabel: 'Redirect page to:',
+                name: ''
             },
             {
                 xtype: 'textfield',
