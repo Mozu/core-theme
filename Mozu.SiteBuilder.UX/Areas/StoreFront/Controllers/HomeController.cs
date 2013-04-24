9fbb25ff05b60efaa8b5c3772b5c97c169e53920
Foster Hersey <ADS\foster_hersey>
2013-04-24 18:13:23 -0500

    Added an IsHomePage field to runtime navigation nodes.

--- Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/HomeController.cs
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
             var nav = SiteContext.Navigation;
             if ( nav != null && nav.Count() > 0)
             {
-                var item = nav.FirstOrDefault(x => !x.NodeType.IsLink && !string.IsNullOrEmpty(x.Url));
+                var item = nav.FirstOrDefault(x => x.IsHomePage);
                 if ( item != null  && item.Url.Length >0 && item.Url != "/pages/home")
                 {
                     return new TransferResult(item.Url );
