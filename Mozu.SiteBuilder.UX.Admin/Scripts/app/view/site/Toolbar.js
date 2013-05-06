c052462746c3331ed3026a61ed29e3c007b110c6
Jimmy Sanford <ADS\Jimmy_Sanford>
2013-05-06 17:07:39 -0500

    fixing site editor toolbar

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/site/Toolbar.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/site/Toolbar.js
@@ -4,31 +4,74 @@
 Ext.define('Taco.view.site.Toolbar', {
     extend: 'Ext.container.Container',
     requires: ['Taco.core.ux.action.Button'],
+
     cls: Taco.baseCSSPrefix + 'inline-editor-tools',
-    margin: '3 0 0 0',
     layout: 'auto',
+    defaults: {
+        xtype: 'taco.button'
+    },
+
     inMoreMenu: {
         'destroy': true,
         'copy': true
     },
-    defaults: {
-        xtype: 'taco.button'
+
+    initComponent: function () {
+        var self = this;
+
+        this.items = [{
+            text: 'Preview',
+            itemId: 'preview',
+            onClick: function () {
+                self.editor.viewPage()
+            }
+        }, {
+            text: 'More &#9662;',
+            itemId: 'more',
+            menu: {
+                plain: true,
+                items: [{
+                    text: 'Delete',
+                    itemId: 'destroy',
+                    handler: function () {
+                        self.editor.deleteRecord();
+                    }
+                }, {
+                    text: 'Duplicate',
+                    itemId: 'copy',
+                    handler: function () {
+                        alert('tbd!');
+                    }
+                }]
+            }
+        }];
+
+        this.callParent(arguments);
+
+        this.on({
+            added: function (cmp) {
+                cmp.resetButtons();
+            }
+        });
     },
-    getButton:function(key){
+
+    getButton: function (key) {
        return  this.down('#'+key);
     },
+
     populate: function (adapter) {
         this.enableButtons(adapter.allowedActions);
     },
-    resetButtons:function()
-    {
-        this.enableButtons( {
+
+    resetButtons: function () {
+        this.enableButtons({
             copy: false,
             more: false,
             preview: false,
             destroy: false
         });
     },
+
     enableButtons: function (config) {
         var enableMoreMenu = false;
         Ext.Object.each(config, function (key, value) {
@@ -37,46 +80,5 @@ Ext.define('Taco.view.site.Toolbar', {
             if (button) button[value ? "show" : "hide"]();
         }, this);
         this.getButton('more')[enableMoreMenu ? "show" : "hide"]();
-    },
-    listeners: {
-        added: function (me) {
-            me.resetButtons();
-        }
-    },
-    initComponent: function () {
-        var self = this;
-        this.items = [
-        {
-            text: 'Preview',
-            itemId: 'preview',
-            onClick: function() {
-                self.editor.viewPage()
-            }
-        },
-        {
-            text: 'More &#9662;',
-            itemId: 'more',
-            menu: {
-                plain: true,
-                items: [
-            {
-                text: 'Delete',
-                itemId: 'destroy',
-                handler: function () {
-                    self.editor.deleteRecord();
-                }
-            },
-            {
-                text: 'Duplicate',
-                itemId: 'copy',
-                handler: function () {
-                    alert('tbd!');
-                }
-            }
-                ]
-            }
-        }
-        ];
-        this.callParent(arguments);
     }
 });
