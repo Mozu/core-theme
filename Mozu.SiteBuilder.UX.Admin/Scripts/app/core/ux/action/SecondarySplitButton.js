5dbd463a34bf36c99baae61e89d7c341fa0159c3
James Zetlen <ADS\james_zetlen>
2013-05-07 17:06:14 -0500

    now that's a nice ol' publish button

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/action/SecondarySplitButton.js
+++
@@ -1,8 +0,0 @@
-/**
- * @class Taco.core.ux.action.SecondarySplitButton
- */
-Ext.define('Taco.core.ux.action.SecondarySplitButton', {
-    extend: 'Taco.core.ux.action.SplitButton',
-    alias: 'widget.secondarysplitbutton',
-    cls: Taco.baseCSSPrefix + 'action ' + Taco.baseCSSPrefix + 'splitbutton ' + Taco.baseCSSPrefix + 'action-secondary',
-});
