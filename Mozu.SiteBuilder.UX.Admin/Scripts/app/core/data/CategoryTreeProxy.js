3d096460a2fde99cfb536d5d79355e148b87b422
Thomas Phipps <ADS\Thomas_Phipps>
2013-05-16 19:31:10 -0500

    fix to category proxy sort issue.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/data/CategoryTreeProxy.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/data/CategoryTreeProxy.js
@@ -65,6 +65,7 @@ Ext.define('Taco.core.data.CategoryTreeProxy', {
                 if (siteId) {
                     jsonData = Ext.JSON.decode(data);
                     jsonData.items = Ext.Array.filter(jsonData.items, function (item) { return item.siteId == siteId });
+                    jsonData.items.sort(function(a, b) { return (a.sequence || 99) - (b.sequence || 99); });
                     response.responseText = Ext.JSON.encode(jsonData);
 
                 }
