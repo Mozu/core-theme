4d4f083a41b1c27dab0ad13019af4a45b187d9d1
Jimmy Sanford <ADS\Jimmy_Sanford>
2013-05-17 15:24:58 -0500

    menu no koto

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/navigation/SecondaryMenu.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/navigation/SecondaryMenu.js
@@ -1,48 +1,46 @@
 ﻿/**
  * @class Taco.view.navigation.SecondaryMenu
  */
-    Ext.define('Taco.view.navigation.SecondaryMenu', {
-        extend: 'Ext.container.Container',
-        alias: 'widget.secondarymenu',
-        requires: ['Taco.core.ux.action.Button'],
+Ext.define('Taco.view.navigation.SecondaryMenu', {
+    extend: 'Ext.container.Container',
+    alias: 'widget.secondarymenu',
+    requires: ['Taco.core.ux.action.Button'],
 
-        cls: Taco.baseCSSPrefix + 'secondary-nav',
-        layout: { type: 'auto' },
+    cls: Taco.baseCSSPrefix + 'secondary-nav',
+    layout: { type: 'auto' },
 
-        initComponent: function () {
-            var me = this;
+    initComponent: function () {
+        var me = this;
 
-            this.items = [{
-                xtype: 'taco.button',
-                autoEl: 'a',
-                text: Taco.User.name || Taco.User.email || '[user]',
-                menu: {
-                    plain: true,
-                    shadow: false,
-                    items: [{
-                        text: 'My Account',
-                        handler: function() {
-                            Taco.app.StateManager.attemptNavigate('account');
-                        },
-                        padding: '4 8'
-                    }, {
-                        text: 'Log Out',
-                        href: '/admin/auth/logout',
-                        padding: '4 8'
-                    }]
-                }
-            }, {
-                xtype: 'action',
-                text: 'Help',
-                click: function () {
-                    Taco.app.refreshStyle();
-                }
-            }, {
-                xtype: 'action',
-                text: 'Search'
-               
-            }];
+        this.items = [{
+            xtype: 'taco.button',
+            autoEl: 'a',
+            text: Taco.User.name || Taco.User.email || '[user]',
+            menu: {
+                plain: true,
+                shadow: false,
+                items: [{
+                    text: 'My Account',
+                    handler: function() {
+                        Taco.app.StateManager.attemptNavigate('account');
+                    }
+                }, {
+                    text: 'Log Out',
+                    href: '/admin/auth/logout'
+                }]
+            }
+        }, {
+            xtype: 'action',
+            text: 'Help',
+            click: function () {
+                Taco.app.refreshStyle();
+            }
+        }, {
+            xtype: 'action',
+            text: 'Search'
+           
+        }];
 
-            this.callParent(arguments);
-        }
-    });
+        this.callParent(arguments);
+    }
+});
