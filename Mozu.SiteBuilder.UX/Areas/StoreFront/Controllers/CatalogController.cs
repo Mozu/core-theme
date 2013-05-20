f59deb64dbe6518ed2da3bcd320d3c2f30d104b5
James Zetlen <ADS\james_zetlen>
2013-05-20 15:28:22 -0500

    fixing productlisting error that bonked carousel widget

--- Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/CatalogController.cs
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/CatalogController.cs
@@ -95,7 +95,7 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
             string filter = null;
             if (productCodes != null && productCodes.Count > 0)
             {
-                var productCodes2 = productCodes.Where( x=> !string.IsNullOrWhiteSpace(  (string)x)).Select(x => string.Format("productCode eq {0}", x.ToString())).ToList();
+                var productCodes2 = productCodes.Select(x => x.ToString()).Where( x=> !string.IsNullOrWhiteSpace(x)).Select(x => string.Format("productCode eq {0}", x)).ToList();
                 if (productCodes2.Count > 0)
                 {
                     filter = string.Join(" or ", productCodes2);
