05a8a893a23e6419d543bb87a978d16f7b25fc8f
Thomas Phipps <ADS\Thomas_Phipps>
2013-05-20 17:01:39 -0500

    404

---
+++ Mozu.SiteBuilder.UX/ActionFilters/NotFoundActionFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mozu.SiteBuilder.UX.ActionFilters
+{
+    public class NotFoundActionFilter : System.Web.Mvc.IActionFilter {
+
+
+        public void OnActionExecuted(System.Web.Mvc.ActionExecutedContext filterContext)
+        {
+            if (filterContext.Result != null && filterContext.Result is HttpNotFoundResult)
+            {
+                filterContext.Result = new ViewResult { ViewName = "404", ViewData = filterContext.Controller.ViewData , TempData = filterContext.Controller.TempData };
+
+            }
+        }
+
+        public void OnActionExecuting(System.Web.Mvc.ActionExecutingContext filterContext)
+        {
+           
+        }
+    }
+}
