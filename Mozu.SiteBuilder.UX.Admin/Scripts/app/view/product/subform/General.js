297df8c2970cc2c5d48f4cb4370f6c9d6bfdba8d
Thomas Phipps <ADS\Thomas_Phipps>
2013-05-20 21:33:56 -0500

    html fix 2

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/General.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/General.js
@@ -75,7 +75,7 @@ Ext.define('Taco.view.product.subform.General', {
                 width: '100%',
                 required: true
             }, {
-                xtype: 'textarea',
+                xtype: 'htmleditor',
                 fieldLabel: 'Short Description',
                 name: 'productShortDescription',
                 emptyText: 'Words',
@@ -83,7 +83,7 @@ Ext.define('Taco.view.product.subform.General', {
                 fontFamilies: ['MyriadWebProRegular', 'Arial', 'Courier New', 'Tahoma', 'Times New Roman', 'Verdana'],
                 required: true
             }, {
-                xtype: 'textarea',
+                xtype: 'htmleditor',
                 fieldLabel: 'Full Description',
                 name: 'productFullDescription',
                 emptyText: 'Words, words, and more words.  Also, with lists.',
