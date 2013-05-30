79ec1417a775b907f9b80f702ea95664229f12e1
Travis Johnson <ADS\Travis_Johnson>
2013-05-30 13:10:53 -0500

    Extra fixers

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/ListExtraEditor.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/ListExtraEditor.js
@@ -38,7 +38,7 @@ Ext.define('Taco.view.product.subform.ListExtraEditor', {
 
     loadData: function () {
         var extra = this.product.getExtras().findRecord('attributeFQN', this.productTypeAttribute.getId());
-
+        
         this.listItems = [];
 
         if (!extra) {
@@ -51,7 +51,9 @@ Ext.define('Taco.view.product.subform.ListExtraEditor', {
     },
 
     buildList: function (values) {
-        var items = []
+        var items = [],
+            existingValues = this.productExtra.get('values') || [],
+            newValues = [];
 
         if (values) {
             this.listItems = values;
@@ -59,21 +61,15 @@ Ext.define('Taco.view.product.subform.ListExtraEditor', {
         
         this.listContainer.removeAll();
 
-        Ext.each(this.listItems, function (li) {
-            items.push({
-                xtype: 'formflexbox',
-                justify: false,
-                items: [{
-                    xtype: 'component',
-                    html: li.value,
-                    width: 300
-                }, {
-                    xtype: 'textfield',
-                    fieldLabel: 'Extra Cost'
-                }]
-            });
-        });
 
+
+        Ext.each(this.listItems, function (li) {
+            items.push(this.buildListItem(existingValues, newValues, li));
+        }, this);
+
+        this.extraValues = newValues;
+        this.updateProductExtra();
+        
         items.push({
             xtype: 'action',
             text: 'Add/Edit',
@@ -82,6 +78,56 @@ Ext.define('Taco.view.product.subform.ListExtraEditor', {
         });
 
         this.listContainer.add(items);
+    },
+
+    buildListItem: function (existingValues, newValues, listItem) {
+        var value;
+
+        Ext.each(existingValues, function (val) {
+            if (val.value !== listItem.id) {
+                return;
+            }
+            value = val;
+            return false;  
+        });
+
+        if (!value) {
+            value = {
+                value: listItem.id,
+                delta: 0
+            };
+        }
+
+        newValues.push(value);
+
+        return {
+            xtype: 'formflexbox',
+            justify: false,
+            items: [{
+                xtype: 'component',
+                html: listItem.value,
+                width: 275
+            }, {
+                xtype: 'textfield',
+                fieldLabel: 'Extra Cost',
+                value: value.delta,
+                extraValueObj: value,
+                listeners: {
+                    change: this.onListItemChange,
+                    scope: this
+                }
+            }]
+        };
+    },
+
+    onListItemChange: function (field, newValue) {
+        field.extraValueObj.delta = newValue;
+        this.updateProductExtra();
+    },
+
+    updateProductExtra: function () {
+        console.log('updating product extra', this.extraValues);
+        this.productExtra.set('values', this.extraValues);
     },
 
     buildEditor: function () {
@@ -114,6 +160,7 @@ Ext.define('Taco.view.product.subform.ListExtraEditor', {
                 });
 
                 this.buildList(results);
+                this.productExtra.set('values', results);
                 this.getLayout().setActiveItem(0);
             },
             scope: this
