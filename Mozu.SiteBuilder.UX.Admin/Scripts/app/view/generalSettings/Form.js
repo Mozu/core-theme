b397f9c645413e837122837dfd41a12ddbe8b79d
Bradley Friemel <CORP\bradley_friemel>
2013-06-11 17:01:25 -0500

    Hooked up general settings to a store and form dirty state set up.

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/generalSettings/Form.js
@@ -0,0 +1,37 @@
+/**
+ * @class Taco.view.generalsettings.Form
+ * @author Bradley Friemel
+ * @date 6/11/2013
+ *
+ */
+Ext.define('Taco.view.generalsettings.Form', {
+    extend: 'Taco.core.ux.form.Form',
+    alias: 'widget.generalsettingseditor',
+    requires: [
+        'Taco.view.generalSettings.subform.About',
+        'Taco.view.generalSettings.subform.Rules',
+        /*'Taco.view.generalSettings.subform.Notifications',
+        'Taco.view.generalSettings.subform.Analytics',
+        'Taco.view.generalSettings.subform.Robots',
+        'Taco.view.generalSettings.subform.Tools',*/
+        'Taco.view.generalSettings.subform.Maintenance'],
+    title: 'General Settings',
+    manageHeight: false,
+    createTitle: 'General Settings',
+    editTitle: 'General Settings',
+    initComponent: function () {
+        var me = this;
+
+        me.items = [
+                Ext.create('Taco.view.generalSettings.subform.About', me),
+                Ext.create('Taco.view.generalSettings.subform.Rules', me),
+                Ext.create('Taco.view.generalSettings.subform.Maintenance', me)/*,
+                Ext.create('Taco.view.generalSettings.subform.Notifications'), 
+                Ext.create('Taco.view.generalSettings.subform.Analytics'),  
+                Ext.create('Taco.view.generalSettings.subform.Robots'), 
+                Ext.create('Taco.view.generalSettings.subform.Tools')*/
+        ];
+
+        me.callParent(arguments);
+    }
+});
