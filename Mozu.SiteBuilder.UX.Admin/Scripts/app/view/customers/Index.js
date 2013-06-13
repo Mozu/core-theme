73a6d45d8a2c17613780879a3a50dd78a11e511e
Jimmy Sanford <CORP\Jimmy_Sanford>
2013-06-13 10:10:47 -0500

    customers

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/customers/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/customers/Index.js
@@ -1,188 +1,49 @@
 ﻿/**
  * @class Taco.view.customers.Index
  */
-    Ext.define('Taco.view.customers.Index', {
-        extend: 'Taco.core.ux.content.Container',
-        alias: 'widget.customerlist',
-        requires: ['Ext.form.Panel', 'Taco.core.ux.BaseGrid', 'Ext.tip.QuickTipManager', 'Taco.core.ux.TextFilter', 'Taco.core.ux.FilterableDataView', 'Taco.store.Customers', 'Taco.view.customers.Edit', 'Taco.view.customers.ItemBrowser'],
-        mixins: {
-            protectable: 'Taco.core.util.Protectable'
-        },
-        filters: [],
+Ext.define('Taco.view.customers.Index', {
+    extend: 'Taco.core.ux.browser.BrowserPage',
+    alias: 'widget.taco.index.customer',
+    requires: ['Taco.model.CustomerAccount', 'Taco.store.Customers'],
 
-        initComponent: function (eOpts) {
-            var me = this,
-                basegridview;
-                
+    typeName: 'Customer',
+    modelName: 'Taco.model.CustomerAccount',
+    store: { type: 'Taco.store.Customers' },
+    editorName: 'Taco.view.customer.Edit',
+    useTilePanel: false,
 
-            me.header = {
-                title: 'Customers'
-            };
-
-            me.store = Ext.create('Taco.store.Customers', { filters: me.filters });
-
-            var nameRenderer = function(value, metaData, record, rowIndex, colIndex, store) {
-                return '<a href="#" class="taco-launch-editor">' + value + '</a>';
-            };
-
-            me.basegrid = Ext.create('Taco.core.ux.BaseGrid', {
-                store: me.store,
-                enableColumnHide: true,
-                dockedItems: [{
-                    xtype: 'pagingtoolbar',
-                    dock: 'bottom',
-                    store: me.store,
-                    displayInfo: true
-                }, {
-                    xtype: 'toolbar',
-                    items: [{
-                        text: 'clear filters',
-                        xtype: 'secondarybutton',
-                        hidden: me.filters == null || me.filters.length == 0,
-                        listeners: {
-                            click: function () {
-                                me.store.filters.clear();
-                                me.store.load();
-                                this.setVisible(false);
-                            }
-                        }
-                    }]
-                }],
-
-                columns: [{
-                    xtype: 'gridcolumn',
-                    dataIndex: 'firstName',
-                    text: 'First Name',
-                    hideable: false,
-                    flex: 1,
-                    minWidth: 110,
-                    renderer: nameRenderer
-                }, {
-                    xtype: 'gridcolumn',
-                    dataIndex: 'lastName',
-                    text: 'Last Name',
-                    flex: 1,
-                    minWidth: 110,
-                    renderer: nameRenderer
-                }, {
-                    xtype: 'gridcolumn',
-                    dataIndex: 'email',
-                    text: 'Email',
-                    flex: 1,
-                    minWidth: 110,
-                    hidden: false
-                }, {
-                    xtype: 'gridcolumn',
-                    dataIndex: 'totalOrders',
-                    flex: 1,
-                    minWidth: 40,
-                    text: 'Total Orders'
-                }, {
-                    xtype: 'numbercolumn',
-                    dataIndex: 'spent',
-                    flex: 1,
-                    minWidth: 40,
-                    format: '$0,000.00',
-                    text: 'Spent'
-                }, {
-                    xtype: 'gridcolumn',
-                    dataIndex: 'groups',
-                    flex: 1,
-                    minWidth: 100,
-                    text: 'Group',
-                    renderer: function (groups) {
-                        var mapped = Ext.Array.map(groups, function (group) {
-                            return ' ' + group.name;
-                        })
-                        var text = Ext.Array.flatten(mapped).toString();
-                        var trimmed = Ext.String.trim(text);
-                        return '<span class="tooltip">' + trimmed + '</span>';
-                    }
-                }, {
-                    xtype: 'datecolumn',
-                    dataIndex: 'lastOrderedOn',
-                    text: 'Date of Last Order',
-                    hidden: true
-                }, {
-                    xtype: 'gridcolumn',
-                    dataIndex: 'phoneNumber',
-                    text: 'Phone No.',
-                    hidden: true
-                }, {
-                    xtype: 'gridcolumn',
-                    dataIndex: 'marketingList',
-                    text: 'On Marketing List',
-                    hidden: true
-                }, {
-                    xtype: 'datecolumn',
-                    dataIndex: 'createdOn',
-                    text: 'Customer Since',
-                    hidden: true
-                }]
-            });
-
-            me.itembrowser = Ext.create('Taco.view.customers.ItemBrowser', {
-                uniquePanels: [this.basegrid],
-                itemStore: me.store,
-                itemType: 'customers',
-                filterProperty: 'id',
-                flex: 1
-            });
-
-            Ext.apply(me.body, {
-                layout: 'fit',
-                items: [me.itembrowser]
-            });
-
-            me.callParent(arguments);
-
-            me.store.load();
-            
-            basegridview = me.basegrid.view;
-            basegridview.mon(basegridview, 'itemclick', me.onItemClick, me);
-        },
-
-        
-        onNavigate: function (newState) {
-            var md = newState.getMetaData();
-            if (md.controller && md.controller === "customers" && md.action === "edit") {
-                this.launchEditor(md.args[0]);
-                return false;
+    gridPanelConf: {
+        columns: [{
+            dataIndex: 'firstName',
+            text: 'First Name',
+            width: 150
+        }, {
+            dataIndex: 'lastName',
+            text: 'Last Name',
+            width: 150
+        }, {
+            dataIndex: 'email',
+            text: 'Email',
+            width: 200
+        }, {
+            dataIndex: 'cityOrTown',
+            text: 'Location',
+            width: 150,
+            renderer: function (value, metaData, record) {
+                return [value, record.get('stateOrProvince')].join(', ');
             }
-        },
-
-        launchEditor: function (record) {
-            var me = this,
-                token = 'customers/edit/',
-                editorView = Ext.create('Taco.view.customers.Edit',
-                {
-                    logicalParent: me,
-                    listeners:
-                    {
-                        cancel: function () {
-                            editorView.destroy();
-                            Taco.core.StateManager.addState('customers');
-                            if (me.isDirty) {
-                                me.store.load();
-                                me.isDirty = false;
-                            }
-                        },
-                        save: function () {
-                            me.isDirty = true;
-                        }
-                    },
-                    recordId: record
-                });
-
-            Taco.app.contentView.add(editorView);
-        },
-
-        onItemClick: function (view, record, elm, index, e) {
-            if (e.target.className === 'taco-launch-editor') {
-                e.preventDefault();
-                this.launchEditor(record);
-                Taco.app.StateManager.addState('customers/edit/' + record.getId(), { id: record.getId() });
-            }
-        }
-    });
-
+        }, {
+            dataIndex: 'totalOrders',
+            text: 'Total Orders',
+            width: 100
+        }, {
+            dataIndex: 'spent',
+            text: 'Spent',
+            width: 100
+        }, {
+            dataIndex: 'groups',
+            text: 'Groups',
+            flex: 1
+        }]
+    }
+});
