3900f82a311db19179314e313849bc8ba6753589
Thomas Phipps <CORP\Thomas_Phipps>
2013-06-14 01:13:00 -0500

    payment settings done ish

--- Mozu.SiteBuilder.UX.Admin/Api/Models/Checkout/Setting.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/Checkout/Setting.cs
@@ -27,67 +27,74 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.Models.Checkout
         [DataMember(Name = "customerCheckoutType")]
         public string CustomerCheckoutType { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal1")]
-        public string GatewayFieldVal1 { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal2")]
-        public string GatewayFieldVal2 { get; set; }
+        [DataMember(Name = "credentials")]
+        public Newtonsoft.Json.Linq.JObject Credentials { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal3")]
-        public string GatewayFieldVal3 { get; set; }
+        //[DataMember(Name = "gatewayFieldVal1")]
+        //public string GatewayFieldVal1 { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal4")]
-        public string GatewayFieldVal4 { get; set; }
+        //[DataMember(Name = "gatewayFieldVal2")]
+        //public string GatewayFieldVal2 { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal5")]
-        public string GatewayFieldVal5 { get; set; }
+        //[DataMember(Name = "gatewayFieldVal3")]
+        //public string GatewayFieldVal3 { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal6")]
-        public string GatewayFieldVal6 { get; set; }
+        //[DataMember(Name = "gatewayFieldVal4")]
+        //public string GatewayFieldVal4 { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal7")]
-        public string GatewayFieldVal7 { get; set; }
+        //[DataMember(Name = "gatewayFieldVal5")]
+        //public string GatewayFieldVal5 { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal8")]
-        public string GatewayFieldVal8 { get; set; }
+        //[DataMember(Name = "gatewayFieldVal6")]
+        //public string GatewayFieldVal6 { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal9")]
-        public string GatewayFieldVal9 { get; set; }
+        //[DataMember(Name = "gatewayFieldVal7")]
+        //public string GatewayFieldVal7 { get; set; }
 
-        [DataMember(Name = "gatewayFieldVal10")]
-        public string GatewayFieldVal10 { get; set; }
+        //[DataMember(Name = "gatewayFieldVal8")]
+        //public string GatewayFieldVal8 { get; set; }
+
+        //[DataMember(Name = "gatewayFieldVal9")]
+        //public string GatewayFieldVal9 { get; set; }
+
+        //[DataMember(Name = "gatewayFieldVal10")]
+        //public string GatewayFieldVal10 { get; set; }
         
-        [DataMember(Name = "gatewayFieldId1")]
-        public string GatewayFieldId1 { get; set; }
+        //[DataMember(Name = "gatewayFieldId1")]
+        //public string GatewayFieldId1 { get; set; }
 
-        [DataMember(Name = "gatewayFieldId2")]
-        public string GatewayFieldId2 { get; set; }
+        //[DataMember(Name = "gatewayFieldId2")]
+        //public string GatewayFieldId2 { get; set; }
 
-        [DataMember(Name = "gatewayFieldId3")]
-        public string GatewayFieldId3 { get; set; }
+        //[DataMember(Name = "gatewayFieldId3")]
+        //public string GatewayFieldId3 { get; set; }
 
-        [DataMember(Name = "gatewayFieldId4")]
-        public string GatewayFieldId4 { get; set; }
+        //[DataMember(Name = "gatewayFieldId4")]
+        //public string GatewayFieldId4 { get; set; }
 
-        [DataMember(Name = "gatewayFieldId5")]
-        public string GatewayFieldId5 { get; set; }
+        //[DataMember(Name = "gatewayFieldId5")]
+        //public string GatewayFieldId5 { get; set; }
 
-        [DataMember(Name = "gatewayFieldId6")]
-        public string GatewayFieldId6 { get; set; }
+        //[DataMember(Name = "gatewayFieldId6")]
+        //public string GatewayFieldId6 { get; set; }
 
-        [DataMember(Name = "gatewayFieldId7")]
-        public string GatewayFieldId7 { get; set; }
+        //[DataMember(Name = "gatewayFieldId7")]
+        //public string GatewayFieldId7 { get; set; }
 
-        [DataMember(Name = "gatewayFieldId8")]
-        public string GatewayFieldId8 { get; set; }
+        //[DataMember(Name = "gatewayFieldId8")]
+        //public string GatewayFieldId8 { get; set; }
 
-        [DataMember(Name = "gatewayFieldId9")]
-        public string GatewayFieldId9 { get; set; }
+        //[DataMember(Name = "gatewayFieldId9")]
+        //public string GatewayFieldId9 { get; set; }
 
-        [DataMember(Name = "gatewayFieldId10")]
-        public string GatewayFieldId10 { get; set; }
+        //[DataMember(Name = "gatewayFieldId10")]
+        //public string GatewayFieldId10 { get; set; }
 
         [DataMember(Name = "payByMail")]
         public bool? PayByMail { get; set; }
+
+        [DataMember(Name = "credentialsSet")]
+        public bool AreGatewayCredentialFieldsSet { get; set; }
     }
 }
--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/settings/paymentAndCheckout/Gateway.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/settings/paymentAndCheckout/Gateway.js
@@ -1,1 +1,90 @@
-﻿
+﻿/**
+ * The discount editor view
+ */
+Ext.define('Taco.view.settings.paymentAndCheckout.Gateway', {
+    //extend: 'Taco.core.ux.form.Form',
+    extend: 'Taco.core.ux.form.Form',
+    requires: [],
+    //layout: {
+    //    type: 'vbox'
+      
+    //},
+    padding: '0 0 20 0',
+    //margin: '10,0,10,10',
+
+    header: null,
+
+    initComponent: function() {
+        this.header = null;
+        var credFieldDefs = this.gatewayDefinition.get('credentialDefinitions'),
+            credentials = this.record.get('credentials') || {},
+            supportedCards = this.record.get('supportedCards') || [],
+            supportedCardsDef = this.gatewayDefinition.get('supportedCards') || [],
+            supportedCardsCbs = [],
+            credentialsSet = this.record.get('credentialsSet');
+        this.items = [];
+        this.credFields = [];
+        Ext.Array.each(credFieldDefs, function(fieldDef) {
+            var credField = Ext.widget(
+                {
+                    xtype: 'textfield',
+                    fieldLabel: fieldDef.displayName,
+                    name: credentials[fieldDef.name],
+                    inputType: 'password',
+                    value: credentialsSet ? '        ' : ''
+                });
+            this.credFields.push(credField);
+            this.items.push(credField);
+        }, this);
+
+
+        Ext.Array.each(supportedCardsDef, function (card) {
+            supportedCardsCbs.push(
+                { boxLabel: card.Value, name: 'cards', inputValue: card.Key, checked: supportedCards.indexOf(card.value)>-1 }
+            );
+        });       
+        
+       
+        this.supportedCardsCbg = Ext.widget({
+            xtype: 'checkboxgroup',
+            fieldLabel: 'Supported Cards',
+            // Arrange checkboxes into two columns, distributed vertically
+            columns: 2,
+            vertical: true,
+            items: supportedCardsCbs
+        });
+
+        this.paymentProcessingFlowTypeRg = Ext.widget(
+            {
+                xtype: 'radiogroup',
+                fieldLabel: 'Order Processing',
+                // Arrange radio buttons into two columns, distributed vertically
+                columns: 1,
+                vertical: true,
+                items: [
+                    { boxLabel: 'Authorize And Capture On Order Placement', name: 'paymentProcessingFlowType', inputValue: 'AuthorizeAndCaptureOnOrderPlacement' },
+                    { boxLabel: 'Authorize On Order Placement And Capture On Order Shipment', name: 'paymentProcessingFlowType', inputValue: 'AuthorizeOnOrderPlacementAndCaptureOnOrderShipment' },
+                    { boxLabel: 'Authorize And Capture On Order Shipment', name: 'paymentProcessingFlowType', inputValue: 'AuthorizeAndCaptureOnOrderShipment' }
+                ]
+            });
+        ;
+        this.items.push(this.supportedCardsCbg, this.paymentProcessingFlowTypeRg);
+
+        this.callParent(arguments);
+    },
+    initTitle: Ext.emptyFn,
+    beforeSave: function() {
+        var isDirty = false, val = {};
+        Ext.each(this.credFields, function(field) {
+            if (field.isDirty()) {
+                isDirty = true;
+            }
+            val[field.name] = field.getValue();
+        });
+        if (isDirty) {
+            this.record.set('credentialDefinitions', val);
+        }
+        this.record.set('supportedCards', this.supportedCardsCbg.getValue().cards);
+
+    }
+});
