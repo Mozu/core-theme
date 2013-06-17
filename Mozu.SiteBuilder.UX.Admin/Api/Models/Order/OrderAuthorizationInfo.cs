04f59e0ce579211339f2bf85227934d7117d2f63
Foster Hersey <tuxedo25@gmail.com>
2013-06-17 14:46:47 -0500

    Fixed some syntax errors.

--- Mozu.SiteBuilder.UX.Admin/Api/Models/Order/OrderAuthorizationInfo.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/Order/OrderAuthorizationInfo.cs
@@ -16,31 +16,37 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.Models.Order
         /// <summary>
         ///  total order amount
         /// </summary>
+        [DataMember(Name="totalAmount")]
         public decimal TotalAmount { get; set; }
 
         /// <summary>
         /// amount collected so far
         /// </summary>
+        [DataMember(Name = "amountCollected")]
         public decimal AmountCollected { get; set; }
 
         /// <summary>
         /// amount eligible to be captured
         /// </summary>
+        [DataMember(Name = "captureAmount")]
         public decimal CaptureAmount { get; set; }
 
         /// <summary>
         ///  auth ready is when you have an authorized card with id
         /// </summary>
+        [DataMember(Name = "authReady")]
         public bool AuthReady { get; set; }
 
         /// <summary>
         /// canCapture is when you are authReady and you have a capture amount that is greater then 0
         /// </summary>
+        [DataMember(Name = "canCapture")]
         public bool CanCapture { get; set; }
 
         /// <summary>
         /// shortcut to an authorized payment to capture
         /// </summary>
+        [DataMember(Name = "captureData")]
         public OrderPayment CaptureData { get; set; }
     }
 }
