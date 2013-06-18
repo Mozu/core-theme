65877d35633368e25a176a57d628aab5d291060a
Foster Hersey <tuxedo25@gmail.com>
2013-06-18 11:41:01 -0500

    Fixed order payment actions.

--- Mozu.SiteBuilder.UX.Admin/Api/OrderPaymentController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/OrderPaymentController.cs
@@ -11,42 +11,59 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
 {
     public partial class OrderController
     {
+        public class CapturePaymentArg
+        {
+            public OrderPayment Payment { get; set; }
+            public decimal Amount { get; set; }
+        }
         [WebInvoke(Method = "POST", UriTemplate = "payment/capture")]
-        public async Task<Response<List<Order>>> CapturePayment(OrderPayment payment, decimal amount)
+        public async Task<Response<List<Order>>> CapturePayment(CapturePaymentArg arg)
         {
             // Possible actions can be "AuthAndCapture", "AuthorizePayment", "CapturePayment", "VoidPayment", "CreditPayment", "RequestCheck", "ApplyCheck", "DeclineCheck"
             var action = new DCp.PaymentAction
             {
                 ActionName = "CapturePayment",
                 ISOCurrencyCode = "USD",
-                Amount = amount,
+                Amount = arg.Amount,
                 ReferenceSourcePaymentId = null
             };
 
-            var order = (await _orderWebApiClient.PerformPaymentAction(payment.OrderId, payment.Id, action)).ReadAsSync();
+            var order = (await _orderWebApiClient.PerformPaymentAction(arg.Payment.OrderId, arg.Payment.Id, action)).ReadAsSync();
 
             //_orderWebApiClient.GetPackageLabel
             return List2(order.Map<Order>());
         }
 
-        [WebInvoke(Method = "POST", UriTemplate = "payment/credit")]
-        public async Task<Response<List<Order>>> CreditPayment(OrderPayment payment, decimal amount)
+        public class CreditPaymentArg
+        {
+            public OrderPayment Payment { get; set; }
+            public decimal Amount { get; set; }
+            public string Reason { get; set; }
+        }
+        [WebInvoke(UriTemplate = "payment/credit")]
+        public async Task<Response<List<Order>>> CreditPayment(CreditPaymentArg args)
         {
             // Possible actions can be "AuthAndCapture", "AuthorizePayment", "CapturePayment", "VoidPayment", "CreditPayment", "RequestCheck", "ApplyCheck", "DeclineCheck"
             var action = new DCp.PaymentAction
             {
                 ActionName = "CreditPayment",
                 ISOCurrencyCode = "USD",
-                Amount = amount,
+                Amount = args.Amount,
                 ReferenceSourcePaymentId = null
             };
 
-            var order = (await _orderWebApiClient.PerformPaymentAction(payment.OrderId, payment.Id, action)).ReadAsSync();
+            var order = (await _orderWebApiClient.PerformPaymentAction(args.Payment.OrderId, args.Payment.Id, action)).ReadAsSync();
+
+            // TODO: we don't currently do anything with the "reason"
 
             return List2(order.Map<Order>());
         }
 
-
+        public class VoidPaymentArg
+        {
+            public string OrderId { get; set; }
+            public string PaymentId { get; set; }
+        }
         [WebInvoke(Method = "POST", UriTemplate = "payment/void")]
         public async Task<Response<List<Order>>> VoidPayment(string orderId, string paymentId)
         {
