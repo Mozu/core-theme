4e2165283c411a7426c27b2911991f09ab502b29
Foster Hersey <tuxedo25@gmail.com>
2013-06-18 14:54:09 -0500

    Added MovePackageItem action.

--- Mozu.SiteBuilder.UX.Admin/Api/OrderShippingController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/OrderShippingController.cs
@@ -13,13 +13,128 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
 {
     public partial class OrderController
     {
-        [WebInvoke(Method="POST", UriTemplate="shipping/createpackage")]
+        [WebInvoke(Method="POST", UriTemplate="shipping/package/create")]
         public async Task<Response<List<OrderPackage>>> CreatePackage(OrderPackage package)
         {
             var dc = Mapper.Map<DCs.Package>(package);
             var ret = (await _orderWebApiClient.CreatePackage(package.OrderId, dc)).ReadAsSync();
 
             return List2( Mapper.Map<OrderPackage>(ret) );
+        }
+
+        public class DeletePackageArgs
+        {
+            public string OrderId { get; set; }
+            public List<string> PackageIds { get; set; }
+        }
+        [WebInvoke(Method = "POST", UriTemplate = "shipping/package/delete")]
+        public async Task<Response<List<OrderPackage>>> DeletePackage(DeletePackageArgs args)
+        {
+            var tasks = args.PackageIds.Select(pid => _orderWebApiClient.DeletePackage(args.OrderId, pid));
+            await Task.WhenAll(tasks);
+
+            return SuccessWithTotal2<List<OrderPackage>>(args.PackageIds.Count);
+        }
+
+        public class MovePackageItemArgs
+        {
+            public string OrderId { get; set; }
+            public string SourcePackageId { get; set; }
+            public string DestinationPackageId { get; set; }
+            public List<OrderPackageItem> Items { get; set; }
+        }
+        [WebInvoke(Method = "POST", UriTemplate = "shipping/package/moveitems")]
+        public async Task<Response<List<OrderPackage>>> MovePackageItems(MovePackageItemArgs args)
+        {
+            var updateTasks = new List<Task<Mozu.Core.Api.Contracts.Client.ServiceClientResponse<DCs.Package>>>();
+
+            // case 1: moving from 1 package to another
+            if (!String.IsNullOrEmpty(args.SourcePackageId) && !String.IsNullOrEmpty(args.DestinationPackageId))
+            {
+                DCs.Package source;
+                DCs.Package dest;
+
+                var getPackageTasks = new Task<Mozu.Core.Api.Contracts.Client.ServiceClientResponse<DCs.Package>> [] {
+                    _orderWebApiClient.GetPackage(args.OrderId, args.SourcePackageId),
+                    _orderWebApiClient.GetPackage(args.OrderId, args.DestinationPackageId)
+                };
+
+                await Task.WhenAll(getPackageTasks);
+                source = getPackageTasks[0].Result.ReadAsSync();
+                dest = getPackageTasks[1].Result.ReadAsSync();
+
+                foreach (var argItem in args.Items)
+                {
+                    var sourcePackageItem = source.Items.First(i => i.OrderItemId == argItem.OrderItemId);
+                    if (sourcePackageItem.Quantity == argItem.Quantity)
+                    {
+                        source.Items.Remove(sourcePackageItem);
+                    }
+                    else
+                    {
+                        sourcePackageItem.Quantity -= argItem.Quantity;
+                    }
+
+                    var destPackageItem = dest.Items.FirstOrDefault(i => i.OrderItemId == argItem.OrderItemId);
+                    if (destPackageItem == null)
+                    {
+                        destPackageItem = new DCs.PackageItem { OrderItemId = argItem.OrderItemId, Quantity = 0 };
+                        dest.Items.Add(destPackageItem);
+                    }
+                    destPackageItem.Quantity += argItem.Quantity;
+                }
+
+                updateTasks.AddRange(new [] {
+                    _orderWebApiClient.UpdatePackage(args.OrderId, args.SourcePackageId, source),
+                    _orderWebApiClient.UpdatePackage(args.OrderId, args.DestinationPackageId, dest)
+                });
+            }
+            // case 2: removing item from a package
+            if (!String.IsNullOrEmpty(args.SourcePackageId) && String.IsNullOrEmpty(args.DestinationPackageId))
+            {
+                var source = (await _orderWebApiClient.GetPackage(args.OrderId, args.SourcePackageId)).ReadAsSync();
+
+                foreach (var argItem in args.Items)
+                {
+                    var sourcePackageItem = source.Items.First(i => i.OrderItemId == argItem.OrderItemId);
+                    if (sourcePackageItem.Quantity == argItem.Quantity)
+                    {
+                        source.Items.Remove(sourcePackageItem);
+                    }
+                    else
+                    {
+                        sourcePackageItem.Quantity -= argItem.Quantity;
+                    }
+                }
+                updateTasks.Add( _orderWebApiClient.UpdatePackage(args.OrderId, args.SourcePackageId, source) );
+            }
+            // case 3: adding item to a package
+            if (String.IsNullOrEmpty(args.SourcePackageId) && !String.IsNullOrEmpty(args.DestinationPackageId))
+            {
+                var dest = (await _orderWebApiClient.GetPackage(args.OrderId, args.DestinationPackageId)).ReadAsSync();
+
+                foreach (var argItem in args.Items)
+                {
+                    var destPackageItem = dest.Items.FirstOrDefault(i => i.OrderItemId == argItem.OrderItemId);
+                    if (destPackageItem == null)
+                    {
+                        destPackageItem = new DCs.PackageItem { OrderItemId = argItem.OrderItemId, Quantity = 0 };
+                        dest.Items.Add(destPackageItem);
+                    }
+                    destPackageItem.Quantity += argItem.Quantity;
+                }
+                updateTasks.Add( _orderWebApiClient.UpdatePackage(args.OrderId, args.DestinationPackageId, dest) );
+            }
+            else
+            {
+                throw new ArgumentException("SourcePackageId and DestinationPackageId cannot both be empty.");
+            }
+
+            await Task.WhenAll(updateTasks);
+
+            var newPackages = Mapper.Map<List<OrderPackage>>( updateTasks.Select(t => t.Result.ReadAsSync()) );
+
+            return List2(newPackages);
         }
 
         public Task<Response<List<Order>>> EditPackage(OrderPackage package)
