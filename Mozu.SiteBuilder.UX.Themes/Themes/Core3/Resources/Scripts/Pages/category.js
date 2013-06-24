e043e8b88d81ae4d5322b77e53e114eb5452209c
Zetlen <CORP\james_zetlen>
2013-06-24 17:00:41 -0500

    quantities

--- Mozu.SiteBuilder.UX.Themes/Themes/Core3/Resources/Scripts/Pages/category.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core3/Resources/Scripts/Pages/category.js
@@ -8,31 +8,33 @@ define(['modules/jquery-plus', 'knockout', 'shim!vendor/jquery.history[jquery=jQ
             defaultPageSize = parseInt($.getMozuData('defaultpagesize')),
             facetingVM;
 
-        productListData.baseRequestParams = {
-            filter: 'categoryId req ' + categoryId,
-            facetTemplate: 'categoryId:' + categoryId,
-            facetHierValue: 'categoryId:' + categoryId,
-            facetHierDepth: 'categoryId:2'
-        };
+        if (productListData) {
+            productListData.baseRequestParams = {
+                filter: 'categoryId req ' + categoryId,
+                facetTemplate: 'categoryId:' + categoryId,
+                facetHierValue: 'categoryId:' + categoryId,
+                facetHierDepth: 'categoryId:2'
+            };
 
-        facetingVM = window.facetingVM = new FacetingModels.FacetedProductCollection(productListData);
+            facetingVM = window.facetingVM = new FacetingModels.FacetedProductCollection(productListData);
 
-        ko.applyBindings(facetingVM, $facetingForm[0]);
+            ko.applyBindings(facetingVM, $facetingForm[0]);
+            facetingVM.PageSize.subscribe(function (newVal) {
+                if (!facetingVM.isUpdating) facetingVM.updateFacets();
+            });
+
+            facetingVM.on('update', function () {
+                var newURL, lrClone = JSON.parse(JSON.stringify(facetingVM.lastRequest));
+                $.each(lrClone, function (p) { if (p in productListData.baseRequestParams) delete lrClone[p] });
+                if (lrClone.pageSize === defaultPageSize) delete lrClone.pageSize;
+                newURL = $.isEmptyObject(lrClone) ? window.location.href.replace(window.location.search, '') : "?" + $.param(lrClone);
+                History.replaceState(null, null, newURL);
+            });
+        }
 
         $('#mz-category-loading').remove();
         $facetingForm.noFlickerFadeIn();
 
-        facetingVM.PageSize.subscribe(function (newVal) {
-            if (!facetingVM.isUpdating) facetingVM.updateFacets();
-        });
-
-        facetingVM.on('update', function () {
-            var newURL, lrClone = JSON.parse(JSON.stringify(facetingVM.lastRequest));
-            $.each(lrClone, function (p) { if (p in productListData.baseRequestParams) delete lrClone[p] });
-            if (lrClone.pageSize === defaultPageSize) delete lrClone.pageSize;
-            newURL = $.isEmptyObject(lrClone) ? window.location.href.replace(window.location.search, '') : "?" + $.param(lrClone);
-            History.replaceState(null, null, newURL);
-        });
 
     });
     
