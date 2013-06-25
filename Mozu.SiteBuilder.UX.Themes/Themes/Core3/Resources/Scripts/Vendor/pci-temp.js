a5c8039a7a3f00bf67be935ae9f127a7328feeb3
Zetlen <CORP\james_zetlen>
2013-06-25 16:46:48 -0500

    fixing payment cards

--- Mozu.SiteBuilder.UX.Themes/Themes/Core3/Resources/Scripts/Vendor/pci-temp.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core3/Resources/Scripts/Vendor/pci-temp.js
@@ -35,7 +35,7 @@ p = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u
 
         apiCall = {
             base: {
-                uri: function () { return removeTrailingSlash(settings.get('apiBase')) + "/cards/"; }
+                uri: function () { return removeTrailingSlash(settings.get('apiBase')); }
             },
             save: {
                 uri: function () { return apiCall.base.uri(); },
