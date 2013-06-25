083a2619f28723ae06fad12216b946a1f615538f
Travis Johnson <CORP\Travis_Johnson>
2013-06-25 17:02:45 -0500

    NavForm fixes

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/NavForm.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/NavForm.js
@@ -5,10 +5,10 @@ Ext.define('Taco.core.ux.form.NavForm', {
     extend: 'Taco.core.ux.form.Form',
     alias: 'widget.taco.navform',
     requires: [],
+    sectionOffset: 39,
+    enableScrollSpy: true,
 
     initComponent: function() {
-
-        //this.nav = Ext.widget();
 
         this.formContainer = Ext.widget({
             xtype: 'container',
@@ -40,16 +40,93 @@ Ext.define('Taco.core.ux.form.NavForm', {
         this.callParent(arguments);
 
         this.nav = this.down('#navFormNav');
+
+        this.on({
+            afterrender: this.onAfterRender,
+            scope: this
+        });
+    },
+
+    getWrapper: function () {
+        if (!this.wrapper) {
+            this.wrapper = Taco.app.viewPort.down('contentbody');
+        }
+        return this.wrapper;
+    },
+
+    onAfterRender: function () {
+        if (!this.enableScrollSpy) return;
+
+        this.getWrapper().on({
+            afterlayout: this.rebuildMap,
+            scope: this
+        });
+
+        this.getWrapper().getEl().on({
+            scroll: this.checkTop,
+            scope: this
+        });
+
+        this.rebuildMap();
+    },
+
+    rebuildMap: function () {
+        this.locationMap = [];
+        this.recordMap = [];
+
+        if (!this.nav || !this.nav.store) return;
+        
+        this.nav.store.each(function (record, index) {
+            var el = record.raw.getEl();
+
+            if (!el) return;
+
+            this.recordMap.push(record);
+            
+            if (index === 0) {
+                this.locationMap.push(0);
+                return;
+            }
+
+            this.locationMap.push(el.dom.offsetTop + this.sectionOffset);
+        }, this);
+
+        this.checkTop();
+    },
+
+    checkTop: function () {
+        
+        var scrollTop = this.getWrapper().getEl().dom.scrollTop,
+            max,
+            li,
+            active;
+        
+        if (this.isHidden()) return;
+
+        Ext.each(this.locationMap, function (top, index) {
+            if (scrollTop >= top) max = index;
+            else return false;
+        }, this);
+
+        active = this.nav.getEl().down('.active');
+
+        if (active) active.removeCls('active');
+
+        li = this.nav.getEl().query('li')[max];
+
+        if (!li) return;
+
+        Ext.fly(li).addCls('active');
+
     },
 
     onNavClick: function(view, record) {
-        var wrapper = Taco.app.viewPort.down('contentbody').getEl(),
-            offset = 39,
+        var wrapper = this.getWrapper().getEl(),
             targetY;
 
         if (record.raw.getEl) {
             targetY = view.store.indexOf(record)
-                        ? record.raw.getEl().dom.offsetTop + offset
+                        ? record.raw.getEl().dom.offsetTop + this.sectionOffset
                         : 0;
             wrapper.scrollTo('top', targetY, true);
         }
