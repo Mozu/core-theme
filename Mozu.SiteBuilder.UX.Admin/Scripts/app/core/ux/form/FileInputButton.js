e1d8eadb0b8ab3672a7b3660a967512ba3dd1db2
Travis Johnson <CORP\Travis_Johnson>
2013-06-28 12:43:17 -0500

    Merging in Fix 12936 from Main to Dev

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/FileInputButton.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/FileInputButton.js
@@ -6,10 +6,25 @@ Ext.define('Taco.core.ux.form.FileInputButton', {
     extend: 'Ext.form.field.File',
     alias: 'widget.tacofilefield',
     buttonOnly: true,
-    // cls: 'taco-action-primary',
-    //baseCls: 'taco-action',
 
-    
+    buttonConfig: {
+        renderTpl: [
+            '<em id="{id}-btnWrap"<tpl if="splitCls"> class="{splitCls}"</tpl>>',
+                '<button id="{id}-btnEl" type="{type}" class="{btnCls}" hidefocus="true"',
+                    // the autocomplete="off" is required to prevent Firefox from remembering
+                    // the button's disabled state between page reloads.
+                    '<tpl if="tabIndex"> tabIndex="{tabIndex}"</tpl>',
+                    '<tpl if="disabled"> disabled="disabled"</tpl>',
+                    ' role="button" autocomplete="off">',
+                    '<span id="{id}-btnInnerEl" class="{baseCls}-inner" style="{innerSpanStyle}">',
+                        '{text}',
+                    '</span>',
+                    '<span id="{id}-btnIconEl" class="{baseCls}-icon {iconCls}"<tpl if="iconUrl"> style="background-image:url({iconUrl})"</tpl>></span>',
+                '</button>',
+            '</em>',
+            '<input id="{id}-fileInputEl" class="{inputCls}" type="file"  multiple="multiple" size="1" name="{inputName}">'
+        ]
+    },
 
     initComponent: function () {
         var me = this;
@@ -24,25 +39,7 @@ Ext.define('Taco.core.ux.form.FileInputButton', {
                 element: 'el'
             }
         });
-    },
-    
-    //overriding the template to add multiple="multiple"
-    renderTpl: [
-        '<em id="{id}-btnWrap"<tpl if="splitCls"> class="{splitCls}"</tpl>>',
-            '<button id="{id}-btnEl" type="{type}" class="{btnCls}" hidefocus="true"',
-                // the autocomplete="off" is required to prevent Firefox from remembering
-                // the button's disabled state between page reloads.
-                '<tpl if="tabIndex"> tabIndex="{tabIndex}"</tpl>',
-                '<tpl if="disabled"> disabled="disabled"</tpl>',
-                ' role="button" autocomplete="off">',
-                '<span id="{id}-btnInnerEl" class="{baseCls}-inner" style="{innerSpanStyle}">',
-                    '{text}',
-                '</span>',
-                '<span id="{id}-btnIconEl" class="{baseCls}-icon {iconCls}"<tpl if="iconUrl"> style="background-image:url({iconUrl})"</tpl>></span>',
-            '</button>',
-        '</em>',
-        '<input id="{id}-fileInputEl" class="{inputCls}" type="file"  multiple="multiple" size="1" name="{inputName}">'
-    ]
+    }
 
 
 
