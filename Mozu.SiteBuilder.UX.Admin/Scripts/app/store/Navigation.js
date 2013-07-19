19c20d1a8d378378bfc8b82eb9d04b95f87fa16e
Thom 'Pig Pen' Phipps <CORP\Thomas_Phipps>
2013-07-19 11:45:46 -0500

    security trimming of the navigatino

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/store/Navigation.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/store/Navigation.js
@@ -5,7 +5,23 @@ Ext.define('Taco.store.Navigation', {
     extend: 'Ext.data.Store',
     model: 'Taco.model.NavigationItem',
     autoLoad: true,
-    
+    filters:[{
+            filterFn: function (record) {
+                var ret = true;
+                if (record.raw.behaviorIds && record.raw.behaviorIds.length) {
+                    Ext.each(record.raw.behaviorIds, function (behaviorId) {
+                        if (Taco.User.behaviors.indexOf(behaviorId) == -1) {
+                            ret = false;
+                            return false;
+                        }
+                        return true;
+                    });
+                }
+                return ret;
+                
+            }
+        }
+    ],
     proxy: {
         type: 'ajax',
         api: {
