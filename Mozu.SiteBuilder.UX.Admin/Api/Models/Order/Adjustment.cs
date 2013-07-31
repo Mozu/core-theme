d284236ca12edc920ac8f885fae4d337715df4b3
Foster Hersey <tuxedo25@gmail.com>
2013-07-31 11:32:49 -0500

    Added OrderAdjustment and ShippingAdjustment to order.

---
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/Order/Adjustment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Mozu.SiteBuilder.UX.Admin.Api.Models.Order
+{
+    [DataContract]
+    public class Adjustment
+    {
+        [DataMember(Name = "amount")]
+        public decimal? Amount { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "internalComment")]
+        public string InternalComment { get; set; }
+    }
+}
