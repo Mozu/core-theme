3f1bfecb2779bfced9adde2900fc13de2f8f29a0
Bradley Friemel <CORP\bradley_friemel>
2013-08-01 11:54:43 -0500

    Discounts: removed edit btn, added action column

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/customers/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/customers/Index.js
@@ -54,60 +54,100 @@ Ext.define('Taco.view.customers.Index', {
     },
     initComponent: function () {
         var me = this;
-        this.tagStore = Taco.core.data.StoreManager.getOrCreate('Taco.store.CustomerTags');
-        this. gridPanelConf= {
-            columns: [{
-                dataIndex: 'primaryFirstName',
-                text: 'First Name',
-                width: 130
-            }, {
-                dataIndex: 'primaryLastName',
-                text: 'Last Name',
-                width: 130
-            }, {
-                dataIndex: 'primaryEmail',
-                text: 'Email',
-                width: 200
-            }, {
-                dataIndex: 'primaryCityOrTown',
-                text: 'Location',
-                width: 150,
-                renderer: function (value, metaData, record) {
-                    return value ? [Ext.String.capitalize(value), record.get('primaryState')].join(', ') : '';
+        me.header = {
+            title: 'Customers',
+            actions: [{
+                xtype: 'primarybutton',
+                text: 'Create New Customer',
+                click: function () {
+                    me.launchEditor(Ext.create('Taco.model.CustomerAccount'));
                 }
-            }, {
-                dataIndex: 'orderCount',
-                text: 'Total Orders',
-                width: 100
-            }, {
-                dataIndex: 'totalSpent',
-                text: 'Spent',
-                width: 100
-            }, {
-                dataIndex: 'groups',
-                text: 'Groups',
-                width:300,
-                renderer: function (value, metaData, record) {
-                    if (value && value.length) {
-                        var names = [];
-                        Ext.each(value || [], function (tagId) {
-                            var tagRecord = me.tagStore.getById(tagId);
-                            if (tagRecord) {
-                                names.push(tagRecord.get('Value'));
-                            }
-                                
-                            
-                        });
-
-                        if (names.length)
-                            return names.join(', ');
-                    }
-                },
-                minWidth: 100,
-                flex: 1
             }]
+        };
+        this.tagStore = Taco.core.data.StoreManager.getOrCreate('Taco.store.CustomerTags');
+        this.gridPanelConf = {
+            columns: [{
+                    dataIndex: 'primaryFirstName',
+                    text: 'First Name',
+                    width: 130
+                }, {
+                    dataIndex: 'primaryLastName',
+                    text: 'Last Name',
+                    width: 130
+                }, {
+                    dataIndex: 'primaryEmail',
+                    text: 'Email',
+                    width: 200
+                }, {
+                    dataIndex: 'primaryCityOrTown',
+                    text: 'Location',
+                    width: 150,
+                    renderer: function(value, metaData, record) {
+                        return value ? [Ext.String.capitalize(value), record.get('primaryState')].join(', ') : '';
+                    }
+                }, {
+                    dataIndex: 'orderCount',
+                    text: 'Total Orders',
+                    width: 100
+                }, {
+                    dataIndex: 'totalSpent',
+                    text: 'Spent',
+                    width: 100
+                }, {
+                    dataIndex: 'groups',
+                    text: 'Groups',
+                    width: 300,
+                    renderer: function(value, metaData, record) {
+                        if (value && value.length) {
+                            var names = [];
+                            Ext.each(value || [], function(tagId) {
+                                var tagRecord = me.tagStore.getById(tagId);
+                                if (tagRecord) {
+                                    names.push(tagRecord.get('Value'));
+                                }
+
+
+                            });
+
+                            if (names.length)
+                                return names.join(', ');
+                        }
+                    },
+                    minWidth: 100,
+                    flex: 1
+                },{
+            xtype: 'taco.menucolumn',
+        text: 'Actions',
+        flex:1,
+        menuItems: [
+        {
+            text: 'Edit',
+            requiredBehaviors: {
+                model: 'Taco.model.CustomerAccount',
+                behavior: 'update'
+            },
+            menuColumnHandler: function (item, eventData) {
+                var page = eventData.grid.getParentPage(),
+                    record = eventData.record,
+                    metaData = { id: record.getId() };
+
+                page.launchEditor(record, metaData);
+
+            }
+        }],
+        // do any processing needed to show menu
+        onMenuShow: function (menu, eventData) {
         }
+    }]
+        };
         
         this.callParent(arguments);
-    }
+    },
+
+    launchEditor: function (record) {
+        Ext.defer(function () {
+            Taco.core.StateManager.attemptNavigate('customers/edit/' + record.getId(), { complexMetaData: { record: record } });
+        }, 1, this);
+        return;
+    },
 });
