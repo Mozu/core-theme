4c77c096daa60fa6c6f941b9dc770527df785558
Thom 'Pig Pen' Phipps <CORP\Thomas_Phipps>
2013-08-03 20:25:40 -0500

    new nugets plus

---
+++ Mozu.SiteBuilder.UX.Admin/Api/ReturnController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper;
+using Mozu.CommerceRuntime.Contracts.Clients;
+using Mozu.Core.Api.Routing;
+using Mozu.Core.Settings;
+using Mozu.SiteBuilder.Mvc.ActionFilters;
+using Mozu.SiteBuilder.UX.Admin.Api.Models;
+using Mozu.SiteBuilder.UX.Admin.Api.Models.Order;
+using Mozu.SiteBuilder.UX.Admin.Helpers.OrderHelpers;
+using DCo = Mozu.CommerceRuntime.Contracts.Orders;
+
+namespace Mozu.SiteBuilder.UX.Admin.Api
+{
+
+    [WebApi("app/return", SuppressDescriptorGeneration = true)]
+    public partial class ReturnController : BaseController
+    {
+        private readonly ISettings _settings;
+        private IOrderWebApiClient _orderWebApiClient;
+        private readonly IReturnWebApiClient _returnWebApiClient;
+
+        /// <summary>
+        /// Public constructor.
+        /// </summary>
+        public ReturnController(IOrderWebApiClient orderWebApiClient, IReturnWebApiClient returnWebApiClient, ISettings settings)
+        {
+            _settings = settings;
+            _orderWebApiClient = orderWebApiClient;
+            _returnWebApiClient = returnWebApiClient;
+        }
+
+		[HttpGetRoute(UriTemplate = "list")]
+        public async Task<Response<List<Order>>> List([FromUri]PagingParamaters pagingParams, [FromUri]FilterCollection extFilter, [FromUri]bool draft=false)
+		{
+            throw new NotImplementedException();
+            //var originalOrderId = extFilter.GetValue("originalOrderId");
+            //if (originalOrderId != null)
+            //{
+                
+            //}
+            //_returnWebApiClient.GetReturns()
+            //int? startIndex = pagingParams.startIndex;
+            //int? pageSize = pagingParams.pageSize ?? 20;
+            //string sort = (pagingParams != null && pagingParams.sort != null) ? pagingParams.sort.ToSortString() : null;
+
+            //DCo.OrderCollection dcOrders = null;
+            
+            //// get single order
+            //if (!string.IsNullOrEmpty(pagingParams.id))
+            //{
+            //    dcOrders = new DCo.OrderCollection() { Items = new List<DCo.Order>() };
+            //    var order = (await _orderWebApiClient.GetOrder(pagingParams.id, draft)).ReadAsSync();
+            //    if (order != null)
+            //    {
+            //        dcOrders.Items.Add(order);
+            //    }
+            //}
+            //// get list of orders
+            //else
+            //{
+            //    var filter = extFilter.ToFilterString();
+            //    try
+            //    {
+            //        dcOrders = (await _orderWebApiClient.GetOrders(startIndex, pageSize, pagingParams.sort.ToSortString(), filter)).ReadAsSync();
+            //    }
+            //    catch (Exception)
+            //    {
+            //        dcOrders = new DCo.OrderCollection { Items = new List<DCo.Order>() };
+            //    }
+            //}
+
+            //var orders = dcOrders != null ? Mapper.Map<List<Order>>(dcOrders.Items) : new List<Order>();
+
+            //return List2(orders,(int) dcOrders.TotalCount );
+        }
+
+		[HttpPostRoute(UriTemplate = "cancel")]
+        public async Task<Response<List<Order>>> CancelOrder(string orderId)
+        {
+            var dc = (await _orderWebApiClient.PerformOrderAction(orderId, new DCo.OrderAction { ActionName = "CancelOrder" })).ReadAsSync();
+
+            return List2( Mapper.Map<Order>(dc) );
+        }
+    }
+}
---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/subform/Return.js
@@ -0,0 +1,209 @@
+﻿/**
+ * @class Taco.view.order.subform.Payment
+ */
+Ext.define('Taco.view.order.subform.Return', {
+    extend: 'Taco.view.order.subform.Subform',
+    requires: [
+      
+    ],
+    config: {
+        // order model
+        originalRecord: null,
+        record: null,
+        itemId: "orderPayment",
+        // title for the panel header
+        title: 'RMA',
+        // components to add to the panel header. typically used to add an actions menu button
+        tools: []
+    },
+
+    initComponent: function (eOpts) {
+        var me = this;
+
+        this.cls = [this.cls, Taco.baseCSSPrefix + 'orderform-payment'].join(' ');
+
+        // after the record is reloaded we will need to refresh the ui
+        this.record.on("aftercommit", function () {
+            this.onRecordChange();
+        }, this);
+
+        // initialize the ui
+        // this will be called every time the record is updated
+        me.bodyCont = Ext.create('Ext.container.Container', {
+            items: []
+        });
+        me.initUI();
+
+
+        Ext.apply(me, {
+            items: [
+                me.bodyCont
+            ]
+        });
+        this.callParent(arguments);
+    },
+
+    initActionsMenu: function () {
+        var me = this,
+            canVoidPayment = false,
+            canApplyCheck = false,
+            canCapture = false,
+            canAppPayment = false,
+            canCreditPayment = false,
+            data = this.record.getData(),
+            authPayment = data.payments[0],
+            availableActions;
+
+        me.addPaymentAction = new Ext.Action({
+            text: 'Add Payment',
+            handler: function () {
+                var me = this;
+
+                var modal = Ext.create('Taco.view.order.modal.AddPayment', {
+                    record: me.record
+                });
+
+                modal.show();
+            },
+            scope: this
+        });
+
+        me.requestCheckAction = new Ext.Action({
+            text: 'Request Check',
+            handler: function () {
+                var me = this;
+
+                var modal = Ext.create('Taco.view.order.modal.RequestCheck', {
+                    record: me.record
+                });
+
+                modal.show();
+            },
+            scope: this
+        });
+        me.applyManualPayment = new Ext.Action({
+            text: 'Add Manual Payment',
+            handler: function () {
+                var me = this;
+
+                var modal = Ext.create('Taco.view.order.modal.AddPaymentManual', {
+                    record: me.record
+                });
+
+                modal.show();
+            },
+            scope: this
+        });
+
+        // add the tools the header using the pre defined actions above;
+        me.setTools({
+            xtype: 'taco.button',
+            width: 50,
+            height: 30,
+            text: ' ',
+            menuAlign: 'tr-br',
+            cls: Taco.baseCSSPrefix + 'editcontainer-menu-button',
+            autoEl: {
+                tag: 'a'
+            },
+            menu: {
+                plain: true,
+                items: [
+                    me.addPaymentAction,
+                    //me.issueCreditAction,
+                    //me.voidTransactionAction,
+                    //me.issueCreditPaymentAction,
+                    me.requestCheckAction,
+                    me.applyManualPayment
+                ]
+            }
+        });
+    },
+
+    // initialize the views and actions menu
+    initUI: function () {
+        var me = this;
+
+        me.initActionsMenu();
+        me.initHeader();
+        me.initPaymentsUI();
+    },
+
+    initPaymentsUI: function () {
+        var me = this;
+
+        me.paymentPanels = [];
+
+        this.record.payments().each(function (payment) {
+            var panel = Ext.create('Taco.view.order.widget.PaymentPanel',
+                {
+                    order: me.record,
+                    record: payment
+                });
+            me.paymentPanels.push(panel);
+        });
+
+        // add the panels all at once to speed up layout.
+        me.bodyCont.add(me.paymentPanels);
+
+    },
+
+    // clear out the payment panels
+    destroyPaymentsUI: function () {
+        var me = this;
+
+        if (me.paymentPanels) {
+            Ext.Array.each(me.paymentPanels, function (panel) { panel.destroy(); });
+            delete me.paymentPanels;
+        }
+    },
+
+    initHeader: function () {
+        var me = this,
+           data = this.record.getData();
+
+        me.headerDetails = Ext.create('Ext.Component', {
+            // cls: "orderform-payment-paymentDetails",
+            tpl: [
+                '<div class="statusField">',
+                    //'<span class="orderTotal">Partially Collected</span>',
+                    '<tpl if="authorizationInfo.amountCollected &gt; 0 && authorizationInfo.amountCollected != total">',
+                        '<span class="orderTotal">Partially Collected</span>',
+                    '</tpl>',
+                    '<tpl if="authorizationInfo.amountCollected == 0">',
+                        '<span class="orderTotal">None Collected</span>',
+                    '</tpl>',
+                    '<tpl if="authorizationInfo.amountCollected == total">',
+                        '<span class="orderTotal">Fully Collected</span>',
+                    '</tpl>',
+                '</div>',
+                '<div class="orderSummary">',
+                    '<span class="orderTotal">Order Total: {total:usMoney}</span>',
+                    '<span class="seperator">|</span>',
+                    '<span class="Received">Received: {authorizationInfo.amountCollected:usMoney}</span>',
+                    '<span class="seperator">|</span>',
+                    '<span class="balance">Balance: {authorizationInfo.captureAmount:usMoney}</span>',
+                '</div>'
+            ],
+            data: data
+        });
+
+        me.bodyCont.add(me.headerDetails);
+    },
+    // called when the record has been updated
+    onRecordChange: function () {
+        var me = this;
+        Ext.suspendLayouts();
+
+        Ext.resumeLayouts(true);
+    },
+
+    /*
+     * ExtJS doesn't handle reload of data with sub-stores well.
+     * So we manually re-populate the order.payments() and the payment.interactions() stores.
+     */
+    rebuildPayments: function () {
+     
+    }
+
+});
