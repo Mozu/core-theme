8a4261f018066fe8666dc877eb07ed3cba43d284
Zetlen <CORP\james_zetlen>
2013-08-09 14:22:30 -0500

    had some outstanding ext4.2 changes from yesterday including tabs. all mergeded now!

--- Mozu.SiteBuilder.UX.Admin/Scripts/build/config.rb
+++ Mozu.SiteBuilder.UX.Admin/Scripts/build/config.rb
@@ -2,7 +2,8 @@ require '..\ext\packages\ext-theme-base\sass\utils.rb'
 require '..\packages\mozuadmintheme\sass\config.rb'
 
             output_style = :expanded
-            images_path = %q{C:\mz\UI\Dev\Dev-Ext4.2Audit\Mozu.SiteBuilder\Mozu.SiteBuilder.UX.Admin\Scripts\resources\images}.gsub!(/\\/, '/')
+            images_path = %q{C:\mzuir\Mozu.SiteBuilder.UX.Admin\Scripts\resources\images}.gsub!(/\\/, '/')
             http_generated_images_path = "/admin/Scripts/build/images"
-            fonts_path = %q{C:\mz\UI\Dev\Dev-Ext4.2Audit\Mozu.SiteBuilder\Mozu.SiteBuilder.UX.Admin\Scripts\resources\fonts}.gsub!(/\\/, '/')
+            fonts_path = %q{C:\mzuir\Mozu.SiteBuilder.UX.Admin\Scripts\resources\fonts}.gsub!(/\\/, '/')
+        
         
