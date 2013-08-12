29691c9ad762b142c33eeb84be415e87d2557702
Foster Hersey <tuxedo25@gmail.com>
2013-08-12 15:40:14 -0500

    ShippingAdjustment changes were incorrectly affecting OrderAdjustment.

--- Mozu.SiteBuilder.UX.Admin/Api/OrderController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/OrderController.cs
@@ -129,15 +129,15 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
  
             if (args.OrderAdjustment != null && args.OrderAdjustment.Amount.HasValue)
             {
-                if (args.OrderAdjustment.Amount <= 0)
+                if (args.OrderAdjustment.Amount == 0)
                     dcOrder = (await _orderWebApiClient.RemoveAdjustment( args.OrderId, APPLY_TO_DRAFT )).ReadAsSync();
                 else
                     dcOrder = (await _orderWebApiClient.ApplyAdjustment( args.OrderId, args.OrderAdjustment.Map<Mozu.CommerceRuntime.Contracts.Commerce.Adjustment>(), APPLY_TO_DRAFT )).ReadAsSync();
             }
             if (args.ShippingAdjustment != null && args.ShippingAdjustment.Amount.HasValue)
             {
-                if (args.ShippingAdjustment.Amount <= 0)
-                    dcOrder = (await _orderWebApiClient.RemoveAdjustment( args.OrderId, APPLY_TO_DRAFT )).ReadAsSync();
+                if (args.ShippingAdjustment.Amount == 0)
+                    dcOrder = (await _orderWebApiClient.RemoveShippingAdjustment( args.OrderId, APPLY_TO_DRAFT )).ReadAsSync();
                 else
                     dcOrder = (await _orderWebApiClient.ApplyShippingAdjustment(args.OrderId, args.ShippingAdjustment.Map<Mozu.CommerceRuntime.Contracts.Commerce.Adjustment>(), APPLY_TO_DRAFT)).ReadAsSync();
             }
