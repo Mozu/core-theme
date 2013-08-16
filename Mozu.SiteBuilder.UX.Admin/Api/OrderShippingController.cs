724826a5cd38d4b5e56930591eae478e59b4c568
Thom 'Pig Pen' Phipps <CORP\Thomas_Phipps>
2013-08-16 14:06:16 -0500

    adding shipping runtime methods

--- Mozu.SiteBuilder.UX.Admin/Api/OrderShippingController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/OrderShippingController.cs
@@ -16,7 +16,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
 {
     public partial class OrderController
     {
-        [HttpPostRoute(UriTemplate = "shipping/runtimemethods")]
+        [HttpGetRoute(UriTemplate = "shipping/runtimemethods")]
         public async Task<Response<List<DCs.ShippingRate>>> GetRuntimeShipmentMethods([FromUri]string orderId)
         {
             List<DCs.ShippingRate> rates = (await _orderWebApiClient.GetAvailableShipmentMethods(orderId)).ReadAsSync();
---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/model/ShippingMethod.js
@@ -0,0 +1,18 @@
+﻿/**
+ * @class Taco.model.Discount
+ */
+Ext.define('Taco.model.ShippingMethod', {
+    extend: 'Taco.core.data.Model',
+   
+    idProperty: 'ShippingMethodCode',
+    fields: [{
+        name: 'ShippingMethodCode',
+            type: 'string'
+        }, {
+            name: 'ShippingMethodName',
+            type: 'string'
+        }, {
+            name: 'Price',
+            type: 'number'
+        }]
+});
