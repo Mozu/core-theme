704c6622c83a303cf0b429ec1da59897c24951f3
Bradley Friemel <CORP\bradley_friemel>
2013-08-19 17:30:06 -0500

    Fixed bug 15246

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/form/Address.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/form/Address.js
@@ -108,7 +108,6 @@ Ext.define('Taco.shared.view.form.Address', {
             width: 206,
             name: 'homePhone',
             fieldLabel: 'Home Phone',
-            allowBlank: false,
             margin: '0 13 5 0',
             style: { 'display': 'inline-table' }
         }, {
