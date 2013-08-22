98b8f17a4e9992c306789fbb5816428831598f3e
Foster Hersey <tuxedo25@gmail.com>
2013-08-22 18:39:12 -0500

    Discount grid would not update after creating a discount.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/AjaxProxy.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/AjaxProxy.js
@@ -7,15 +7,6 @@
     Ext.define('Taco.core.data.AjaxProxy', {
         extend: 'Ext.data.proxy.Ajax',
         alias: 'proxy.ajaxproxy',
-        constructor: function (config) {
-            
-            var me = this;
-            this.callParent([config]);
-            me.on('exception', function() {
-                console.log('ajaxproxy-exception', arguments);
-            }, me);
-         
-        },
         actionMethods: {
             create: 'POST',
             read: 'GET',
@@ -23,40 +14,9 @@
             destroy: 'POST',
             duplicate: 'POST'
         },
-        afterRequest: function ( request, success ){
-            var me = this;
-            this.callParent(request, success);
-            if (success && request && request.action != 'read' && this.model && this.model.$className) {
-                this.signalCacheFlush();
-            }
-            
-        },
         duplicate: function() {
             return this.doRequest.apply(this, arguments);
         },
-        /**
-         * Flushes the global application signal cache.
-         */
-        signalCacheFlush: function () {
-            Taco.app.signalCacheFlush({ model: this.model.$className });
-        },
-        encodeFilters: function (filters) {
-            var min = [],
-                length = filters.length,
-                i = 0;
-
-            for (; i < length; i++) {
-                min[i] = {
-                    property: filters[i].property,
-                    value: filters[i].value
-                };
-                if (filters[i].comparison) {
-                    min[i].comparison = filters[i].comparison;
-                }
-            }
-            return this.applyEncoding(min);
-        },
-
         
         reader: {
             type: 'json',
