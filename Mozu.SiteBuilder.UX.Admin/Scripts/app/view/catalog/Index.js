4fdb95a23c8d2bcb8d501a213dbd29951f8b1af4
Jimmy Sanford <CORP\Jimmy_Sanford>
2013-08-23 11:32:15 -0500

    new panel ui, subform

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/catalog/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/catalog/Index.js
@@ -40,25 +40,30 @@ Ext.define('Taco.view.catalog.Index', {
             }]
         });
 
+        panel = Ext.create('Ext.panel.Panel', {
+            ui: 'subform',
+            title: 'Subform',
+            closable: true,
+            bodyPadding: '20 0',
+            header: {
+                layout: {
+                    type: 'hbox',
+                    align: 'middle',
+                    alignRoundingMethod: 'ceil'
+                }
+            },
+            items: [{
+                xtype: 'component',
+                html: 'Hello world'
+            }]
+        });
+
         uibtns = Ext.create('Ext.Container', {
             layout: {
                 type: 'hbox',
                 defaultMargins: '5'
             },
             items: [{
-                xtype: 'button',
-                text:'senaro1',
-                handler:function () {
-                    me.doIt(1);
-                }
-            }, {
-                xtype: 'button',
-                text: 'senaro2',
-                handler: function () {
-                    me.doIt(2);
-                }
-            },
-                {
                     xtype: 'button',
                     frame: false,
                     scale: 'medium',
@@ -119,106 +124,15 @@ Ext.define('Taco.view.catalog.Index', {
         Ext.apply(this.body, {
             cls: Taco.baseCSSPrefix + 'catalog',
             layout: 'auto',
-            items: [uibtns]
+            items: [uibtns, panel]
         });
 
-        this.initStoreTest();
         this.callParent(arguments);
 
         this.modal = modal;
         this.modaless = modaless;
     },
 
-    initStoreTest: function () {
-        var model1, model2, model3, store1;
-
-        Ext.define('thom.Model3', {
-            extend: 'Taco.core.data.Model',
-            fields: [
-                { name: 'id', type: 'int' },
-                { name: 'name', type: 'string' }
-            ]
-        });
-
-        Ext.define('thom.Model2', {
-            extend: 'Taco.core.data.Model',
-            fields: [
-                { name: 'id', type: 'int' },
-                { name: 'name', type: 'string' },
-                { name: 'model3s', type: 'auto' }
-            ],
-            getModel3s: function () {
-                return this.getOrCreateHasManyStore({
-                    model: 'thom.Model3',
-                    associationKey: 'model3s',
-                    foreignProperty: 'parent'
-                });
-            },
-        });
-
-        Ext.define('thom.Model1', {
-            extend: 'Taco.core.data.Model',
-            fields: [
-                { name: 'id', type: 'int' },
-                { name: 'name', type: 'string' },
-                { name: 'model2s', type: 'auto' }
-            ],
-            getModel2s: function () {
-                return this.getOrCreateHasManyStore({
-                    model: 'thom.Model2',
-                    associationKey: 'model2s',
-                    foreignProperty: 'parent'
-                });
-            },
-        });
-        window.thomStore = this.thomStore = Ext.create('Ext.data.Store', {
-            model: 'thom.Model1'
-        });
-
-        window.theData = [
-            {
-                id: 1,
-                name: 'l1a',
-                model2s: [{
-                        id: 1,
-                        name: 'l1a-l2a',
-                        model3s: [
-                            {
-                                id: 1,
-                                name: 'l1a-l2a-l3a'
-                            },
-                            {
-                                id: 2,
-                                name: 'l1a-l2a-l3b'
-                            },
-                            {
-                                id: 3,
-                                name: 'l1a-l2a-l3c'
-                            }
-                        ]
-                    }
-                ]
-            }
-        ];
-        
-        this.thomStore.loadData(window.theData);
-        window.model1 = this.thomStore.getAt(0);
-        window.model2Store = this.thomStore.getAt(0).getModel2s();
-        window.model3Store = window.model2Store.getAt(0).getModel3s();
-        window.model3s = window.model3Store.getAt(0);
-
-    },
-    doIt: function (senario) {
-        var newData = Ext.clone(window.theData);
-        if (senario == 1) {
-            newData[0].model2s[0].name = theData[0].model2s[0].name + new Date().getTime();
-        }
-        if (senario == 2) {
-            newData[0].model2s[0].model3s[0].name = theData[0].model2s[0].model3s[0].name + new Date().getTime();
-        }
-        window.model1.set(newData[0]);
-        //this.thomStore.loadData(newData);
-    },
     launchModal: function (type) {
         var modal = this[type];
 
