5318cb6075d009bb9836d3479626960ede091654
Jimmy Sanford <CORP\Jimmy_Sanford>
2013-08-26 11:01:37 -0500

    4.2 upgrade broke it; fixed

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/FileInputButton.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/form/FileInputButton.js
@@ -8,22 +8,24 @@ Ext.define('Taco.core.ux.form.FileInputButton', {
     buttonOnly: true,
 
     buttonConfig: {
-        renderTpl: [
-            '<em id="{id}-btnWrap"<tpl if="splitCls"> class="{splitCls}"</tpl>>',
-                '<button id="{id}-btnEl" type="{type}" class="{btnCls}" hidefocus="true"',
-                    // the autocomplete="off" is required to prevent Firefox from remembering
-                    // the button's disabled state between page reloads.
-                    '<tpl if="tabIndex"> tabIndex="{tabIndex}"</tpl>',
-                    '<tpl if="disabled"> disabled="disabled"</tpl>',
-                    ' role="button" autocomplete="off">',
-                    '<span id="{id}-btnInnerEl" class="{baseCls}-inner" style="{innerSpanStyle}">',
-                        '{text}',
-                    '</span>',
-                    '<span id="{id}-btnIconEl" class="{baseCls}-icon {iconCls}"<tpl if="iconUrl"> style="background-image:url({iconUrl})"</tpl>></span>',
-                '</button>',
-            '</em>',
-            '<input id="{id}-fileInputEl" class="{inputCls}" type="file"  multiple="multiple" size="1" name="{inputName}">'
-        ]
+        ui: 'action-primary',
+        scale: 'medium'
+        // renderTpl: [
+        //     '<em id="{id}-btnWrap"<tpl if="splitCls"> class="{splitCls}"</tpl>>',
+        //         '<button id="{id}-btnEl" type="{type}" class="{btnCls}" hidefocus="true"',
+        //             // the autocomplete="off" is required to prevent Firefox from remembering
+        //             // the button's disabled state between page reloads.
+        //             '<tpl if="tabIndex"> tabIndex="{tabIndex}"</tpl>',
+        //             '<tpl if="disabled"> disabled="disabled"</tpl>',
+        //             ' role="button" autocomplete="off">',
+        //             '<span id="{id}-btnInnerEl" class="{baseCls}-inner" style="{innerSpanStyle}">',
+        //                 '{text}',
+        //             '</span>',
+        //             '<span id="{id}-btnIconEl" class="{baseCls}-icon {iconCls}"<tpl if="iconUrl"> style="background-image:url({iconUrl})"</tpl>></span>',
+        //         '</button>',
+        //     '</em>',
+        //     '<input id="{id}-fileInputEl" class="{inputCls}" type="file"  multiple="multiple" size="1" name="{inputName}">'
+        // ]
     },
 
     initComponent: function () {
