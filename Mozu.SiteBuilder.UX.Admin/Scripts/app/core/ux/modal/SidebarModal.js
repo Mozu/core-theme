0c8f1db8c6293ce7869b5fa42912795e72151839
Zetlen <CORP\james_zetlen>
2013-08-27 16:02:53 -0500

    fixing resize

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/modal/SidebarModal.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/modal/SidebarModal.js
@@ -14,16 +14,38 @@ Ext.define('Taco.core.ux.modal.SidebarModal', {
     shadow: false,
     width: 320,
 
-    initComponent: function () {
-        this.callParent(arguments);
-        this.mon(Taco.app.viewPort, 'resize', function () {
-            if (this.rendered && !this.hidden) this.alignTo(this.sidebar.getEl(), 'tl-tl');
-        }, this);
-        this.on('boxready',function () {
+    statics: {
+        active: []
+    },
+
+    isActive: function() {
+        var active = this.statics().active;
+        return active[active.length - 1] === this && this.rendered && !this.hidden;
+    },
+
+    fitOverSidebar: function() {
+        if (this.isActive()) {
             this.sidebar = this.sidebar || this.up('sidebar');
             this.setSize(this.sidebar.getSize());
-            this.alignTo(this.sidebar.getEl(), 'tl-tr');
-        }, this);
+            this.alignTo(this.sidebar.getEl(), 'tl-tl');
+        }
+    },
+
+    initComponent: function () {
+        this.callParent(arguments);
+        //this.mon(Taco.app.viewPort, 'resize', function () {
+        //    if (this.rendered && !this.hidden) {
+        //        this.alignTo(this.sidebar.getEl(), 'tl-tl');
+        //        this.
+        //    }
+        //}, this);
+        //this.on('boxready',function () {
+        //    this.sidebar = this.sidebar || this.up('sidebar');
+        //    this.setSize(this.sidebar.getSize());
+        //    this.anchorTo(this.sidebar.getEl(), 'tl-tr');
+        //}, this);
+        this.mon(Taco.app.viewPort, 'resize', this.fitOverSidebar, this);
+        this.on('boxready', this.fitOverSidebar, this);
     },
 
     show: function () {
@@ -32,11 +54,13 @@ Ext.define('Taco.core.ux.modal.SidebarModal', {
     //        el.removecls('sliding');
     //    }, 500);
         this.callParent(arguments);
-        this.alignTo(this.sidebar.getEl(), 'tl-tl');
+        this.statics().active.push(this);
+        this.fitOverSidebar();
     },
 
     hide: function () {
         this.alignTo(this.sidebar.getEl(), 'tl-tr');
         this.callParent(arguments);
+        this.statics().active.pop();
     }
 });
