ffa19e79d88ce52b4b76543207b60027f1c42b58
Foster Hersey <foster.hersey@gmail.com>
2013-09-04 21:49:51 -0500

    Removed hidden logic from generate random coupon button (hidden logic belongs on its container).

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/discount/Form.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/discount/Form.js
@@ -346,7 +346,6 @@ Ext.define('Taco.view.discount.Form', {
                 me.couponCodeInput,
                 {
                     xtype: 'secondarybutton',
-                    hidden: !(me.record.get('couponCode') || me.record.get('requiresCoupon')),
                     text: 'Random',
                     click: function() {
                         var randomizer = Ext.data.IdGenerator.get('uuid'),
