f51c8995c55601f5130fd91fc6f89459787dc04b
Foster Hersey <foster.hersey@gmail.com>
2013-09-17 16:58:32 -0500

    Added ProductPublishingMode to TaContext.

--- Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/TenantMapping.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/TenantMapping.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using AutoMapper;
 using Mozu.SiteBuilder.UX.Models.Admin;
+using DC = Mozu.Tenant.Contracts;
 
 namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
 {
@@ -12,40 +13,46 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
 
     public class TenantMapping : Profile
     {
-        public override string ProfileName
-        {
-            get
-            {
-                return this.GetType().FullName;
-            }
-        }
+        public override string ProfileName { get { return this.GetType().FullName; } }
 
         protected override void Configure()
         {
-            AutoMapper.Mapper.CreateMap<Mozu.Tenant.Contracts.Tenant, Mozu.SiteBuilder.UX.Models.Admin.TaContext>()
-                      .ForMember(x => x.Id, op => op.MapFrom(x => x.Id))
-                      .ForMember(x => x.Name, op => op.MapFrom(x => x.Name))
-                      .ForMember(x => x.SiteCollections, op => op.MapFrom(x => x.SiteGroups));
-
-
-            AutoMapper.Mapper.CreateMap<Mozu.Tenant.Contracts.SiteGroup , Mozu.SiteBuilder.UX.Models.Admin.TaContextSiteCollection >()
+            AutoMapper.Mapper.CreateMap<DC.Tenant, TaContext>()
                 .ForMember(x => x.Id, op => op.MapFrom(x => x.Id))
-                      .ForMember(x => x.Name, op => op.MapFrom(x => x.Name))
-                      .ForMember(x => x.Sites, op => op.MapFrom(x => x.Sites));
+                .ForMember(x => x.Name, op => op.MapFrom(x => x.Name))
+                .ForMember(x => x.SiteCollections, op => op.MapFrom(x => x.SiteGroups))
+                ;
 
 
-            AutoMapper.Mapper.CreateMap<Mozu.Tenant.Contracts.Site, Mozu.SiteBuilder.UX.Models.Admin.TaContextSite>()
-                      .ForMember(x => x.Id, op => op.MapFrom(x => x.Id))
-                      .ForMember(x => x.Name, op => op.MapFrom(x => x.Name))
-                      .ForMember(x => x.StagingHost, op => op.MapFrom(x => x.Domains == null ? null : x.Domains.Where(d => d.IsSystemAssigned).Select(d => d.DomainName).FirstOrDefault()));
+            AutoMapper.Mapper.CreateMap<DC.SiteGroup, TaContextSiteCollection >()
+                .ForMember(x => x.Id, op => op.MapFrom(x => x.Id))
+                .ForMember(x => x.Name, op => op.MapFrom(x => x.Name))
+                .ForMember(x => x.Sites, op => op.MapFrom(x => x.Sites))
+                ;
 
-            AutoMapper.Mapper.CreateMap<Mozu.Tenant.Contracts.Tenant, Mozu.SiteBuilder.UX.Models.Admin.TaContext>()
-              .ForMember(x => x.Id, op => op.MapFrom(x => x.Id))
-              .ForMember(x => x.Name, op => op.MapFrom(x => x.Name))
-;
+
+            AutoMapper.Mapper.CreateMap<DC.Site, TaContextSite>()
+                .ForMember(x => x.Id, op => op.MapFrom(x => x.Id))
+                .ForMember(x => x.Name, op => op.MapFrom(x => x.Name))
+                .ForMember(x => x.StagingHost, op => op.MapFrom(x => x.Domains == null ? null : x.Domains.Where(d => d.IsSystemAssigned).Select(d => d.DomainName).FirstOrDefault()));
+
+
+            AutoMapper.Mapper.CreateMap<Mozu.ProductAdmin.Contracts.SiteGroupCollection, TaContext>()
+                .AfterMap((dc, tacontext) => {
+                    // map ProductPublishingMode to each sitegroup in the collection.
+                    foreach (var sitegroup in dc.Items)
+                    {
+                        var sitegroupInTacontext = tacontext.SiteCollections.FirstOrDefault(s => s.Id == sitegroup.Id);
+                        if (sitegroupInTacontext != null)
+                            sitegroupInTacontext.ProductPublishingMode = sitegroup.ProductPublishingMode;
+                    }
+
+                    // TODO: hard-coding a default value for now, in case the service doesn't always return a value.
+                    tacontext.SiteCollections.Each(sc => { if (sc.ProductPublishingMode == null) sc.ProductPublishingMode = Mozu.ProductAdmin.Contracts.SiteGroup.ProductPublishingModeConst.Live; });
+
+                })
+                .ForAllMembers(op => op.Ignore())
+                ;
         }
     }
 }
-
-
-
--- Mozu.SiteBuilder.UX.Admin/Api/PublishingSettingsController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/PublishingSettingsController.cs
@@ -2,27 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Mozu.Core.Api.Routing;
 using Mozu.Core.Settings;
+using Mozu.ProductAdmin.Contracts.Clients;
 using Mozu.SiteBuilder.Mvc;
+using Mozu.SiteBuilder.UX.Admin.Api.Models;
+using Mozu.SiteSettings.General.Contracts.Clients;
+using DC = Mozu.ProductAdmin.Contracts;
 
 namespace Mozu.SiteBuilder.UX.Admin.Api
 {
     [WebApi("app/settings/publishing", SuppressDescriptorGeneration = true)]
     public class PublishingSettingsController : BaseController
     {
-        private readonly ISettings _settings;
         private ISiteBuilderApiContext _ctx;
+        private IGeneralSettingsWebApiClient _siteSettingsClient;
+        private ISiteGroupWebApiClient _siteGroupClient;
 
         /// <summary>
         /// Public constructor.
         /// </summary>
-        public PublishingSettingsController(ISettings settings, ISiteBuilderApiContext ctx)
+        public PublishingSettingsController(ISiteBuilderApiContext ctx, IGeneralSettingsWebApiClient siteSettingsClient, ISiteGroupWebApiClient siteGroupClient)
         {
-            _settings = settings;
             _ctx = ctx;
+            _siteSettingsClient = siteSettingsClient;
+            _siteGroupClient = siteGroupClient;
         }
 
+        public async Task<Response<DC.SiteGroup>> UpdateCatalogPublishingPreferences(int siteGroupId)
+        {
+            var dcSettings = (await _siteGroupClient.GetSiteGroup(siteGroupId)).ReadAsSync();
+            dcSettings.ProductPublishingMode = DC.SiteGroup.ProductPublishingModeConst.Pending;
+            var res = (await _siteGroupClient.UpdateSiteGroup(dcSettings, siteGroupId)).ReadAsSync();
 
+            return Single2(res);
+        }
+
+        public async void UpdateContentPublishingPreferences(int siteId)
+        {
+            var dcSettings = (await _siteSettingsClient.GetGeneralSettings()).ReadAsSync();
+        }
     }
 }
