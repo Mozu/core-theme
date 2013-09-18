1ff56594c17e0466d2b31578a30416940e47489f
Bradley Friemel <CORP\bradley_friemel>
2013-09-18 14:51:19 -0500

    Changed UI for edit product

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Subform.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Subform.js
@@ -7,18 +7,16 @@
 Ext.define('Taco.view.product.subform.Subform', {
     extend: 'Taco.core.ux.form.Form',
     requires: ['Taco.view.product.subform.OverrideForm'],
-
-    bodyCls: Taco.baseCSSPrefix + 'product-admin-subform',
-    cls: Taco.baseCSSPrefix + 'form-section',
+    ui: 'subform',
     defaults: {
         xtype: 'textfield',
         labelAlign: 'top',
         labelSeparator: '',
         width: 250
     },
-
+    bodyPadding: '19 0',
+    margin: '0 0 20 0',
     cascadeRecordLoad: false,
-
     initComponent:function() {
         this.callParent(arguments);
         if (this.readOnly != undefined) {
