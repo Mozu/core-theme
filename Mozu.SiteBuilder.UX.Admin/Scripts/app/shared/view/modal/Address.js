d214c38feeb207505342d1dad2e66ebddcb0dfa8
Ojas (ツ) <CORP\ojas_patel>
2013-09-19 17:58:21 -0500

    added validateAddress config

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/modal/Address.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/modal/Address.js
@@ -13,6 +13,7 @@ Ext.define('Taco.shared.view.modal.Address', {
     width: 700,
 
     addressHasNames: true,
+    validateAddress: false,
 
     formCfg: null,
 
@@ -35,7 +36,56 @@ Ext.define('Taco.shared.view.modal.Address', {
 
         this.on({
             save: function () {
-                this.form.save();
+                if (this.validateAddress) {
+                    console.log('TODO: Validate Address');
+                    this.form.save();
+/* TODO: Replace two lines above with the following:
+
+1) invoke /app/address/validate with a body that looks like this:
+    {
+        "firstName": "ojas",
+        "middleName": "",
+        "lastName": "patel",
+        "companyName": "ojas's company",
+        "address1": "po box 1271",
+        "address2": "",
+        "address3": "",
+        "address4": "",
+        "cityOrTown": "austin",
+        "countryCode": "US",
+        "zipCode": "78701",
+        "state": "TX",
+        "homePhone": "512-589-2634",
+        "mobilePhone": "",
+        "workPhone": ""
+    }
+
+2) response will contain array of validated addresses like this:
+    {
+        "items": [
+            {
+            "address1": "PO BOX 1271", 
+            "address2": "", 
+            "cityOrTown": "AUSTIN", 
+            "countryCode": "US", 
+            "state": "TX", 
+            "zipCode": "78767-1271"
+            }
+        ], 
+        "success": true, 
+        "total": 1
+    }
+
+3) if total == 0:
+        this.form.save()
+   else:
+        display confirmation window "Use this address?" that shows first address with two buttons:
+        - Yes: update record with validate address, then call this.form.save()
+        - No: call this.form.save()
+*/
+                } else {
+                    this.form.save();
+                }
             },
             scope: this
         });
