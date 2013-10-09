f85b6e8c592da55d68fbcfd91b62c8b4ed6644b7
Bradley Friemel <CORP\bradley_friemel>
2013-10-09 15:03:11 -0500

    Order attribute detail page pops when its clicked now

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/orderAttribute/Edit.js
@@ -0,0 +1,7 @@
+Ext.define('Taco.view.orderAttribute.Edit', {
+    extend: 'Taco.core.ux.form.FullEditor',
+    requires: [
+        'Taco.view.orderAttribute.Form'
+    ],
+    formCls: 'Taco.view.orderAttribute.Form'
+});
