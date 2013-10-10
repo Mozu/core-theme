bdcf84162685492d5c6984195b5b24ab4b348add
Bradley Friemel <CORP\bradley_friemel>
2013-10-10 15:23:42 -0500

    added a required field to order attr and customer attr

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/customerAttribute/Form.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/customerAttribute/Form.js
@@ -408,6 +408,10 @@ Ext.define('Taco.view.customerAttribute.Form', {
                 scope: this
             }*/
         }, {
+            xtype: 'checkboxfield',
+            name: 'isRequired',
+            boxLabel: 'This attribute is required'
+        }, {
             xtype: 'selectfield',
             fieldLabel: 'Display Group',
             name: 'displaygroup',
