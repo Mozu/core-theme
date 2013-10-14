8832e300c5eed3871fe6a81a9acdc42eb404f1e8
Foster Hersey <foster.hersey@gmail.com>
2013-10-14 16:34:53 -0500

    Moved mapping logic out of the controller.

--- Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/CheckoutMapping.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/CheckoutMapping.cs
@@ -1,69 +1,83 @@
-﻿//using AutoMapper;
-//using Mozu.PaymentService.Contracts;
-//using Mozu.SiteSettings.Order.Contracts;
+﻿using AutoMapper;
+using System.Linq;
+using System.Collections.Generic;
 
-//namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
-//{
-//    public class CheckoutMapping: Profile
-//    {
-//        public override string ProfileName
-//        {
-//            get
-//            {
-//                return this.GetType().FullName;
-//            }
-//        }
+using Mozu.SiteBuilder.UX.Admin.Api.Models.Checkout;
+using DCss = Mozu.SiteSettings.Order.Contracts;
+using DCp = Mozu.PaymentService.Contracts;
 
-//        protected override void Configure()
-//        {
-//            // To model
-//            Mapper.CreateMap<CheckoutSettings, Models.Checkout.CheckoutSettings>();
-//            Mapper.CreateMap<PaymentSettings, Models.Checkout.PaymentSettings>();
-//            Mapper.CreateMap<CustomerCheckoutSettings, Models.Checkout.CustomerCheckoutSettings>();
-//            Mapper.CreateMap<GatewayAccount, Models.Checkout.GatewayAccount>();
-//            Mapper.CreateMap<GatewayCredentialFieldValue, Models.Checkout.GatewayCredentialFieldValue>();
-//            Mapper.CreateMap<OrderProcessingSettings, Models.Checkout.OrderProcessingSettings>();
-//            Mapper.CreateMap<GatewayDefinition, Models.Checkout.GatewayDefinition>();
-//            Mapper.CreateMap<GatewayCredentialFieldDefinition, Models.Checkout.GatewayCredentialFieldDefinition>();
-//            Mapper.CreateMap<PreAuthorizeDefinition, Models.Checkout.PreAuthorizeDefinition>();
-//            Mapper.CreateMap<PreAuthorizeTransactionTypeDataContract, Models.Checkout.PreAuthorizeTransactionTypeDataContract>();
+namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
+{
+    public class CheckoutMapping : Profile
+    {
+        public override string ProfileName
+        {
+            get
+            {
+                return this.GetType().FullName;
+            }
+        }
 
-//            // To contract
-//            Mapper.CreateMap<Models.Checkout.CheckoutSettings, CheckoutSettings>()
-//                .ForMember(x => x.CreateBy, op => op.Ignore())
-//                .ForMember(x => x.CreateDate, op => op.Ignore())
-//                .ForMember(x => x.UpdateBy, op => op.Ignore())
-//                .ForMember(x => x.UpdateDate, op => op.Ignore())
-//                .ForMember(x => x.ActivePaymentProvider, op => op.Ignore());
+        protected override void Configure()
+        {
+            Mapper.CreateMap<DCss.CheckoutSettings, Setting>()
+                .BeforeMap( (dc, x) => Mapper.Map<DCss.PaymentSettings, Setting>(dc.PaymentSettings, x) )
+                .ForMember(x => x.PaymentProcessingFlowType, op => op.MapFrom(dc => (dc.OrderProcessingSettings ?? new DCss.OrderProcessingSettings()).PaymentProcessingFlowType))
+                .ForMember(x => x.CustomerCheckoutType, op => op.MapFrom(dc => (dc.CustomerCheckoutSettings ?? new DCss.CustomerCheckoutSettings()).CustomerCheckoutType))
+                ;
 
-//            Mapper.CreateMap<Models.Checkout.PaymentSettings, PaymentSettings>()
-//                .ForMember(x => x.CreateBy, op => op.Ignore())
-//                .ForMember(x => x.CreateDate, op => op.Ignore())
-//                .ForMember(x => x.UpdateBy, op => op.Ignore())
-//                .ForMember(x => x.UpdateDate, op => op.Ignore())
-//                .ForMember(x => x.CustomAttributes, op => op.Ignore());
 
-//            Mapper.CreateMap<Models.Checkout.CustomerCheckoutSettings, CustomerCheckoutSettings>()
-//                .ForMember(x => x.CreateBy, op => op.Ignore())
-//                .ForMember(x => x.CreateDate, op => op.Ignore())
-//                .ForMember(x => x.UpdateBy, op => op.Ignore())
-//                .ForMember(x => x.UpdateDate, op => op.Ignore())
-//                .ForMember(x => x.CustomAttributes, op => op.Ignore());
+            Mapper.CreateMap<DCss.PaymentSettings, Setting>()
+                .ConvertUsing(dc =>
+                {
+                    var ret = new Setting();
+                    if (dc == null)
+                        return ret;
 
-//            Mapper.CreateMap<Models.Checkout.GatewayAccount, GatewayAccount>();
-//            Mapper.CreateMap<Models.Checkout.GatewayCredentialFieldValue, GatewayCredentialFieldValue>();
+                    ret.PayByMail = dc.PayByMail;
+                    var gateway = dc.Gateways != null && dc.Gateways.Count > 0 ? dc.Gateways.FirstOrDefault(x => x.GatewayAccount != null && x.GatewayAccount.IsActive) : null;
 
-//            Mapper.CreateMap<Models.Checkout.OrderProcessingSettings, OrderProcessingSettings>()
-//                .ForMember(x => x.CreateBy, op => op.Ignore())
-//                .ForMember(x => x.CreateDate, op => op.Ignore())
-//                .ForMember(x => x.UpdateBy, op => op.Ignore())
-//                .ForMember(x => x.UpdateDate, op => op.Ignore())
-//                .ForMember(x => x.CustomAttributes, op => op.Ignore());
+                    if (gateway != null)
+                    {
+                        ret.SupportedCards = gateway.SupportedCards;
+                        if (gateway.GatewayAccount != null)
+                        {
+                            ret.GatewayDefinitionId = gateway.GatewayAccount.GatewayDefinitionId;
+                            ret.AreGatewayCredentialFieldsSet = gateway.AreGatewayCredentialFieldsSet;
+                        }
+                    }
 
-//            Mapper.CreateMap<Models.Checkout.GatewayDefinition, GatewayDefinition>();
-//            Mapper.CreateMap<Models.Checkout.GatewayCredentialFieldDefinition, GatewayCredentialFieldDefinition>();
-//            Mapper.CreateMap<Models.Checkout.PreAuthorizeDefinition, PreAuthorizeDefinition>();
-//            Mapper.CreateMap<Models.Checkout.PreAuthorizeTransactionTypeDataContract, PreAuthorizeTransactionTypeDataContract>();
-//        }
-//    }
-//}
+                    return ret;
+                });
+
+
+            Mapper.CreateMap<Setting, DCss.PaymentSettings>()
+                .ConvertUsing(x =>
+                {
+                    var gatewayAccount = new Mozu.PaymentService.Contracts.GatewayAccount
+                    {
+                        CountryCode = "US",
+                        GatewayDefinitionId = x.GatewayDefinitionId,
+                        Id = x.Id,
+                        IsActive = true,
+                        CredentialFields = new List<DCp.GatewayCredentialFieldValue>()
+                    };
+
+                    if (x.Credentials != null && x.Credentials.HasValues)
+                    {
+                        foreach (var credential in x.Credentials)
+                        {
+                            gatewayAccount.CredentialFields.Add(new DCp.GatewayCredentialFieldValue() { Name = credential.Key, Value = (string)credential.Value });
+                        }
+                    }
+
+                    var pSettings = new Mozu.SiteSettings.Order.Contracts.PaymentSettings
+                    {
+                        Gateways = new List<Mozu.SiteSettings.Order.Contracts.Gateway> { new DCss.Gateway { GatewayAccount = gatewayAccount, SupportedCards = x.SupportedCards } },
+                        PayByMail = x.PayByMail.GetValueOrDefault(false)
+                    };
+                    return pSettings;
+                });
+        }
+    }
+}
