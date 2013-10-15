1b6848d0c26fe1b22af45b142050fc391847a3d3
Zetlen <CORP\james_zetlen>
2013-10-15 12:43:00 -0500

    adding a templatecontext for eventual serialization into hyprlive

---
+++ Mozu.SiteBuilder.Mvc/TemplateContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mozu.SiteBuilder.Mvc.Themes;
+
+namespace Mozu.SiteBuilder.Mvc
+{
+    public class TemplateContext
+    {
+        public ThemeRuntimeSettingsCollection ThemeSettings { get; set; }
+    }
+}
