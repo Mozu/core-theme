282e7b3f80f216a63d310640ae520a9ed2e3f276
Foster Hersey <foster.hersey@gmail.com>
2013-10-15 12:30:58 -0500

    Made SiteBuilderContext serialize more good.

--- Mozu.SiteBuilder.Mvc/Themes/Theme.cs
+++ Mozu.SiteBuilder.Mvc/Themes/Theme.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Mozu.SiteBuilder.Mvc.Themes
@@ -8,50 +9,59 @@ namespace Mozu.SiteBuilder.Mvc.Themes
     /// <summary>
     /// Internal class implementing an IThemeInfo.
     /// </summary>
+    [DataContract]
     public  class Theme
     {
         /// <summary>
         /// Id. Returns this theme's Id.
         /// </summary>
+        [DataMember(Name="id")]
         public string Id { get; set; }
 
         /// <summary>
         /// Contains this theme's name.
         /// </summary>
+        [DataMember(Name="name")]
         public string Name { get; set; }
 
 
         /// <summary>
         /// Contains this theme's author.
         /// </summary>
+        [DataMember(Name = "author")]
         public string Author { get; set; }
 
         /// <summary>
         /// Indicated whether this theme is intended to be used for desktop display.
         /// </summary>
+        [DataMember(Name = "isDesktop")]
         public bool? IsDesktop { get; set; }
 
         /// <summary>
         /// Indicated whether this theme is intended to be used for mobile display.
         /// </summary>
+        [DataMember(Name = "isMobile")]
         public bool? IsMobile { get; set; }
 
         /// <summary>
         /// If this theme inherits from another theme, contains the inherited theme.
         /// </summary>
+        [DataMember(Name = "parent")]
         public Theme Parent { get; set; }
 
         /// <summary>
         /// Contains the thumbnail for this theme or null.
         /// </summary>
+        [IgnoreDataMember]
         public Thumbnail Thumbnail { get; set; }
 
-
+        
         private LinkedList<Theme> _themeStack;
         /// <summary>
         /// Returns a complete theme inheritance stack with this theme as the first.
         /// </summary>
         [Obsolete]
+        [IgnoreDataMember]
         public ICollection<Theme> Stack
         {
             get
@@ -77,10 +87,13 @@ namespace Mozu.SiteBuilder.Mvc.Themes
 
         public List<ThemeSetting> MergedSettings { get; set; }
 
+        [DataMember(Name="pageTypes")]
         public List<Models.CMS.PageTypeDefinition> PageTypes { get; set; }
 
+        [IgnoreDataMember]
         public List<Models.CMS.WidgetDefinition> Widgets { get; set; }
         
+        [IgnoreDataMember]
         public ThemeFileSystemInfo[] FileListing { get; set; }
 
         /// <summary>
@@ -89,6 +102,7 @@ namespace Mozu.SiteBuilder.Mvc.Themes
         /// </summary>
         internal Theme() {}
 
+        [IgnoreDataMember]
         public string ThemePath { get; set; }
     }
 
