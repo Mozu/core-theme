8a8e5c1bc0b53fbe35266681acbd14802759c6f2
Jimmy Sanford <CORP\Jimmy_Sanford>
2013-10-16 19:35:34 -0500

    fixed address editor to not close prematurely

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/modal/Address.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/modal/Address.js
@@ -51,81 +51,101 @@ Ext.define('Taco.shared.view.modal.Address', {
 
         //ToDo: Fix this. If multiple recs are returned
         me.on({
-            save: function () {
-                if (this.validateAddress) {
-                    me.record.set('addressIsValidated', false);
-                    this.setLoading(true);
-                    Ext.Ajax.request({
-                        url: '/admin/app/address/validate',
-                        method: 'POST',
-                        jsonData: this.form.getValues(),
-                        scope: this,
-                        success: function (response) {
-                            this.json = JSON.parse(response.responseText);
-                            if (this.json.total > 0) {
-                                var addrChanged = false;
-                                var validatedAddr = this.json.items[0];
-                                var rawAddr = this.form.getValues();
-
-                                for (item in validatedAddr) {
-                                    if (item == 'addressIsValidated')
-                                        continue;
-                                    if (rawAddr[item] != validatedAddr[item]) {
-                                        addrChanged = true;
-                                        break;
-                                    }
-                                }
-
-                                if (addrChanged) {
-                                    var message = '';
-                                    message += validatedAddr['address1'] + ', ';
-                                    if (validatedAddr['address2'] != '') {
-                                        message += validatedAddr['address2'] + ', ';
-                                    }
-                                    message += validatedAddr['cityOrTown'] + ' ';
-                                    message += validatedAddr['state'] + ', ';
-                                    message += validatedAddr['countryCode'] + ', ';
-                                    message += validatedAddr['zipCode'];
-                                    Ext.Msg.show({
-                                        title: 'Address',
-                                        msg: 'Did you mean: ' + message,
-                                        buttons: Ext.Msg.YESNO,
-                                        closable: false,
-                                        rightJustifyButtons: true,
-                                        scope: this,
-                                        fn: function (rec) {
-                                            if (rec === "yes") {
-                                                validatedAddr['addressIsValidated'] = true;
-                                                for (item in validatedAddr) {
-                                                    me.record.set(item, validatedAddr[item]);
-                                                }
-                                                this.form.loadRecord(me.record);
-                                            }
-                                            this.form.save();
-                                        }
-                                    });
-                                } else {
-                                    me.record.set('addressIsValidated', true);
-                                    this.form.save();
-                                }
-                            }
-                            this.setLoading(false);
-
-                        },
-                        failure: function () {
-                            Ext.Msg.show({
-                                title: 'Address',
-                                msg: 'Unable to validate address'
-                            });
-                            this.setLoading(false);
-                            this.form.save();
-                        }
-                    });
-                } else {
-                    this.form.save();
-                }
+            beforesave: {
+                scope: this,
+                fn: 'maybeValidate'
             },
-            scope: me
+            save: {
+                scope: this,
+                fn: 'save'
+            }
+        });
+    },
+
+    maybeValidate: function () {
+        var wasValid = this.validateAddress ? !!(this.record.get('addressIsValidated')) : true;
+
+        if (!wasValid) {
+            this.validate();
+        }
+
+        return wasValid;
+    },
+
+    save: function () {
+        this.form.save();
+    },
+
+    validate: function () {
+        var me = this;
+
+        me.record.set('addressIsValidated', false);
+        this.setLoading(true);
+        Ext.Ajax.request({
+            url: '/admin/app/address/validate',
+            method: 'POST',
+            jsonData: this.form.getValues(),
+            scope: this,
+            success: function (response) {
+                this.json = JSON.parse(response.responseText);
+                if (this.json.total > 0) {
+                    var addrChanged = false;
+                    var validatedAddr = this.json.items[0];
+                    var rawAddr = this.form.getValues();
+
+                    for (item in validatedAddr) {
+                        if (item == 'addressIsValidated')
+                            continue;
+                        if (rawAddr[item] != validatedAddr[item]) {
+                            addrChanged = true;
+                            break;
+                        }
+                    }
+
+                    if (addrChanged) {
+                        var message = '';
+                        message += validatedAddr['address1'] + ', ';
+                        if (validatedAddr['address2'] != '') {
+                            message += validatedAddr['address2'] + ', ';
+                        }
+                        message += validatedAddr['cityOrTown'] + ' ';
+                        message += validatedAddr['state'] + ', ';
+                        message += validatedAddr['countryCode'] + ', ';
+                        message += validatedAddr['zipCode'];
+                        Ext.Msg.show({
+                            title: 'Address',
+                            msg: 'Did you mean: ' + message,
+                            buttons: Ext.Msg.YESNO,
+                            closable: false,
+                            rightJustifyButtons: true,
+                            scope: this,
+                            fn: function (rec) {
+                                if (rec === "yes") {
+                                    validatedAddr['addressIsValidated'] = true;
+                                    for (item in validatedAddr) {
+                                        me.record.set(item, validatedAddr[item]);
+                                    }
+                                    this.form.loadRecord(me.record);
+                                }
+                                this.form.save();
+                            }
+                        });
+                    } else {
+                        me.record.set('addressIsValidated', true);
+                        this.form.save();
+                    }
+                }
+                this.setLoading(false);
+
+            },
+            failure: function () {
+                Ext.Msg.show({
+                    title: 'Address',
+                    msg: 'Unable to validate address'
+                });
+                this.setLoading(false);
+                this.form.save();
+            }
         });
     }
 });
