168fa05c2daa65e73a22f14797c83fb6b22873b7
Simeon Kessler <CORP\simeon_kessler>
2013-10-23 10:01:37 -0500

    fixing bug in location model. adding grid formatting for locationTypes

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/overrides/form/field/ComboBox.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/overrides/form/field/ComboBox.js
@@ -1,17 +1,15 @@
 /*
- 
-
+ Override of Ext.form.field.ComboBox
 */
 
 Ext.define('Taco.overrides.form.field.ComboBox', {
     override: 'Ext.form.field.ComboBox',
-    requires: [
-        
-    ],    
-    initComponent: function () {
-        this.callParent(arguments);
-    },
-    
+    /*
+    * Gets array of selected combobox records and extracts the record.data.
+    * Used when the persisted data structure is an array of objects instead of an array of id's
+    *
+    * returns array of record data objects
+    */
     getValueRecordsData: function () {    
         var records = this.getValueRecords();
         var data = [];
