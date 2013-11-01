e147f1aa1321e0fc66bb17f57cfdf27b2c73e03f
Jimmy Sanford <CORP\Jimmy_Sanford>
2013-11-01 13:08:07 -0500

    website controller and index exist, ready for first steps of integration with new inline editing

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/catalog/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/catalog/Index.js
@@ -7,66 +7,33 @@
 
 Ext.define('Taco.view.catalog.Index', {
     extend: 'Taco.core.ux.content.Container',
-    requires: [
-        'Taco.overrides.panel.Tool',
-        'Taco.overrides.form.Basic',
-        'Taco.core.ux.window.Alert',
-        'Taco.core.ux.form.field.InlinePicker'
-    ],
+    requires: [],
 
     header: {
         title: 'Catalog Testing'
     },
 
     initComponent: function () {
-        var items = [],
-            store,
-            editor;
+        var items;
 
-        store = Ext.create('Ext.data.Store', {
-            fields: ['model', 'color'],
-            data: [
-                { color: 'red', model: 'Mustang' },
-                { color: 'yellow', model: 'Corvette' },
-                { color: 'blue', model: 'Camaro' }
-            ]
-        });
-
-        editor = Ext.create('Ext.grid.plugin.RowEditing', {
-            clicksToEdit: 2
-        });
-
-        items.push({
-            xtype: 'grid',
-            store: store,
-            // enableColumnResize: false,
-            selType: 'checkboxmodel',
-            selModel: {
-                checkOnly: true,
-                ignoreRightMouseSelection: true,
-                headerWidth: 37
+        items = [{
+            xtype: 'tabpanel',
+            tabBar: {
+                tools: [{
+                    xtype: 'button',
+                    ui: 'action-primary',
+                    scale: 'medium',
+                    text: 'click'
+                }]
             },
-            plugins: [
-                editor
-            ],
-            columns: [{
-                dataIndex: 'model',
-                text: 'Model',
-                flex: 3,
-                editor: {
-                    xtype: 'textfield',
-                    allowOnlyWhitespace: false
-                }
+            items: [{
+                title: 'Editor',
+                html: 'editor'
             }, {
-                dataIndex: 'color',
-                text: 'Color',
-                width: 200,
-                editor: {
-                    xtype: 'textfield',
-                    allowOnlyWhitespace: false
-                }
+                title: 'Settings',
+                html: 'settings'
             }]
-        });
+        }];
 
         Ext.apply(this.body, {
             cls: Taco.baseCSSPrefix + 'catalog',
@@ -75,39 +42,5 @@ Ext.define('Taco.view.catalog.Index', {
         });
 
         this.callParent(arguments);
-    },
-
-    launchModal: function () {
-        if (this.modal) {
-            this.modal.show();
-            return;
-        }
-
-        this.modal = Ext.create('Taco.core.ux.window.Modal', {
-            title: 'Hello',
-            scale: 'large',
-            items: [{
-                xtype: 'formform',
-                items: [{
-                    xtype: 'taco-inlinepicker',
-                    fieldConfig: {
-                        name: 'color',
-                        fieldStyle: {
-                            backgroundColor: '#fff !important'
-                        },
-                        store: ['red', 'green', 'blue']
-                    }
-                }]
-            }]
-        });
-
-        this.modal.on({
-            save: {
-                scope: this,
-                fn: function (dialog) { console.log('save clicked', dialog.getForm().getValues()); }
-            }
-        });
-
-        this.modal.show();
     }
 });
---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/website/Index.js
@@ -0,0 +1,195 @@
+﻿/**
+ * @class Taco.view.website.Index
+ * @author Jimmy Sanford
+ */
+
+Ext.define('Taco.view.website.Index', {
+    extend: 'Taco.core.ux.content.Container',
+    requires: [
+        'Taco.model.NavigationTreeNode',
+        'Taco.store.NavigationTreeNodes',
+        'Taco.view.website.Tree'
+    ],
+
+    header: {
+        title: false,
+        actions: [{
+            xtype: 'button',
+            ui: 'action',
+            scale: 'medium',
+            text: 'Page Editor',
+            toggleGroup: 'websiteEditorTabs',
+            enableToggle: true,
+            pressed: true,
+            style: {
+                borderRadius: '2px 0px 0px 2px'
+            },
+            handler: function () {
+                var cardpanel = this.down('#editorCardPanel');
+
+                cardpanel.getLayout().setActiveItem(0);
+            }
+        }, {
+            xtype: 'button',
+            ui: 'action',
+            scale: 'medium',
+            text: 'Page Settings',
+            toggleGroup: 'websiteEditorTabs',
+            enableToggle: true,
+            style: {
+                borderRadius: '0px 2px 2px 0px'
+            },
+            handler: function () {
+                var cardpanel = this.down('#editorCardPanel');
+
+                cardpanel.getLayout().setActiveItem(1);
+            }
+        }, {
+            xtype: 'component',
+            flex: 1
+        }, {
+            xtype: 'button',
+            ui: 'action',
+            scale: 'medium',
+            text: 'Widgets',
+            margin: '0 0 0 10',
+        }, {
+            xtype: 'button',
+            ui: 'action',
+            scale: 'medium',
+            text: 'More',
+            margin: '0 0 0 10',
+            menu: {
+                plain: true,
+                shadow: false,
+                items: [{
+                    text: 'thom'
+                }]
+            }
+        }, {
+            xtype: 'button',
+            ui: 'action',
+            scale: 'medium',
+            text: 'Cancel',
+            margin: '0 0 0 10'
+        }, {
+            xtype: 'button',
+            itemId: 'primaryAction',
+            ui: 'action-primary',
+            scale: 'medium',
+            text: 'Save',
+            margin: '0 0 0 10'
+        }]
+    },
+
+    initComponent: function () {
+        var store,
+            items;
+
+        store = Taco.core.data.StoreManager.getOrCreate('Taco.store.NavigationTreeNodes');
+
+        items = [{
+            xtype: 'panel',
+            itemId: 'editorCardPanel',
+            bodyStyle: {
+                'border-right-width': '1px'
+            },
+            layout: {
+                type: 'card'
+            },
+            items: [{
+                xtype: 'panel',
+                title: 'Editor',
+                header: false,
+                layout: {
+                    type: 'fit'
+                },
+                items: [{
+                    xtype: 'uxiframe',
+                    src: 'http://obscure-tundra-3709.herokuapp.com'
+                }]
+            }, {
+                xtype: 'panel',
+                title: 'Settings',
+                header: false,
+                items: [{
+                    xtype: 'formform',
+                    itemId: 'pageSettings',
+                    ui: 'subform',
+                    title: 'Page Settings',
+                    margin: '20 30 10 30',
+                    items: [{
+                        xtype: 'textfield',
+                        fieldLabel: 'foo',
+                        emptyText: 'bar',
+                        allowOnlyWhitespace: false
+                    }]
+                }]
+            }],
+            dockedItems: [{
+                dock: 'right',
+                title: 'Sidebar',
+                padding: '0 0 10',
+                header: false,
+                width: 240,
+                layout: {
+                    type: 'card'
+                },
+                items: [{
+                    xtype: 'taco-website-tree',
+                    store: store,
+                    viewConfig: {
+                        stripeRows: true
+                    }
+                }, {
+                    xtype: 'panel',
+                    title: 'Results',
+                    html: 'Results go here.'
+                }],
+                dockedItems: [{
+                    xtype: 'container',
+                    dock: 'top',
+                    padding: '14 20 0 14',
+                    height: 60,
+                    items: [{
+                        xtype: 'textfield',
+                        emptyText: 'Search',
+                        width: '100%',
+                        listeners: {
+                            change: function (field, newValue) {
+                                field.up('panel').getLayout().setActiveItem(Ext.String.trim(newValue).length > 0 ? 1 : 0);
+                            }
+                        }
+                    }]
+                }]
+            }]
+        }];
+
+        Ext.apply(this.body, {
+            layout: 'fit',
+            padding: '0 0 0 0',
+            items: items
+        });
+
+        this.callParent(arguments);
+
+        this.down('#pageSettings').getForm().getBoundItems().add(this.down('#primaryAction'));
+
+        this.down('#primaryAction').setHandler(function () {
+            console.log('handler updated');
+        });
+    },
+
+    onRender: function () {
+        this.callParent(arguments);
+
+        // TODO: remove this when hbox layouts position their items correctly
+        Ext.defer(function () {
+            var header = this.getHeader();
+
+            if (header && header.isComponent) {
+                header.updateLayout();
+            }
+        }, 600, this);
+    }
+});
