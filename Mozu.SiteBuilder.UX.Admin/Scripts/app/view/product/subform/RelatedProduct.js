c6ea0b31405364e15077977bd5b918b421c95c6e
Bradley Friemel <CORP\bradley_friemel>
2013-11-04 13:36:02 -0600

    First pass, added related products to product detail

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/RelatedProduct.js
@@ -0,0 +1,30 @@
+Ext.define('Taco.view.product.subform.RelatedProduct', {
+    extend: 'Taco.view.product.subform.Subform',
+
+    title: 'Related Product',
+    
+    initComponent: function () {
+        this.defaults.width = '100%';
+        this.defaults.product = this.product;
+        this.defaults.productInCatalogInfo = this.productInCatalogInfo;
+        this.defaults.persistChangesToModel = true;
+
+
+        this.ProductStore = Taco.core.data.StoreManager.getOrCreate('Taco.store.Products');
+
+
+        this.ProductInput = Ext.create('Taco.core.ux.form.BoxSelect', {
+            forceSelection: false,
+            fieldLabel: 'Choose Product',
+            store: this.ProductStore,
+            queryMode: 'local',
+            displayField: 'productName',
+            valueField: 'productCode'
+        });
+
+
+        this.items = [this.ProductInput];
+
+        this.callParent( arguments );
+    }
+});
