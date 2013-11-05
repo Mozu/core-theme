65427af8c87995b0e6abdc740377869430188a44
Bradley Friemel <CORP\bradley_friemel>
2013-11-05 16:54:00 -0600

    Stubbed out the edit page for application

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/controller/Capability.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/controller/Capability.js
@@ -1,10 +1,12 @@
 Ext.define('Taco.controller.Capability', {
     extend: 'Taco.core.Controller',
     requires: [
-        'Taco.view.application.Index'
+        'Taco.view.application.Index',
+        'Taco.view.application.Edit'
     ],
     models: ['Taco.model.Capability'],
     stores: ['Taco.store.Capability'],
     views: ['application.Index'],
-    modelName: 'Capability'
+    modelName: 'Capability',
+    editorView: 'Taco.view.application.Edit'
 });
