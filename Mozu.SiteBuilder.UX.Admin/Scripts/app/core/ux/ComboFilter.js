7f43ea966d9d26f27bcf7ef2658534255f82b335
Foster Hersey <foster.hersey@gmail.com>
2013-11-05 15:35:05 -0600

    Oops.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/ComboFilter.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/ComboFilter.js
@@ -351,7 +351,6 @@ Ext.define('Taco.core.ux.ComboFilter', {
      * @private
      */
     onValueChange: function (field, newValue, oldValue) {
-        debugger;
         var records, filters;
         if (!newValue && !oldValue) return;
 
