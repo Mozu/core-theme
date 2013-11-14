a09b4cb95295ff3d799c91178ed4765c45a4ec6b
Foster Hersey <foster.hersey@gmail.com>
2013-11-14 11:13:03 -0600

    Added productname LocationInventory filter.

--- Mozu.SiteBuilder.UX.Admin/Helpers/LocationInventoryHelpers/LocationInventoryFilterExtensions.cs
+++ Mozu.SiteBuilder.UX.Admin/Helpers/LocationInventoryHelpers/LocationInventoryFilterExtensions.cs
@@ -7,8 +7,8 @@ namespace Mozu.SiteBuilder.UX.Admin.Helpers.LocationInventoryHelpers
     public static class LocationInventoryFilterExtensions
     {
         private const string PRODUCT_NAME = "productname";
+        private const string BASE_PRODUCT_CODE = "baseproductcode";
         private const string PRODUCT_CODE = "productcode";
-        private const string LOCATION_NAME = "locationname";
         private const string LOCATION_CODE = "locationcode";
 
         /// <summary>
@@ -18,8 +18,8 @@ namespace Mozu.SiteBuilder.UX.Admin.Helpers.LocationInventoryHelpers
         {
             var statements = extFilter.Where(x => x.property != "all").Select(GetFilter).ToList();
             var allFilter = extFilter.FirstOrDefault(x => x.property == "all");
-            //if (allFilter != null)
-            //    statements.Add(String.Format("({1} cont {0} or {2} cont {0} or {3} cont {0} or {4} cont {0} or {5} cont {0} or {6} cont {0})", allFilter.value, NAME, STATE, COUNTRYCODE, ZIPCODE, CODE, LOCATIONTYPECODE));
+            if (allFilter != null)
+                statements.Add(String.Format("({1} sw {0} or {2} sw {0} or {3} sw {0})", allFilter.value, PRODUCT_CODE, LOCATION_CODE, PRODUCT_NAME));
 
             return string.Join(" and ", statements);
         }
@@ -29,13 +29,13 @@ namespace Mozu.SiteBuilder.UX.Admin.Helpers.LocationInventoryHelpers
             switch (filter.property.ToLowerInvariant())
             {
                 case "productname":
-                    return string.Format("({1} cont {0})", filter.value, PRODUCT_NAME);
+                    return string.Format("({1} sw {0})", filter.value, PRODUCT_NAME);
+                case "baseproductcode":
+                    return string.Format("({1} {2} {0})", filter.value, BASE_PRODUCT_CODE, filter.comparison == "sw" ? "sw" : "eq");
                 case "productcode":
-                    return string.Format("({1} eq {0})", filter.value, PRODUCT_CODE);
-                case "locationname":
-                    return string.Format("({1} cont {0})", filter.value, LOCATION_NAME);
+                    return string.Format("({1} {2} {0})", filter.value, PRODUCT_CODE, filter.comparison == "sw" ? "sw" : "eq");
                 case "locationcode":
-                    return string.Format("({1} eq {0})", filter.value, LOCATION_CODE);
+                    return string.Format("({1} {2} {0})", filter.value, LOCATION_CODE, filter.comparison == "sw" ? "sw" : "eq");
                 default:
                     throw new NotImplementedException("unable to filter on property " + filter.property);
             }
