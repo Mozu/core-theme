4383572fd73927e66bbe68950de301693ac54792
Thom 'Pig Pen' Phipps <CORP\Thomas_Phipps>
2013-11-18 15:49:03 -0600

    theme controller fix

--- Mozu.SiteBuilder.UX.Admin/Api/ThemeController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ThemeController.cs
@@ -142,7 +142,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
 
 
             var localThemes = entitlements.Items.Where(x => x.ApplicationType == "Theme")
-                .Select(x => x.ApplicationAssetPath )
+                .Select(x => (x.ApplicationAssetPath??"").Replace( "\\","/") )
                 .Union(Directory.GetDirectories(localThemeDir)
                 .Select(x => Path.GetFileName(x)));
 
