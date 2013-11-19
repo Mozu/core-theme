4f1d0f2564c530fada492ff69c0fb34d9a99e8fe
Ojas (ツ) <CORP\ojas_patel>
2013-11-19 09:34:40 -0600

    remove non-functional subforms from settings

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/generalSettings/Form.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/generalSettings/Form.js
@@ -24,13 +24,19 @@ Ext.define('Taco.view.generalsettings.Form', {
             record: me.record
         };
 
+        //me.items = [
+        //    Ext.create('Taco.view.generalSettings.subform.About', subFormConfig),
+        //    Ext.create('Taco.view.generalSettings.subform.Maintenance', subFormConfig),
+        //    Ext.create('Taco.view.generalSettings.subform.Notifications', subFormConfig),
+        //    Ext.create('Taco.view.generalSettings.subform.Analytics', subFormConfig),
+        //    Ext.create('Taco.view.generalSettings.subform.Robots', subFormConfig),
+        //    Ext.create('Taco.view.generalSettings.subform.Tools', subFormConfig)
+        //];
+
         me.items = [
             Ext.create('Taco.view.generalSettings.subform.About', subFormConfig),
-            Ext.create('Taco.view.generalSettings.subform.Maintenance', subFormConfig),
             Ext.create('Taco.view.generalSettings.subform.Notifications', subFormConfig),
             Ext.create('Taco.view.generalSettings.subform.Analytics', subFormConfig),
-            Ext.create('Taco.view.generalSettings.subform.Robots', subFormConfig),
-            Ext.create('Taco.view.generalSettings.subform.Tools', subFormConfig)
         ];
 
         me.callParent(arguments);
