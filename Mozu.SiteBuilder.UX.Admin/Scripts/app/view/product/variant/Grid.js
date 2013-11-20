77bfe866f031fd539886318edc5d3ac01980dfb9
Taco <CORP\Travis_Johnson>
2013-11-20 15:36:10 -0600

    Variant things...

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/variant/Grid.js
@@ -0,0 +1,14 @@
+﻿
+/**
+ * @class  Taco.view.product.variant.Grid
+ * @author Travis Johnson
+ * @description The grid panelt to edit and enable variants
+ */
+Ext.define('Taco.view.product.variant.Grid', {
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.taco-product-variant-grid',
+
+    initComponent: function () {
+        this.callParent(arguments)
+    }
+});
