996981618b2224b720d217f81a97dff25f02b8a0
Bradley Friemel <CORP\bradley_friemel>
2013-11-21 15:28:11 -0600

    instock request: ui interactions correct, prints out corrct email with product id.

--- Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/widgets/instock-request.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/widgets/instock-request.js
@@ -1,19 +1,41 @@
-define(['modules/jquery-mozu', 'shim!vendor/underscore>_', "modules/api", "modules/backbone-mozu", "modules/models-product"],
-    function ($, _, api, Backbone, ProductModels) {
+define(['modules/jquery-mozu', 'shim!vendor/underscore>_', "modules/api", "modules/backbone-mozu", "modules/models-product", 'modules/models-user'],
+    function ($, _, api, Backbone, ProductModels, UserModels) {
         
-        var pageContext = require.mozuData('pagecontext');
         var InstockReqView = Backbone.MozuView.extend({
-                    templateName: 'modules/product/product-instock-request'
-                });
+                templateName: 'modules/product/product-instock-request',
+                widgetNotifyUserAction: function () {
+                    var user = UserModels.User.fromCurrent();
+                    var product = ProductModels.Product.fromCurrent();
+                    var email = '';
+                                     
+                    if (user.attributes.isAnonymous) {
+                        //get email address from text box
+                        email = $('.mz-intstock-request-email').val();
+                    } else {
+                        //get email from customer model 
+                        email = user.attributes.email;
+                    }
+                    console.log(email);
+                    console.log(product.attributes.productCode);
+                   
+                }
+            });
         
         $(document).ready(function () {
+            var currentProduct = ProductModels.Product.fromCurrent();
             
-            var relatedProductsView = new InstockReqView({
-            el: 'mz-instock-request'
-            });
-            
-        relatedProductsView.render();
-           
+            if (currentProduct.attributes.inventoryInfo.onlineStockAvailable < 1) {
+                var relatedProductsView = new InstockReqView({
+                    model: ProductModels.Product.fromCurrent(),
+                    el: $('.mz-instock-request').parent()
+                });
+
+                relatedProductsView.render();
+            } else {
+                $('.mz-instock-request').parent().html('');
+            }
+
+
         });
 
     });
