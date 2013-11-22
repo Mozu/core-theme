aa7b87a915c1994c49bd20a02085ce609f241ee3
Bradley Friemel <CORP\bradley_friemel>
2013-11-22 12:06:30 -0600

    Added the changes to related products so it could be limited on products for checkout page.

--- Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/widgets/related-products.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/widgets/related-products.js
@@ -1,9 +1,19 @@
 ﻿define(['modules/jquery-mozu', 'shim!vendor/underscore>_', "modules/api", "modules/backbone-mozu", "modules/models-product"],
     function ($, _, api, Backbone, ProductModels) {
 
-        var getRelatedProducts = function(codes) {
+        var getRelatedProducts = function(pageType, codes, pageSize) {
             var filter = _.map(codes, function (c) { return "ProductCode eq " + c }).join(' or ');
-            return api.get("search", { filter: filter });
+            var retval = '';
+            
+            switch(pageType) {
+                case 'product': retval =api.get("search", { filter: filter});
+                    break;
+                case 'cart': retval = api.get("search", { filter: filter, pageSize: pageSize });
+                    debugger
+                    break;
+            }
+            
+            return retval;
         };
 
         
@@ -11,31 +21,45 @@
         var pageContext = require.mozuData('pagecontext');
 
         $(document).ready(function() {
-            var currentProduct = require.mozuData('product');
+            var productCollection = [];
+
+            switch(pageContext.pageType) {
+                case 'product':
+                    productCollection.push(require.mozuData('product'));
+                    break;
+                case 'cart':
+                    var cartItems = require.mozuData('cart').items;
+                    $.each(cartItems, function(index, value) {
+                        productCollection.push(value.product);
+                    });
+                    break;
+            }
             
             $('[data-mz-related-products]').each(function (index, rp) {
                 rp = $(rp);
              
-
-                
-              
                 var config = rp.data('mzRelatedProducts');
                 var attId = config.attributeId || 'tenant~product-crosssell';
                 var template = config.template || 'modules/product/product-list-carousel';
                 var title = config.title;
-                var productCodes;// = _.pluck(currentProduct.properties[0].values, "value");
-
+                var numberToDisplay = config.count || 5;
+                var productCodes = [];// = _.pluck(currentProduct.properties[0].values, "value");
+                
 
                 var RelatedProductsView = Backbone.MozuView.extend({
                     templateName: template
                 });
-
-                if (currentProduct &&  currentProduct.properties) {
-
-                    for (var x = 0; x < currentProduct.properties.length; x++) {
-                        if (currentProduct.properties[x].attributeFQN == attId) {
-                            productCodes = _.pluck(currentProduct.properties[x].values, "value");
-                            productCodes = $.grep(productCodes || [], function (x) { return !!x });
+                
+                for (var i = 0; i < productCollection.length; i++) {
+                    var currentProduct = productCollection[i];
+                    
+                    if (currentProduct && currentProduct.properties) {
+                        for (var x = 0; x < currentProduct.properties.length; x++) {
+                            if (currentProduct.properties[x].attributeFQN == attId) {
+                                var temp = _.pluck(currentProduct.properties[x].values, "value");
+                                productCodes = productCodes.concat($.grep(temp || [], function (x) { return !!x }));
+                                
+                            }
                         }
                     }
                 }
@@ -47,8 +71,7 @@
                     return;
                 }
 
-                getRelatedProducts(productCodes).then(function (collection) {
-
+                getRelatedProducts(pageContext.pageType, productCodes, numberToDisplay).then(function (collection) {
 
                     var relatedProductsCollection = new ProductModels.ProductCollection(collection.data);
                     var relatedProductsView = new RelatedProductsView({
