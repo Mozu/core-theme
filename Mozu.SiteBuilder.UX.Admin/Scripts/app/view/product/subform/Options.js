4b3f79af267cb22384d84f8e45200e07c6db451f
Taco <CORP\Travis_Johnson>
2013-12-02 16:49:55 -0600

    Variant changes

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Options.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/Options.js
@@ -4,6 +4,7 @@
 
 Ext.define('Taco.view.product.subform.Options', {
     extend: 'Taco.view.product.subform.Subform',
+    alias: 'widget.taco-product-options',
 
     requires: ['Taco.view.product.option.Form'],
 
@@ -15,14 +16,92 @@ Ext.define('Taco.view.product.subform.Options', {
             outOfStockState,
             options;
 
+        this.productTypeStore = Taco.core.data.StoreManager.getOrCreate('Taco.store.ProductTypes');
+
         this.record = this.product;
 
-        options = Ext.create('Taco.view.product.option.Form', {
-            product: this.product
-        });
+        // options = Ext.create('Taco.view.product.option.Form', {
+        //     product: this.product
+        // });
 
-        this.items = [options];
+        // this.items = [options];
+
+        this.items = [{
+            xtype: 'component',
+            itemId: 'list',
+            html: ''
+        }, {
+            xtype: 'button',
+            text: 'Select Values',
+            scale: 'medium',
+            ui: 'action',
+
+            handler: function() {
+                Ext.create('Taco.view.product.variant.Modal', {
+                    product: this.product,
+                    productType: this.productType,
+                    listeners: {
+                        hide: function () {
+                            console.log('rebuilding when variations are loaded');
+                            this.product.getVariations().whenLoaded(this.rebuild, this);
+                        },
+                        scope: this
+                    }
+                });
+            },
+            scope: this
+        }]
 
         this.callParent(arguments);
-    } 
+
+        this.list = this.down('#list');
+
+        this.productTypeStore.whenLoaded(this.loadByProductTypeId, this);
+    },
+
+    buildOptionsHtml: function () {
+        var ret = [];
+
+        if (!this.productType) return '';
+
+        this.product.getOptions().each(function (option) {
+            var attributeName;
+            console.log(option.raw);
+
+            if (!option.get('values').length) return;
+
+            attributeName = this.findAttributeName(option);
+
+            ret.push(attributeName + ' - ');
+
+            Ext.each(option.get('values'), function (val, i) {
+                if (i > 0) ret.push(', ');
+                ret.push(val);
+            });
+
+            ret.push('<br>');
+        }, this);
+
+        return ret.join('');
+    },
+
+    findAttributeName: function (record) {
+        var option = this.productType.getOptions().findRecord('attributeFQN', record.get('attributeFQN'));
+
+        if (!option) return;
+
+        return option.get('attributeName');
+    },
+
+    loadByProductTypeId: function (value) {
+        var productTypeId = typeof value === 'number'
+                            ? value
+                            : this.product.get('productTypeId');
+        this.productType = this.productTypeStore.getById(productTypeId);
+        this.rebuild();
+    },
+
+    rebuild: function () {
+        this.list.update(this.buildOptionsHtml());
+    }
 });
