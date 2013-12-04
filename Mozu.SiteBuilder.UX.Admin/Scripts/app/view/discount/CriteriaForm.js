bc35cd3a53af23d0795376b021bea3fecace0003
Taco <CORP\Travis_Johnson>
2013-12-04 16:57:24 -0600

    Initial discount reorg

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/discount/CriteriaForm.js
@@ -0,0 +1,19 @@
+﻿/**
+ * @class  Taco.view.discount.CriteriaForm
+ * @author Travis Johnson
+ * @description Discount Target Criteria
+ */
+Ext.define('Taco.view.discount.CriteriaForm', {
+    extend: 'Taco.core.ux.form.Form',
+    alias: 'widget.taco-discount-criteria',
+    ui: 'subform',
+
+    title: 'Target Criteria',
+
+    initComponent: function () {
+        
+
+        this.callParent(arguments);
+
+    }
+});
