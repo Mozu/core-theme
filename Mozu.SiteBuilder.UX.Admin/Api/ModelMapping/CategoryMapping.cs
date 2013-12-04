b11ab5660e194282517a8e74e708e5e3d6872fed
Foster Hersey <foster.hersey@gmail.com>
2013-12-04 17:30:00 -0600

    I saved category images.

--- Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/CategoryMapping.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/CategoryMapping.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
-using Mozu.ProductAdmin.Contracts;
+using Mozu.SiteBuilder.UX.Admin.Api.Models.Category;
+using DC = Mozu.ProductAdmin.Contracts;
 using System.Linq;
+
 namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
 {
     public class CategoryMapping : Profile
@@ -15,7 +17,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
 
         protected override void Configure()
         {
-            Mapper.CreateMap<Category, Models.Category.Category>()
+            Mapper.CreateMap<DC.Category, Category>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(c => c.Id))
                 .ForMember(dest => dest.IsHidden, opt => opt.MapFrom(c => !c.IsDisplayed))
                 .ForMember(dest => dest.CatalogId, opt => opt.MapFrom(c => c.CatalogId))
@@ -29,10 +31,11 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
                 .ForMember(dest => dest.MetaTitle, opt => opt.MapFrom(c => ((c.Content != null) ? c.Content.MetaTagTitle : null)))
                 .ForMember(dest => dest.MetaKeywords, opt => opt.MapFrom(c => ((c.Content != null) ? c.Content.MetaTagKeywords : null)))
                 .ForMember(dest => dest.Slug, opt => opt.MapFrom(c => ((c.Content != null) ? c.Content.Slug : null)))
+                .ForMember(dest => dest.CategoryImages, opt => opt.MapFrom(c => (c.Content != null ? c.Content.CategoryImages : null)))
 
                 .ForMember(dest => dest.Path, opt => opt.Ignore());
 
-            Mapper.CreateMap<Category, Mozu.SiteBuilder.UX.Admin.Api.Models.Category.CategoryTreeNode>()
+            Mapper.CreateMap<DC.Category, CategoryTreeNode>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(c => c.Id))
                 
                 .ForMember(dest => dest.ParentId, opt => opt.MapFrom(c => c.ParentCategoryId))
@@ -42,47 +45,36 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
                 .ForMember(dest => dest.ProductCount, opt => opt.MapFrom(c => c.ProductCount))
                 .ForMember(dest => dest.Items, opt => opt.Ignore());
 
-            // TODO: Ignore these for now  
-            Mapper.CreateMap<Models.Category.Category, Category>()
+            Mapper.CreateMap<Category, DC.Category>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(c => c.Id))
                 .ForMember(dest => dest.CatalogId, opt => opt.MapFrom(c => c.CatalogId))
                 .ForMember(dest => dest.ParentCategoryId, opt => opt.MapFrom(c => c.ParentId.GetValueOrDefault(-1) == -1 ? null : c.ParentId))
                 .ForMember(dest => dest.Sequence, opt => opt.MapFrom(c => c.Index))
 
-                .ForMember(dest => dest.Content, opt => opt.MapFrom(c =>
-                                                                    new CategoryLocalizedContent
-                                                                    {
-                                                                        //CategoryId = c.Id.GetValueOrDefault (-1), 
-                                                                        Description = c.Description,
-                                                                        Name = c.Name,
-                                                                        PageTitle = c.PageTitle,
-                                                                        MetaTagDescription = c.MetaDescription,
-                                                                        MetaTagTitle = c.MetaTitle,
-                                                                        MetaTagKeywords = c.MetaKeywords,
-                                                                        Slug = c.Slug,
-                                                                        LocaleCode = "en-US" // Hack
+                .ForMember(dest => dest.Content, opt => opt.MapFrom(c => c))
 
-                                                                    }))
-                //.ForMember(dest => dest.Catalogs, opt => opt.Ignore())
+                .ForMember(dest => dest.IsDisplayed, opt => opt.MapFrom(c => !c.IsHidden))
+                ;
 
-                //.ForMember(dest => dest.ProductSet, opt => opt.Ignore())
+            Mapper.CreateMap<Category, DC.CategoryLocalizedContent>()
+                .ForMember(dc => dc.Description, op => op.MapFrom(x => x.Description))
+                .ForMember(dc => dc.Name, op => op.MapFrom(x => x.Name))
+                .ForMember(dc => dc.PageTitle, op => op.MapFrom(x => x.PageTitle))
+                .ForMember(dc => dc.MetaTagDescription, op => op.MapFrom(x => x.MetaDescription))
+                .ForMember(dc => dc.MetaTagTitle, op => op.MapFrom(x => x.MetaTitle))
+                .ForMember(dc => dc.MetaTagKeywords, op => op.MapFrom(x => x.MetaKeywords))
+                .ForMember(dc => dc.Slug, op => op.MapFrom(x => x.Slug))
+                .ForMember(dc => dc.CategoryImages, op => op.MapFrom(x => x.CategoryImages))
+                .ForMember(dc => dc.LocaleCode, op => op.UseValue("en-US"))
+                ;
 
-                .ForMember(dest => dest.IsDisplayed, opt => opt.MapFrom(c => !c.IsHidden));
+            Mapper.CreateMap<CategoryImage, DC.CategoryLocalizedImage>()
+                .ForMember(dc => dc.ImageUrl, op => op.MapFrom(x => x.Url))
+                ;
 
-            //.ForMember(dest => dest.ProductSetId, opt => opt.MapFrom(c => 1)); // Default 1 for now
-
-            //Mapper.AssertConfigurationIsValid();
-
-            // moved to CmsPagesMapping in MVC project.
-            //Mapper.CreateMap<Mozu.Content.Contracts.Document, Mvc.Models.CMS.Admin.Document>()
-            //    .ForMember(x => x.Items, m => m.MapFrom(x => x.Properties));
-            //Mapper.CreateMap<Mvc.Models.CMS.Admin.Document, Mozu.Content.Contracts.Document>()
-            //    .ForMember(x => x.Properties, m => m.MapFrom(x => x.Items));
-            //
-            //Mapper.CreateMap<Mozu.Content.Contracts.PropertyValue, Mvc.Models.CMS.Admin.DocumentProperty>()
-            //    .ForMember(x => x.Key, m => m.MapFrom(x => x.PropertyType));
-            //Mapper.CreateMap<Mvc.Models.CMS.Admin.DocumentProperty, Mozu.Content.Contracts.PropertyValue>()
-            //    .ForMember(x => x.PropertyType, m => m.MapFrom(x => x.Key));
+            Mapper.CreateMap<DC.CategoryLocalizedImage, CategoryImage>()
+                .ForMember(x => x.Url, op => op.MapFrom(x => x.ImageUrl))
+                ;
         }
     }
 }
