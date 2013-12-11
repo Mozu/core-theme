61990efc93c3e367b9c853d2d1118adc351de221
Ojas (ツ) <CORP\ojas_patel>
2013-12-10 19:12:05 -0600

    Fix CSV download

--- Mozu.SiteBuilder.UX.Admin/Api/ReportController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ReportController.cs
@@ -58,7 +58,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
         [HttpGetRoute(UriTemplate = "download/{name}")]
         public async Task<HttpResponseMessage> Download([FromUri]string filter, [FromUri] string groupBy, string name)
         {
-            var serviceResponse = await _reportWebApiClient.GetReportAsync( name: name, filter:filter,groupBy: groupBy);
+            var serviceResponse = await _reportWebApiClient.GetReportFileAsync( name: name, filter:filter,groupBy: groupBy);
             var httpContent = serviceResponse.ResponseMessage.Content;
 
             var contentStream = await httpContent.ReadAsStreamAsync();
