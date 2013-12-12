c24cf5566cfb88d31ba9d8d8a14a6854bbc601bc
Foster Hersey <foster.hersey@gmail.com>
2013-12-11 20:00:26 -0600

    UserId is not AccountId.

--- Mozu.SiteBuilder.UX/Messaging/VisitEventPublisher.cs
+++ Mozu.SiteBuilder.UX/Messaging/VisitEventPublisher.cs
@@ -32,7 +32,7 @@ namespace Mozu.SiteBuilder.UX.Messaging
                 WebSiteId = _apiContext.SiteId,
                 TenantId = _apiContext.TenantId,
                 UserId = _apiContext.UserClaims.UserId,
-                CustomerId = _pageContext.User != null && !String.IsNullOrEmpty(_pageContext.User.UserId) ? (int?)Convert.ToInt32(_pageContext.User.UserId) : null,
+                CustomerId = _pageContext.User.AccountId,
                 Date = DateTime.Now,
                 WebUserAgent = _httpContext.Request.UserAgent,
                 BrowserLocationCode = null, // location on mobile devices.
@@ -45,7 +45,7 @@ namespace Mozu.SiteBuilder.UX.Messaging
                     catalogId:        _apiContext.CatalogId,
                     userId:           _apiContext.UserClaims.UserId,
                     correlationId:    Trace.CorrelationManager.ActivityId.ToString("N")
-                ) { CustomerId = _pageContext.User != null ? _pageContext.User.UserId : null }
+                ) { CustomerId = _pageContext.User != null  && _pageContext.User.AccountId != null ? _pageContext.User.AccountId.ToString() : null }
                 // TODO: would be nice to have a place to track landing page.
             };
 
