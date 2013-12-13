d53d9dc7509384914fb0b43e8dfeb8a02b433e65
Taco <CORP\Travis_Johnson>
2013-12-13 15:57:19 -0600

    Improved the mac build

--- build-client.js
+++ build-client.js
@@ -1,28 +1,65 @@
-var net = require('net');
+var net = require('net')
+var argv = require('optimist').argv
+var exec = require('child_process').exec
 
-var build = function () {
+var connection = (function () {
     var socket = new net.Socket()
     socket.setKeepAlive(true)
     socket.on('data', function (data) {
-        var msg = data.toString()
-        console.log(msg)
+        process.stdout.write(data)
     })
     socket.on('error', function (e) {
-        console.log('ERROR')
+        console.log('error')
         console.log(e)
     })
     socket.on('close', function () {
+        process.stdout.write('\n')
         console.log('closing connection')
     })
     socket.on('end', function () {
-        console.log('connection ending')
+        console.log('\nconnection ending')
     })
 
-    socket.connect(8124, 'mozu.volusion.com', function () {
-        //console.log('connected');
-    })
+    socket.connect(8124, 'mozu.volusion.com')
 
     return socket
+}())
+
+var restoreNuget = function() {
+    if (!argv.nuget && !argv.n) {
+        connection.write('restoreNuget')
+        connection.on('data', function(data) {
+            if (data.toString().indexOf('nuget complete') < 0) return
+            msbuild()
+        })
+    } else {
+        msbuild()
+    }
 }
 
-var client = build()
+var msbuild = function() {
+    if (!argv.build && !argv.b) {
+        connection.write('msbuild')
+        connection.on('data', function(data) {
+            if (data.toString().indexOf('msbuild complete') < 0) return
+            connection.end()
+        })
+    } else {
+        connection.end()
+    }
+}
+
+if (!argv.pull && !argv.p) {
+    child = exec('git tf pull --rebase', function(error, stdout, stderr) {  
+         restoreNuget()
+    })
+
+    child.stdout.on('data', function(data) {
+        process.stdout.write(data)
+    })
+
+} else {
+    
+    restoreNuget()
+
+}
--- build-server.js
+++ build-server.js
@@ -1,8 +1,7 @@
 var net = require('net')
 var exec = require('child_process').exec
-var build = function(socket) {
-    var cmd,
-        buildChild
+
+var accept = function(socket) {
 
     socket.setKeepAlive(true)
     socket.on('error', function(e) {
@@ -15,32 +14,72 @@ var build = function(socket) {
     socket.on('end', function() {
         console.log("END")
     });
+
+    socket.on('data', function(data) {
+        var msg = data.toString()
+
+        if (typeof ops[msg] !== 'function') return
+
+        ops[msg](socket)
+    })
+
     console.log('OPEN')
-    socket.pipe(socket)
-    socket.write('Connected')
-    socket.write('\tStarting build... please wait')
-
-    cmd = 'msbuild Mozu.SiteBuilder.sln /p:BuildingInsideVisualStudio=true;Configuration=Debug;Platform="Any CPU"';
-
-    socket.write('\n\t\t> ' + cmd)
-    buildChild = exec(cmd, {maxBuffer: 200*1024*20}, function(error, stdout, stderr) {
-        if (error !== null) {
-            console.log('stderr: ' + stderr)
-            console.log('exec error: ' + error)
-            socket.write('\n\n======ERROR======')
-        }
-
-        socket.write('\n\n\t Build Completed')
-        socket.end()
-    })
-
-    buildChild.stdout.on('data', function(data) {
-        if (!data) return
-        socket.write(data)   
-    })
+    //socket.pipe(socket)
+    
+    socket.write('\nConnected')
+        
+    socket.write('\nStarting build... please wait\n\n')
 }
 
-var server = net.createServer(build)
+
+var ops = { 
+    msbuild: function(socket) {
+        var cmd,
+            child
+
+        cmd = 'msbuild Mozu.SiteBuilder.sln /p:BuildingInsideVisualStudio=true;Configuration=Debug;Platform="Any CPU"';
+
+        socket.write('\n\t\t> ' + cmd)
+        child = exec(cmd, {maxBuffer: 200*1024*20}, function(error, stdout, stderr) {
+            if (error !== null) {
+                console.log('stderr: ' + stderr)
+                console.log('exec error: ' + error)
+                socket.write('\n\n======ERROR======')
+            }
+
+            socket.write('msbuild complete')
+        })
+
+        child.stdout.on('data', function(data) {
+            if (!data) return
+            socket.write(data)   
+        })
+    },
+    restoreNuget: function(socket) {
+            var cmd,
+            child
+
+        cmd = 'nuget restore';
+
+        socket.write('\n\t\t> ' + cmd)
+        child = exec(cmd, {maxBuffer: 200*1024*20}, function(error, stdout, stderr) {
+            if (error !== null) {
+                console.log('stderr: ' + stderr)
+                console.log('exec error: ' + error)
+                socket.write('\n\n======ERROR======')
+            }
+
+            socket.write('nuget complete')
+        })
+
+        child.stdout.on('data', function(data) {
+            if (!data) return
+            socket.write(data)   
+        })
+    }
+}
+
+var server = net.createServer(accept)
 server.listen(8124, function() {
     console.log('server bound');
 })
