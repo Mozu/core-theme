a94af434b274ff38fc7f9c2bc7eab2fbc3bcf60a
Taco <CORP\Travis_Johnson>
2013-12-13 11:42:40 -0600

    Optimized the build command

--- build-server.js
+++ build-server.js
@@ -1,44 +1,42 @@
 var net = require('net')
 var exec = require('child_process').exec
 var build = function(socket) {
+    var cmd,
+        buildChild
+
     socket.setKeepAlive(true)
-    // socket.on('data', function(data) {
-    //     console.log("DATA");
-    //     console.log(data);
-    // });
     socket.on('error', function(e) {
-        console.log("ERROR");
-        console.log(e);
+        console.log("ERROR")
+        console.log(e)
     });
     socket.on('close', function() {
-        console.log("CLOSE");
+        console.log("CLOSE")
     });
     socket.on('end', function() {
-        console.log("END");
+        console.log("END")
     });
-    console.log('OPEN');
+    console.log('OPEN')
     socket.pipe(socket)
     socket.write('Connected')
     socket.write('\tStarting build... please wait')
-    socket.write('\n\t\t> nuget restore')
-    exec('nuget restore', function(error, stdout, stderr) {
-        socket.write(stdout)
+
+    cmd = 'msbuild Mozu.SiteBuilder.sln /p:BuildingInsideVisualStudio=true;Configuration=Debug;Platform="Any CPU"';
+
+    socket.write('\n\t\t> ' + cmd)
+    buildChild = exec(cmd, {maxBuffer: 200*1024*20}, function(error, stdout, stderr) {
         if (error !== null) {
             console.log('stderr: ' + stderr)
             console.log('exec error: ' + error)
+            socket.write('\n\n======ERROR======')
         }
 
-        socket.write('\n\t\t> msbuild /p:BuildingInsideVisualStudio=true')
-        exec('msbuild /p:BuildingInsideVisualStudio=true', function(error, stdout, stderr) {
-            socket.write(stdout)
-            if (error !== null) {
-                console.log('stderr: ' + stderr)
-                console.log('exec error: ' + error)
-            }
+        socket.write('\n\n\t Build Completed')
+        socket.end()
+    })
 
-            socket.write('\n\n\t Build Completed')
-            socket.end()
-        })
+    buildChild.stdout.on('data', function(data) {
+        if (!data) return
+        socket.write(data)   
     })
 }
 
