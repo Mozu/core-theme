aab8d89a10c323461508bf9fd0289851dcf7a521
Taco <CORP\Travis_Johnson>
2013-12-14 13:58:45 -0600

    Chorizo - styles fix

--- Mozu.SiteBuilder.UX.Admin/Scripts/build/config.rb
+++ Mozu.SiteBuilder.UX.Admin/Scripts/build/config.rb
@@ -6,3 +6,8 @@ require '..\packages\mozuadmintheme\sass\config.rb'
             http_generated_images_path = "/admin/Scripts/build/images"
             fonts_path = %q{C:\projects\mzt\UI\Dev\Dev-branch\Mozu.SiteBuilder\Mozu.SiteBuilder.UX.Admin\Scripts\resources\fonts}.gsub!(/\\/, '/')
         
+            output_style = :expanded
+            images_path = %q{/Volumes/Data/Users/travis_johnson/Sites/Mozu/Mozu.SiteBuilder.UX.Admin/Scripts/resources\images}.gsub!(/\\/, '/')
+            http_generated_images_path = "/admin/Scripts/build/images"
+            fonts_path = %q{/Volumes/Data/Users/travis_johnson/Sites/Mozu/Mozu.SiteBuilder.UX.Admin/Scripts/resources\fonts}.gsub!(/\\/, '/')
+        
