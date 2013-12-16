c93bfe6992a0c9579335c5bc5222674646c87b6e
Taco <CORP\Travis_Johnson>
2013-12-16 17:38:12 -0600

    Create Location PageType and Merge of Project File

---
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/pages/location.js
@@ -0,0 +1,9 @@
+require(["modules/jquery-mozu", "hyprlive", "modules/backbone-mozu",
+    function($, Hypr, Backbone) {
+        var LocationView = Backbone.MozuView.extend({
+            tempalteName: 'modules/location'
+        });
+
+        //var view = new LocationView();
+    }
+);
