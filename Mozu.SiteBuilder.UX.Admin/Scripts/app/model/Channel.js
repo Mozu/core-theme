178965b893ce58fcd641c40ee7ea530db327e3c2
Simeon Kessler <CORP\simeon_kessler>
2013-12-18 11:07:44 -0600

    fix for preloading countries store for channel

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/model/Channel.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/model/Channel.js
@@ -3,7 +3,9 @@
  */
 Ext.define('Taco.model.Channel', {
     extend: 'Taco.core.data.Model',
-    
+    requiredStores: [
+        'Taco.store.Countries'
+    ],
     fields: [{
         "name": "code",
         "type": "string",
