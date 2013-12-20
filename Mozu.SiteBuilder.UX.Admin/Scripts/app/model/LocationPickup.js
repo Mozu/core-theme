7326901302ec1af4c62ca19913b23b8a82be5378
Thom 'Pig Pen' Phipps <CORP\Thomas_Phipps>
2013-12-20 16:38:17 -0600

    fixing fosters autofac issues

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/model/LocationPickup.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/model/LocationPickup.js
@@ -36,7 +36,7 @@ Ext.define('Taco.model.LocationPickup', {
         type: 'ajaxproxy',
 
         api: {
-            read: '/admin/app/locaitoninventory/pickup'
+            read: '/admin/app/locationinventory/pickup'
             //read: '/admin/app/locationPickup/list'
         },
         reader: {
