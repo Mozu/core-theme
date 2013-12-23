e9d6537dd7a370075537a63837c6aa8845dda849
TFS Service <CORP\svc_tfsservice>
2013-12-23 17:10:01 -0600

    Solution updated to latest package versions ***NO_CI***

--- Mozu.SiteBuilder.UX.Admin/Scripts/build/config.rb
+++ Mozu.SiteBuilder.UX.Admin/Scripts/build/config.rb
@@ -2,7 +2,7 @@ require '..\ext\packages\ext-theme-base\sass\utils.rb'
 require '..\packages\mozuadmintheme\sass\config.rb'
 
             output_style = :expanded
-            images_path = %q{D:\Build2\09\Mozu\z_Alm.UpdPkgVers\Sources\Mozu.SiteBuilder.UX.Admin\Scripts\resources\images}.gsub!(/\\/, '/')
+            images_path = %q{D:\Build\03\Mozu\z_Alm.UpdPkgTrgt\Sources\Mozu.SiteBuilder.UX.Admin\Scripts\resources\images}.gsub!(/\\/, '/')
             http_generated_images_path = "/admin/Scripts/build/images"
-            fonts_path = %q{D:\Build2\09\Mozu\z_Alm.UpdPkgVers\Sources\Mozu.SiteBuilder.UX.Admin\Scripts\resources\fonts}.gsub!(/\\/, '/')
+            fonts_path = %q{D:\Build\03\Mozu\z_Alm.UpdPkgTrgt\Sources\Mozu.SiteBuilder.UX.Admin\Scripts\resources\fonts}.gsub!(/\\/, '/')
         
