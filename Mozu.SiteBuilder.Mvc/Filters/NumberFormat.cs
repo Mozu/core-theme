21de29d7fa45d13f26c3022bf39d794182a04861
Thom 'Pig Pen' Phipps <CORP\Thomas_Phipps>
2014-01-08 17:37:31 -0600

    adding divide and number_format filter

---
+++ Mozu.SiteBuilder.Mvc/Filters/NumberFormat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozu.SiteBuilder.Mvc.Filters
+{
+    [NDjango.Interfaces.Name("number_format")]
+    public class NumberFormat : NDjango.Interfaces.IFilter
+    {
+
+        object NDjango.Interfaces.IFilter.DefaultValue
+        {
+            get { return ""; }
+        }
+
+        object NDjango.Interfaces.IFilter.PerformWithParam(object value, object parameter)
+        {
+            var format = parameter as string;
+            var convertableValue = value as IConvertible;
+
+            if (format == null || convertableValue == null)
+            {
+                return value;
+            }
+
+            try
+            {
+                var d = Convert.ToDecimal(convertableValue);
+                return d.ToString(format);
+            }
+            catch 
+            {
+                return value;
+            }
+
+        }
+
+        object NDjango.Interfaces.ISimpleFilter.Perform(object value)
+        {
+            return value;
+        }
+    }
+}
