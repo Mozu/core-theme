f45fc47d5ad6262e25f48f678b2933c8edc2dc96
Thom 'Pig Pen' Phipps <CORP\Thomas_Phipps>
2014-01-13 22:39:10 -0600

    swapping out reflection for delegates  in  django member resolver.

--- Mozu.SiteBuilder.Mvc/ViewEngine/CaseInsensitiveMemberResolver.cs
+++ Mozu.SiteBuilder.Mvc/ViewEngine/CaseInsensitiveMemberResolver.cs
@@ -4,6 +4,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using Magnum.Extensions;
@@ -17,33 +18,108 @@ namespace Mozu.SiteBuilder.Mvc.ViewEngine
     {
         private static ConcurrentDictionary<Type, MemberAccessors> _lookupDic = new ConcurrentDictionary<Type, MemberAccessors>();
 
+        private static MethodInfo _createGetterGeneric = typeof(CaseInsensitiveMemberResolver).GetMethod("CreateGetterGeneric", BindingFlags.NonPublic | BindingFlags.Static);
+        private static MethodInfo _createFieldGetterGeneric = typeof(CaseInsensitiveMemberResolver).GetMethod("CreateFieldGetterGeneric", BindingFlags.NonPublic | BindingFlags.Static);
+        private static MethodInfo _createContainsKeyDelegateGeneric = typeof(CaseInsensitiveMemberResolver).GetMethod("CreateContainsKeyDelegateGeneric", BindingFlags.NonPublic | BindingFlags.Static);
+        private static MethodInfo _createIndexedGetterGeneric = typeof(CaseInsensitiveMemberResolver).GetMethod("CreateIndexedGetterGeneric", BindingFlags.NonPublic | BindingFlags.Static);
+
+        
+        static Func<object, object> CreateGetterGeneric<T, R>(MethodInfo getter) where T : class
+        {
+            var getterTypedDelegate = (Func<T, R>)Delegate.CreateDelegate(typeof(Func<T, R>), getter);
+            var getterDelegate = (Func<object, object>)((object instance) => getterTypedDelegate((T)instance));
+            return getterDelegate;
+        }
+
+
+        static Func<object, object> CreateFieldGetterGeneric<S, T>(FieldInfo field)
+        {
+            var methodName = field.ReflectedType.FullName + ".get_" + field.Name;
+            var getterMethod = new DynamicMethod(methodName, typeof(T), new Type[1] { typeof(S) }, true);
+            var gen = getterMethod.GetILGenerator();
+            if (field.IsStatic)
+            {
+                gen.Emit(OpCodes.Ldsfld, field);
+            }
+            else
+            {
+                gen.Emit(OpCodes.Ldarg_0);
+                gen.Emit(OpCodes.Ldfld, field);
+            }
+            gen.Emit(OpCodes.Ret);
+
+
+            var getterTypedDelegate = (Func<S, T>)getterMethod.CreateDelegate(typeof(Func<S, T>));
+            var getterDelegate = (Func<object, object>)((object instance) => getterTypedDelegate((S)instance));
+            return getterDelegate;
+        }
+
+        static Func<T, string, bool> CreateContainsKeyDelegateGeneric<T>(MethodInfo mi) where T : class
+        {
+            var typedDelegate = (Func<T, string, bool>)Delegate.CreateDelegate(typeof(Func<T, string, bool>), mi);
+            var objectDelegate = (Func<object, string, bool>)((object instance, string key) => typedDelegate((T)instance, key));
+            return objectDelegate;
+        }
+
+        static Func<object, string, bool> CreateContainsKeyDelegate(MethodInfo mi)
+        {
+
+            var genericHelper = _createContainsKeyDelegateGeneric.MakeGenericMethod(mi.DeclaringType);
+            return (Func<object, string, bool>)genericHelper.Invoke(null, new object[] { mi });
+        }
+
+
+        static Func<object, object> CreateDelegate(PropertyInfo pi)
+        {
+            var genericHelper = _createGetterGeneric.MakeGenericMethod(pi.DeclaringType, pi.PropertyType);
+            return (Func<object, object>)genericHelper.Invoke(null, new object[] { pi.GetGetMethod() });
+        }
+
+        static Func<object, object> CreateDelegate(FieldInfo fi)
+        {
+            var genericHelper = _createFieldGetterGeneric.MakeGenericMethod(fi.DeclaringType, fi.FieldType);
+            return (Func<object, object>)genericHelper.Invoke(null, new object[] { fi });
+        }
+
+        static Func<object, string, object> CreateIndexedGetterGeneric<T, R>(MethodInfo getter) where T : class
+        {
+            var getterTypedDelegate = (Func<T, string, R>)Delegate.CreateDelegate(typeof(Func<T, string, R>), getter);
+            var getterDelegate = (Func<object, string, object>)((object instance, string key) => getterTypedDelegate((T)instance, key));
+            return getterDelegate;
+        }
+
+
+
+        static Func<object, string, object> CreateIndexedDelegate(PropertyInfo pi)
+        {
+            var genericHelper = _createIndexedGetterGeneric.MakeGenericMethod(pi.DeclaringType, pi.PropertyType);
+            return (Func<object, string, object>)genericHelper.Invoke(null, new object[] { pi.GetGetMethod() });
+        }
+
 
         private static MemberAccessors Doit(Type t)
         {
            
 
-            var dic = new Dictionary<string, MethodInfo[]>(StringComparer.OrdinalIgnoreCase);
+            var dic = new Dictionary<string, Func<object,object>[]>(StringComparer.OrdinalIgnoreCase);
 
 
             var props = t.GetProperties(BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.Instance).Where(x => x.CanRead );
             
             foreach (var g in props.Where(x => x.GetIndexParameters().Length == 0).GroupBy(x=> x.Name ))
             {
-                dic[g.Key] = g.Select(x=> x.GetMethod ).ToArray();
+                dic[g.Key] = g.Select(CreateDelegate).ToArray();
 
             }
-            Dictionary<string, FieldInfo> fields = null;
+            Dictionary<string, Func<object, object>> fields = null;
             if ( t.FullName.StartsWith( "NDjango"))
             {
-                fields = t.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).ToDictionary(x => x.Name.Replace("@",""), StringComparer.OrdinalIgnoreCase);
-
-
-
+                fields = t.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).ToDictionary(x => x.Name.Replace("@",""), y=> CreateDelegate(y), StringComparer.OrdinalIgnoreCase);
             }
 
 
 
-            var containsKey = t.GetMethods(BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.Public).FirstOrDefault(x => x.Name == "ContainsKey" && x.GetParameters().Count() == 1);
+            var containsKey = t.GetMethods(BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.Public).Where(x => x.Name == "ContainsKey" && x.GetParameters().Count() == 1 && x.GetParameters().First().ParameterType == typeof ( string )).Select(x => CreateContainsKeyDelegate(x)).FirstOrDefault();
 
 
             
@@ -52,7 +128,7 @@ namespace Mozu.SiteBuilder.Mvc.ViewEngine
                    {
                        Fields = fields,
                        PropertyDictionary = dic,
-                       Indexers = props.Where(x => x.GetIndexParameters().Length > 0).ToArray(),
+                       Indexers = props.Where(x => x.GetIndexParameters().Length ==1  && x.GetIndexParameters().First().ParameterType== typeof(string)).Select(CreateIndexedDelegate).ToArray(),
                        ContainsKey = containsKey
                    };
 
@@ -61,14 +137,14 @@ namespace Mozu.SiteBuilder.Mvc.ViewEngine
 
         public class MemberAccessors
         {
-            public Dictionary<string, MethodInfo[]> PropertyDictionary { get; set; }
+            public Dictionary<string, Func<object, object>[]> PropertyDictionary { get; set; }
 
-            public PropertyInfo[] Indexers { get; set; }
+            public Func<object, string,object>[] Indexers { get; set; }
 
-            public Dictionary<string, FieldInfo > Fields { get; set; }
+            public Dictionary<string, Func<object, object>> Fields { get; set; }
 
 
-            public MethodInfo  ContainsKey  { get; set; }
+            public Func<object, string,bool > ContainsKey { get; set; }
         }
        
         public static  object CleanJson(object val)
@@ -201,14 +277,14 @@ namespace Mozu.SiteBuilder.Mvc.ViewEngine
 
             }
             
-            MethodInfo[] mis;
+            Func<object,object >[] mis;
             if (lookup.PropertyDictionary.TryGetValue(memberName, out mis))
             {
                 for (int i = 0; i < mis.Length; i++)
                 {
                     try
                     {
-                        object obj = mis[i].Invoke(container, null);
+                        object obj = mis[i](container);
                         if (obj != null)
                         {
                             return new FSharpOption<object>(CleanJson(obj));
@@ -228,35 +304,13 @@ namespace Mozu.SiteBuilder.Mvc.ViewEngine
             {
                 try
                 {
-                    Type paramType = lookup.Indexers[i].GetIndexParameters()[0].ParameterType;
-                    object param = null;
-                    if (paramType == typeof (string))
-                    {
-                        param = memberName;
-                    }else if ( paramType == typeof (int))
-                    {
-                        int tmpInt;
-                        if (int.TryParse(memberName, out tmpInt))
-                        {
-                            param = tmpInt;
-                        }
-                        
-                    }else if (paramType == typeof ( object ))
-                    {
-                        param = memberName;
-                    }
-                    else
-                    {
-                        throw new Exception("tell phipps 1 " + paramType);
-                    }
+                    
 
-
-                    if (param != null)
-                    {
+                   
                      //   bool contanisKey = true;
-                        if (lookup.ContainsKey != null && lookup.ContainsKey.GetParameters()[0].ParameterType == paramType)
+                        if (lookup.ContainsKey != null )
                         {
-                            bool containsKey = (bool) lookup.ContainsKey.Invoke(container, new object[] {param});
+                            bool containsKey = (bool) lookup.ContainsKey(container, memberName);
                             if (!containsKey)
                             {
                                 continue;
@@ -264,8 +318,8 @@ namespace Mozu.SiteBuilder.Mvc.ViewEngine
                         }
 
 
-                        return new FSharpOption<object>(CleanJson(lookup.Indexers[i].GetMethod.Invoke(container, new object[] { param })));    
-                    }
+                        return new FSharpOption<object>(CleanJson(lookup.Indexers[i](container,  memberName )));    
+                    
                     
                 }
                 catch 
@@ -274,12 +328,12 @@ namespace Mozu.SiteBuilder.Mvc.ViewEngine
                     
                 }
             }
-            FieldInfo fi;
+            Func<object,object> fi;
             if (lookup.Fields != null && lookup.Fields.TryGetValue(memberName, out fi))
             {
                 try
                 {
-                    return new FSharpOption<object>(CleanJson(fi.GetValue(container)));
+                    return new FSharpOption<object>(CleanJson(fi(container)));
                 }
                 catch
                 {
