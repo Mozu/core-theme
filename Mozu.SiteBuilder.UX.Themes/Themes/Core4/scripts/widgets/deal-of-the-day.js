1d78d0fa75974c13391c9f0b4263ae8f877f6849
Jimmy Sanford <CORP\Jimmy_Sanford>
2014-01-24 12:19:27 -0600

    deal of the day stuff

---
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/widgets/deal-of-the-day.js
@@ -0,0 +1,41 @@
+﻿define(
+    ['modules/jquery-mozu', 'shim!vendor/underscore>_', "modules/api", "modules/backbone-mozu", "modules/models-product"],
+    function ($, _, api, Backbone, ProductModels) {
+        $(function () {
+            $('[data-mz-deal-of-the-day]').each(function (index, deal) {
+                deal = $(deal);
+
+                var config = deal.data('mzDealOfTheDay'),
+                    products, DealView;
+
+                products = api.get('search', {
+                    // filter: 'discountId eq ' + config.discount
+                });
+
+                DealView = Backbone.MozuView.extend({
+                    templateName: 'modules/product/product-list',
+                    getRenderContext: function () {
+                        var context = Backbone.MozuView.prototype.getRenderContext.apply(this, arguments);
+
+                        context.dealOfTheDay = config;
+
+                        return context;
+                    }
+                });
+
+                products.then(function (collection) {
+                    var productCollection = new ProductModels.ProductCollection(collection.data),
+                        dealView;
+
+                    dealView = new DealView({
+                        model: productCollection,
+                        el: deal
+                    });
+
+                    dealView.render();
+                    deal.prepend('<h3>' + 'food' + '</h3>');
+                });
+            });
+        });
+    }
+);
