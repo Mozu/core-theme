655a2f7430cb5e6e8d79a2988d71b986664d62a9
Foster Hersey <foster.hersey@gmail.com>
2014-02-03 15:46:21 -0600

    Thom is the worst at merging.

--- Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/CustomerMapping.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/CustomerMapping.cs
@@ -20,7 +20,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
             .ForMember(x => x.Id, op => op.ResolveUsing(dc => dc.Id))
             .ForMember(x => x.UserId, op => op.ResolveUsing(dc => string.IsNullOrWhiteSpace( dc.UserId) ? null : dc.UserId))
             .ForMember(x => x.EmailAddress, op => op.ResolveUsing(dc => dc.EmailAddress))
-            .ForMember(x => x.UserName, op => op.ResolveUsing(dc => dc.UserName))
+            .ForMember(x => x.UserName, op => op.ResolveUsing(dc => !String.IsNullOrEmpty(dc.UserName) ? dc.UserName : (!dc.IsAnonymous ? dc.EmailAddress : null)))
             .ForMember(x => x.FirstName, op => op.ResolveUsing(dc => dc.FirstName))
             .ForMember(x => x.LastName, op => op.ResolveUsing(dc => dc.LastName))
             .ForMember(x => x.Contacts, op => op.ResolveUsing(dc => dc.Contacts))
@@ -39,6 +39,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
             .ForMember(x => x.TaxId, op => op.ResolveUsing(dc => dc.TaxId))
             //todo: confirm default of 0 Greg Murray on 2014-01-27 (ordercount defaults to 0 above so should be ok)
             .ForMember(x => x.VisitCount, op => op.ResolveUsing(dc => (dc.CommerceSummary != null) ? dc.CommerceSummary.VisitsCount : 0))
+            .ForMember(x => x.IsAnonymous, op => op.ResolveUsing(dc => dc.IsAnonymous))
             //ignores
             .ForMember(x => x.WishlistCount, op => op.Ignore())
             .ForMember(x => x.PaymentCards, op => op.Ignore())
@@ -69,48 +70,16 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
             .ForMember(dc => dc.TaxExempt, op => op.ResolveUsing(x => x.TaxExempt))
             .ForMember(dc => dc.TaxId, op => op.ResolveUsing(x => x.TaxId))
             .ForMember(dc => dc.Notes, op => op.Ignore())
-            .ForMember(dc => dc.TaxExempt, op => op.MapFrom(x => x.TaxExempt))
-            .ForMember(dc => dc.TaxId, op => op.MapFrom(x => x.TaxId))
-            .ForMember(dc => dc.IsAnonymous, op => op.MapFrom(x => x.IsAnonymous))
+            .ForMember(dc => dc.TaxExempt, op => op.ResolveUsing(x => x.TaxExempt))
+            .ForMember(dc => dc.TaxId, op => op.ResolveUsing(x => x.TaxId))
+            .ForMember(dc => dc.IsAnonymous, op => op.ResolveUsing(x => x.IsAnonymous))
 
-                // add AccountId to all the contacts
+            // ensure that all contacts have the appropriate AccountId set
             .AfterMap((x, dc) => dc.Contacts.ForEach(con => con.AccountId = dc.Id))
             //ignore
             .ForMember(dc => dc.LocaleCode, op => op.Ignore())
             .ForMember(dc => dc.ExternalId, op => op.Ignore())
             .ForMember(dc => dc.AuditInfo, op => op.Ignore())
-            ;
-
-            Mapper.CreateMap<Contact, DC.CustomerContact>()
-            // DC.CustomerContact is a subclass of DC.Contact, so use the parent class mapping
-            .BeforeMap((x, dc) => { Mapper.Map<Contact, Mozu.Core.Api.Contracts.Contact>(x, dc); })
-            
-            //todo: confirm new name, phone, address mappings Greg Murray on 2014-01-24
-            .ForMember(dc => dc.MiddleNameOrInitial, op => op.ResolveUsing(x => x.MiddleName))
-            .ForMember(dc => dc.LastNameOrSurname, op => op.ResolveUsing(x => x.LastName))
-            .ForMember(dc => dc.PhoneNumbers, op => op.ResolveUsing(x => new Core.Api.Contracts.Phone
-                {
-                    Home = x.HomePhone,
-                    Mobile = x.MobilePhone,
-                    Work = x.WorkPhone
-                }))
-            .ForMember(dc => dc.Address, op => op.ResolveUsing(x => new Core.Api.Contracts.Address()
-            {
-                Address1 = x.Address1,
-                Address2 = x.Address2,
-                Address3 = x.Address3,
-                Address4 = x.Address4,
-                CityOrTown = x.CityOrTown,
-                StateOrProvince = x.StateOrProvince,
-                PostalOrZipCode = x.PostalOrZipCode,
-                CountryCode = x.CountryCode,
-                IsValidated = x.AddressIsValidated
-            }))
-             //ignore
-            .ForMember(dc => dc.AccountId, op => op.Ignore())
-            .ForMember(dc => dc.Types, op => op.Ignore())
-            .ForMember(dc => dc.AuditInfo, op => op.Ignore())
-            .ForMember(dc => dc.FaxNumber, op => op.Ignore())  //todo: Greg Murray consider adding FaxNumber to Contact?
             ;
         }
     }
