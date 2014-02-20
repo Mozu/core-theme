a8d0c2f953d2d37d318a7cff4fab2077d7ef854c
Foster Hersey <foster.hersey@gmail.com>
2014-02-19 19:24:34 -0600

    New version of Mozu.Core.Messaging & Burrows.

--- Mozu.SiteBuilder.UX/Configuration/AutofacModule.cs
+++ Mozu.SiteBuilder.UX/Configuration/AutofacModule.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
 using System.Web.Http;
 using Autofac;
 using Autofac.Integration.WebApi;
+using Burrows.Autofac;
+using Burrows.Configuration;
+using Burrows.Log4Net;
 using Burrows.Publishing;
-using Mozu.AdminUser.Contracts.Clients;
 using Mozu.Content.Contracts.Clients;
 using Mozu.Core;
 using Mozu.Core.Api.Client;
@@ -17,84 +15,64 @@ using Mozu.Core.Configuration;
 using Mozu.Core.Logging;
 using Mozu.Core.Messaging.Publish;
 using Mozu.Core.Settings;
-using Mozu.ProductRuntime.Contracts.Clients;
 using Mozu.SiteBuilder.Mvc;
 using Mozu.SiteBuilder.Mvc.Catalog;
-using Mozu.SiteBuilder.Mvc.CMS;
-using Mozu.SiteBuilder.Mvc.Extensions;
 using Mozu.SiteBuilder.Mvc.Logging;
 using Mozu.SiteBuilder.Mvc.Mobile;
 using Mozu.SiteBuilder.Mvc.Navigation;
 using Mozu.SiteBuilder.Mvc.Settings;
-using Mozu.SiteBuilder.Mvc.TempMocks;
 using Mozu.SiteBuilder.Mvc.Users;
-using Mozu.SiteBuilder.Mvc.ViewEngine;
 using Mozu.SiteBuilder.UX.Messaging;
-using Mozu.SiteBuilder.UX.Models;
 using Mozu.SiteBuilder.UX.Navigation;
 using Mozu.SiteSettings.General.Contracts.Clients;
-using NDjango;
-using NDjango.Interfaces;
 using Module = Autofac.Module;
-
 
 namespace Mozu.SiteBuilder.UX.Configuration
 {
-	public class AutofacModule : Module
-	{
-		public string ApiBaseUri { get; set; }
+    public class AutofacModule : Module
+    {
+        public string ApiBaseUri { get; set; }
         class SbApiContextBuilder : IApiContextBuilder
         {
 
             public IApiContext BuildApiContext(IApiContext apiContext, System.Net.Http.HttpRequestMessage request)
             {
                 return (IApiContext)request.GetDependencyScope().GetService(typeof(IApiContext));
-             
+
             }
         }
-		protected override void Load(ContainerBuilder builder)
-		{
-
-
-
-
-
-
-
-
-
-
-
+        protected override void Load(ContainerBuilder builder)
+        {
 
             builder.RegisterHttpRequestMessage(GlobalConfiguration.Configuration);
 
             builder.RegisterType<SiteBuilderApiContext>().As<Mozu.Core.IApiContext>().As<ISiteBuilderApiContext>().InstancePerApiRequest();
-           // builder.RegisterType<Mozu.SiteBuilder.Mvc.Security.AuthenticationHelper>().InstancePerHttpRequest();
+            // builder.RegisterType<Mozu.SiteBuilder.Mvc.Security.AuthenticationHelper>().InstancePerHttpRequest();
             builder.RegisterType<ServiceClientMessageHandler>().As<IServiceClientMessageHandler>().InstancePerApiRequest();
             builder.RegisterType<SbApiContextBuilder>().As<IApiContextBuilder>();
-		    
-		    builder.RegisterClassesMatchingInterfaceName(typeof (Mozu.SiteBuilder.Mvc.Customers.CustomerRepository).Assembly);
-		    builder.RegisterClassesMatchingInterfaceName(typeof (Mozu.Customer.Contracts.Clients.CustomerAccountWebApiClient).Assembly);
-            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.AdminUser.Contracts.Clients.IMultiScopeInvitationWebApiClient ).Assembly);
-           // builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.Content.Contracts.Clients.DocumentListWebApiClient    ).Assembly);
-            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.ProductAdmin.Contracts.Category  ).Assembly);
-            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.ProductRuntime.Contracts.Clients.ProductRuntimeWebApiClient ).Assembly);
-            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.CommerceRuntime.Contracts.Clients.CartWebApiClient ).Assembly);
-            
+
+            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.SiteBuilder.Mvc.Customers.CustomerRepository).Assembly);
+            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.Customer.Contracts.Clients.CustomerAccountWebApiClient).Assembly);
+            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.AdminUser.Contracts.Clients.IMultiScopeInvitationWebApiClient).Assembly);
+            // builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.Content.Contracts.Clients.DocumentListWebApiClient    ).Assembly);
+            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.ProductAdmin.Contracts.Category).Assembly);
+            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.ProductRuntime.Contracts.Clients.ProductRuntimeWebApiClient).Assembly);
+            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.CommerceRuntime.Contracts.Clients.CartWebApiClient).Assembly);
+
             builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.SiteSettings.Order.Contracts.Clients.CheckoutSettingsWebApiClient).Assembly);
             builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.Reference.Contracts.Clients.ReferenceDataWebApiClient).Assembly);
-            
-            
+
+
             builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.SiteSettings.Shipping.Contracts.Clients.ShippingSettingsWebApiClient).Assembly);
-            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.SiteSettings.Order.Contracts.CheckoutSettings ).Assembly);
-		    
+            builder.RegisterClassesMatchingInterfaceName(typeof(Mozu.SiteSettings.Order.Contracts.CheckoutSettings).Assembly);
+
             builder.RegisterType<ThemeSettingsRepository>().As<IThemeSettingsRepository>().InstancePerApiRequest();
 
             builder.RegisterType<FiftyOneDegreesMobileDetectionProvider>().As<IMobileDetectionProvider>().InstancePerApiRequest();
 
-          
-          //  builder.RegisterType<BehaviorWebApiClient>().As<IBehaviorWebApiClient>();
-		    builder.RegisterType<PermissionsRepository>().As<IPermissionsRepository>().InstancePerApiRequest();
+
+            //  builder.RegisterType<BehaviorWebApiClient>().As<IBehaviorWebApiClient>();
+            builder.RegisterType<PermissionsRepository>().As<IPermissionsRepository>().InstancePerApiRequest();
 
 
 
@@ -107,49 +85,47 @@ namespace Mozu.SiteBuilder.UX.Configuration
             //builder.Register(c => new ProductCategoryRuntimeWebApiClient(c.Resolve<ServiceClientMessageHandler>())).As<IProductCategoryRuntimeWebApiClient>().InstancePerLifetimeScope();
             //builder.Register(c => new ProductRuntimeWebApiClient(c.Resolve<ServiceClientMessageHandler>())).As<IProductRuntimeWebApiClient>().InstancePerLifetimeScope();
 
-		  //  builder.RegisterType<MockProductCategoryRuntimeWebApiClient>().As<IProductCategoryRuntimeWebApiClient>();
+            //  builder.RegisterType<MockProductCategoryRuntimeWebApiClient>().As<IProductCategoryRuntimeWebApiClient>();
             //builder.RegisterType<DjangoMozuViewEngine>().As<DjangoMozuViewEngine>().As<IViewEngine>().InstancePerLifetimeScope();
 
             builder.RegisterType<RuntimeCategoryTreeProvider>().As<ICategoryTreeProvider>().InstancePerApiRequest();
-		    builder.RegisterType<CategoryNavigationProvider>().As<ICategoryNavigationProvider>().InstancePerApiRequest();
+            builder.RegisterType<CategoryNavigationProvider>().As<ICategoryNavigationProvider>().InstancePerApiRequest();
 
-		    // builder.RegisterType<MozuServiceClientMessageHandler>().As<IServiceClientMessageHandler>();
-
-            builder.RegisterType<DocumentListWebApiClient>().As<IDocumentListWebApiClient>().InstancePerApiRequest();
-            builder.RegisterType<GeneralSettingsWebApiClient>().As<IGeneralSettingsWebApiClient>().InstancePerApiRequest();
+            // builder.RegisterType<MozuServiceClientMessageHandler>().As<IServiceClientMessageHandler>();
 
             builder.RegisterType<DocumentListWebApiClient>().As<IDocumentListWebApiClient>().InstancePerApiRequest();
             builder.RegisterType<GeneralSettingsWebApiClient>().As<IGeneralSettingsWebApiClient>().InstancePerApiRequest();
-            
 
-
+            builder.RegisterType<DocumentListWebApiClient>().As<IDocumentListWebApiClient>().InstancePerApiRequest();
+            builder.RegisterType<GeneralSettingsWebApiClient>().As<IGeneralSettingsWebApiClient>().InstancePerApiRequest();
 
 
             builder.RegisterType<PropertyTypeWebApiClient>().As<IPropertyTypeWebApiClient>().InstancePerApiRequest();
-		    builder.RegisterType<DocumentTypeWebApiClient>().As<IDocumentTypeWebApiClient>().InstancePerApiRequest();
+            builder.RegisterType<DocumentTypeWebApiClient>().As<IDocumentTypeWebApiClient>().InstancePerApiRequest();
 
             builder.RegisterType<GeneralSettingsWebApiClient>().As<IGeneralSettingsWebApiClient>().InstancePerApiRequest();
 
-            
+
 
             // add these two logging context providers for loggers provided by the DI framework.
             builder.RegisterType<CurrentRequestLoggingContextProvider>().As<ILoggingContextProvider>().InstancePerLifetimeScope();
             builder.RegisterType<ApplicationNameLoggingContextProvider>().As<ILoggingContextProvider>().WithParameter("applicationName", ApplicationConstants.APPLICATION_NAME).InstancePerLifetimeScope();
 
-            builder.RegisterType<VisitEventPublisher>().As<VisitEventPublisher>().InstancePerApiRequest();
+            builder.RegisterType<VisitEventPublisher>().AsSelf().InstancePerApiRequest();
+            builder.RegisterType<CacheItemsInvalidConsumer>().AsSelf().SingleInstance();
 
-            // Register a MassTransit IPublisher.
+            // Register a MassTransit/Burrows IPublisher.
             // The rabbitMQ connectionstring is used to recieve control messages sent to our application by MassTransit.
             builder.Register(c => c.Resolve<ISettings>().CreatePublisher("SiteBuilderMessageQueue", "Mozu.SiteBuilder.UX")).As<IPublisher>().SingleInstance();
-		}
 
-        //object BuildClient<T>(IComponentContext c)
-        //{
-        //    var ctx = c.Resolve<ISiteBuilderApiContext>();
-        //    var scmh = c.Resolve<IServiceClientMessageHandler>();
-        //    var client = (ServiceClientBase) Activator.CreateInstance(typeof (T), scmh);
-        //    client.Options.DisableCache = ctx.DataViewMode == DataViewModeType.Pending;
-        //    return client;
-        //}
-	}
+            // Register a MassTransit/Burrows Consumer.
+            // The rabbitMQ connectionstring is used to recieve control messages sent to our application by MassTransit.
+            //builder.Register(c => new Publisher(sbc => sbc.Configure("SiteBuilderMessageQueue", subs => subs.LoadFrom(c.Resolve<ILifetimeScope>()))
+            //    .UseLog4Net()
+            //    .SetConcurrentConsumerLimit(10), ps => ps.UsePublisherConfirms("SiteBuilderMessageQueue2").WithFileBackingStore())
+            //).As<IPublisher>().SingleInstance();
+
+            // builder.Register(c => c.Resolve<ISettings>().ConfigureConsumer("SiteBuilderMessageQueue", 
+        }
+    }
 }
---
+++ Mozu.SiteBuilder.UX/Messaging/CacheItemsInvalidConsumer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Burrows;
+using Mozu.Core.Messaging.Contracts;
+
+namespace Mozu.SiteBuilder.UX.Messaging
+{
+    public class CacheItemsInvalidConsumer : Consumes<IEntityEvent>.Selected
+    {
+        /// <summary>
+        /// Accepts or ignores messages before they are consumed by us.
+        /// </summary>
+        public bool Accept(IEntityEvent message)
+        {
+            return true;
+        }
+
+        public void Consume(IEntityEvent message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
