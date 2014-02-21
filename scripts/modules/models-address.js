43977cc5cc6b1ebb8f910407b22fa8ede53ca030
Zetlen <CORP\james_zetlen>
2014-02-21 15:20:33 -0600

    Updated addressmodels to handle international state situations

--- scripts/modules/models-address.js
+++ scripts/modules/models-address.js
@@ -2,6 +2,16 @@
     ["modules/backbone-mozu", 'hyprlive'],
     function (Backbone, Hypr) {
 
+
+        var requiresZipCode = {
+            US: true,
+            CA: true,
+            JP: true,
+            TW: true
+        },
+            requiresStateProv = requiresZipCode,
+            defaultStateProv = "n/a";
+
         var PhoneNumbers = Backbone.MozuModel.extend({
             validation: {
                 home: {
@@ -13,6 +23,13 @@
 
         StreetAddress = Backbone.MozuModel.extend({
             mozuType: 'address',
+            initialize: function () {
+                this.on('change:countryCode', this.clearStateAndZipWhenCountryChanges, this);
+            },
+            clearStateAndZipWhenCountryChanges: function () {
+                this.unset('postalOrZipCode');
+                this.unset('stateOrProvince');
+            },
             validation: {
                 address1: {
                     required: true,
@@ -26,15 +43,32 @@
                     required: true,
                     msg: Hypr.getLabel("countryMissing")
                 },
+                stateOrProvince: {
+                    fn: function (value) {
+                        if (requiresStateProv[this.attributes.countryCode] && (!value || value === defaultStateProv)) return Hypr.getLabel('stateProvMissing');
+                    }
+                },
                 postalOrZipCode: {
                     fn: function (value) {
-                        if (this.attributes.countryCode === "US" && !value) return Hypr.getLabel("postalCodeMissing")
+                        if (requiresZipCode[this.attributes.countryCode] && !value) return Hypr.getLabel("postalCodeMissing")
                     }
                 }
             },
             defaults: {
                 candidateValidatedAddresses: null,
-                countryCode: Hypr.getThemeSetting('preselectCountryCode') || ''
+                countryCode: Hypr.getThemeSetting('preselectCountryCode') || '',
+                addressType: 'Residential'
+            },
+            toJSON: function (options) {
+                // workaround for SA
+                var j = Backbone.MozuModel.prototype.toJSON.apply(this, arguments);
+                if ((!options || !options.helpers) && !j.stateOrProvince) {
+                    j.stateOrProvince = "n/a";
+                }
+                if (options && options.helpers && j.stateOrProvince === "n/a") {
+                    delete j.stateOrProvince;
+                }
+                return j;
             }
         });
 
