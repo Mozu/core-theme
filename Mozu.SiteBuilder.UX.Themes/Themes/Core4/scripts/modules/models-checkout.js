e669f8c31607185a6064010bf95cbe3c5480ff5a
Ojas (ツ) <CORP\ojas_patel>
2014-02-24 14:20:59 -0600

    Add IP Address to Order

--- Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/modules/models-checkout.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/modules/models-checkout.js
@@ -587,7 +587,7 @@
                     process.push(this.addNewCustomer);
                 } 
 
-                if (this.get('shopperNotes').has('comments')) {
+                if (this.get('shopperNotes').has('comments') || this.get('ipAddress')) {
                     process.push(this.update);
                 }
 
