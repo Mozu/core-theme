89feb8356d19a9940e8c3b32fb4f37df6b189e7d
Ojas (ツ) <CORP\ojas_patel>
2014-02-25 11:07:10 -0600

    rev merge add yeat to order grid

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/Index.js
@@ -43,7 +43,7 @@ Ext.define('Taco.view.order.Index', {
             flex: 1,
             minWidth: 180,
             xtype: 'datecolumn',
-            format: 'M d g:ia'
+            format: 'M d Y g:ia'
             //resizable: false,
            
         }, {
