abc280770f7bd826bb0ee54b28dcf6048cbf33bc
Zetlen <CORP\james_zetlen>
2014-02-27 19:44:47 -0600

    Checking in loose ends that have been outstanding for weeks

--- Mozu.SiteBuilder.UX.Themes/buildcore.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\TF.exe" checkout .\Themes\Core3 /recursive && "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\TF.exe" checkout .\Themes\MobileCore /recursive  && moz -d .\Themes -jv Core3 && moz -d .\Themes -jv MobileCore
