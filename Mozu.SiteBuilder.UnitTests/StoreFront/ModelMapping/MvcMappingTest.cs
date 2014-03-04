10cb0bcba11817bd05e2942df562f8b2fd755c5d
Greg Murray <CORP\greg_murray>
2014-03-04 07:47:38 -0600

    fix build

---
+++ Mozu.SiteBuilder.UnitTests/StoreFront/ModelMapping/MvcMappingTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using AutoMapper;
+using Mozu.SiteBuilder.Mvc.Models.Mapping;
+using Mozu.SiteBuilder.Mvc.Models.ModelMapping;
+using Mozu.SiteBuilder.UX.Areas.StoreFront.ModelMapping;
+using NUnit.Framework;
+
+namespace Mozu.SiteBuilder.UnitTests.StoreFront.ModelMapping
+{
+    public class MvcMappingTest
+    {
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            Mapper.AddProfile<CartMapping>();
+            Mapper.AddProfile<CmsPagesMapping>();
+            Mapper.AddProfile<GeneralSettingsMapping>();
+            Mapper.AddProfile<NavigationMapping>();
+            Mapper.AddProfile<ProductMapping>();
+            
+            //Mapper.AddProfile<CustomerMapping>();
+            //Mapper.AddProfile<DiscountMapping>();
+            //Mapper.AddProfile<ExtensibleAttributeMapping>();
+            //Mapper.AddProfile<FacetMapping>();
+            //Mapper.AddProfile<FileManagementModelMapping>();
+            //Mapper.AddProfile<GeneralSettingsMapping>();
+            //Mapper.AddProfile<OptionMapping>();
+            //Mapper.AddProfile<OrderMapping>();
+            //Mapper.AddProfile<ProductMapping>();
+            //Mapper.AddProfile<ReturnMapping>();
+            //Mapper.AddProfile<RuntimeProductMapping>();
+            //Mapper.AddProfile<ShippingMapping>();
+            //Mapper.AddProfile<TaxMapping>();
+            //Mapper.AddProfile<TenantMapping>();
+            //Mapper.AddProfile<UserMapping>();
+
+            //replace with?
+            //List<Type> types = _containerFactory.AssembliesToScan.SelectMany(assy => assy.GetTypes())
+            //    .Where(x => x.IsSubclassOf(typeof(Profile))).ToList();
+
+            //types.Each(t =>
+            //{
+            //    var profile = (Profile)Activator.CreateInstance(t);
+            //    Mapper.AddProfile(profile);
+            //});
+
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            Mapper.Reset();
+        }
+
+        [Test, Ignore]
+        public void AdminMappings_should_be_valid()
+        {
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
