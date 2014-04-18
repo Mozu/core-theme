8e4f04efacbf155a6b3043944f058e484fb044a1
Greg Murray <CORP\greg_murray>
2014-04-18 08:35:18 -0500

    merge from main, plus added capabilities diagram

--- Mozu.SiteBuilder.UX.Admin/Api/WishlistController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/WishlistController.cs
@@ -6,10 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Mozu.CommerceRuntime.Contracts.Clients;
+using Mozu.Core;
+using Mozu.Core.Api.Contracts;
+using Mozu.Core.Api.Contracts.Client;
 using Mozu.Core.Api.Routing;
 using Mozu.SiteBuilder.UX.Admin.Api.Models;
 using Mozu.Core.Extensions;
 using Mozu.Core.Api.Client;
+using Mozu.Tenant.Contracts.Clients;
 using DC = Mozu.CommerceRuntime.Contracts.Wishlists;
 
 namespace Mozu.SiteBuilder.UX.Admin.Api
@@ -19,22 +23,34 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
     {
         private const string DEFAULT_WISHLIST_NAME = "my_wishlist";
         private IWishlistWebApiClient _wishlistWebApiClient;
-    
+        private readonly ITenantsWebApiClient _tenantsWebApiClient;
+        private readonly IApiContext _apiContext;
+
         /// <summary>
         /// Public constructor.
         /// </summary>
-        public WishlistController(IWishlistWebApiClient wishlistWebApiClient)
+        public WishlistController(IWishlistWebApiClient wishlistWebApiClient, ITenantsWebApiClient tenantsWebApiClient, IApiContext apiContext)
         {
             _wishlistWebApiClient = wishlistWebApiClient;
+            _tenantsWebApiClient = tenantsWebApiClient;
+            _apiContext = apiContext;
         }
 
         [HttpGetRoute(UriTemplate = "items")]
         public async Task<Response<List<DC.Wishlist>>> ListItems([FromUri]int customerAccountId, [FromUri]PagingParamaters pagingParams, [FromUri]FilterCollection extFilter)
         {
-            string filter = String.Format("CustomerAccountId eq {0} and Name eq \"{1}\"", customerAccountId, DEFAULT_WISHLIST_NAME);
-            var wishlist = (await _wishlistWebApiClient.GetWishlists(0, 1, null, filter)).ReadAsSync().Items.FirstOrDefault();
-
-            return List2(wishlist);
+            var tenant = (await _tenantsWebApiClient.GetTenantInternal(_apiContext.TenantId)).ReadAsSync();
+            var tasks = tenant.Sites.Select(site => GetWishlistBySite(customerAccountId, site.Id));
+            var taskResults = (await Task.WhenAll(tasks));
+            var wishes = taskResults.Where(x => ! x.HasException).Select(x => x.ReadAsSync()).ToList();
+            return List2(wishes);
         }
+
+        private Task<ServiceClientResponse<DC.Wishlist>> GetWishlistBySite(int customerAccountId, int siteId)
+        {
+            var siteWishListClient = _wishlistWebApiClient.CloneWithApiContext(x => x.SiteId = siteId);
+            return siteWishListClient.GetWishlistByName(customerAccountId, DEFAULT_WISHLIST_NAME);
+        }
+
     }
 }
