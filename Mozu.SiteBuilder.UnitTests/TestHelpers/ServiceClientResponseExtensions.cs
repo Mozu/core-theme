97be319a2613ec7b3ee74ca37633b3020c90acd5
Foster Hersey <foster.hersey@gmail.com>
2014-05-15 17:04:18 -0500

    Cleaned up testing around LocationInventoryController.

---
+++ Mozu.SiteBuilder.UnitTests/TestHelpers/ServiceClientResponseExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Mozu.Core.Api.Contracts.Client;
+
+namespace Mozu.SiteBuilder.UnitTests.Extensions
+{
+    public static class ServiceClientResponseExtensions
+    {
+        /// <summary>
+        /// Wraps an object as the result of a Task<ServiceClientResponse<T>>.
+        /// Ideal for mocking the return object of service clients.
+        /// </summary>
+        public static Task<ServiceClientResponse<T>> AsServiceClientResponseAsync<T>(this T response)
+        {
+            var serviceResult = new ServiceClientResponse<T> {
+                HasException = false,
+                ReadAsSync = () => response,
+                ReadAsAsync = () => response.AsTaskResult()
+            };
+
+            return serviceResult.AsTaskResult();
+        }
+
+        /// <summary>
+        /// Wraps an object in a TaskResult and returns the Task.
+        /// </summary>
+        public static Task<T> AsTaskResult<T>(this T wrapped)
+        {
+            var task = new TaskCompletionSource<T>();
+            task.SetResult(wrapped);
+            return task.Task;
+        }
+    }
+}
