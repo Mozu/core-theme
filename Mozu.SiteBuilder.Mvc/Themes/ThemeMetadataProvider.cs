78a048dcac107cbcac903a752392a4b88ae5f458
Greg Murray <CORP\greg_murray>
2014-05-22 15:10:35 -0500

    Removed PostDeploy.ps1 content under Themes

--- Mozu.SiteBuilder.Mvc/Themes/ThemeMetadataProvider.cs
+++ Mozu.SiteBuilder.Mvc/Themes/ThemeMetadataProvider.cs
@@ -270,7 +270,7 @@ namespace Mozu.SiteBuilder.Mvc.Themes
                 }
                 else
                 {
-                    if (! Path.IsPathRooted(coreThemeDir))
+                    if (Path.GetPathRoot(coreThemeDir).Length <3  )
                     {
                         coreThemeDir = Path.GetFullPath(HostingEnvironment.MapPath(coreThemeDir));
 
