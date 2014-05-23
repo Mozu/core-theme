2c00dd09c45aebc5c81e6a836b886c8b57036c5a
Greg Murray <CORP\greg_murray>
2014-05-23 16:58:51 -0500

    Fulfillment type on product editor

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/widget/ProductFulfillmentTypes.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/widget/ProductFulfillmentTypes.js
@@ -16,20 +16,39 @@ Ext.define('Taco.view.product.widget.ProductFulfillmentTypes', {
     product: null,
     isReadOnly: false,
     allowBlank: false,
+    //listeners: {
+    //    onProductTypeChange : function(selectField, value) {
+    //        var me = this,
+    //            productTypeRecord = selectField.store.getById(value),
+    //            goodsType = productTypeRecord.get("goodsType");
+
+    //        if (goodsType === 'DigitalGiftCard') {
+    //            this.directShipCheckbox.setDisabled(true);
+    //            this.inStorePickupCheckbox.setDisabled(true);
+    //            this.digitalGiftCardCheckbox.show();
+    //            this.digitalGiftCardCheckbox.setValue(true);
+    //            this.digitalGiftCardCheckbox.setReadOnly();
+    //        }
+
+    //    }
+    //},
 
     initComponent: function () {
         var me = this,
             fulfillmentTypes = this.product.get("fulfillmentTypesSupported");
 
         me.record = this.product;
+        me.productTypeRecord = this.productType;
+        var isPhysicalGood = (this.productType.get('goodsType') === 'Physical');
 
         this.directShipCheckbox = Ext.widget({
             xtype: 'checkboxfield',
             boxLabel: 'Direct Ship',
             name: 'directShipCb',
             inputValue: 'DirectShip',
-            checked: (fulfillmentTypes.indexOf('DirectShip') != -1),
+            checked: ((fulfillmentTypes.indexOf('DirectShip') != -1) && isPhysicalGood),
             readOnly: this.isReadOnly,
+            hidden: ! isPhysicalGood,
             handler: (this.isReadOnly) ? '' : me.onFulfillmentChange,
             scope: me,
             fulfillmentType: 1
@@ -40,8 +59,9 @@ Ext.define('Taco.view.product.widget.ProductFulfillmentTypes', {
             boxLabel: 'In Store Pickup',
             name: 'inStoreCb',
             inputValue: 'InStorePickup',
-            checked: (fulfillmentTypes.indexOf('InStorePickup') != -1),
+            checked: ((fulfillmentTypes.indexOf('InStorePickup') != -1) && isPhysicalGood),
             readOnly: this.isReadOnly,
+            hidden: !isPhysicalGood,
             handler: (this.isReadOnly) ? '' : me.onFulfillmentChange,
             scope: me,
             fulfillmentType: 1
@@ -49,12 +69,13 @@ Ext.define('Taco.view.product.widget.ProductFulfillmentTypes', {
 
         this.digitalGiftCardCheckbox = Ext.widget({
             xtype: 'checkboxfield',
-            boxLabel: 'Gift Card',
+            boxLabel: 'Email',
             name: 'digitalGiftCardCb',
-            inputValue: 'DigitalGiftCard',
-            checked: (fulfillmentTypes.indexOf('DigitalGiftCard') != -1),
-            readOnly: this.isReadOnly,
-            handler: (this.isReadOnly) ? '' : me.onFulfillmentChange,
+            inputValue: 'Digital',
+            checked: ((fulfillmentTypes.indexOf('Digital') != -1) || !isPhysicalGood),
+            readOnly: true,
+            disabled: true,
+            hidden: isPhysicalGood,
             scope: me,
             fulfillmentType: 1
         });
