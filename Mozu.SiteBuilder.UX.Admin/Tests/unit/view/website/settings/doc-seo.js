f334c1dfcda8455e48c78e673017e27d215d67fb
Thom 'Pig Pen' Phipps <CORP\Thomas_Phipps>
2014-06-03 21:36:03 -0500

    xxx

--- Mozu.SiteBuilder.UX.Admin/Tests/unit/view/website/settings/doc-seo.js
+++ Mozu.SiteBuilder.UX.Admin/Tests/unit/view/website/settings/doc-seo.js
@@ -86,8 +86,11 @@
             
         },
         function (next) {
-            t.is(m.doc.get('name'), "seo-name2",'the model name was set propertly');
+            t.is(m.doc.get('name'), "seo-name2", 'the model name was set propertly');
+
             t.is(m.doc.get('meta_description'), "test meta descriptoin2", 'the model description was set propertly');
+
+            t.is(1,2,'I am a test failure');
 
         }
     );
