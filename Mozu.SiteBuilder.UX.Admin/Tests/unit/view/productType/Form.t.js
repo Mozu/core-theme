05dc1a4c883463f20338cf92d1d9ffb35634a808
Greg Murray <CORP\greg_murray>
2014-06-05 19:35:44 -0500

    ProductType test for DigitalCredit

--- Mozu.SiteBuilder.UX.Admin/Tests/unit/view/productType/Form.t.js
+++ Mozu.SiteBuilder.UX.Admin/Tests/unit/view/productType/Form.t.js
@@ -1,97 +1,82 @@
-﻿StartTest(function (t) {
+﻿StartTest(function(t) {
     t.setOnlyMocks();
 
     t.describe("Product Type", function(t) {
         var m = {};
 
-        t.setOnlyMocks();
-        t.simManager().register([
+        t.requireOk('Taco.view.productType.Form', 'Taco.core.ux.form.field.MultiSelect',
+            'Taco.core.ux.BoxReorderer',
+            'Taco.model.ProductTypeAttribute',
+            'Taco.model.ProductType',
+            'Taco.view.productType.AttributeGroup');
+
+        t.describe('To increase revenue and profits, as a merchant, I want to offer Gift Cards', function(t) {
+
+            t.it("Should allow a merchant to designate a digital credit/gift card goods type.", function(t) {
+
+                m = {};
+
+                t.chain(
+
+                    function setup (next) {
+                        Taco.app.viewPort.removeAll(true);
+                        m.record = Ext.create('Taco.model.ProductType', { });
+                        m.form = Ext.create('Taco.view.productType.Form', {
+                            record: m.record
+                        });
+                        Taco.app.viewPort.add(m.form);
+                        t.waitForComponentVisible(m.form, next);
+                    },
+
+                    function setName (next) {
+                        m.name = m.form.down('#nameItemId');
+                        t.type(m.name, 'MyFirstSiesta_Olay!', next);
+                    },
+
+                    function selectDigitalCreditCheckbox (next) {
+                        m.digitalCreditCheckbox = m.form.down('#digitalCreditItemId');
+                        t.click(m.digitalCreditCheckbox, next);
+                    },
+
+                    function saveForm (next) {
+                        t.is(m.record.get('goodsType'), 'Physical', "Before save, goods type should be default 'Physical'");
+                        //t.expect(m.record.get('goodsType')).toBe('Physical'); //default expected
+                        m.form.save();
+                        next();
+                    },
+
+                    function assert (next) {
+                        t.is(m.record.get('goodsType'), 'DigitalCredit', "After save, goods type should be 'DigitalCredit'");
+                        //t.expect(m.record.get('goodsType')).toBe('DigitalCredit');
+                    }
+                );
+            });
+        });
+
+    });
+});
+
+function RegisterMocks (t, next, m) {
+    t.simManager().register([
             //{
-            //    url: '/admin/app/ProductType/update',
+            //    url: '/admin/app/ProductType/read',
             //    jsonFile: '/admin/tests/mocks/Mystic1/ProductTypes1.json'
             //},
             {
                 url: '/admin/app/ProductType/create',
                 stype: 'json',
-                getData: function() {
+                getData: function () {
                     return [
                         {
                             goodsType: m.goodsType
                         }
                     ];
                 },
-                doPost: function() {
+                doPost: function () {
                     return this.doGet.apply(this, arguments);
                 }
             }
-        ]);
-
-        t.describe('To increase revenue and profits, as a merchant, I want to offer Gift Cards', function (t) {
-
-            t.it("Should allow a merchant to designate a digital gift card goods type.", function(t) {
-
-                m = {};
-
-                t.chain(
-                    function requireClasses(next) {
-                        //t.diag('extra add tests');
-                        t.requireOk('Taco.view.productType.Form', 'Taco.core.ux.form.field.MultiSelect',
-                            'Taco.core.ux.BoxReorderer',
-                            'Taco.model.ProductTypeAttribute',
-                            'Taco.model.ProductType',
-                            'Taco.view.productType.AttributeGroup', next);
-                    },
-                    function setup(next) {
-                        SetupModesAndViewForCreateTests(t, next, m);
-                    },
-                    function(next) {
-
-                        t.subTest('Select Digital Gift Card checkbox', function(t) {
-                            t.chain([
-                                function (next) {
-                                    m.digitalCreditCheckbox = m.form.down('#digitalCreditItemId');
-                                    t.clickSelect(m.digitalCreditCheckbox, next);
-                                },
-                                function (next) {
-                                    m.nameItemId = m.form.down('#nameItemId');
-                                    //t.is(m.grid.store.getAt(0).getId(), 'aaaa', 'item found in store');
-                                    //m.grid.store.commitChanges();
-                                    t.clickSelect(m.itemAdder, 'Gift Card Test', next);
-
-                                },
-                                function(next) {
-                                    //t.matchGridCellContent(m.grid, 1, 0, 'bbb', 'found second extra value');
-                                    //t.is(m.grid.store.getAt(1).getId(), 'bbb', 'item found in store');
-                                    //t.is(m.grid.store.isDirty(), true, 'extras store was updated as dirty');
-                                    next();
-                                }
-                            ]);
-
-                        }, next);
-                    });
-
-            });
-        });
-
-    });
-
-});
-
-function SetupModesAndViewForCreateTests (t, next, m) {
-
-    m.productType = Ext.create('Taco.model.ProductType', {
-
-    });
-
-    t.chain(
-        function (next) {
-            t.waitForComponentVisible(m.form, next);
-        },
-        //function (next) {
-        //    m.digitalCreditCheckbox = m.form.down('#digitalCreditItemId');
-        //    next();
-        //},
-        next);
+    ]);
 
 
 }
