e3865aa772726a47f62cb65e3a7163ed69ffafc4
Greg Murray <CORP\greg_murray>
2014-06-10 13:09:55 -0500

    commented out Product gift card test (WIP)

--- Mozu.SiteBuilder.UX.Admin/Tests/MozuTestSuite.js
+++ Mozu.SiteBuilder.UX.Admin/Tests/MozuTestSuite.js
@@ -140,11 +140,12 @@ Harness.start(
                     {
                         url: 'customunit/view/product/subforms/ListExtraEditor.t.js',
                         title: 'product List Extra Tests'
-                    },
-                    {
-                        url: 'customunit/view/product/subforms/General.t.js',
-                        title: 'Gift Cards'
                     }
+                    //,
+                    //{
+                    //    url: 'customunit/view/product/subforms/General.t.js',
+                    //    title: 'Gift Cards'
+                    //}
                 ]
             },
             {
