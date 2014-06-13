e5b1e88a74c437098ade676e9579b4386da0f2fb
Foster Hersey <foster.hersey@gmail.com>
2014-06-13 16:12:13 -0500

    Add digitalPackages to order model & mapping.

---
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/Order/OrderDigitalPackage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mozu.SiteBuilder.UX.Admin.Api.Models.Order
+{
+    public class OrderDigitalPackage
+    {
+        public string Id { get; set; }
+        public string OrderId { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public string FulfillmentEmailAddress { get; set; }
+        public DateTime? FulfillmentDate { get; set; }
+        public int TotalQuantity { get; set; }
+        public List<OrderDigitalPackageItem> Items { get; set; }
+
+        /// <summary>
+        /// "Fulfilled", "NotFulfilled", or "PartiallyFulfilled"
+        /// </summary>
+        public string Status { get; set; }
+        
+        #region workflow
+        public List<string> AvailableActions { get; set; }
+        #endregion
+    }
+}
