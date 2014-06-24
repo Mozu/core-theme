40bb793f189a7e10696adde2095fc03bc0071204
Greg Murray <CORP\greg_murray>
2014-06-24 13:07:39 -0500

    Admin Customer Gift Cards and Store Credits grid

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/modal/StoreCredit.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/modal/StoreCredit.js
@@ -8,7 +8,7 @@ Ext.define('Taco.shared.view.modal.StoreCredit', {
     autoShow: true,
     width: 900,
     minHeight:50,
-    title: 'Store Credit',
+    title: 'Gift Cards and Store Credits',
     formCfg: null,
     closable: true,
     layout:'fit',
@@ -51,15 +51,44 @@ Ext.define('Taco.shared.view.modal.StoreCredit', {
                     menuDisabled: true
                 },
                 items: [
-                    { text: 'Code', dataIndex: 'code', width: 140 },
-                    { text: 'Date Issued', dataIndex: 'activationDate', flex: 1 },
-                    { text: 'Amount', dataIndex: 'initialBalance', width: 80, align: "right", renderer: 'usMoney'},
+                    {
+                        text: 'Code',
+                        dataIndex: 'code',
+                        width: 160
+                    },
+                    {
+                        text: 'Type',
+                        dataIndex: 'creditType',
+                        flex: 1,
+                        renderer: function(val) {
+                            switch (val) {
+                                case 'StoreCredit':
+                                    return 'Store Credit';
+                                case 'GiftCard':
+                                    return 'Gift Card';
+                                default:
+                                    return val;
+                            }
+                        }
+                    },
+                    {
+                        text: 'Date Issued',
+                        dataIndex: 'activationDate',
+                        flex: 1,
+                        renderer: function (dt) {
+                            return (dt) ? Ext.Date.format(dt, Ext.Date.defaultFormat) : '';
+                        }
+                    },
+                    
                     // no data for this column
                     //{ text: 'Issued By', dataIndex: 'issuedBy', flex: 1 },
                     {
                         text: 'Expires',
                         dataIndex: 'expirationDate',
-                        flex: 1
+                        flex: 1,
+                        renderer: function (dt) {
+                            return (dt) ? Ext.Date.format(dt, Ext.Date.defaultFormat) : '';
+                        }
                         /*,
                         editor: {
                             emptyText: "Amount",
@@ -70,11 +99,20 @@ Ext.define('Taco.shared.view.modal.StoreCredit', {
                         }*/
                     },
                     {
+                        text: 'Amount',
+                        dataIndex: 'initialBalance',
+                        //width: 80,
+                        flex: 1,
+                        align: "right",
+                        renderer: 'usMoney'
+                    },
+                    {
                         text: 'Balance',
                         dataIndex: 'currentBalance',
                         align: "right",
                         renderer: 'usMoney',
-                        width: 100
+                        flex: 1
+                        //width: 100,
                         /*,
                         editor: {
                             emptyText: "Amount",
