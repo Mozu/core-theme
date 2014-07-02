39bacfb1310a7af2d5037a27e8e1701501864348
Greg Murray <CORP\greg_murray>
2014-07-02 12:55:53 -0500

    defect on ProductType attributes throwing can't find widget.dirtybutton.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/productType/AttributeForm.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/productType/AttributeForm.js
@@ -1,7 +1,7 @@
 ﻿Ext.define('Taco.view.productType.AttributeForm', {
     extend: 'Taco.core.ux.form.Form',
     alias: 'widget.taco.producttype.attributeform',
-    requires: ['Taco.store.Attributes'],
+    requires: ['Taco.store.Attributes', 'Taco.core.ux.action.DirtyButton'],
 
     layout: {
         type: 'hbox',
