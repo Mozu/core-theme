71c3032f6b4efcd842fdee9d614635b133caa519
Zetlen <CORP\james_zetlen>
2014-07-02 12:50:08 -0500

    better testability

--- Mozu.SiteBuilder.UX.Admin/Tests/unit/view/order/widget/PaymentPanel.js
+++ Mozu.SiteBuilder.UX.Admin/Tests/unit/view/order/widget/PaymentPanel.js
@@ -1,143 +1,69 @@
 ﻿StartTest(function(t) {
     var m = {};
 
-    //Bug 28406:SEO fields missing for CMS pages in Site Builder
-
-    function makeOrder(details) {
-        var o = Ext.create('Taco.model.Order', Ext.Object.merge({
-            "id": "abc",
-            "orderNumber": 2,
-            "name": "seo-name",
-            "payments": [
-                {
-                    "orderId": "abc",
-                    "status": "New",
-                    "amountCollected": 0.0,
-                    "amountAuthorized": 0.0,
-                    "amountCredited": 0.0,
-                    "interactions": [
-                    ],
-                    "availableActions": [
-                    ],
-                    "isManual": false,
-                    "createDate": "2014-05-30T23:22:36.122Z"
-                },
-                {
-                    "orderId": "abc",
-                    "paymentServiceTransactionId": "6bbbef677ae74f92aeb80dbdfc631c8d",
-                    "status": "Authorized",
-                    "amountCollected": 0.0,
-                    "amountAuthorized": 64.0,
-                    "amountCredited": 0.0,
-                    "interactions": [
-                      {
-                          "id": "ba8b3fa1bc214a2cada7a33b010de3ce",
-                          "paymentId": "3164ef3ae479426fb7c2a33b010de136",
-                          "gatewayTransactionId": "2214106066",
-                          "gatewayInteractionId": 0,
-                          "interactionType": "Authorization",
-                          "status": "Authorized",
-                          "gatewayResponseCode": "1",
-                          "gatewayResponseText": "This transaction has been approved.",
-                          "amount": 64.0,
-                          "isManual": false,
-                          "createDate": "2014-05-30T16:22:38.337Z",
-                          "canEdit": false,
-                          "canDelete": false
-                      }
-                    ],
-                    "paymentType": "CreditCard",
-                    "cardType": "VISA",
-                    "cardNumber": "************1111",
-                    "nameOnCard": "CreditCardName",
-                    "availableActions": [
-                      "VoidPayment",
-                      "CapturePayment",
-                      "DeclinePayment",
-                      "ManualVoidPayment",
-                      "ManualCapturePayment",
-                      "ManualDeclinePayment"
-                    ],
-                    "isManual": false,
-                    "createDate": "2014-05-30T16:22:36.122Z"
-                },
-                {
-                    "orderId": "abc",
-                    "paymentServiceTransactionId": "bb09f5c71c764d8b8cc0783be5f37eb6",
-                    "status": "Pending",
-                    "amountCollected": 0.0,
-                    "amountAuthorized": 0.0,
-                    "amountCredited": 0.0,
-                    "interactions": [
-                      {
-                          "id": "1c50ce8fce5e4c2f928ca33b010dcd5b",
-                          "paymentId": "2f3c12151b6945708dc6a33b010dc352",
-                          "gatewayTransactionId": "0",
-                          "gatewayInteractionId": 0,
-                          "interactionType": "Authorization",
-                          "status": "Failed",
-                          "gatewayResponseCode": "78",
-                          "gatewayResponseText": "The card code is invalid.",
-                          "amount": 64.0,
-                          "isManual": false,
-                          "createDate": "2014-05-30T16:22:19.179Z",
-                          "canEdit": false,
-                          "canDelete": false
-                      }
-                    ],
-                    "paymentType": "CreditCard",
-                    "cardType": "VISA",
-                    "cardNumber": "************1111",
-                    "nameOnCard": "CreditCardName",
-                    "availableActions": [
-                      "VoidPayment",
-                      "ManualVoidPayment"
-                    ],
-                    "isManual": false,
-                    "createDate": "2014-05-30T16:22:10.614Z"
-                }
-            ],
-        }, details)
-        );
-        var passoc = o.associations.getByKey('payments');
-        passoc.read(o, passoc.getReader(), o.data.payments);
-        return o;
+    function getOrder(cb) {
+        Taco.model.Order.load('Orders2Payments', {
+            success: function(o) {
+                //var passoc = o.associations.getByKey('payments');
+                //passoc.read(o, passoc.getReader(), o.data.payments);
+                o.associations.each(function(assoc) {
+                    if (assoc.name in o.data) assoc.read(o, assoc.getReader(), o.data[assoc.name]);
+                });
+                cb(o);
+            },
+            failure: function() {
+                t.fail('Failed to load the order');
+            }
+        })
     }
 
-    var completedOrder = makeOrder({
-        orderStatus: "Completed",
-        authorizationInfo: {
-            amountCollected: 121,
-            captureAmount: 0,
-            totalAmount: 121
-        }
-    });
-
+   
     function setupPanels(details) {
-        var record = makeOrder(details);
         return function(next) {
             t.diag('order status ' + details.orderStatus);
-            Taco.app.viewPort.removeAll(true);
-            m.newPanel = Ext.create(
-                'Taco.view.order.widget.PaymentPanel', { order: record, record: record.payments().getAt(0), width: 500 }
-            );
-            Taco.app.viewPort.add(m.newPanel);
-            m.authorizedPanel = Ext.create(
-                'Taco.view.order.widget.PaymentPanel', { order: record, record: record.payments().getAt(1), width: 500 }
-            );
-            Taco.app.viewPort.add(m.authorizedPanel);
-            m.pendingPanel = Ext.create(
-                'Taco.view.order.widget.PaymentPanel', { order: record, record: record.payments().getAt(2), width: 500 }
-            );
-            Taco.app.viewPort.add(m.pendingPanel);
+            getOrder(function(record) {
+                record.set(details);
+                Taco.app.viewPort.removeAll(true);
+                m.newPanel = Ext.create(
+                    'Taco.view.order.widget.PaymentPanel', { order: record, record: record.payments().getAt(0), width: 500 }
+                );
+                Taco.app.viewPort.add(m.newPanel);
+                m.pendingPanel = Ext.create(
+                    'Taco.view.order.widget.PaymentPanel', { order: record, record: record.payments().getAt(2), width: 500 }
+                );
+                Taco.app.viewPort.add(m.pendingPanel);
+                m.authorizedPanel = Ext.create(
+                    'Taco.view.order.widget.PaymentPanel', { order: record, record: record.payments().getAt(1), width: 500 }
+                );
+                Taco.app.viewPort.add(m.authorizedPanel);
 
-            t.waitForComponentVisible(m.pendingPanel, next);
-        }
+                t.waitForComponentVisible(m.authorizedPanel, next);
+            });
+        }   
+       
     }
 
     t.setOnlyMocks();
 
+    t.simManager().register([
+        {
+            url: '/admin/app/order/list',
+            jsonFile: '/admin/tests/mocks/Mystic1/Orders2Payments.json'
+        }
+    ]);
     t.chain(
+
+        function(next) {
+            t.it("Should have requireable files", function(t) {
+                t.requireOk(
+                    'Taco.model.Order',
+                    'Taco.view.order.subform.Payment',
+                    'Taco.view.order.widget.PaymentPanel',
+                    next
+                );
+            });
+        },
+
         setupPanels({
             orderStatus: "Accepted",
             authorizationInfo: {
@@ -176,7 +102,55 @@
 
             t.ok(m.pendingPanel.captureButton && m.pendingPanel.captureButton.getEl() && m.pendingPanel.captureButton.isDisabled(), "capturebutton exists but disabled");
 
-            next();
+            // displayamount
+            var displayAmountElement;
+            t.ok(m.authorizedPanel.displayAmount && (displayAmountElement = m.authorizedPanel.displayAmount.getEl()), "displayAmount exists and has an element");
+
+            function confirmVisible(name) {
+                t.is(t.cq1('window{isVisible()}'), m.authorizedPanel.actionModal, name + " modal is showing");
+            }
+
+
+
+
+            Taco.app.viewPort.removeAll(true);
+            var r = m.authorizedPanel.order;
+            m.authorizedPanel = Ext.create(
+                    'Taco.view.order.widget.PaymentPanel', { order: r, record: r.payments().getAt(1), width: 500 }
+                );
+            Taco.app.viewPort.add(m.authorizedPanel);
+
+            // actions
+            var chain = [function(next) {
+                t.diag('capture button');
+                t.clickCQ('#captureButton', m.authorizedPanel, function() {
+                    confirmVisible("capture");
+                    m.authorizedPanel.actionModal.destroy();
+                    next();
+                });
+            }, function(next) {
+                t.waitForComponentVisible(m.authorizedPanel, function() {
+                    moreActionsButton = m.authorizedPanel.down('#moreActionsButton');
+                    t.diag("More Actions menu");
+                    next(); 
+                })
+            }].concat(m.authorizedPanel.record.data.availableActions.filter(function(action) { return action !== "CapturePayment"; }).map(function(action) {
+                return function(next) {
+                    t.click(moreActionsButton, function() {
+                        t.waitForComponentVisible(moreActionsButton.menu, function() {
+                            t.click(moreActionsButton.menu.down('#' + action), function() {
+                                t.waitForCQ('window{isVisible()}', function() {
+                                    confirmVisible(action);
+                                    m.authorizedPanel.actionModal.destroy();
+                                    t.waitForComponentVisible(moreActionsButton, next);
+                                });
+                            });
+                        });
+                    });
+                }
+            }));
+
+            t.chain(chain.concat([next]));
 
         },
         setupPanels({
