e349e9901b03b48cfae5d83d42ee7606e4267c2d
Zetlen <CORP\james_zetlen>
2014-07-03 12:39:02 -0500

    fixing crazy collection inheritance bug

--- Mozu.SiteBuilder.UX/BuiltinScripts/SDK/src/utils.js
+++ Mozu.SiteBuilder.UX/BuiltinScripts/SDK/src/utils.js
@@ -45,6 +45,12 @@
         clone: function(obj) {
             return JSON.parse(JSON.stringify(obj)); // cheap copy :)
         },
+        compose: function(first, second) {
+            return function() {
+                first.call(this, arguments);
+                second.call(this, arguments);
+            }
+        },
         flatten: function (obj, into, prefix, separator, depth) {
             if (depth === 0) throw "Cannot flatten circular object.";
             if (!depth) depth = maxFlattenDepth;
@@ -70,15 +76,22 @@
 
             return into;
         },
-        inherit: function (parent, more) {
-            var ApiInheritedObject = function () {
-                if (this.construct) this.construct.apply(this, arguments);
-                parent.apply(this, arguments);
-                if (this.postconstruct) this.postconstruct.apply(this, arguments);
+        inherit: (function(composedMethods) {
+            return function (parent, more) {
+                var ApiInheritedObject = function() {
+                    if (this.construct) this.construct.apply(this, arguments);
+                    parent.apply(this, arguments);
+                    if (this.postconstruct) this.postconstruct.apply(this, arguments);
+                },
+                parentObject = new parent();
+                for (var i = 0; i < composedMethods.length; i++) {
+                    if (parentObject[composedMethods[i]] && more[composedMethods[i]])
+                        more[composedMethods[i]] = utils.compose(parentObject[composedMethods[i]], more[composedMethods[i]]);
+                }
+                ApiInheritedObject.prototype = utils.extend(parentObject, more);
+                return ApiInheritedObject;
             };
-            ApiInheritedObject.prototype = utils.extend(new parent(), more);
-            return ApiInheritedObject;
-        },
+        })(['construct','postconstruct']),
         map: function (arr, fn, scope) {
             var newArr = [], len = arr.length;
             scope = scope || window;
