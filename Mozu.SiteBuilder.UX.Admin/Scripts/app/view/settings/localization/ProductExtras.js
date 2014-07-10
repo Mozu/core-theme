c9920363bcf596bc04bf1a238e5f5b5fd6db2d97
Greg Murray <CORP\greg_murray>
2014-07-10 11:15:04 -0500

    moved getColumn configs to individual classes.  Added more fake data.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/settings/localization/ProductExtras.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/settings/localization/ProductExtras.js
@@ -13,7 +13,91 @@ Ext.define('Taco.view.settings.localization.ProductExtras', {
     createRoute: 'localization/zonescreate',
     editorRoute: 'localization/zonesedit',
    
-    store: { type: 'Taco.store.LocalizedProductExtras' }
+    store: { type: 'Taco.store.LocalizedProductExtras' },
+
+    // override this method and adjust the columns if your need a grid with a subset of columns;
+    getColumnConfig: function () {
+        var me = this;
+        return [
+            {
+                xtype: 'gridcolumn',
+                dataIndex: 'code',
+                text: 'Code',
+                hideable: false,
+
+                minWidth: 300
+                //renderer: function (value, metaData, record, rowIndex, colIndex, store) {
+                //    return '<a href="#" class="taco-launch-editor">' + (value + '</a>');
+                //}
+            }, {
+                xtype: 'gridcolumn',
+                dataIndex: 'description',
+                text: 'Description',
+                flex: 1,
+                width: 150,
+
+            }, {
+                xtype: 'taco.menucolumn',
+                text: 'Actions',
+                flex: 1,
+                menuItems: [
+                    {
+                        text: 'Add Customers',
+                        requiredBehaviors: {
+                            model: 'Taco.model.CustomerAccount',
+                            behavior: 'update'
+                        },
+                        menuColumnHandler: function (item, eventData) {
+                            var modal = Ext.create('Taco.view.customers.Segments.AddRemoveModal',
+                            {
+                                segmentId: eventData.record.getId(),
+                                batchMethod: 'add',
+                                segmentCode: eventData.record.get('code')
+                            });
+                        }
+                    }, {
+                        text: 'Remove Customers',
+                        requiredBehaviors: {
+                            model: 'Taco.model.CustomerAccount',
+                            behavior: 'update'
+                        },
+                        menuColumnHandler: function (item, eventData) {
+                            var modal = Ext.create('Taco.view.customers.Segments.AddRemoveModal',
+                            {
+                                segmentId: eventData.record.getId(),
+                                batchMethod: 'remove',
+                                segmentCode: eventData.record.get('code')
+                            });
+                        }
+                    }, {
+                        text: 'Delete Segment',
+                        requiredBehaviors: {
+                            model: 'Taco.model.CustomerAccount',
+                            behavior: 'update'
+                        },
+                        menuColumnHandler: function (item, eventData) {
+                            var modal = Ext.create('Taco.core.ux.window.Alert', {
+                                autoShow: true,
+                                closeAction: 'destroy',
+                                items: [
+                                    {
+                                        html: 'Do you really want to Delete Segment: ' + eventData.record.get('code')
+                                    }
+                                ],
+                                listeners: {
+                                    confirm: function () {
+                                        me.store.remove([eventData.record]);
+                                        me.store.sync();
+                                    }
+                                }
+                            });
+
+                        }
+                    }
+                ]
+            }
+        ];
+    }
 });
 
 
---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/settings/localization/subform/Attribute.js
@@ -0,0 +1,90 @@
+﻿///**
+// * @class Taco.view.product.subform.Bundle 
+// *
+// */
+
+//Ext.define('Taco.view.settings.localization.subform.Attribute', {
+//    extend: 'Taco.view.product.subform.Subform',
+//    requires: [
+//        'Taco.view.settings.localization.widget.AttributeGrid'
+//    ],
+//    itemId: 'bundleSubForm',
+//    title: 'Bundle Items',
+//    margin: '20 0',
+//    initComponent: function () {
+//        var me = this,
+//            readOnly,
+//            requiredContent,
+//            visable;
+
+//        me.tools = [{
+//            xtype: 'button',
+//            ui: "action-primary",
+//            scale: "medium",
+//            margin: "0 0 20, 0",
+//            text: "Add",
+//            handler: function() {
+//                this.productBundleGrid.addItem();
+//            },
+//            scope: me
+//        }];
+
+//        this.defaults = {
+//            width: 200,
+//            product: this.product,
+//            productInCatalogInfo: this.productInCatalogInfo,
+//            labelAlign: 'top',
+//            labelSeparator: '',
+//            persistChangesToModel: true
+//        };
+
+//        this.record = this.product;
+
+//        this.productBundleGrid = Ext.create('Taco.view.product.widget.ProductBundleGrid', {
+//            product : me.product
+//        });
+
+//        readOnly = this.isEdit() || !(this.isSingleSite || this.isGlobal);
+//        visable = !readOnly || this.isEdit();
+//        requiredContent = this.isSingleSite || this.isGlobal;
+        
+//        this.items = [
+//            this.productBundleGrid
+//        ];
+
+//        this.callParent(arguments);
+
+        
+
+//        me.mon(me.productBundleGrid.store, 'datachanged', me.onStoreDataChanged, me);
+        
+
+//    },
+    
+//    onStoreDataChanged: function (bundleStore) {
+//        var me = this,
+//            productForm = me.up("productform");
+        
+//        // when the contents of the bundle store chanes, we need to notifiy the other subForms of the changes so that they can react. Specifically, the shipping and price area will update;
+//        //todo change this to fire on the record instead of the productForm
+//        if (productForm) {
+//            productForm.fireEvent('bundleItemChange');
+//        }
+//    },
+
+//    // Called before the updateTask of Taco.core.ux.form.Form is executed; Return false to cancel the save; Can be used to manipulate the record data prior to saving;
+//    beforeSave: function () {
+//        /*
+//        var me = this;
+//        // need to serialize the store into jsons for persistance
+//        var store = this.productBundleGrid.store;
+//        var data = [];
+//        store.each(function(record) {
+//            data.push(Ext.clone(record.data));
+//        });
+        
+//        this.product.set('bundledProducts', data);
+//        */
+//        return true;
+//    }
+//});
