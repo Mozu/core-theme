d2f4a84ff6884af7da265807094246a8953b94e5
Greg Murray <CORP\greg_murray>
2014-07-22 18:36:37 -0500

    Fixed attribute updates.  Quick filter wasn't working for hasRecords as they implemented it as exists (and didnt' tell).  Search WIP.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/settings/localization/util/Locales.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/settings/localization/util/Locales.js
@@ -1,51 +1,68 @@
-﻿///** 
-// * @class Taco.view.settings.localization.util.LocaleUtil
-// * common localization methods 
-// * 
-// */
-//Ext.define('Taco.view.settings.localization.util.LocaleUtil', {
-//    singleton:true,
+﻿/** 
+ * @class Taco.view.settings.localization.util.Locales
+ * @singleton
+ * common localization methods
+ */
+Ext.define('Taco.view.settings.localization.util.Locales', {
+    singleton:true,
    
-//    getSupported: function () {
-//        var ctx = Taco.app.context.getCurrentContext(),
-//            ctxType = (!ctx) ? '' : ctx.contextType,
-//            mc = Taco.app.context.getMasterCatalog(),
-//            excludeDefaultLocale = true,
-//            supportedLocales = [];
-//        if (ctxType === 'm' && mc) {
-//            supportedLocales = mc.getSupportedLocales(excludeDefaultLocale);
-//        }
-//        else if (ctxType === 'c') {
-//            var cat = Taco.app.context.getCatalog();
-//            if (cat) {
-//                supportedLocales.push(cat.localeCode);
-//            }
-//        }
-//        return supportedLocales;
-//    },
+    getSupported: function () {
+        var ctx = Taco.app.context.getCurrentContext(),
+            ctxType = (!ctx) ? '' : ctx.contextType,
+            mc = Taco.app.context.getMasterCatalog(),
+            excludeDefaultLocale = true,
+            supportedLocales = [];
+        if (ctxType === 'm' && mc) {
+            supportedLocales = mc.getSupportedLocales(excludeDefaultLocale);
+        }
+        else if (ctxType === 'c') {
+            var cat = Taco.app.context.getCatalog();
+            if (cat) {
+                supportedLocales.push(cat.localeCode);
+            }
+        }
+        return supportedLocales;
+    },
 
-//    getQuickFilterData: function () {
-//        // need to 
-//        var supportedLocales = this.getSupportedLocales(),
-//              quickFilters = [
-//                              [{ hasRecord: false }, 'Missing Translation'],
-//                              [{ hasRecord: true }, 'Has Translation'],
-//                              [{}, 'All Records']
-//              ];
+    getQuickFilterData: function () {
+        // need to 
+        var supportedLocales = Taco.view.settings.localization.util.Locales.getSupported(),
+              quickFilters = [
+                              [{ exists: false }, 'Missing Translation'],
+                              [{ exists: true }, 'Has Translation'],
+                              [{}, 'All Records']
+              ];
 
-//        Ext.Array.each(supportedLocales, function (loc) {
-//            quickFilters.push([{ localeNotExists: loc }, 'Missing ' + loc]);
-//            quickFilters.push([{ localeExists: loc }, 'Has ' + loc]);
-//        });
-//        return quickFilters;
-//    },
+        Ext.Array.each(supportedLocales, function (loc) {
+            quickFilters.push([{ localeNotExists: loc }, 'Missing ' + loc]);
+            quickFilters.push([{ localeExists: loc }, 'Has ' + loc]);
+        });
+        return quickFilters;
+    },
 
-//    getAdvancedSearchConfig: function () {
-//        return {
-//            advancedFormCls: 'Taco.view.settings.localization.AdvancedSearchLocaleForm',
+    getAdvancedSearchFilterData: function () {
+        // need to 
+        var supportedLocales = Taco.view.settings.localization.util.Locales.getSupported(),
+              quickFilters = [{ id: { exists: false }, name: 'Missing Translation' },
+                  { id: { exists: true }, name: 'Has Translation' }
+                              //[{ hasRecord: false }, 'Missing Translation'],
+                              //[{ hasRecord: true }, 'Has Translation'],
+                              //[{}, 'All Records']
+              ];
 
-//            quickFilterData: this.getQuickFilterData()
-//                //Taco.view.settings.localization.util.Locales.getQuickFilterData()
-//        };
-//    }
-//});
+        //Ext.Array.each(supportedLocales, function (loc) {
+        //    quickFilters.push([{ localeNotExists: loc }, 'Missing ' + loc]);
+        //    quickFilters.push([{ localeExists: loc }, 'Has ' + loc]);
+        //});
+        return quickFilters;
+    },
+
+    getAdvancedSearchConfig: function () {
+        return {
+            advancedFormCls: 'Taco.view.settings.localization.AdvancedSearchLocaleForm',
+
+            quickFilterData: Taco.view.settings.localization.util.Locales.getQuickFilterData()
+                //Taco.view.settings.localization.util.Locales.getQuickFilterData()
+        };
+    }
+});
