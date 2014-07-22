5c21dfd2ab9bc73a63f8afd689d940b857c32fb5
Zetlen <CORP\james_zetlen>
2014-07-22 10:29:34 -0500

    adding base document collection

--- Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/modules/models-documents.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/modules/models-documents.js
@@ -1,6 +1,6 @@
 ﻿define(
-    ["modules/backbone-mozu", "hyprlivecontext"],
-    function (Backbone, context) {
+    ["modules/backbone-mozu", "shim!vendor/underscore>_", "hyprlivecontext"],
+    function (Backbone, _, PagingMixin, context) {
         
         var locals = context.locals;
 
@@ -10,10 +10,51 @@
                 // attributes available through this.get, theme settings and sitecontext available through "locals.themeSettings" and "locals.siteContext"
                 return "/cms/" + this.get('id');
             }
-        })
+        }),
+
+        DocumentCollection = Backbone.MozuModel.extend(_.extend({
+            validation: {
+                pageSize: { min: 1 },
+                pageCount: { min: 1 },
+                startIndex: { min: 0 }
+            },
+            dataTypes: {
+                pageSize: Backbone.MozuModel.DataTypes.Int,
+                pageCount: Backbone.MozuModel.DataTypes.Int,
+                startIndex: Backbone.MozuModel.DataTypes.Int,
+                totalCount: Backbone.MozuModel.DataTypes.Int,
+            },
+            relations: {
+                items: Backbone.Collection.extend({
+                    model: Document
+                })
+            },
+            getFilter: function() {
+                // unimplemented in default collection
+            },
+            getQuery: function() {
+                // unimplemented in default collection
+            },
+            buildPagingRequest: function() {
+                var conf = this.baseRequestParams ? _.clone(this.baseRequestParams) : {},
+                pageSize = this.get("pageSize"),
+                startIndex = this.get("startIndex"),
+                filter = this.getFilter(),
+                query = this.getQuery();
+                conf.pageSize = pageSize;
+                if (startIndex) conf.startIndex = startIndex;
+                if (filter) conf.filter = filter;
+                if (query) conf.query = this.query;
+                return conf;
+            },
+            initialize: function() {
+                this.lastRequest = this.buildPagingRequest();
+            }
+        }, PagingMixin));
 
         return {
-            Document: Document
+            Document: Document,
+            DocumentCollection: DocumentCollection
         }
 
 });
