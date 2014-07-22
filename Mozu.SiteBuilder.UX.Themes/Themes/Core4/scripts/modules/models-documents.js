3cf4fa351aca369ae783c71f2be03c7d1c742f7d
Zetlen <CORP\james_zetlen>
2014-07-22 10:02:16 -0500

    adding models-documents for thom

---
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core4/scripts/modules/models-documents.js
@@ -0,0 +1,19 @@
+﻿define(
+    ["modules/backbone-mozu", "hyprlivecontext"],
+    function (Backbone, context) {
+        
+        var locals = context.locals;
+
+        var Document = Backbone.MozuModel.extend({
+            helpers: ['url'],
+            url: function() {
+                // attributes available through this.get, theme settings and sitecontext available through "locals.themeSettings" and "locals.siteContext"
+                return "/cms/" + this.get('id');
+            }
+        })
+
+        return {
+            Document: Document
+        }
+
+});
