c861c799f481f43e2049ef95220c9304a63abd06
Greg Murray <CORP\greg_murray>
2014-08-01 13:08:37 -0500

    Added Token generator with support for TenantId

---
+++ Mozu.SiteBuilder.IntegrationTests/Admin/Api/BirstTokenGeneratorTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Mozu.Core;
+using Mozu.Core.Exceptions;
+using Mozu.Core.Settings;
+using Mozu.SiteBuilder.UX.Admin.Api;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace Mozu.SiteBuilder.IntegrationTests.Admin.Api
+{
+    [TestFixture]
+    public class BirstTokenGeneratorTest
+    {
+        private const string TOKEN_KEY = "BirstTokenGeneratorUrl";
+        private const string SSO_KEY = "BirstSsoUrl";
+        private const string USER_KEY = "BirstUserName";
+        private const string SPACE_ID_KEY = "BirstSpaceId";
+        private const string SSO_PASSWORD_KEY = "BirstSsoPassword";
+
+        [TestCase("happy path case", 9581)]
+        public async Task It_Should_Return_Token_When_Calling_Birst_Token_Generator(string scenario, int tenantId)
+        {
+            //arrange
+            var apiContext = SubstituteApiContext(tenantId);
+            var sut = new BirstTokenGenerator(apiContext, new MozuSettings());
+
+            //act
+            var actual = await sut.GenerateToken();
+
+            //assert
+            Assert.That(actual, Is.Not.Null, scenario);
+        }
+
+        [TestCase("bad URL exception test", 10)]
+        public void It_Should_Throw_Unexpected_Exception_When_Given_An_Invalid_Url(string scenario, int tenantId)
+        {
+            //arrange
+            var settings = Substitute.For<ISettings>();
+            settings.AppSettings(TOKEN_KEY).Returns("http://thisIsAnInvalidUrlForTestingOnly.mozu.com/");
+            settings.AppSettings(SSO_KEY).Returns("3234234");
+            settings.AppSettings(USER_KEY).Returns("testy");
+            settings.AppSettings(SPACE_ID_KEY).Returns("3234234");
+            settings.AppSettings(SSO_PASSWORD_KEY).Returns("3234234");
+            var apiContext = SubstituteApiContext(tenantId);
+            var sut = new BirstTokenGenerator(apiContext, settings);
+
+            //act
+            Assert.Throws<VaeUnexpectedErrorException>(
+                   async () => await sut.GenerateToken(), scenario);
+
+        }
+
+        private IApiContext SubstituteApiContext(int tenantId)
+        {
+            var apiContext = Substitute.For<IApiContext>();
+            apiContext.TenantId.Returns(tenantId);
+            return apiContext;
+        }
+
+    }
+}
---
+++ Mozu.SiteBuilder.UX.Admin/Api/BirstTokenGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Mozu.Core;
+using Mozu.Core.Exceptions;
+using Mozu.Core.Settings;
+
+namespace Mozu.SiteBuilder.UX.Admin.Api
+{
+    public class BirstTokenGenerator
+    {
+        private const string TOKEN_URI_KEY = "BirstTokenGeneratorUrl";
+        private const string USER_KEY = "BirstUserName";
+        private const string PASSWORD_KEY = "BirstSsoPassword";
+        private const string SPACE_KEY = "BirstSpaceId";
+
+        private readonly ISettings _settings;
+        private readonly IApiContext _apiContext;
+
+        public BirstTokenGenerator(IApiContext apiContext, ISettings settings)
+        {
+            _settings = settings;
+            _apiContext = apiContext;
+        }
+
+        public async Task<string> GenerateToken()
+        {
+            var tokenGenUri = GetBirstTokenGeneratorUri(_apiContext.TenantId);
+            var httpClient = new HttpClient(new HttpClientHandler());
+            HttpResponseMessage responseMsg = await httpClient.PostAsync(tokenGenUri, new HttpMessageContent(new HttpResponseMessage()));
+            if (responseMsg.IsSuccessStatusCode)
+            {
+                return await responseMsg.Content.ReadAsStringAsync();
+            }
+            throw new VaeUnexpectedErrorException(string.Format("Unexpected error with status code:{0}", responseMsg.StatusCode));
+        }
+
+        private string GetBirstTokenGeneratorUri(int tenantId)
+        {
+            return string.Format("{0}?username={1}&ssopassword={2}&BirstSpaceId={3}&birst.sessionVars=tenantId%3D{4}",
+                _settings.AppSettings(TOKEN_URI_KEY),
+                _settings.AppSettings(USER_KEY),
+                _settings.AppSettings(PASSWORD_KEY),
+                _settings.AppSettings(SPACE_KEY),
+                tenantId
+                );
+        }
+    }
+}
