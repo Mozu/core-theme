fb6efc629edd2ff5e32e2dd0d8b9434a7133113b
Jimmy Sanford <CORP\Jimmy_Sanford>
2014-08-14 17:32:16 -0500

    returnable items grid shows more quantities

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/widget/ReturnableItemGrid.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/widget/ReturnableItemGrid.js
@@ -15,7 +15,8 @@ Ext.define('Taco.view.order.widget.ReturnableItemGrid', {
         sortable: false,
         resizable: false,
         menuDisabled: false,
-        width: 150
+        minWidth: 100,
+        flex: 1
     }, {
         dataIndex: 'productName',
         text: 'Products',
@@ -23,6 +24,7 @@ Ext.define('Taco.view.order.widget.ReturnableItemGrid', {
         sortable: false,
         resizable: false,
         menuDisabled: true,
+        minWidth: 100,
         flex: 1,
         renderer: function(val, md, record) {
             var parentBundleName = record.get('parentBundleName');
@@ -35,7 +37,7 @@ Ext.define('Taco.view.order.widget.ReturnableItemGrid', {
         sortable: false,
         resizable: false,
         menuDisabled: true,
-        width: 150,
+        width: 100,
         editor: {
             xtype: 'combobox',
             allowOnlyWhitespace: false,
@@ -50,7 +52,7 @@ Ext.define('Taco.view.order.widget.ReturnableItemGrid', {
         sortable: false,
         resizable: false,
         menuDisabled: true,
-        width: 150,
+        width: 100,
         editor: {
             xtype: 'combobox',
             showBorder: true,
@@ -60,21 +62,29 @@ Ext.define('Taco.view.order.widget.ReturnableItemGrid', {
             store: Taco.model.Return.getValidReasons()
         }
     }, {
-    //    dataIndex: 'quantity',
-    //    text: 'Qty Fulfilled',
-    //    draggable: false,
-    //    sortable: false,
-    //    resizable: false,
-    //    menuDisabled: true,
-    //    width:150
-    //}, {
+        dataIndex: 'quantityOrdered',
+        text: 'Qty Ordered',
+        draggable: false,
+        sortable: false,
+        resizable: false,
+        menuDisabled: true,
+        width:100
+    }, {
+        dataIndex: 'quantityReturned',
+        text: 'Qty Returned',
+        draggable: false,
+        sortable: false,
+        resizable: false,
+        menuDisabled: true,
+        width:100
+    }, {
         dataIndex: 'quantity',
         text: 'Qty to Return',
         draggable: false,
         sortable: false,
         resizable: false,
         menuDisabled: true,
-        width: 150,
+        width: 100,
         editor: {
             xtype: 'numberfield',
             showBorder:true,
@@ -100,7 +110,6 @@ Ext.define('Taco.view.order.widget.ReturnableItemGrid', {
     },
     
     initComponent: function () {
-        
         this.store = this.getReturnableItemsStore();
 
         this.plugins.push(Ext.create('Ext.grid.plugin.CellEditing', {
@@ -127,45 +136,33 @@ Ext.define('Taco.view.order.widget.ReturnableItemGrid', {
                 fn: function () { this.addCls('returnable-items-grid'); }
             }
         });
-    },
 
-    getReturnedItemQuantityByProductCode: function() {
-        var returnedItemsByProductCode = {};
-        this.returnsStore.each(function(rtn) {
-            if (rtrn.get('status') !== Taco.model.Return.constants.statuses.CANCELLED) {
-                Ext.Array.each(rtn.get('items'), function(item) {
-                    if (!(item.productCode in returnedItemsByProductCode)) {
-                        returnedItemsByProductCode[item.productCode] = 0;
-                    }
-                    returnedItemsByProductCode[item.productCode] += item.quantity;
-                });
+        this.mon(this.order.getReturnsStore(), {
+            load: {
+                scope: this,
+                fn: 'addReturnableItems'
             }
         });
-        return returnedItemsByProductCode;
     },
 
-    getReturnableItemsStore: function() {
+    addReturnableItems: function (store, records) {
+        var returnedItemQuantities = {};
+        var eligibleItems = [];
 
-        var returnedItemQuantities = this.getReturnedItemQuantityByProductCode(),
-            allReturned = function(orderItem) {
-                return orderItem.productCode in returnedItemQuantities && returnedItemQuantities[orderItem.productCode] >= orderItem.quantity;
+        // track the returned quantities of each item in the order
+        Ext.Array.each(records, function (record) {
+            if (record.get('status') !== Taco.model.Return.constants.statuses.CANCELLED) {
+                Ext.Array.each(record.get('items'), function (item) {
+                    if (!(item.orderItemId in returnedItemQuantities)) {
+                        returnedItemQuantities[item.orderItemId] = 0;
+                    }
+                    returnedItemQuantities[item.orderItemId] += item.quantity;
+                });
             }
+        }, this);
 
-        var eligibleItems = [],
-            addItem = function(item, orderItemId, parentBundleName) {
-                if (!allReturned(item)) {
-                    eligibleItems.push({
-                        orderItemId: parentBundleName ? null : orderItemId,
-                        productCode: item.productCode,
-                        productName: item.productName || item.name,
-                        quantity: 0,
-                        parentBundleName: parentBundleName,
-                        parentItemId: parentBundleName && orderItemId
-                    });
-                }
-            }
-
-        Ext.Array.each(this.order.get('items'), function(orderItem) {
+        // add items to the returnable items store
+        Ext.Array.each(this.order.get('items'), function (orderItem) {
             if (orderItem.bundledProducts && orderItem.bundledProducts.length > 0) {
                 Ext.Array.each(orderItem.bundledProducts, function(item) {
                     addItem(item, orderItem.id, orderItem.productName);
@@ -175,6 +172,32 @@ Ext.define('Taco.view.order.widget.ReturnableItemGrid', {
             }
         });
 
+        this.store.add(eligibleItems);
+
+        // adds an item unless it has been fully returned
+        function addItem (item, orderItemId, parentBundleName) {
+            console.log(item);
+            if (!allReturned(item)) {
+                eligibleItems.push({
+                    orderItemId: parentBundleName ? null : orderItemId,
+                    productCode: item.productCode,
+                    productName: item.productName || item.name,
+                    quantity: 0,
+                    quantityOrdered: item.quantity,
+                    quantityReturned: returnedItemQuantities[orderItemId],
+                    parentBundleName: parentBundleName,
+                    parentItemId: parentBundleName && orderItemId
+                });
+            }
+        }
+
+        // checks if an item has been fully returned
+        function allReturned (orderItem) {
+            return orderItem.id in returnedItemQuantities && returnedItemQuantities[orderItem.id] >= orderItem.quantity;
+        }
+    },
+
+    getReturnableItemsStore: function () {
         return Ext.create('Ext.data.Store', {
             fields: [
                 { type: 'string',  name: 'productCode' },
@@ -184,10 +207,12 @@ Ext.define('Taco.view.order.widget.ReturnableItemGrid', {
                 { type: 'string',  name: 'returnType', defaultValue: 'Refund' },
                 { type: 'string',  name: 'reason', defaultValue: 'Damaged' },
                 { type: 'number',  name: 'quantity', defaultValue: 0 },
+                { type: 'number',  name: 'quantityOrdered' },
+                { type: 'number',  name: 'quantityReturned', defaultValue: 0 },
                 { type: 'string', name: 'orderItemId' },
                 { type: 'string', name: 'parentItemId'}
             ],
-            data: eligibleItems
+            data: []
         });
     },
 
