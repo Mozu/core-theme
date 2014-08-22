4895b5303d3ed47a21a17befbd8a510324375bba
Zetlen <CORP\james_zetlen>
2014-08-22 07:16:52 -0500

    replaced discountedsubtotal with discountedtotal to reflect order level discounts in total panel

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/widget/OrderTotalPanel.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/widget/OrderTotalPanel.js
@@ -263,7 +263,7 @@ Ext.define('Taco.view.order.widget.OrderTotalPanel', {
         
         '<tr class="subtotalrow">',
             '<td class="{tdCls}"><div class="{tdInnerCls}">SubTotal</div></td>',
-            '<td class="subtotalcell {tdCls}"><div class="{tdInnerCls}">{[this.getCurrencyFormat(values.discountedSubtotal)]}</div></td>',
+            '<td class="subtotalcell {tdCls}"><div class="{tdInnerCls}">{[this.getCurrencyFormat(values.discountedTotal)]}</div></td>',
         '</tr>',
             
         '<tpl for="orderDiscounts">',
