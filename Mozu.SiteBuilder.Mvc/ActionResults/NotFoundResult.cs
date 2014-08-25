efaa38dfe1970cdc023e68f4dca5e2b36ff7fb43
Sparkel Phipps <CORP\Thomas_Phipps>
2014-08-25 17:01:01 -0500

    Bug 33807:Product images are not linked in Storefront and Admin

---
+++ Mozu.SiteBuilder.Mvc/ActionResults/NotFoundResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Web;
+using Mozu.SiteBuilder.Mvc.ViewEngine;
+
+namespace Mozu.SiteBuilder.Mvc.ActionResults
+{
+    public class NotFoundResult : ActionResult 
+    {
+ 
+
+        public override void ExecuteResult(HttpRequestMessage requestMessage)
+        {
+            HttpResponseBase response = requestMessage.HttpContext().Response;
+            response.StatusCode = 404;
+        }
+    }
+}
