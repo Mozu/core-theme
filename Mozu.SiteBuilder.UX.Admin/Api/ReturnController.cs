e7208ba84685dc4d5d2c814814059a9948c5d523
Foster Hersey <foster.hersey@gmail.com>
2014-08-28 22:30:10 -0500

    Made multiple refunds play nicer and split refund vs add store credit for a return.

--- Mozu.SiteBuilder.UX.Admin/Api/ReturnController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ReturnController.cs
@@ -10,6 +10,7 @@ using Mozu.Core.Settings;
 using Mozu.Customer.Contracts.Clients;
 using Mozu.SiteBuilder.UX.Admin.Api.Models;
 using Mozu.SiteBuilder.UX.Admin.Api.Models.Returns;
+using DCp = Mozu.CommerceRuntime.Contracts.Payments;
 using DCr = Mozu.CommerceRuntime.Contracts.Returns;
 using DCu = Mozu.Customer.Contracts;
 
@@ -130,34 +131,68 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
         }
 
 
-        [HttpPostRoute(UriTemplate = "paymentActions")]
-        public async Task<Response<List<Return>>> CreatePaymentActionsForReturn(List<PaymentAction> actions)
+        public class RefundPaymentsArgs
         {
-            var retList = new List<Return>();
-            foreach (var action in actions)
-            {
-                var dcPaymentAction = new CommerceRuntime.Contracts.Payments.PaymentAction()
-                {
-                    ActionName = "CreditPayment",
-                    //ReferenceSourcePaymentId = action.paymentId,
-                    Amount = action.amount
+            public string ReturnId { get; set; }
+            public List<RefundPaymentAction> Refunds { get; set; }
 
-                };
-                if (action.paymentType == "CreditCard")
-                {
-                    dcPaymentAction.ReferenceSourcePaymentId = action.paymentId;
-                }
-                else if (action.paymentType == "StoreCredit")
-                {
-                    dcPaymentAction.NewBillingInfo = new CommerceRuntime.Contracts.Payments.BillingInfo() { PaymentType = "StoreCredit" };
-                }
+            public class RefundPaymentAction {
+                public string OrderPaymentId { get; set; }
+                public decimal Amount { get; set; }
+            }
+        }
 
-                var dcRma = (await _returnWebApiClient.CreatePaymentActionForReturn(action.returnId, dcPaymentAction)).ReadAsSync();
-                retList.Add(Mapper.Map<Return>( dcRma ));
+        [HttpPostRoute(UriTemplate = "refundPayments")]
+        public async Task<Response<Return>> RefundPayments(RefundPaymentsArgs args)
+        {
+            var existingPayments = (await _returnWebApiClient.GetPayments(args.ReturnId)).ReadAsSync();
+
+            foreach(var refund in args.Refunds) {
+                if (existingPayments != null && existingPayments.Items.Any(i => i.Id == refund.OrderPaymentId)) {
+                    var ep = existingPayments.Items.First(i => i.Id == refund.OrderPaymentId);
+
+                    var dcPaymentAction = new DCp.PaymentAction {
+                        ActionName = DCp.PaymentAction.PaymentActionNameConst.CREDIT_PAYMENT,
+                        Amount = refund.Amount
+                    };
+
+                    (await _returnWebApiClient.PerformPaymentActionForReturn(args.ReturnId, refund.OrderPaymentId, dcPaymentAction)).ReadAsSync();
+                }
+                else {
+                    var dcPaymentAction = new DCp.PaymentAction {
+                        ActionName = DCp.PaymentAction.PaymentActionNameConst.CREDIT_PAYMENT,
+                        ReferenceSourcePaymentId = refund.OrderPaymentId,
+                        Amount = refund.Amount
+                    };
+
+                    (await _returnWebApiClient.CreatePaymentActionForReturn(args.ReturnId, dcPaymentAction)).ReadAsSync();
+                }
             }
 
+            var dcReturn = (await _returnWebApiClient.GetReturn(args.ReturnId)).ReadAsSync();
 
-            return List2(retList);
+            return Single2(Mapper.Map<Return>(dcReturn));
+        }
+
+
+        public class CreateStoreCreditArgs
+        {
+            public string ReturnId { get; set; }
+            public decimal Amount { get; set; }
+        }
+
+        [HttpPostRoute(UriTemplate = "createStoreCredit")]
+        public async Task<Response<Return>> RefundPayments(CreateStoreCreditArgs args) {
+            var dcPaymentAction = new DCp.PaymentAction {
+                ActionName = DCp.PaymentAction.PaymentActionNameConst.CREDIT_PAYMENT,
+                Amount = args.Amount,
+                NewBillingInfo = new DCp.BillingInfo {
+                    PaymentType = DCp.PaymentTypeConst.STORE_CREDIT
+                }
+            };
+
+            var dcReturn = (await _returnWebApiClient.CreatePaymentActionForReturn(args.ReturnId, dcPaymentAction)).ReadAsSync();
+            return Single2(Mapper.Map<Return>(dcReturn));
         }
 
         [HttpPostRoute(UriTemplate = "edit")]
