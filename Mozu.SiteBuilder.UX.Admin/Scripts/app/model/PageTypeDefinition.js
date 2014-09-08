e2ab0ff00c2f2bb7a554c097eb2996f6c0e97a2d
Sparkel Phipps <CORP\Thomas_Phipps>
2014-09-08 17:08:55 -0500

    Bug 34792:Product Page Setting Editor for Bundles error

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/model/PageTypeDefinition.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/model/PageTypeDefinition.js
@@ -6,7 +6,7 @@
     Ext.define('Taco.model.PageTypeDefinition', {
 
         extend: 'Taco.core.data.Model',
-        fields: ['id', 'title', 'template', 'entityType',  'documentTypeFQN', 'userCreatable', 'properties', 'zones','widgets','customEditors',
+        fields: ['id', 'title', 'template', 'entityType',  'documentTypeFQN', 'userCreatable', 'properties', 'zones','widgets','customEditor',
             {
                 name: 'isDefault',
                 convert: function fullName(v, record) {
