f4c577cc58b62d487638ee206f8749ab0001e4a4
Simeon Kessler <CORP\simeon_kessler>
2014-09-16 16:59:10 -0500

    Ui for change of product code. initial drop.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/Edit.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/Edit.js
@@ -2,6 +2,7 @@
     extend: 'Taco.core.ux.form.FullEditor',
     requires: [
         'Taco.view.product.Form',
+        'Taco.view.product.widget.productCode.Modal',
         'Ext.button.Button'
     ],
     
@@ -83,11 +84,17 @@
                     itemId: 'delete',
                     text: 'Delete',
                     handler: Ext.bind(me.destroyRecord, me)
-                }],
+                }, {
+                    itemId: 'changeProductCode',
+                    text: 'Change Product Code',
+                    handler: Ext.bind(me.changeProductCode,me)
+                }
+                ],
                 listeners: {
                     show: function (menu) {
                         var previewItem = menu.items.get('preview'),
                             liveItems = menu.items.get('live'),
+                            changeProductCodeItem = menu.items.get('changeProductCode'),
                             previewMenu,
                             liveMenu,
                             previewSites = [],
@@ -122,6 +129,14 @@
                                 liveMenu.removeAll();
                                 liveMenu.add(liveSites);
                             }
+
+                            // check to see if the product is phantom if so disable the change product code option
+                            if (me.record.phantom) {
+                                changeProductCodeItem.hide();
+                            } else {
+                                changeProductCodeItem.show();
+                            }
+
                            
                         }
                     },
@@ -243,5 +258,37 @@
             },
             scope: this
         });
+    },
+
+
+    changeProductCode: function () {
+        var me = this,
+            focusEl = me.down("#moreButton");
+        
+        
+
+        var productTypeId = me.record.get('productTypeId'),
+            productType = this.productTypeStore.getById(productTypeId);
+
+
+        Ext.create("Taco.view.product.widget.productCode.Modal", {
+            product: me.record,
+            productType: productType,
+            //productType: me.productType,
+            listeners: {
+                'aftersaveclose': function () {                    
+                    // need to update the view since the product codes have changed;
+                     
+                    // need to pass focus back to the more menu button
+                    focusEl.focus();
+                },
+                'aftercancelclose': function () {                    
+                    // need to pass focus back to the more menu button
+                    focusEl.focus();
+                },
+                scope:me
+            }
+        });
+
     }
 });
