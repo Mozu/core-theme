ef6a33722c246fd87bd91967b6b1d59349f894f3
Zetlen <james_zetlen@volusion.com>
2014-09-18 22:18:42 -0500

    removed tfs checkout tasks from all gruntfiles

--- Mozu.SiteBuilder.UX.Themes/Themes/Core4/Gruntfile.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core4/Gruntfile.js
@@ -43,11 +43,6 @@
                 }
             }
         },
-        tfscheckout: {
-            main: {
-                dir: 'compiled'
-            }
-        },
         zubat: {
             main: {
                 dir: '.',
@@ -91,8 +86,6 @@
 
     ['grunt-jsonlint', 'grunt-contrib-jshint', 'grunt-contrib-watch'].forEach(grunt.loadNpmTasks);
     grunt.loadTasks('./tasks/');
-    grunt.registerTask('default', ['jsonlint', 'jshint', 'tfscheckout', 'zubat']);
-    grunt.registerTask('notfs', ['jsonlint', 'jshint', 'zubat']);
-    grunt.registerTask('release', ['jsonlint', 'jshint', 'tfscheckout', 'zubat', 'setver']);
-    grunt.registerTask('releasenotfs', ['jsonlint', 'jshint', 'tfscheckout', 'zubat', 'setver']);
+    grunt.registerTask('default', ['jsonlint', 'jshint', 'zubat']);
+    grunt.registerTask('release', ['jsonlint', 'jshint', 'zubat', 'setver']);
 };
