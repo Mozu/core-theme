d48e41b888472587bbb0e175210e4c3f2477fc9f
Simeon Kessler <simeon_kessler@volusion.com>
2014-10-02 15:07:15 -0500

    duplicate feature initial checkin

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Index.js
@@ -76,6 +76,21 @@ Ext.define('Taco.view.category.Index', {
                         }, 1, this);
 
                     }
+                },
+                {
+                    text: 'Duplicate',
+                    requiredBehaviors: {
+                        model: 'Taco.model.Category',
+                        behavior: 'create'
+                    },
+                    menuColumnHandler: function (item, eventData) {                        
+                        var record = eventData.record,
+                            metaData = {
+                                id: record.getId()
+                            };
+                        
+                        Taco.app.StateManager.attemptNavigate('categories/duplicate/' + record.getId(), metaData);
+                    }
                 }]
                 
             }],
