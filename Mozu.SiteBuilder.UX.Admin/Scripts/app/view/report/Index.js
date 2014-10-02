dc5e3654edfb7cec4c31091f5a1617ed58c2c02e
Bradley Friemel <bradley_friemel@volusion.com>
2014-10-02 16:38:13 -0500

    Switch the reports ui to show birst reports.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/report/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/report/Index.js
@@ -1,13 +1,9 @@
 Ext.define('Taco.view.report.Index', {
-    //extend: 'Ext.panel.Panel',
     extend: 'Taco.core.ux.content.Container',
     requires: [
         'Ext.ux.IFrame',
         'Ext.panel.Panel'
     ],
-    //mixins: {
-    //    navHeader: 'Taco.core.ux.mixins.NavHeader'
-    //},
 
     cls: undefined,
 
@@ -26,7 +22,7 @@ Ext.define('Taco.view.report.Index', {
         me.header = {
             title: 'Reports'
         };
-
+        me.layout = 'fit';
         me.dashboardPanel = Ext.create('Ext.panel.Panel', {
             itemId: 'dashboardPanel',
             flex: 1,
@@ -35,13 +31,13 @@ Ext.define('Taco.view.report.Index', {
             }
         });
 
-        Ext.apply(me.body, {
-            layout: 'fit',
-            items: [me.dashboardPanel]
-        });
+        me.items = [
+            me.dashboardPanel
+        ];
 
         me.callParent(arguments);
         me.loadDashboard();
+
     },
 
     loadDashboard: function () {
@@ -54,14 +50,17 @@ Ext.define('Taco.view.report.Index', {
                 if (!obj || !obj.items) return;
                 var dashboardLocation = obj.items;
 
+
+
+                console.log('Tenant Id: ', Taco.app.context.id);
                 if (Taco.app.context.getSiteId()) {
-                    dashboardLocation = dashboardLocation + '&SiteId=' + Taco.app.context.getSiteId();
-                } else {
-                    dashboardLocation += '&SiteId=all';
+                    console.log('siteid: ' + Taco.app.context.getSiteId());
                 }
+                console.log(dashboardLocation);
+
 
                 if (!dashboardLocation) return;
-               var iFrameChild = Ext.create('Ext.ux.IFrame', {
+                var iFrameChild = Ext.create('Ext.ux.IFrame', {
                     itemId: 'dashboardIframe',
                     height: '100%',
                     width: '100%',
@@ -69,8 +68,6 @@ Ext.define('Taco.view.report.Index', {
                 });
                 me.dashboardPanel.add(iFrameChild);
                 me.dashboardPanel.doLayout();
-
-                console.log(dashboardLocation);
             }
         });
 
