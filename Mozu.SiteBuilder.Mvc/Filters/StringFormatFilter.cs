30cbe0289120c095fa9c5d9460c8d4dff24e8463
Chet Husk <chet_husk@volusion.com>
2014-10-03 16:20:01 -0500

    Revert "remove stringFormatFilter because it lives down in core Hypr now", again due to bugs
    This reverts commit 1c5fa9c0334b4a0cd83d1b87d25e7cffb46d6821.

---
+++ Mozu.SiteBuilder.Mvc/Filters/StringFormatFilter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozu.SiteBuilder.Mvc.Filters
+{
+    /// <summary>
+    /// formats object(s) given a string format using {0} ,{1},..{n} place holders
+    /// 
+    /// <code>"the {0} fox jumped over the {2} {1}"("brown","dog","lazy")</code>
+    /// </summary>
+    [NDjango.Interfaces.Name("string_format")]
+    public class StringFormatFilter: NDjango.Interfaces.IFilterWithContext
+    {
+        object NDjango.Interfaces.IFilterWithContext.PerformWithParamAndContext(object value, IEnumerable<object> parameter, NDjango.Interfaces.IContext context)
+        {
+            return string.Format((string)value, parameter.ToArray());
+        }
+
+        object NDjango.Interfaces.IFilter.DefaultValue
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        object NDjango.Interfaces.IFilter.PerformWithParam(object value, object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        object NDjango.Interfaces.ISimpleFilter.Perform(object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
