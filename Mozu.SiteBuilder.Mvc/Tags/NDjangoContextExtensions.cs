6f39771c865403c2ad3461992af938e80be17030
Chet Husk <chet_husk@volusion.com>
2014-10-06 13:21:30 -0500

    address hypr defect by updating to latest hypr package

--- Mozu.SiteBuilder.Mvc/Tags/NDjangoContextExtensions.cs
+++ Mozu.SiteBuilder.Mvc/Tags/NDjangoContextExtensions.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
 using Autofac;
-using Microsoft.FSharp.Core;
 using Mozu.SiteBuilder.Mvc.Contexts;
 using Mozu.SiteBuilder.Mvc.ObjectPools;
 using Mozu.SiteBuilder.Mvc.ViewEngine;
