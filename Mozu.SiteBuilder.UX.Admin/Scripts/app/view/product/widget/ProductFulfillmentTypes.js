291dddbf19bd92dbf2e0400d985a8ad71120fbea
Simeon Kessler <simeon_kessler@volusion.com>
2014-10-14 14:32:46 -0500

    EXTRA stuff for the product ui refactor

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/widget/ProductFulfillmentTypes.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/widget/ProductFulfillmentTypes.js
@@ -9,10 +9,14 @@ Ext.define('Taco.view.product.widget.ProductFulfillmentTypes', {
         'Taco.store.Products'
     ],
     fieldLabel: 'Fulfillment Types',
-    flex: 2,
+    //flex: 2,
+    width: "100%",
+    layout: {
+        type: "hbox"
+    },
     bodyPadding: 10,
     hidden: false,
-    columns: 3,
+    //columns: 3,
     product: null,
     isPhysical: true,
     isReadOnly: false,
@@ -41,6 +45,7 @@ Ext.define('Taco.view.product.widget.ProductFulfillmentTypes', {
             xtype: 'checkboxfield',
             boxLabel: 'In Store Pickup',
             name: 'inStoreCb',
+            margin:"0 0 0 50",
             inputValue: 'InStorePickup',
             checked: ((this.fulfillmentTypes.indexOf('InStorePickup') != -1) && this.isPhysical),
             readOnly: this.isReadOnly,
@@ -54,6 +59,7 @@ Ext.define('Taco.view.product.widget.ProductFulfillmentTypes', {
             xtype: 'checkboxfield',
             boxLabel: 'Email',
             name: 'digitalCreditCb',
+            margin: "0 0 0 50",
             inputValue: 'Digital',
             checked: ((this.fulfillmentTypes.indexOf('Digital') != -1) || !this.isPhysical),
             readOnly: true,
