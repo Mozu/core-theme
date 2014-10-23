ebef19840818837f896cb534f8aedd7f037964b1
Simeon Kessler <simeon_kessler@volusion.com>
2014-10-23 13:03:35 -0500

    adding check for label for allowBlank base field enhancement

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/overrides/form/field/Base.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/overrides/form/field/Base.js
@@ -14,12 +14,14 @@ Ext.define('Taco.overrides.form.field.Base', {
         this.allowBlank = (allowBlank);
         if (this.rendered) {            
             var labelEl = this.labelEl;
-            // need to find the label and add/remove the css cls
-            if (allowBlank === false) {
-                //   this.labelClsExtra = 'x-form-item-required';
-                labelEl.addCls(requiredCls)
-            } else {
-                labelEl.removeCls(requiredCls)
+            if (labelEl) {
+                // need to find the label and add/remove the css cls
+                if (allowBlank === false) {
+                    //   this.labelClsExtra = 'x-form-item-required';
+                    labelEl.addCls(requiredCls)
+                } else {
+                    labelEl.removeCls(requiredCls)
+                }
             }
         } else {
             if (allowBlank === false) {
