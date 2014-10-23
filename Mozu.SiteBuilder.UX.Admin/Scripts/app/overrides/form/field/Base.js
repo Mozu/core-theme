a47abcd83eccf00994751475381ef25379a55950
Simeon Kessler <simeon_kessler@volusion.com>
2014-10-23 13:01:40 -0500

    update to the field base class so that allowBlank can bet set at runtime

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/overrides/form/field/Base.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/overrides/form/field/Base.js
@@ -6,10 +6,25 @@ Ext.define('Taco.overrides.form.field.Base', {
     msgTarget: 'under',
 
     initComponent: function () {
-        if (this.allowBlank === false) {
-            this.labelClsExtra = 'x-form-item-required';
-        }
-
+        this.setAllowBlank(this.allowBlank);
         this.callParent(arguments);
+    },
+    setAllowBlank: function (allowBlank) {
+        var requiredCls = "x-form-item-required"
+        this.allowBlank = (allowBlank);
+        if (this.rendered) {            
+            var labelEl = this.labelEl;
+            // need to find the label and add/remove the css cls
+            if (allowBlank === false) {
+                //   this.labelClsExtra = 'x-form-item-required';
+                labelEl.addCls(requiredCls)
+            } else {
+                labelEl.removeCls(requiredCls)
+            }
+        } else {
+            if (allowBlank === false) {
+                this.labelClsExtra = requiredCls;
+            }
+        }
     }
 });
