4fb387dda1d0cf219194ee12c584ad3181b54510
Greg Murray <greg_murray@volusion.com>
2014-11-04 16:40:41 -0600

    dirtyFlag to set record to isUploaded

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/field/Image.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/shared/view/field/Image.js
@@ -494,7 +494,7 @@ Ext.define('Taco.shared.view.field.Image', {
         }
 
         this.selectedImages.each(function (record) {
-            value.push({ url: record.get('url'), cmsId: record.get('cmsId'), alt: record.get('alt'), isMerged: record.get('isMerged') });
+            value.push({ url: record.get('url'), cmsId: record.get('cmsId'), alt: record.get('alt'), isUploaded: record.get('isUploaded'), isMerged: record.get('isMerged') });
         }, this);
 
         if (value && value.length) {
