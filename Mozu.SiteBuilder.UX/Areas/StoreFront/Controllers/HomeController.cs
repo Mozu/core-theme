34b19b935b7e6daa6d9d261e2b84a9fb2c9f1506
Chet Husk ☜(ﾟヮﾟ☜) <CORP\chet_husk>
2014-11-05 16:45:21 -0600

    merge down from main Conflicts: 	Mozu.SiteBuilder.UX.Admin/Api/FileManagementController.cs 	Mozu.SiteBuilder.UX.Admin/Scripts/app/view/website/entityAdapters/BaseEntityAdapter.js 	Mozu.SiteBuilder.UX.Themes/Themes/Core5/compiled/scripts/modules/common.js 	Mozu.SiteBuilder.UX.Themes/Themes/Core5/scripts/modules/backbone-mozu-pagedcollection.js 	Mozu.SiteBuilder.UX.Themes/Themes/Core5/scripts/modules/models-faceting.js
    fixes from Greg, Thom, and Zetlen.
    
    this was TFS Changeset Id 88807

--- Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/HomeController.cs
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
@@ -21,38 +22,38 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
     public class HomeController : BaseApiController
     {
         // private INavigationRuntimeFactory _navigationRuntimeFactory;
-    
+
 
         public HomeController()
         {
-            
+
         }
 
         //
         // GET: /StoreFront/Home/
-        [System.Web.Http.HttpGet  ]
+        [System.Web.Http.HttpGet]
         public async Task<ActionResult> Index()
         {
-            var nav = NavigationContext.Tree  ;
-            if ( nav != null && nav.Count() > 0)
+            var nav = NavigationContext.Tree;
+            if (nav != null && nav.Count() > 0)
             {
                 var item = nav.FirstOrDefault(x => x.IsHomePage);
-                if ( item != null  && item.Url.Length >0 && item.Url != "/pages/home")
+                if (item != null && item.Url.Length > 0 && item.Url != "/pages/home")
                 {
-                    return new TransferResult(item.Url );
+                    return new TransferResult(item.Url);
                 }
             }
 
 
-            PageContext.CmsContext= new CmsPageContext()
-                                                    {
-                                                        Initialized=false,
-                                                        Template = new DocumentRequest()
-                                                                          {
-                                                                              ListFQN ="pageTemplateContent@mozu",
-                                                                              Path = "home"
-                                                                          }
-                                                    };
+            PageContext.CmsContext = new CmsPageContext()
+            {
+                Initialized = false,
+                Template = new DocumentRequest()
+                {
+                    ListFQN = "pageTemplateContent@mozu",
+                    Path = "home"
+                }
+            };
 
 
             return this.View("home");
@@ -61,42 +62,44 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
         }
 
         [System.Web.Http.HttpGet]
-        public async Task<ActionResult> NotFound()
+        public HttpResponseMessage NotFound()
         {
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Page not found.");
+            //PageContext.CmsContext = new CmsPageContext()
+            //{
+            //    Initialized = false,
+            //    Template = new DocumentRequest()
+            //    {
+            //        ListFQN = "pageTemplateContent@mozu",
+            //        Path = "404"
+            //    }
+            //};
 
-            PageContext.CmsContext = new CmsPageContext()
-            {
-                Initialized = false,
-                Template = new DocumentRequest()
-                {
-                    ListFQN = "pageTemplateContent@mozu",
-                    Path = "404"
-                }
-            };
-        
-            return this.View("404");
+
+
+            //return this.View("404");
         }
-          [System.Web.Http.HttpGet]
-        public async Task<HttpResponseMessage > SeoProcessor(string url= null)
+        [System.Web.Http.HttpGet]
+        public async Task<HttpResponseMessage> SeoProcessor(string url = null)
         {
             return this.Request.CreateResponse();
         }
 
         [System.Web.Http.HttpGet]
-        public async Task<ActionResult> GoogleSiteVerification(string hash)
+        public async Task<HttpResponseMessage> GoogleSiteVerification(string hash)
         {
             var webToolsRepository = LifetimeScope.Resolve<IWebToolsRepository>();
             var resp = await webToolsRepository.GetWebMasterToolsFile(string.Format("google{0}.html", hash));
             if (resp.ResponseMessage.IsSuccessStatusCode)
             {
                 var stream = await resp.ResponseMessage.Content.ReadAsStreamAsync();
-                return File(stream, "text/html");
+                return this.Request.CreateResponse(HttpStatusCode.OK, File(stream, "text/html"));
             }
-               
-            return await this.NotFound();
-           
+
+            return this.NotFound();
+
         }
-[System.Web.Http.HttpGet]
+        [System.Web.Http.HttpGet]
         public async Task<ActionResult> RobotsTxt()
         {
             var webToolsRepository = LifetimeScope.Resolve<IWebToolsRepository>();
