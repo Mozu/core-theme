5fe90f7073fce0af252895aaf7ac20ff629b9d2d
Ben <ben_cripps@volusion.com>
2014-11-25 12:06:51 -0600

    better solution for select-existing bug in image-widget

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/website/widgetEditors/v2/Image.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/website/widgetEditors/v2/Image.js
@@ -546,9 +546,7 @@ Ext.define('Taco.view.website.widgetEditors.v2.Image', {
     toggleAssociator: function (state, button, store) {
         if (state) {
             if (!this.associator) {
-                this.associator = Ext.create('Taco.view.fileManager.Associator', {});
-                //override close action for modal
-                Ext.override(this.associator, {closeAction: 'hide'});
+                this.associator = Ext.create('Taco.view.fileManager.Associator', {closeAction: 'hide'});
             } else {
                 this.associator.show();
             }
