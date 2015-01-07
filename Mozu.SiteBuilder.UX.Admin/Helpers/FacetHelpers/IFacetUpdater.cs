68a6c668f49eb5f2bf6eec0e76527ceb7b9f5439
Greg Murray <greg_murray@volusion.com>
2015-01-07 09:25:40 -0600

    Refactoring Facet controller, initial check-in to avoid merge problems.

---
+++ Mozu.SiteBuilder.UX.Admin/Helpers/FacetHelpers/IFacetUpdater.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mozu.SiteBuilder.UX.Admin.Api.Models.Facets;
+
+namespace Mozu.SiteBuilder.UX.Admin.Helpers.FacetHelpers
+{
+    public interface IFacetUpdater
+    {
+        Task<FacetSet> UpdateFacetSet(FacetSet set);
+    }
+}
---
+++ Mozu.SiteBuilder.UnitTests/Admin/Api/Helpers/FacetHelpers/FacetUpdaterTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using NSubstitute;
+using Mozu.SiteBuilder.UX.Admin.Helpers.FacetHelpers;
+
+namespace Mozu.SiteBuilder.UnitTests.Admin.Api.Helpers.FacetHelpers
+{
+    [TestFixture]
+    public class FacetUpdaterTest
+    {
+        //[TestCase("happy path case")]
+        public void Given_A_When_B_Then_Should_Do_C(string scenario)
+        {
+            //arrange
+
+            //act
+
+            //assert
+            //Assert.That(actual, Is.EqualTo(), scenario);
+        }
+
+        //[TestCase("negative test")]
+        public void Given_A_When_B_Then_Should_Throw_Exception(string scenario)
+        {
+            //arrange
+
+
+            //act
+            //Assert.Throws<ApiWebClientException>(
+            //        () => client.UpdateDeveloperAccountOwner(devAccountActual.Id, newUserId));
+
+        }
+
+    }
+}
