9b9bc1487c6809fdfaf5b2850f8ee45fe60d77b7
Chet Husk <chet_husk@volusion.com>
2015-01-12 10:58:54 -0600

    add tests for the new caching controller, as well as slightly refactor the implementation to be more test-friendly

--- Mozu.SiteBuilder.Mvc/Caching/ContextAwareStorefrontCache.cs
+++ Mozu.SiteBuilder.Mvc/Caching/ContextAwareStorefrontCache.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Mozu.Core;
-using Mozu.SiteBuilder.Mvc.Contexts;
+using Mozu.SiteBuilder.UX.Models;
 
 namespace Mozu.SiteBuilder.Mvc.Caching
 {
@@ -8,24 +8,24 @@ namespace Mozu.SiteBuilder.Mvc.Caching
     
     public class ContextAwareStorefrontCache : IContextAwareStorefrontCache
     {
-        private readonly ISiteBuilderApiContext _apiContext;
-        private readonly PageContext _pageContext;
-        private readonly SiteContext _siteContext;
+        private readonly IApiContext _apiContext;
+        private readonly IEditableContext _pageContext;
+        private readonly Func<bool> _siteContextPartialCacheFunc;
         private readonly IStorefrontCache _backingCache;
 
-        public ContextAwareStorefrontCache(ISiteBuilderApiContext apiContext, PageContext pageContext, SiteContext siteContext, IStorefrontCache backingCache)
+        public ContextAwareStorefrontCache(IApiContext apiContext, IEditableContext pageContext, Func<bool> siteContextPartialCacheFunc, IStorefrontCache backingCache)
         {
             _apiContext = apiContext;
             _pageContext = pageContext;
-            _siteContext = siteContext;
+            _siteContextPartialCacheFunc = siteContextPartialCacheFunc;
+
             _backingCache = backingCache;
         }
 
         public T Get<T>(string key, CacheScope scope = CacheScope.Site)
         {
-            return CachingIsDisabled(_pageContext, _apiContext, _siteContext)
-                ? default(T)
-                : _backingCache.Get<T>(key, scope);
+            if(CachingIsDisabled(_pageContext, _apiContext, _siteContextPartialCacheFunc)) return default(T);
+            return  _backingCache.Get<T>(key, scope);
         }
 
         public void Set(string key, object value, CacheScope scope = CacheScope.Site)
@@ -33,13 +33,12 @@ namespace Mozu.SiteBuilder.Mvc.Caching
             _backingCache.Set(key, value, scope);
         }
 
-        private static bool CachingIsDisabled(PageContext pageCtx, ISiteBuilderApiContext apiCtx, SiteContext siteCtx)
+        private static bool CachingIsDisabled(IEditableContext pageCtx, IApiContext apiCtx, Func<bool> getSiteContextPartialCacheFunc )
         {
-            var enablePartialCaching = Convert.ToBoolean(siteCtx.ThemeSettings["enablePartialCaching"]);
             return
                 pageCtx.IsEditMode ||
                 apiCtx.DataViewMode == DataViewModeType.Pending ||
-                !enablePartialCaching;
+                getSiteContextPartialCacheFunc();
         }
     }
 }
---
+++ Mozu.SiteBuilder.UnitTests/Mvc/Caching/ContextualCacheTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mozu.Core;
+using Mozu.SiteBuilder.Mvc.Caching;
+using Mozu.SiteBuilder.UX.Models;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Mozu.SiteBuilder.UnitTests.Mvc.Caching
+{
+    [TestFixture]
+    public class ContextualCacheTests
+    {
+        [TestCaseSource("Cases")]
+        [Test]
+        public void When_Context_Is_In_Edit_Mode_Nothing_Is_Cached(bool isEditMode, DataViewModeType dvm, bool partialCacheEnabled, bool expectedNull)
+        {
+            var pageCtx = Substitute.For<IEditableContext>();
+            pageCtx.IsEditMode.Returns(isEditMode);
+            var context = Substitute.For<IApiContext>();
+            context.DataViewMode.Returns(dvm);
+
+            // preload cache with an item
+            var backingCache = new TestCache();
+            var key = new Random().Next().ToString();
+            backingCache.Set(key, 5);
+
+            var contextCache = new ContextAwareStorefrontCache(context, pageCtx, () => partialCacheEnabled, backingCache);
+            var cachedItem = contextCache.Get<int?>(key);
+            Assert.AreEqual((cachedItem == null), expectedNull); 
+        }
+
+        private static readonly bool[] Bools = {true, false};
+        private static readonly IEnumerable<DataViewModeType> Dvms = Enum.GetValues(typeof(DataViewModeType)).Cast<DataViewModeType>();
+
+        private readonly IEnumerable<object[]> Cases =
+            from isEditMode in Bools
+            from dvm in Dvms
+            from partialCacheEnabled in Bools
+            select
+                new object[]
+                {
+                    isEditMode, dvm, partialCacheEnabled,
+                    isEditMode || dvm == DataViewModeType.Pending || partialCacheEnabled
+                };
+    }
+
+    internal class TestCache : IStorefrontCache
+    {
+        private struct ScopedName
+        {
+            public string key { get; set; }
+            public CacheScope scope { get; set; }
+        }
+
+        private readonly Dictionary<ScopedName, object> _cache = new Dictionary<ScopedName,object>();
+ 
+        public T Get<T>(string key, CacheScope scope = CacheScope.Site)
+        {
+            var inKey = new ScopedName {key = key, scope = scope};
+            if (_cache.ContainsKey(inKey)) return (T) _cache[inKey];
+            return default(T);
+        }
+
+        public void Set(string key, object value, CacheScope scope = CacheScope.Site)
+        {
+            var inKey = new ScopedName{key = key, scope = scope};
+            _cache[inKey] = value;
+        }
+    }
+}
