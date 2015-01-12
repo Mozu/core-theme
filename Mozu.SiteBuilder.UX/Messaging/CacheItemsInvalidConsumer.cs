5905cbcd145a91861a03dbc0ee04d55f8e4d4c03
Chet Husk <chet_husk@volusion.com>
2015-01-12 11:50:50 -0600

    add blank space consumer for new solr enqueue message.  this message doesn't exist yet, but this is all teh code for it.

--- Mozu.SiteBuilder.UX/Messaging/CacheItemsInvalidConsumer.cs
+++ Mozu.SiteBuilder.UX/Messaging/CacheItemsInvalidConsumer.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using Autofac;
-using Burrows;
+﻿using Burrows;
 using Mozu.Core.Messaging.Consume;
 using Mozu.Core.Messaging.Contracts;
+using Mozu.Core.Messaging.Contracts.Product.Events;
 using Mozu.SiteBuilder.Mvc.Caching;
 
 namespace Mozu.SiteBuilder.UX.Messaging
 {
-
+    //TODO: cache invalidation by tags, so we can only kill subsets of ALL THE THINGS.
     public class CacheItemsInvalidConsumer : LoggingConsumer, 
-        Consumes<Mozu.Core.Messaging.Contracts.Product.Events.IProductEvent >.All, 
-        Consumes<Mozu.Core.Messaging.Contracts.Product.Events.ICategoryEvent>.All,
-        Consumes<Mozu.Core.Messaging.Contracts.Product.Events.IDiscountEvent>.All
+        Consumes<IProductEvent>.All, 
+        Consumes<ICategoryEvent>.All,
+        Consumes<IDiscountEvent>.All,
+        Consumes<CacheItemsInvalidConsumer.ISolrEnqueue>.All
    
     {
         private readonly IStorefrontCacheControl _storefrontCacheControl;
 
-
         public CacheItemsInvalidConsumer(IStorefrontCacheControl storefrontCacheControl)
         {
             _storefrontCacheControl = storefrontCacheControl;
-           // int f = 0;
-            //_storefrontCacheControl = StorefrontCacheControlImpl.Default;
         }
 
-        public void Consume(Core.Messaging.Contracts.Product.Events.IProductEvent message)
+        public void Consume(IProductEvent message)
         {
             if (message.MessagePublishingContext.CatalogId.HasValue)
             {
@@ -34,43 +31,48 @@ namespace Mozu.SiteBuilder.UX.Messaging
             {
                  _storefrontCacheControl.InvalidateSite( message.MessagePublishingContext.SiteId.Value);
             }
+        }
+
+        public void Consume(ICategoryEvent message)
+        {
+            if (message.MessagePublishingContext.CatalogId.HasValue)
+            {
+                _storefrontCacheControl.InvalidateCatalog(message.MessagePublishingContext.TenantId, message.MessagePublishingContext.CatalogId.Value);
+            }
+            if (message.MessagePublishingContext.SiteId.HasValue)
+            {
+                _storefrontCacheControl.InvalidateSite(message.MessagePublishingContext.SiteId.Value);
+            }
+        }
+
+        public void Consume(IDiscountEvent message)
+        {
+            if (message.MessagePublishingContext.CatalogId.HasValue)
+            {
+                _storefrontCacheControl.InvalidateCatalog(message.MessagePublishingContext.TenantId, message.MessagePublishingContext.CatalogId.Value);
+            }
+            if (message.MessagePublishingContext.SiteId.HasValue)
+            {
+                _storefrontCacheControl.InvalidateSite(message.MessagePublishingContext.SiteId.Value);
+            }
+        }
+
+        //TODO: update when we have the new solr enqueue messaging
+        public void Consume(ISolrEnqueue message)
+        {
+            if (message.MessagePublishingContext.CatalogId.HasValue)
+            {
+                _storefrontCacheControl.InvalidateCatalog(message.MessagePublishingContext.TenantId, message.MessagePublishingContext.CatalogId.Value);
+            }
+            if (message.MessagePublishingContext.SiteId.HasValue)
+            {
+                _storefrontCacheControl.InvalidateSite(message.MessagePublishingContext.SiteId.Value);
+            }
+        }
+
+        public interface ISolrEnqueue : IEntityEvent
+        {
             
-           
         }
-
-        public void Consume(Core.Messaging.Contracts.Product.Events.ICategoryEvent message)
-        {
-            if (message.MessagePublishingContext.CatalogId.HasValue)
-            {
-                _storefrontCacheControl.InvalidateCatalog(message.MessagePublishingContext.TenantId, message.MessagePublishingContext.CatalogId.Value);
-            }
-            if (message.MessagePublishingContext.SiteId.HasValue)
-            {
-                _storefrontCacheControl.InvalidateSite(message.MessagePublishingContext.SiteId.Value);
-            }
-        }
-
-        public void Consume(Core.Messaging.Contracts.Product.Events.IDiscountEvent message)
-        {
-            if (message.MessagePublishingContext.CatalogId.HasValue)
-            {
-                _storefrontCacheControl.InvalidateCatalog(message.MessagePublishingContext.TenantId, message.MessagePublishingContext.CatalogId.Value);
-            }
-            if (message.MessagePublishingContext.SiteId.HasValue)
-            {
-                _storefrontCacheControl.InvalidateSite(message.MessagePublishingContext.SiteId.Value);
-            }
-        }
-
     }
-    //public class CacheItemsInvalidConsumer : LoggingConsumer, Consumes<IEntityEvent>.All
-    //{
-    //    public void Consume(IEntityEvent message)
-    //    {
-    //        ProcessWithLogging(() =>
-    //        {
-    //            // do some processing.
-    //        }, message);
-    //    }
-    //}
 }
