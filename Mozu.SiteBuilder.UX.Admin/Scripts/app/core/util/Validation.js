15e60097fe08499579b6865a046013378a89080a
Greg Murray <greg_murray@volusion.com>
2015-01-14 18:00:03 -0600

    Added validation utility and called from Location for location code.
    Related Work Items: #41478

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/util/Validation.js
@@ -0,0 +1,45 @@
+﻿/** 
+ * @class Taco.util.Validation
+ * common validation methods
+ * */
+Ext.define('Taco.core.util.Validation', {
+    singleton: true,
+    alternateClassName: ['TacoValidation'],
+
+    constructor: function(config) {
+        this.initConfig(config);
+    },
+    config: {
+        seoFriendlyRegex : /^[a-z0-9-_/.()]*[a-z0-9-_()]$/i
+    },
+
+    /*  a utility method to store recurring regex and other validation functions.
+     * example:
+     * 
+     {
+       xtype: "textfield",
+       name: "code",
+       allowOnlyWhitespace: false,
+       maxLength: 10,
+       validator: Taco.core.util.Validation.validateSeoFriendlyText
+     }
+     * 
+     * 
+    */
+
+    /**
+     * Validates text is SEO friendly.  Allows empty.
+     * @param value text value
+     * @returns if passes validation then returns true, else returns error string
+     */
+    validateSeoFriendlyText : function(value) {
+        var seoFriendlyRegex = TacoValidation.getSeoFriendlyRegex(),
+            isValid = !value || seoFriendlyRegex.test(value);
+        if (!isValid) {
+            return 'Invalid format: no spaces or special characters are allowed except hyphens, underscores, parentheses, and periods, but may not end with a period.';
+        }
+        return true;
+    }
+    
+
+});
