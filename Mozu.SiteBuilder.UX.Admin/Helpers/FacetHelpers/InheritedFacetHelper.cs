d378f240d7ae300923b888586c68d4475010762b
Greg Murray <greg_murray@volusion.com>
2015-01-26 18:09:55 -0600

    WIP, adding logic for inherited/overrides

---
+++ Mozu.SiteBuilder.UX.Admin/Helpers/FacetHelpers/InheritedFacetHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper.Internal;
+using Mozu.Core.Extensions;
+using Mozu.ShippingRuntime.Contracts;
+using DC = Mozu.ProductAdmin.Contracts;
+
+namespace Mozu.SiteBuilder.UX.Admin.Helpers.FacetHelpers
+{
+    public class InheritedFacetHelper : IInheritedFacetHelper
+    {
+        /// <summary>
+        /// New = inherited && doesn't match server inherited, for isHidden or rangeQuery mismatch
+        /// </summary>
+        /// <returns>any facets to add with current category id</returns>
+        public List<DC.Facet> GetOverridenFacetsToAdd(List<DC.Facet> inheritedClient, List<DC.Facet> inheritedServer, int currentCategoryId)
+        {
+            return (from cli in inheritedClient
+                join srv in inheritedServer on cli.FacetId equals srv.FacetId
+                where cli.IsHidden  || ("RangeQuery".EqualsIgnoreCase(cli.FacetType) && !doRangeQueriesMatch(cli.RangeQueries, srv.RangeQueries))
+                select new DC.Facet
+                {
+                    CategoryId = currentCategoryId,
+                    FacetId = cli.FacetId,
+                    IsHidden = cli.IsHidden,
+                    FacetType = cli.FacetType,
+                    Order = cli.Order,
+                    RangeQueries = cli.RangeQueries,
+                    Source = cli.Source,
+                    Validity = cli.Validity,
+                    ValueSortType = cli.ValueSortType
+                }).ToList();
+        }
+
+        /// <summary>
+        /// update = override && doesn't match server override && doesn't match inherited
+        /// </summary>
+        /// <returns></returns>
+        public List<DC.Facet> GetOverridenFacetsToUpdate(List<DC.Facet> overrideClient, List<DC.Facet> overrideServer, List<DC.Facet> inheritedServer)
+        {
+            return (from cli in overrideClient
+                    join overSrv in overrideServer on cli.FacetId equals overSrv.FacetId
+                    join inheritSrv in inheritedServer on cli.OverrideFacetId equals inheritSrv.FacetId
+                    where cli.IsHidden != overSrv.IsHidden || 
+                            ("RangeQuery".EqualsIgnoreCase(cli.FacetType) && !doRangeQueriesMatch(cli.RangeQueries, overSrv.RangeQueries))
+                        && (cli.IsHidden != inheritSrv.IsHidden 
+                            || ("RangeQuery".EqualsIgnoreCase(cli.FacetType) && !doRangeQueriesMatch(cli.RangeQueries, overSrv.RangeQueries)) )
+                    select cli).ToList();
+        }
+
+        /// <summary>
+        /// delete = override && matches inherited
+        /// </summary>
+        /// <returns></returns>
+        public List<DC.Facet> GetOverridenFacetsToDelete(List<DC.Facet> overrideClient, List<DC.Facet> inheritedServer)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private bool doRangeQueriesMatch(List<DC.FacetRangeQuery> cli, List<DC.FacetRangeQuery> server)
+        {
+            if (cli.IsNullOrEmpty() && server.IsNullOrEmpty())
+            {
+                return true;
+            }
+
+            if (cli.Count != server.Count)
+            {
+                return false;
+            }
+            return !server.Where((srv, i) => cli[i].RangeValueStart.ToNullSafeString() != srv.RangeValueStart.ToNullSafeString() 
+                            || cli[i].RangeValueEnd.ToNullSafeString() != srv.RangeValueEnd.ToNullSafeString()).Any();
+        }
+    }
+}
---
+++ Mozu.SiteBuilder.UnitTests/Admin/Helpers/FacetHelpers/InheritedFacetHelperTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using Mozu.SiteBuilder.UX.Admin.Helpers.FacetHelpers;
+using DC = Mozu.ProductAdmin.Contracts;
+
+namespace Mozu.SiteBuilder.UnitTests.Admin.Helpers.FacetHelpers
+{
+    [TestFixture]
+    public class InheritedFacetHelperTest
+    {
+        private const int _currentCategoryId = 9;
+
+        [TestCase("happy")]
+        public void It_Should_Add_New_When_Client_Inherited_Facet_Is_Hidden(string scenario)
+        {
+            var inheritedClient = new List<DC.Facet>
+            {
+                CreateFacet(1, isHidden:true),
+                CreateFacet(2, isHidden:false)
+            };
+            var inheritedServer = new List<DC.Facet>{
+                CreateFacet(1, isHidden:false),
+                CreateFacet(2, isHidden:false)
+            };
+            var sut = new InheritedFacetHelper();
+
+            var actual = sut.GetOverridenFacetsToAdd(inheritedClient, inheritedServer, _currentCategoryId);
+            
+            Assert.That(actual.Count, Is.EqualTo(1), "should only return 1");
+            Assert.That(actual.First().CategoryId == _currentCategoryId);
+        }
+        
+        [Test]
+        public void It_Should_Add_New_When_Client_Inherited_Facet_RangeQuery_Is_Modified()
+        {
+            //arrange
+            var inheritedClient = new List<DC.Facet>
+            {
+                CreateFacet(facetId:1, isHidden:false, rangeQueryCount:3), //diff range count
+                CreateFacet(facetId:2, isHidden:false, rangeQueryCount:5, rangeQueryModifier:10), //diff range values
+                CreateFacet(facetId:3, isHidden:false, rangeQueryCount:5) //same
+            };
+            var inheritedServer = new List<DC.Facet>{
+                CreateFacet(facetId:1, isHidden:false, rangeQueryCount:5),
+                CreateFacet(facetId:2, isHidden:false, rangeQueryCount:5),
+                CreateFacet(facetId:3, isHidden:false, rangeQueryCount:5)
+            };
+            var sut = new InheritedFacetHelper();
+
+            //act
+            var actual = sut.GetOverridenFacetsToAdd(inheritedClient, inheritedServer, _currentCategoryId);
+            actual = actual.OrderBy(x => x.FacetId).ToList(); //sort to verify
+
+            //assert
+            Assert.That(actual.Count, Is.EqualTo(2), "should return 2, but returned " + actual.Count);
+            Assert.That(actual.First().CategoryId == _currentCategoryId);
+            Assert.That(actual.First().RangeQueries.Count == 3);
+            Assert.That(actual[1].RangeQueries[0].RangeValueStart, Is.EqualTo(10));
+        }
+
+
+
+
+
+
+        private static DC.Facet CreateFacet(int facetId, bool isHidden=true, int rangeQueryCount=0, int rangeQueryModifier=0)
+        {
+            var facet = new DC.Facet
+            {
+                CategoryId = 3,
+                FacetId = facetId,
+                IsHidden = isHidden,
+                FacetType = (rangeQueryCount == 0 ? "Values" : "RangeQuery")
+            };
+            if (rangeQueryCount == 0)
+            {
+                return facet;
+            }
+            facet.RangeQueries = new List<DC.FacetRangeQuery>();
+
+            for (var i = 0; i < rangeQueryCount; i++)
+            {
+                facet.RangeQueries.Add(new DC.FacetRangeQuery
+                    {
+                        RangeValueStart = i*10 + rangeQueryModifier,
+                        RangeValueEnd = i*10 + 9 + rangeQueryModifier
+                    });
+            }
+            return facet;
+        }
+    }
+}
