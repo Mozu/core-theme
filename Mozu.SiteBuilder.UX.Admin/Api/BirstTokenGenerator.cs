e31fd5d429df1a79c2a2e6e6114ba417211be96e
Bradley Friemel <bradley_friemel@volusion.com>
2015-01-27 11:39:01 -0600

    Birst updates for url

--- Mozu.SiteBuilder.UX.Admin/Api/BirstTokenGenerator.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/BirstTokenGenerator.cs
@@ -35,10 +35,11 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
 
         public async Task<string> GenerateDashboardUri()
         {
+            //Leaving here right now, so we can debug quickly since these items work
             //"https://reporting.mozu-qa.com/SSO.aspx",//"https://aus02ndbrst01.dev.volusion.com/SSO.aspx",//
             var token = await GenerateToken();
             return string.Format(
-                "{0}?BirstSSOToken={1}&birst.module=dashboard&birst.dashboard=Dev%2520Dashboard&birst.page=Start%2520Here&birst.helpURL=&birst.exportZoom=2&birst.embedded=true&birst.hideDashboardNavigation=true&birst.hideDashboardPrompts=true&birst.openPageForEdit=false&birst.viewMode=full&birst.filterLayout=left",
+                "{0}?BirstSSOToken={1}&birst.module=dashboard&birst.dashboard=Dev+Dashboard&birst.page=Start+Here&birst.helpURL=&birst.exportZoom=2&birst.embedded=true&birst.hideDashboardNavigation=true&birst.hideDashboardPrompts=true&birst.openPageForEdit=false&birst.viewMode=full&birst.filterLayout=left",
                     _settings.AppSettings(SSO_URL_KEY),
                     token
                 );
@@ -65,9 +66,18 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
 
         private string FormatBirstTokenGeneratorUri(int tenantId)
         {
-            //"https://reporting.mozu-qa.com/TokenGenerator.aspx",//"https://aus02ndbrst01.dev.volusion.com/TokenGenerator.aspx",//
-            // tenantId = 2106;
-            // 1055
+            /*
+            //Leaving here right now, so we can debug quickly since these items work
+            tenantId = 6294;
+            return string.Format("{0}?username={1}&ssopassword={2}&BirstSpaceId={3}&birst.sessionVars=TenantId%3D{4}",
+               "https://reporting.mozu-qa.com/TokenGenerator.aspx",// _settings.AppSettings(TOKEN_URL_KEY),
+                "reportuserqa@mozu.com", //_settings.AppSettings(USER_KEY),
+                "OBcxHsfNxyGT16Pxe6YSJ6clRfdunG22",//"4mSPFxPJcUYhtTMA0Zkh7Akq2BFUUEij",//_settings.AppSettings(PASSWORD_KEY),
+                "c0ca8cb2-3d5f-4fdd-aae5-15468a859661",//"61b81c44-14d9-4e84-bd8f-6c4794f43763",//_settings.AppSettings(SPACE_KEY),
+                tenantId
+            );
+             */
+            
             return string.Format("{0}?username={1}&ssopassword={2}&BirstSpaceId={3}&birst.sessionVars=TenantId%3D{4}",
                 _settings.AppSettings(TOKEN_URL_KEY),
                 _settings.AppSettings(USER_KEY),
@@ -75,6 +85,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
                 _settings.AppSettings(SPACE_KEY),
                 tenantId
             );
+             
         }
     }
 }
