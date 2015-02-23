834a393b5f65d44c0e6f125ecf4468989a9edcc8
Simeon Kessler <simeon_kessler@volusion.com>
2015-02-23 15:52:55 -0600

    adding mod. by combo to adv. search for store credit
    Related Work Items: #51969

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/storeCredit/AdvancedSearchForm.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/storeCredit/AdvancedSearchForm.js
@@ -4,6 +4,7 @@
 Ext.define('Taco.view.storeCredit.AdvancedSearchForm', {
     extend: 'Taco.core.ux.form.Form',
     requires: [
+        'Taco.store.AdminUsers',
         'Ext.form.FieldContainer',
         'Ext.form.field.Date',
         'Taco.core.ux.form.CurrencyField',
@@ -253,8 +254,8 @@ Ext.define('Taco.view.storeCredit.AdvancedSearchForm', {
                 listeners: {
                     expand: {
                         scope: me,
-                        single:true,
-                        fn: function(field) {
+                        single: true,
+                        fn: function (field) {
                             // if the field has no currently set value, default the picker to a default value;
                             if (!field.getValue() && field.initialPickerValue) {
                                 field.setValue(field.initialPickerValue);
@@ -277,7 +278,29 @@ Ext.define('Taco.view.storeCredit.AdvancedSearchForm', {
                     this.clearValue();
                 },
                 store: { type: 'Taco.store.Currencies' }
-                        
+
+            }, {
+                xtype: 'combobox',
+                name: 'updateby',
+                fieldLabel: 'Modified by',
+                flex: 1,
+                valueField: 'id',
+                displayField: 'lastName',
+                queryMode: 'local',
+                valueNotFoundText: 'not found',
+                editable: false,
+                forceSelection: true,
+                //initialValue: "USD",
+                tpl: Ext.create('Ext.XTemplate',
+                  '<tpl for=".">',
+                      '<div class="x-boundlist-item">{firstName} {lastName}, {emailAddress}</div>',
+                  '</tpl>'
+                ),
+                trigger2Cls: 'x-form-clear-trigger',
+                onTrigger2Click: function () {
+                    this.clearValue();
+                },
+                store: { type: 'Taco.store.AdminUsers'}
             }
         ];
 
