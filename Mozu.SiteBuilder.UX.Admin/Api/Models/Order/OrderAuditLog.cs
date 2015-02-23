f4b033d7e3916d6d166d25d7cb40da6a3002b3e6
'Jonathan <'Jonathan_Kuchar.volusion.com'>
2015-02-23 15:38:09 -0600

    Adding new files

---
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/Order/OrderAuditLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mozu.SiteBuilder.UX.Admin.Api.Models.Order
+{
+    public class OrderAuditLog
+    {
+        
+    }
+}
---
+++ Mozu.SiteBuilder.UX.Admin/Api/OrderAuditLogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using AutoMapper;
+using Mozu.CommerceRuntime.Contracts.Clients;
+using Mozu.Core.Api.Routing;
+using Mozu.SiteBuilder.Mvc.Extensions;
+using Mozu.SiteBuilder.UX.Admin.Api;
+using Mozu.SiteBuilder.UX.Admin.Api.Models;
+using Mozu.SiteBuilder.UX.Admin.Api.Models.Order;
+
+namespace Mozu.SiteBuilder.UX.Admin.Api
+{
+
+    public partial class OrderController
+    {
+        [HttpGetRoute(UriTemplate = "changemessages")]
+        public async Task<Response<List<OrderAuditLog>>> ChangeMessages(OrderIdArgs args)
+        {
+            var changeMessages = (await _orderWebApiClient.GetChangeMessages(args.OrderId)).ReadAsSync();
+
+            return List2(Mapper.Map<List<OrderAuditLog>>(changeMessages.Items), (int)changeMessages.TotalCount);
+        }
+    }
+}
