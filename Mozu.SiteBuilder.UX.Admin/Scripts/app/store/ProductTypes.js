debfd7fb27ecc5cebd52a935fc6f123549739319
Simeon Kessler <simeon_kessler@volusion.com>
2015-03-03 15:31:32 -0600

    fix of merge conflict issues. git makes me hate myself

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/store/ProductTypes.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/store/ProductTypes.js
@@ -8,15 +8,7 @@ Ext.define('Taco.store.ProductTypes', {
     requires:['Taco.model.ProductType'],
     extend: 'Ext.data.Store',
     model: 'Taco.model.ProductType',
-<<<<<<< HEAD
-<<<<<<< HEAD
-    pageSize: 50,
-=======
     pageSize: 20,
->>>>>>> initial checkin of productType refactoring
-=======
-    pageSize: 25,
->>>>>>> refactor of product to allow for  paging productType
     buffered: false,
     remoteSort: true,
     remoteFilter: true,
