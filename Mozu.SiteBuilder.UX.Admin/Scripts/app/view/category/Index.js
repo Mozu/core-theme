094f2573dbd5271a57df5709f3d10060877c8b8f
Greg Murray <greg_murray@volusion.com>
2015-03-06 10:40:08 -0600

    re-arranged action menu order per Misha
    Related Work Items: #51487

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Index.js
@@ -66,13 +66,6 @@ Ext.define('Taco.view.category.Index', {
                 xtype: 'taco.menucolumn',
                 text: 'Actions',
                 menuItems: [{
-                    text: 'Delete',
-                    requiredBehaviors: {
-                        model: 'Taco.model.Category',
-                        behavior: 'destroy'
-                    },
-                    menuColumnHandler: 'destroyMenuColumnHandler'
-                }, {
                     text: 'Edit',
                     requiredBehaviors: {
                         model: 'Taco.model.Category',
@@ -101,6 +94,13 @@ Ext.define('Taco.view.category.Index', {
                         
                         Taco.app.StateManager.attemptNavigate('categories/duplicate/' + record.getId(), metaData);
                     }
+                }, {
+                    text: 'Delete',
+                    requiredBehaviors: {
+                        model: 'Taco.model.Category',
+                        behavior: 'destroy'
+                    },
+                    menuColumnHandler: 'destroyMenuColumnHandler'
                 }]
                 
             }],
