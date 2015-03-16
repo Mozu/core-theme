308946823bf34b0748ac1a2383c169e07081c1dc
Jonathan <Jonathan_Kuchar.volusion.com>
2015-03-16 11:25:59 -0500

    Contract and Unit test update.

--- Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/OrderMapping.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/OrderMapping.cs
@@ -578,7 +578,8 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
                 //todo: temp to get unit test to pass - Greg Murray on 2014-05-20 
                 .ForMember(x => x.ProductDiscount, op => op.Ignore())
                 .ForMember(dc => dc.ProductUsage, op => op.Ignore()) // todo: xverify - Greg Murray on 2014-08-28 
-                .ForMember(dc => dc.HandlingAmount, op => op.Ignore()) // todo: xverify - Greg Murray on 2014-08-28 
+                .ForMember(dc => dc.HandlingAmount, op => op.Ignore()) // todo: xverify - Greg Murray on 2014-08-28
+                .ForMember(dc => dc.DutyAmount, op => op.Ignore())
             
 //                             ProductCode = orderItem.ProductCode,
 //                             ProductName = orderItem.ProductName,
