5f7f6fd9bd51f3addb80b51b616de49f7bc7c2df
Greg Murray <greg_murray@volusion.com>
2015-03-26 11:27:16 -0500

    #51581 refactored columns to share with location/inventory/index & LocationInventory.

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/location/inventory/InventoryStockColumns.js
@@ -0,0 +1,88 @@
+﻿/** 
+ * @class Taco.view.location.inventory.InventoryStockColumns
+ * common columns with Index & LocationInventory
+ * */
+Ext.define('Taco.view.location.inventory.InventoryStockColumns', {
+    singleton: true,
+
+    constructor: function(config) {
+        this.initConfig(config);
+    },
+    config: {
+        
+    },
+
+    getInventoryStockColumns: function (primaryKeyFieldName) {
+        var disableFieldOnCreate = function (field, editor, context) {
+            if (!context.record.get(primaryKeyFieldName)) {
+                field.disable();
+            } else {
+                field.enable();
+            }
+        };
+        return [
+            {
+                width: 100,
+                text: "Available",
+                stateId: 'stockAvailable',
+                dataIndex: 'stockAvailable'
+            }, {
+                width: 100,
+                text: 'On Reserve',
+                dataIndex: 'stockReserved',
+                stateId: 'stockReserved',
+                renderer: function(value) {
+                    return value || 0;
+                }
+            }, {
+                dataIndex: 'stockOnHandIncrement',
+                width: 100,
+                itemId: "stockOnHandIncrement",
+                stateId: 'stockOnHandIncrement',
+                text: 'Add',
+                editor: {
+                    emptyText: "Increment",
+                    msgTarget: "qtip",
+                    xtype: "numberfield",
+                    mouseWheelEnabled: false,
+                    selectOnFocus: true,
+                    allowBlank: true,
+                    onEditorShow: disableFieldOnCreate
+                }
+            }, {
+                dataIndex: 'stockOnHandDecrement',
+                width: 100,
+                itemId: "stockOnHandDecrement",
+                stateId: 'stockOnHandDecrement',
+                text: 'Reduce',
+                editor: {
+                    emptyText: "Decrement",
+                    msgTarget: "qtip",
+                    xtype: "numberfield",
+                    fieldStyle: "text-align:right;",
+                    mouseWheelEnabled: false,
+                    selectOnFocus: true,
+                    allowBlank: true,
+                    onEditorShow: disableFieldOnCreate
+                }
+            }, {
+                dataIndex: 'stockOnHand',
+                width: 100,
+                itemId: "stockOnHand",
+                stateId: 'stockOnHand',
+                text: 'On Hand Total',
+                editor: {
+                    emptyText: "On Hand",
+                    msgTarget: "qtip",
+                    xtype: "numberfield",
+                    hideTrigger: true,
+                    defaultValue: 0,
+                    mouseWheelEnabled: false,
+                    selectOnFocus: true,
+                    allowBlank: false
+                }
+            }
+        ];
+    }
+
+});
--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/location/inventory/LocationInventory.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/location/inventory/LocationInventory.js
@@ -8,7 +8,8 @@ Ext.define('Taco.view.location.inventory.LocationInventory', {
         'Taco.shared.view.field.ProductPickerField',
         'Taco.shared.view.field.LocationPickerField',
         'Taco.model.LocationInventory',
-        'Taco.store.LocationInventories'
+        'Taco.store.LocationInventories',
+        'Taco.view.location.inventory.InventoryStockColumns'
     ],
 
     // used by create button
@@ -85,145 +86,54 @@ Ext.define('Taco.view.location.inventory.LocationInventory', {
     },
 
     initColumnConfig: function () {
-        var me = this,
-            disableFieldOnCreate = function (field, editor, context) {
-                if (!context.record.get('locationCode')) {
-                    field.disable();
-                } else {
-                    field.enable();
-                }
-            };
+        var me = this;
 
+        this.columns = Taco.view.location.inventory.InventoryStockColumns.getInventoryStockColumns('locationCode');
+        this.columns.push({
+            xtype: 'taco.menucolumn',
+            text: 'Actions',
+            menuDisabled: true,
+            stateId: 'actionsColumn',
+            sortable: false,
+            menuItems: [
+                {
+                    text: 'Remove Inventory',
+                    /*
+                    requiredBehaviors: {
+                        model: 'Taco.model.LocationInventory',
+                        behavior:'destroy'
+                    },
+                    */
+                    //menuColumnHandler: 'destroyMenuColumnHandler',
+                    menuColumnHandler: function(item, eventData) {
+                        var record = eventData.record,
+                            store = eventData.grid.store;
 
-        this.columns = [
-            {
-                width: 100,
-                text: "Available",
-                stateId: 'stockAvailable',
-                dataIndex: 'stockAvailable'
-                /*
-            ,editor: {
-                emptyText: "Available",
-                msgTarget: "qtip",
-                xtype: "numberfield",
-                hideTrigger: true,
-                mouseWheelEnabled: false,
-                selectOnFocus: true,
-                allowBlank: false
-            }
-            */
-            }, {
-                width: 100,
-                text: 'On Reserve',
-                dataIndex: 'stockReserved',
-                stateId: 'stockReserved',
-                renderer: function (value) {
-                    return value || 0;
-                }
-                /*,
-            editor: {
-                emptyText: "On Reserve",
-                msgTarget: "qtip",
-                xtype: "numberfield",
-                hideTrigger: true,
-                mouseWheelEnabled: false,
-                selectOnFocus: true,
-                allowBlank: false
-            }
-            */
-            }, {
-                dataIndex: 'stockOnHandIncrement',
-                width: 100,
-                itemId: "stockOnHandIncrement",
-                stateId: 'stockOnHandIncrement',
-                text: 'Add',
-                editor: {
-                    emptyText: "Increment",
-                    msgTarget: "qtip",
-                    xtype: "numberfield",
-                    mouseWheelEnabled: false,
-                    selectOnFocus: true,
-                    allowBlank: true,
-                    onEditorShow: disableFieldOnCreate
-                }
-            }, {
-                dataIndex: 'stockOnHandDecrement',
-                width: 100,
-                itemId: "stockOnHandDecrement",
-                stateId: 'stockOnHandDecrement',
-                text: 'Reduce',
-                editor: {
-                    emptyText: "Decrement",
-                    msgTarget: "qtip",
-                    xtype: "numberfield",
-                    fieldStyle: "text-align:right;",
-                    mouseWheelEnabled: false,
-                    selectOnFocus: true,
-                    allowBlank: true,
-                    onEditorShow: disableFieldOnCreate
-                }
-            }, {
-                dataIndex: 'stockOnHand',
-                width: 100,
-                itemId: "stockOnHand",
-                stateId: 'stockOnHand',
-                text: 'On Hand Total',
-                editor: {
-                    emptyText: "On Hand",
-                    msgTarget: "qtip",
-                    xtype: "numberfield",
-                    hideTrigger: true,
-                    defaultValue: 0,
-                    mouseWheelEnabled: false,
-                    selectOnFocus: true,
-                    allowBlank: false
-                }
-            }, {
-                xtype: 'taco.menucolumn',
-                text: 'Actions',
-                menuDisabled: true,
-                stateId: 'actionsColumn',
-                sortable: false,
-                menuItems: [
-                    {
-                        text: 'Remove Inventory',
-                        /*
-                        requiredBehaviors: {
-                            model: 'Taco.model.LocationInventory',
-                            behavior:'destroy'
-                        },
-                        */
-                        //menuColumnHandler: 'destroyMenuColumnHandler',
-                        menuColumnHandler: function(item, eventData) {
-                            var record = eventData.record,
-                                store = eventData.grid.store;
-
-                            Taco.model.LocationInventory.removeInventory({
-                                records: [record],
-                                store: store
-                            });
-                        }
-                    }, {
-                        text: 'Edit Location',
-                        /*
-                        requiredBehaviors: {
-                            model: 'Taco.model.Product',
-                            behavior: 'update'
-                        },
-                        */
-                        menuColumnHandler: function(item, eventData) {
-                            var record = eventData.record;
-
-                            Ext.defer(function() {
-                                Taco.core.StateManager.attemptNavigate('locations/edit/' + record.get("locationCode"));
-                            }, 1, this);
-
-                        }
+                        Taco.model.LocationInventory.removeInventory({
+                            records: [record],
+                            store: store
+                        });
                     }
-                ]
-            }
-        ];
-        
+                }, {
+                    text: 'Edit Location',
+                    /*
+                    requiredBehaviors: {
+                        model: 'Taco.model.Product',
+                        behavior: 'update'
+                    },
+                    */
+                    menuColumnHandler: function(item, eventData) {
+                        var record = eventData.record;
+
+                        Ext.defer(function() {
+                            Taco.core.StateManager.attemptNavigate('locations/edit/' + record.get("locationCode"));
+                        }, 1, this);
+
+                    }
+                }
+            ]
+        });
+
         //if (me.showProductColumns) {
 
             this.columns.unshift(
