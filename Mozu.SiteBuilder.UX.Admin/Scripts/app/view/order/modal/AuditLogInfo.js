079a82d977b2f004b258a9116899bf56c262e68c
Jonathan <Jonathan_Kuchar.volusion.com>
2015-04-08 15:24:38 -0500

    Hide appName fix.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/modal/AuditLogInfo.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/modal/AuditLogInfo.js
@@ -66,7 +66,7 @@ Ext.define('Taco.view.order.modal.AuditLogInfo', {
                     data: userObject,
                     tpl: [
                         '<tpl if="userName && userName.length &gt; 0"><div><b>User:</b> {userName}</div></tpl>',
-                        '<div><b>Application:</b> {appName}</div>'
+                        '<tpl if="appName && appName.length &gt; 0"><div><b>Application:</b> {appName}</div></tpl>'
                     ]
                 }, {
                     flex: 0.5,
