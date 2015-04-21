37c07bc43385f90d9700f040a072cad823fa210a
Jimmy Sanford <jimmy_sanford@volusion.com>
2015-04-21 12:29:45 -0500

    switching payment endpoints in admin

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/modal/AddPayment.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/modal/AddPayment.js
@@ -348,8 +348,9 @@ Ext.define('Taco.view.order.modal.AddPayment', {
                 }
             },
             settings: {
-                apiBase: Taco.paymentApiBaseUrl,
-                framePath: "/../../Assets/pci_receiver.html",
+                // apiBase: Taco.paymentApiBaseUrl,
+                apiBase: 'https://payments.mozu-si.com/Mozu.PaymentService.WebApi/commerce/payments/cards/',
+                framePath: "/../../../Assets/pci_receiver.html",
                 siteId: me.record.get('siteId') || Taco.app.context.getSiteId(),
                 tenantId: me.record.get('tenantId') || Taco.app.context.getTenantId()
             }
