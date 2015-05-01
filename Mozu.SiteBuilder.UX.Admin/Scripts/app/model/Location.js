695779f5aff60ff802aca8b6f89283ce621a46c0
Greg Murray <greg_murray@volusion.com>
2015-05-01 14:47:27 -0500

    #60707 Added displayCode to Location & referenced on LocationPickerField

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/model/Location.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/model/Location.js
@@ -74,6 +74,13 @@ Ext.define('Taco.model.Location', {
             "type": "string",
             "useNull": true
         }, {
+            name: "displayCode",
+            type: "string",
+            convert: function (v, record) {
+                return record.get('code') + (record.get('isDisabled') ? ' (disabled)' : '');
+            },
+            persist: false
+        }, {
             "name": "description",
             "type": "string",
             "useNull": true
