69a9750e28c1533c42ce9bd48935be04fe7955bf
anup_ponnarassery <anup_ponnarassery@volusion.com>
2015-05-06 16:40:24 -0500

    paypal express redirect url #60675

--- Mozu.SiteBuilder.Mvc/Contexts/ClientApiContext.cs
+++ Mozu.SiteBuilder.Mvc/Contexts/ClientApiContext.cs
@@ -258,7 +258,7 @@ namespace Mozu.SiteBuilder.Mvc.Contexts
                         {
                              Id = "paypalExpress",
                              SkipRename= true,
-                            InternalUrl = settings.AppSettings("PaypalExpressUrl")
+                            InternalUrl = settings.AppSettings("PaypalExpressRedirectUrl")
                         }
 
 
