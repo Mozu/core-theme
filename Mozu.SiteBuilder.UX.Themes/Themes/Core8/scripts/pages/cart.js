9b442f6e67a985cb227a4a91024c77f7724cadb4
Ben <ben_cripps@volusion.com>
2015-05-19 10:44:52 -0500

    core8 all up in here

---
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core8/scripts/pages/cart.js
@@ -0,0 +1,102 @@
+﻿define(['modules/backbone-mozu', 'underscore', 'modules/jquery-mozu', 'modules/models-cart', 'modules/cart-monitor'], function (Backbone, _, $, CartModels, CartMonitor) {
+
+    var CartView = Backbone.MozuView.extend({
+        templateName: "modules/cart/cart-table",
+        initialize: function () {
+            var me = this;
+
+            //setup coupon code text box enter.
+            this.listenTo(this.model, 'change:couponCode', this.onEnterCouponCode, this);
+            this.codeEntered = !!this.model.get('couponCode');
+            this.$el.on('keypress', 'input', function (e) {
+                if (e.which === 13) {
+                    if (me.codeEntered) {
+                        me.handleEnterKey();
+                    }
+                    return false;
+                }
+            });
+        },
+        updateQuantity: _.debounce(function (e) {
+            var $qField = $(e.currentTarget),
+                newQuantity = parseInt($qField.val(), 10),
+                id = $qField.data('mz-cart-item'),
+                item = this.model.get("items").get(id);
+
+            if (item && !isNaN(newQuantity)) {
+                item.set('quantity', newQuantity);
+                item.saveQuantity();
+            }
+        },400),
+        removeItem: function(e) {
+            var $removeButton = $(e.currentTarget),
+                id = $removeButton.data('mz-cart-item');
+            this.model.removeItem(id);
+            return false;
+        },
+        empty: function() {
+            this.model.apiDel().then(function() {
+                window.location.reload();
+            });
+        },
+        proceedToCheckout: function () {
+            //commenting  for ssl for now...
+            //this.model.toOrder();
+            // return false;
+            this.model.isLoading(true);
+            // the rest is done through a regular HTTP POST
+        },
+        addCoupon: function () {
+            var self = this;
+            this.model.addCoupon().ensure(function () {
+                self.model.unset('couponCode');
+                self.render();
+            });
+        },
+        onEnterCouponCode: function (model, code) {
+            if (code && !this.codeEntered) {
+                this.codeEntered = true;
+                this.$el.find('#cart-coupon-code').prop('disabled', false);
+            }
+            if (!code && this.codeEntered) {
+                this.codeEntered = false;
+                this.$el.find('#cart-coupon-code').prop('disabled', true);
+            }
+        },
+        autoUpdate: [
+            'couponCode'
+        ],
+        handleEnterKey: function () {
+            this.addCoupon();
+        }
+    });
+
+    $(document).ready(function() {
+
+        var cartModel = CartModels.Cart.fromCurrent(),
+            cartViews = {
+
+                cartView: new CartView({
+                    el: $('#cart'),
+                    model: cartModel,
+                    messagesEl: $('[data-mz-message-bar]')
+                })
+
+            };
+
+        cartModel.on('ordercreated', function (order) {
+            cartModel.isLoading(true);
+            window.location = "/checkout/" + order.prop('id');
+        });
+
+        cartModel.on('sync', function() {
+            CartMonitor.setCount(cartModel.count());
+        });
+
+        window.cartView = cartViews;
+
+        CartMonitor.setCount(cartModel.count());
+
+    });
+
+});
