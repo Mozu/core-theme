b3fa7af25408b689b78dd04c6d9820659c0384a1
Ben <ben_cripps@volusion.com>
2015-06-10 15:07:05 -0500

    fix unit test descriptor

--- Mozu.SiteBuilder.UnitTests/Mvc/Tags/RequireScriptTests.cs
+++ Mozu.SiteBuilder.UnitTests/Mvc/Tags/RequireScriptTests.cs
@@ -22,14 +22,12 @@ namespace Mozu.SiteBuilder.UnitTests.Mvc.Tags
                 {
                     Name = "base case",
                     Template = @"{% require_script ""http://test.script.com/{0}""|string_format(siteId) %}",
-                    Expected = "" // empty string because a fetched script gets added to the render context.
                     ExpectedFunc = TestDescriptor.CompareLiteral(string.Empty) // empty string because a fetched script gets added to the render context.
                 },
                 new TestDescriptor
                 {
                     Name = "url has equals",
                     Template = @"{% require_script ""http://test.script.com/id={0}""|string_format(siteId) %}",
-                    Expected = "" // empty string because a fetched script gets added to the render context.
                     ExpectedFunc = TestDescriptor.CompareLiteral(string.Empty) // empty string because a fetched script gets added to the render context.
                 },
                 new TestDescriptor
@@ -37,7 +35,7 @@ namespace Mozu.SiteBuilder.UnitTests.Mvc.Tags
                     Name = "parent tag loops",
                     Template = @"{% block title-tag-content %}{% parent %}parent{% endblock title-tag-content %}",
                     Context = new Dictionary<string,object>(),
-                    Expected = "parent"
+                    ExpectedFunc = TestDescriptor.CompareLiteral("parent")
                 }
 
             };
