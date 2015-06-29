ed8ef354a94b2c8a73412b5b74b84b5c90c30923
Jonathan <Jonathan_Kuchar.volusion.com>
2015-06-29 14:55:26 -0500

    Added skeleton order status controller Feature: #57266 PBI(s): #60008 Task: #63209

---
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/OrderStatus.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Mozu.CommerceRuntime.Contracts.Clients;
+using Mozu.Core;
+using Mozu.Core.Api.Client;
+using Mozu.Customer.Contracts.Clients;
+using Mozu.SiteBuilder.Mvc;
+using Mozu.SiteBuilder.Mvc.ActionFilters;
+using Mozu.SiteBuilder.Mvc.Customers;
+using Mozu.SiteBuilder.Mvc.Security;
+using Mozu.SiteBuilder.UX.Controllers;
+using Mozu.SiteBuilder.UX.Models.Admin.CMS;
+using Mozu.SiteBuilder.UX.Models.Customers;
+
+using PasswordInfo = Mozu.SiteBuilder.UX.Models.Customers.PasswordInfo;
+using Mozu.SiteBuilder.Mvc.Extensions;
+using Mozu.SiteBuilder.UX.Filters;
+using Mozu.Core.Actions;
+
+namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
+{
+    [ContextInitialization]
+    [HotOnlyAuthActionFilter]
+    [SslOnlyActionFilter]
+    [DataViewModeEnforcement]
+    [ActionExtensionFilter(actionId: ActionFilterConstants.GlobalPageAfterAction, executionType: ActionExtensionExecutionTypes.AfterController)]
+    [ActionExtensionFilter(actionId: ActionFilterConstants.GlobalPageBeforeAction, executionType: ActionExtensionExecutionTypes.BeforeController)]
+    public class OrderStatus : BaseApiController
+    {
+        
+        private readonly IOrderWebApiClient _orderWebApiClient;
+
+        public OrderStatus(ICustomerRepository customerRepository, ICustomerAccountWebApiClient customerAccountWebApiClient, IAccountContactRepository accountContactRepository,  IOrderWebApiClient orderWebApiClient, IWishlistWebApiClient wishlistWebApiClient, ICreditWebApiClient creditWebApiClient, IReturnWebApiClient returnApiClient, IAuthenticationHelper authenticationHelper, ISiteBuilderApiContext apiContext)
+        {
+            _orderWebApiClient = orderWebApiClient;
+            
+        }
+
+        //todo:hyper  remiplement auth att.
+        // [SiteBuilderAuthorize()]
+        [HttpGet]
+        public async Task<HttpResponseMessage> Index()
+        {
+
+            var pc = this.PageContext;
+            pc.CmsContext = new CmsPageContext()
+            {
+                Template = new DocumentRequest()
+                {
+                    Path = "order-status",
+                    DocumentTypeFQN = "pageTemplateContent@mozu"
+                }
+
+            };
+            pc.PageType = "order_status";
+
+            return this.Request.CreateResponse(HttpStatusCode.OK,  View("order-status", new object()));
+        }
+    }
+}
