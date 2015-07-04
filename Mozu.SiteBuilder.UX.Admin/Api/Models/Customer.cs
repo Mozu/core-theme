1206f0acf0de3b55f3acbd91759e4d0116d2c059
Sparkel Phipps <thomas_phipps@volusion.com>
2015-07-03 19:06:30 -0500

    adding missing mappings for customer and category

--- Mozu.SiteBuilder.UX.Admin/Api/Models/Customer.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Mozu.Core.Api.Contracts.Client;
+
 using Newtonsoft.Json;
 using DC = Mozu.Customer.Contracts;
 
@@ -129,5 +130,10 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.Models
         public bool IsDisabled { get; set; }
 
         public DateTime CustomerSinceDate { get; set; }
+
+        public DC.CurrencyAmount LifetimeValue { get; set; }
+
+     
+        public DateTime? LifeTimeValueSetDate { get; set; }
     }
 }
