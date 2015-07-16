c8dbe7f0a211b6fa6a0983cb5176408f5d48e301
Greg Murray <greg_murray@volusion.com>
2015-07-16 10:32:44 -0500

    Remove duplicate redemption label per Misha

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/discount/LimitationsForm.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/discount/LimitationsForm.js
@@ -59,7 +59,7 @@ Ext.define('Taco.view.discount.LimitationsForm', {
                 hideTrigger: true,
                 width: 250,
                 margin: "0px 20px 0px 0px",
-                fieldLabel: 'Total Number of Redemptions ' + (this.record.get('currentRedemptionCount') ? '<br/><i>(current redemptions:&nbsp;' + this.record.get('currentRedemptionCount') + '</i>)' : ''),
+                fieldLabel: 'Total Number of Redemptions',
                 emptyText: 'Unlimited',
                 minValue: 0
             }))
