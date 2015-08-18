a1dc56ad360b169fba5bec774f9becea4bbe1d81
greg_murray <greg_murray@volusion.com>
2015-08-18 10:45:10 -0500

    #66664 AMAZONPAY code changed to PayByAmazon without communication.

--- Mozu.SiteBuilder.UX.Admin/Api/DiscountController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/DiscountController.cs
@@ -161,8 +161,8 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
             var displayNames = new List<KeyValuePair<string, string>>
             {
                 new KeyValuePair<string, string>(SiteSettings.Order.Contracts.Constants.ThirdPartyPayment.PAYPAL_EXPRESS.ToUpper(), "PayPal Express"),
-                new KeyValuePair<string, string>("VISACHECKOUT", "Visa Checkout"),
-                new KeyValuePair<string, string>("AMAZONPAY", "Amazon Pay"),
+                new KeyValuePair<string, string>(SiteSettings.Order.Contracts.Constants.ThirdPartyPayment.VISA_CHECKOUT.ToUpper(), "Visa Checkout"),
+                new KeyValuePair<string, string>("PayByAmazon", "Amazon Pay")
             };
             var paymentSettings = (await _checkoutSettingsClient.GetPaymentSettings()).ReadAsSync();
 
