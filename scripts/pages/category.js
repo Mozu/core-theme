478057ecab06c7f5097c8dc6dd6f4e9a6730d168
Taco <travis_johnson@volusion.com>
2015-08-24 10:55:00 -0500

    initial implementation of seo-friendly faceting
    initial work on make_url tag and partial view
    
    fixing back button bug
    
    Added make_url to image widget
    
    fixing partial url parameter stripping
    
    Bug #68818 - Fixed faceting
    
    add search and cart make_url functionality so that rendered urls take on the custom routes. fixes #68624
    
    fix #67721 by fixing a typo in the image.hypr template
    
    TFS #67464 fixing pagenumbers so current page isn't a link
    
    removing native reduce for IE8 compat
    
    TFS #69175 adding logic to display top level categories on search facets
    
    updating modernizr
    
    TFS #69175 use full postback in IE8 instead of history API

--- scripts/pages/category.js
+++ scripts/pages/category.js
@@ -1,13 +1,8 @@
 define(['modules/jquery-mozu', "modules/views-collections"], function($, CollectionViewFactory) {
-
     $(document).ready(function() {
-
         window.facetingViews = CollectionViewFactory.createFacetedCollectionViews({
             $body: $('[data-mz-category]'),
-            $facets: $('[data-mz-facets]'),
-            data: require.mozuData('facetedproducts')
+            template: "category-interior"
         });
-
     });
-
 });
