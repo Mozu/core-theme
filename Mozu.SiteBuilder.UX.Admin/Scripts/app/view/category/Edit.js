45361493646df0abe59186ad9a9c54a3f57bfa95
Simeon Kessler <simeon_kessler@volusion.com>
2015-09-10 18:22:30 -0500

    added view category in site to more button of category editor

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Edit.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Edit.js
@@ -11,5 +11,101 @@ Ext.define('Taco.view.category.Edit', {
     doCreate: function () {
         var controller = "categories"
         Taco.app.StateManager.attemptNavigate(controller + '/create');
-    }
+    },
+    initComponent: function () {
+        var me = this;
+
+        this.additionalActions = [            
+            {
+                xtype: 'button',
+                itemId: 'moreButton',
+                ui: 'action',
+                scale: 'medium',
+                text: 'More',
+                menuAlign: 'tr-br?',
+                menu: {
+                    plain: true,
+                    shadow: false,
+                    items: [{
+                        itemId: 'live',
+                        text: 'View Live',
+                        menu: {
+                            plain: true,
+                            shadow: false,
+                            items: []
+                        }
+                    }, {
+                        itemId: 'preview',
+                        text: 'View Staged',
+                        menu: {
+                            plain: true,
+                            shadow: false,
+                            items: []
+                        }
+                    }
+                    ],
+                    listeners: {
+                        show: function (menu) {
+                            var previewItem = menu.items.get('preview'),
+                                liveItems = menu.items.get('live'),
+                                previewMenu,
+                                liveMenu,
+                                previewSites = [],
+                                liveSites = [];
+
+
+                            var ctx = Taco.app.context.getCurrentContext();
+
+                            if (previewItem && previewItem.menu) {
+                                previewMenu = previewItem.menu;
+                                liveMenu = liveItems.menu;
+                                debugger;
+
+                                
+                                
+                                    var sites = ctx.sites;
+                                    Ext.each(sites, function (site) {
+                                        if (site.isMozuRendered) {
+                                            previewSites.push({
+                                                itemId: site.id,
+                                                text: site.name,
+                                                handler: Ext.bind(me.viewInSite, me, [site, 'preview'])
+                                            });
+
+                                            liveSites.push({
+                                                itemId: site.id,
+                                                text: site.name,
+                                                handler: Ext.bind(me.viewInSite, me, [site, 'live'])
+                                            });
+                                        }
+                                    });
+                                
+
+
+
+                                if (!Ext.Array.equals(Ext.Array.pluck(previewSites, 'itemId'), previewMenu.items.keys)) {
+                                    previewMenu.removeAll();
+                                    previewMenu.add(previewSites);
+                                    liveMenu.removeAll();
+                                    liveMenu.add(liveSites);
+                                }
+                                
+
+
+                                
+                            }
+                        },
+                        scope: this
+                    }
+                }
+            }];
+
+
+
+        this.callParent(arguments);
+
+    },
+    viewInSite: function (site, env) {
+        window.open('/_gosite/' + site.id + '?environment=' + env + '&redir=' + encodeURIComponent('/c/' + this.record.getId()));
+    },
 });
