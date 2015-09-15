7651c8935286446ce9be58886bb332bb3ecac420
Chet Husk <chet_husk@volusion.com>
2015-09-15 14:36:55 -0500

    add categorycontent active date range editors because convenience

---
+++ Mozu.SiteBuilder.UX.Themes/Themes/Demo2/admin/editors/categoryContent.js
@@ -0,0 +1,24 @@
+﻿Ext.widget({
+    xtype: 'mz-form-categoryPage',
+    initComponent: function () {
+
+        this.containers.push({
+            xtype: 'panel',
+            collapsible: 'true',
+            ui: 'subform',
+            title: 'Active Date Range',
+            itemId: 'activeDateRangePanel',
+
+            items: [{
+                xtype: "mz-input-date",
+                name: "document.startDate",
+                fieldLabel: 'Start Date'
+            }, {
+                xtype: "mz-input-date",
+                name: "document.endDate",
+                fieldLabel: 'End Date'
+            }]
+        });
+        this.superclass.initComponent.apply(this, arguments);
+    }
+});
