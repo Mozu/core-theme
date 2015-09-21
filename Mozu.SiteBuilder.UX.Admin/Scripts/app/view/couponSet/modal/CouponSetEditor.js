14ca5df927db34770ed4496305a7753abed5a5b8
Brandon Jernigan <brandon_jernigan@volusion.com>
2015-09-21 13:37:34 -0500

    updated coupon code create with proper error message on reponse code 409 (conflict)

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/couponSet/modal/CouponSetEditor.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/couponSet/modal/CouponSetEditor.js
@@ -291,8 +291,9 @@ Ext.define('Taco.view.couponSet.modal.CouponSetEditor', {
         }
         this.record.save({
             success: onSuccess,
-            failure: function() {
-                Taco.app.fireEvent('setmessage', 'There was an error saving the Coupon Set', 'error');
+            failure: function(item, response) {
+                var message = (response.error.status == 409) ? 'That code prefix is already in use' : 'There was an error saving the Coupon Set';
+                Taco.app.fireEvent('setmessage', message, 'error');
             },
             scope: me
         });
