9ec7a704e8df3c8322fcb136e5664e1dcd6f1459
forward15 <costin_iorgulescu@L02483.corp.volusion.com>
2015-10-02 14:29:44 -0500

    Revert "Revert "Add Cancel option for 3rd party payment providers (non-Mozu checkout)""
    This reverts commit 6c7f722b509f83ab80dff55c14dad9a3c44c2eda.

--- scripts/modules/models-checkout.js
+++ scripts/modules/models-checkout.js
@@ -12,7 +12,7 @@ define([
     function ($, _, Hypr, Backbone, api, CustomerModels, AddressModels, PaymentMethods, HyprLiveContext) {
 
         var CheckoutStep = Backbone.MozuModel.extend({
-            helpers: ['stepStatus', 'requiresFulfillmentInfo', 'requiresDigitalFulfillmentContact'],  //
+            helpers: ['stepStatus', 'requiresFulfillmentInfo','isMozuCheckout', 'requiresDigitalFulfillmentContact'],  //
             // instead of overriding constructor, we are creating
             // a method that only the CheckoutStepView knows to
             // run, so it can run late enough for the parent
@@ -58,6 +58,13 @@ define([
             requiresFulfillmentInfo: function () {
                 return this.getOrder().get('requiresFulfillmentInfo');
             },
+            isMozuCheckout: function() {
+                var activePayments = this.getOrder().apiModel.getActivePayments();
+                return !(activePayments && (!!_.findWhere(activePayments, { paymentType: 'CreditCard' }) 
+                || !!_.findWhere(activePayments, { paymentType: 'Check' }) 
+                || !!_.findWhere(activePayments, { paymentType: 'PaypalExpress' } 
+                || !!_.findWhere(activePayments, { paymentType: 'VisaCheckout' }))));
+            },
             requiresDigitalFulfillmentContact: function () {
                 return this.getOrder().get('requiresDigitalFulfillmentContact');
             },
@@ -66,6 +73,15 @@ define([
             },
             next: function () {
                 if (this.submit()) this.isLoading(true);
+            },
+            cancelStep: function() {
+                var me = this,
+                order = me.getOrder();
+                me.isLoading(true);
+                order.apiModel.get().ensure(function(){
+                    me.isLoading(false);
+                    return me.stepStatus("complete");
+                });
             }
         }),
 
@@ -1466,7 +1482,12 @@ define([
                     isAuthenticated = require.mozuData('user').isAuthenticated,
                     nonStoreCreditTotal = billingInfo.nonStoreCreditTotal(),
                     requiresFulfillmentInfo = this.get('requiresFulfillmentInfo'),
-                    requiresBillingInfo = nonStoreCreditTotal > 0,
+                    requiresBillingInfo = nonStoreCreditTotal > 0;
+                    
+                    if (!this.isMozuCheckout()) {
+                        billingContact.set("address", null);
+                    }
+
                     process = [function() {
                         return order.update({
                             ipAddress: order.get('ipAddress'),
