b29ce98b84a207fd72cd3886ea083959f35e6059
greg_murray <greg_murray@volusion.com>
2015-10-23 13:13:13 -0500

    #69180 Updated controller to point to modal, included a folder in project.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/controller/StoreFrontProducts.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/controller/StoreFrontProducts.js
@@ -6,7 +6,7 @@ Ext.define('Taco.controller.StoreFrontProducts', {
     extend: 'Taco.core.Controller',
     models: ['Taco.model.StorefrontProduct'],
     stores: ['Taco.store.StorefrontProducts'],
-    views: ['Taco.view.storefrontProduct.Grid'],
+    views: ['Taco.view.storefrontProduct.modal.StorefrontProductModal'],
     modelName: 'StoreFrontProduct'
     
     
