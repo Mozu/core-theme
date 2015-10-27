6d5ba821c6dc2c45de5401fded705d2cb7a559f3
Brandon Jernigan <brandon_jernigan@volusion.com>
2015-10-27 16:11:10 -0500

    added combobox and selectbox for picking/placing products in pinned grid

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/searchTuningRule/PinnedProductForm.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/searchTuningRule/PinnedProductForm.js
@@ -29,15 +29,90 @@ Ext.define('Taco.view.searchTuningRule.PinnedProductForm', {
             enableSearch: false
         });
 
+
+        var productStore = Taco.core.data.StoreManager.getOrCreate({
+            type: 'Taco.store.Products',
+            createOnly: true,
+            autoLoad: true,
+            clearFilters: false,
+            remoteFilter: false,
+            filters: function (record) {
+                //return Ext.Array.indexOf((me.get('categories') || []), record.getId()) > -1;
+            }
+        });
+
+        // MultiSelect is the most optimal Field that uses BoundList without a trigger
+        me.productList = Ext.widget({
+            xtype: 'combobox',
+            name: 'categoryFilters',
+            flex: 1,
+            emptyText: 'Search for products',
+            margin: '10 10 10 0',
+            store: productStore,
+            getStore: function () {
+                return productStore;
+            },
+            queryMode: 'remote',
+            lastQuery: '',
+            hideTrigger: true,
+            triggerOnClick: true,
+            forceSelection: true,
+            disableKeyFilter: true,
+            typeAhead: true,
+            displayField: 'productName',
+            valueField: 'productCode',
+            style: {
+                display: 'inline-table',
+                verticalAlign: 'bottom'
+            },
+            listeners: {
+                select: function (cmp, record) {
+                    debugger; // position key?
+                    me.pinnedGrid.fireEvent('recordadded', record);
+                    cmp.setValue('');
+                },
+                scope: this
+            }
+        });
+
+        me.placementSelect = Ext.create('Ext.form.ComboBox', {
+            queryMode: 'local',
+            displayField: 'text',
+            valueField: 'value',
+            forceSelection: true,
+            margin: '10 0 10 10',
+            store: Ext.create('Ext.data.Store', {
+                fields: ['text', 'value'],
+                data: [
+                    { text: 'Insert at bottom', value: 'bottom' },
+                    { text: 'Insert at top', value: 'top' },
+                    { text: 'Insert above selected', value: 'above' },
+                    { text: 'Insert below selected', value: 'below' }
+                ]
+            })
+        })
+
+        me.productForm = Ext.create('Ext.container.Container', {
+            layout: {
+                type: 'hbox',
+                align: 'left'
+            },
+            defaults: {
+                flex: 1
+            },
+            items: [ me.productList, me.placementSelect]
+        })
+
         me.items = [
             {
                 xtype: 'fieldcontainer',
-                layout: 'hbox',
+                layout: 'fit',
                 width: '100%',
                 defaults: {
                     flex: 1
                 },
                 items: [
+                    me.productForm,
                     me.pinnedGrid
                 ]
             }
