8f863a28741e43cb3165b43af4aafa764e45d971
Jonathan <Jonathan_Kuchar.volusion.com>
2015-10-28 15:25:10 -0500

    Feature #67435 Clean up commit for spacing, look and feel, etc.

--- Mozu.SiteBuilder.UX.Themes/Themes/Core9/scripts/pages/checkout.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core9/scripts/pages/checkout.js
@@ -8,7 +8,6 @@ require(["modules/jquery-mozu", "underscore", "hyprlive", "modules/backbone-mozu
             // wait for blur validation to complete
             var me = this;
             me.editing.savedCard = false;
-            me.editing.updateSavedCard = false;
             _.defer(function () {
                 me.model.next();
             });
@@ -199,13 +198,6 @@ require(["modules/jquery-mozu", "underscore", "hyprlive", "modules/backbone-mozu
                     me.render();
                 });
             }
-        },
-        beginUpdateCard: function () {
-            // This will trigger the edit on the saved card, not edit the payment.
-            var me = this;
-            this.editing.savedCard = true;
-            this.editing.updateSavedCard = true;
-            this.render();
         },
         beginEditingBillingAddress: function() {
             this.editing.savedBillingAddress = true;
