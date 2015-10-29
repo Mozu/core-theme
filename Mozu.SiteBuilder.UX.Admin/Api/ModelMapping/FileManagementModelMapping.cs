4398a5181e833b0570c75f62eb79978a6db7c0e8
Chet Husk <chet_husk@volusion.com>
2015-10-29 14:47:08 -0500

    add function for scrubbing the content mime types into something the frontend can understand

--- Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/FileManagementModelMapping.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/FileManagementModelMapping.cs
@@ -23,13 +23,10 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
         {
             Mapper.CreateMap<Document, FileManagementFile>()
                 .ForMember(x => x.dateModified, op => op.ResolveUsing(x => x.UpdateDate))
-               // .ForMember(x => x.folderId, op => op.ResolveUsing(x => x.FolderId))
-                .ForMember(x => x.fileType, op => op.ResolveUsing(x => x.Extension))
+                .ForMember(x => x.fileType, op => op.ResolveUsing((Document x) => DeriveMimeType(x.ContentMimeType)))
                 .ForMember(x => x.id, op => op.ResolveUsing(x => x.Id))
                 .ForMember(x => x.name, op => op.ResolveUsing(x => x.Name))
                 .ForMember(x => x.tags, op => op.ResolveUsing(_ => _.Get<string[]>("tags")))
-
-                //.ForMember(x => x.thumbnail, op => op.ResolveUsing(DoThumb))
                 .ForMember(x => x.isUploaded, op => op.ResolveUsing(x => x.ContentLength.GetValueOrDefault(0) > 0))
                 .ForMember(x => x.width, op => op.ResolveUsing(_ => _.Get<double>("width")))
                 .ForMember(x => x.height, op => op.ResolveUsing(_ => _.Get<double>("height")))
@@ -48,60 +45,19 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
                 .ForMember(x => x.id, op => op.ResolveUsing(x => x.Id))
                 .ForMember(x => x.name, op => op.ResolveUsing(x => x.Name))
                 .ForMember(x => x.parentId, op => op.ResolveUsing(x => x.ParentId))
-                //ignore
                 .ForMember(x => x.leaf, op => op.Ignore())
                 .ForMember(x => x.items, op => op.Ignore())
                 .ForMember(x => x.expanded, op => op.Ignore())
                 ;
-               // .ForMember(x => x.leaf, op => op.ResolveUsing(x => x.Children == null || x.Children.Count == 0))
-               // .ForMember(x => x.items, op => op.ResolveUsing(x => x.Children));
-
-                
         }
-        //static string DoDate(Document doc)
-        //{
-        //    var dd = doc.InsertDate.Value;
-        //    var now = DateTime.Now;
 
-        //    if (dd.Date == now.Date)
-        //    {
-        //        return "Today";
-        //    }
-        //    return dd.ToLongDateString();
-
-        //}
-        //const long MB = 1048576;
-        //const long KB = 1024;
-        //static string DoFileSize( Document doc )
-        //{
-        //    long size = doc.ContentSummary != null ? doc.ContentSummary.Length.Value  : 0;
-        //    if (size >= MB)
-        //    {
-        //        return Math.Round( (decimal)size/(decimal)MB, 1) + " MB";
-                 
-        //    }
-        //     return Math.Round( (decimal)size/(decimal)KB, 1) + " KB";
-            
-            
-        //}
-        //static string DoThumb(Document doc)
-        //{
-        //    switch ((doc.Extension ?? "").ToLower())
-        //    {
-        //        case "gif":
-        //        case "jpg":
-        //        case "jpeg":
-        //        case "png": {
-        //            if (doc.ContentLength .GetValueOrDefault (0) < 1)
-        //            {
-        //                return "/admin/Scripts/resources/images/legacy/AddPhotos.png";
-        //            }
-        //            return "/admin/img/files/" + doc.Id;
-        //        }
-        //    }
-        //    return "/admin/Scripts/resources/images/file-icon.png";
-
-            
-        //}
+        string DeriveMimeType(string contentMimeType)
+        {
+            if (contentMimeType.IsNullOrEmpty()) return "Unknown";
+            else if (contentMimeType.EndsWith("octet-stream", StringComparison.OrdinalIgnoreCase)) return "Binary";
+            var slashindex = contentMimeType.IndexOf('/');
+            if(slashindex == -1 || slashindex + 1 == contentMimeType.Length) return "Unknown"; // if the '/' is at the end of the string or not there at all
+            else return contentMimeType.Substring(slashindex + 1);
+        }
     }
 }
