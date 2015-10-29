89f43e708fc1c088bf761fc11884807c22a6f1f9
wayne_hearn <wayne_hearn@volusion.com>
2015-10-29 17:18:07 -0500

    adding logic for product preview to dip into price range when price is null

--- Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/StorefrontProductModelMapping.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/StorefrontProductModelMapping.cs
@@ -14,8 +14,12 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
             base.Configure();
             Mapper.CreateMap<ProductRuntime.Contracts.Product, StorefrontProduct>()
                 .ForMember(d=>d.Name, o=>o.ResolveUsing(s=> s.Content != null ? s.Content.ProductName : string.Empty))
-                .ForMember(d=>d.Price, o=>o.ResolveUsing(s=>s.Price != null ? s.Price.CatalogListPrice : null))
-                .ForMember(d=>d.SalePrice, o=>o.ResolveUsing(s=>s.Price != null ? s.Price.CatalogSalePrice : null))
+                .ForMember(d=>d.Price, o=>o.ResolveUsing(s => s.Price != null
+                    ? s.Price.CatalogListPrice
+                    : (s.PriceRange != null ? (s.PriceRange.Lower != null ? s.PriceRange.Lower.Price : null  ): ((decimal?)null))))
+                .ForMember(d=>d.SalePrice, o=>o.ResolveUsing(s=> s.Price != null
+                    ? s.Price.SalePrice
+                    : (s.PriceRange != null ? (s.PriceRange.Lower != null ? s.PriceRange.Lower.SalePrice : null) : ((decimal?)null))))
                 ;
         }
 
