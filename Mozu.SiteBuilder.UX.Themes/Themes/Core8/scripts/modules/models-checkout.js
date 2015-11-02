244213b5bf058e5e759af257b4ebb06c818c7de0
Jonathan <Jonathan_Kuchar.volusion.com>
2015-11-02 17:30:46 -0600

    Bug #71742 - Fix for CVV validation!

--- Mozu.SiteBuilder.UX.Themes/Themes/Core8/scripts/modules/models-checkout.js
+++ Mozu.SiteBuilder.UX.Themes/Themes/Core8/scripts/modules/models-checkout.js
@@ -902,16 +902,7 @@
                     this.get('card').set('isVisaCheckout', currentPayment.paymentWorkflow.toLowerCase() === 'visacheckout');
                 }
 
-                // when we are using the saved card, validation is only to make sure we have one selected.
-                var val = null;
-                if (!order.get('billingInfo.usingSavedCard')) {
-                    val = this.validate();
-
-                // the second condition is to make sure that saved credit card is the operation.
-                } else if (order.get('billingInfo.usingSavedCard') && !this.get('savedPaymentMethodId')) {
-                    var missingSavedCardErrorMsg = Hypr.getLabel('selectASavedCard');
-                    val = { 'card.saved': missingSavedCardErrorMsg };
-                }
+                var val = this.validate();
 
                 if (this.nonStoreCreditTotal() > 0 && val) {
                     // display errors:
