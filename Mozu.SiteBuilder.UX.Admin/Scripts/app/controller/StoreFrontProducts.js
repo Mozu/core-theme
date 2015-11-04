7650122b57ef92da796055f6a2cfdc80caf2c48c
Ben <ben_cripps@volusion.com>
2015-11-04 17:50:24 -0600

    Fix build

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/controller/StoreFrontProducts.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/controller/StoreFrontProducts.js
@@ -6,9 +6,6 @@ Ext.define('Taco.controller.StoreFrontProducts', {
     extend: 'Taco.core.Controller',
     models: ['Taco.model.StorefrontProduct'],
     stores: ['Taco.store.StorefrontProducts'],
-    views: ['Taco.view.storefrontProduct.modal.StorefrontProductModal'],
-    modelName: 'StoreFrontProduct'
-    
-    
+    modelName: 'StoreFrontProduct' 
 });
 
