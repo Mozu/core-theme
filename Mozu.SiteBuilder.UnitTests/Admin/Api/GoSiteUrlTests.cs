2540031a5b0230196924935a642c4e63c989d00f
Chet Husk <chet_husk@volusion.com>
2015-11-06 13:51:42 -0600

    rework gosite to understand potential absolute urls being passed in as the redirect

---
+++ Mozu.SiteBuilder.UnitTests/Admin/Api/GoSiteUrlTests.cs
@@ -0,0 +1,33 @@
+﻿using Mozu.SiteBuilder.UX.Areas.Misc.Controllers;
+using NUnit.Framework;
+using Should;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozu.SiteBuilder.UnitTests.Admin.Api
+{
+    [TestFixture, Category("gosite")]
+    public class GoSiteUrlTests
+    {
+        [TestCaseSource("tests")]
+        public void UriGetsCreatedAsExpected(string input, string newhostname, bool dohostnameredirect, string expected)
+        {
+            TestingController.CreateRedirectUrl(input, newhostname, dohostnameredirect).ToString().ShouldEqual(expected, StringComparer.OrdinalIgnoreCase);
+        }
+
+        private static IEnumerable<object[]> tests()
+        {
+            yield return new object[] { "/test/foo?lol=true", "laughs.com", true, "http://laughs.com/test/foo?lol=true" };
+            yield return new object[] { "/test/foo?lol=true", "laughs.com", false, "~/test/foo?lol=true" };
+            yield return new object[] { "http://whutwhut.com/test/foo?lol=true", "laughs.com", true, "http://laughs.com/test/foo?lol=true" };
+            yield return new object[] { "https://whutwhut.com/test/foo?lol=true", "laughs.com", true, "https://laughs.com/test/foo?lol=true" };
+            yield return new object[] { "https://whutwhut.com/test/foo?lol=true", "laughs.com", false, "https://laughs.com/test/foo?lol=true" };
+            yield return new object[] { null, "laughs.com", true, "http://laughs.com" };
+            yield return new object[] { null, "laughs.com", false, "~/" };
+            yield return new object[] { "foo/bar", "laughs.com", false, "~/foo/bar" };
+        }
+    }
+}
