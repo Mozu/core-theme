8c2074aeb192121f68babc1f082bf6a6b89160fa
Chet Husk <chet_husk@volusion.com>
2015-11-06 11:34:47 -0600

    fix redirect to login scopetype bug

--- Mozu.SiteBuilder.UX.Admin/MessageHandlers/AuthRedirectMessageHandler.cs
+++ Mozu.SiteBuilder.UX.Admin/MessageHandlers/AuthRedirectMessageHandler.cs
@@ -48,7 +48,8 @@ namespace Mozu.SiteBuilder.UX.Admin.MessageHandlers
             // else redirect to the login app!
             var res = new HttpResponseMessage(HttpStatusCode.Redirect);
             var handledByRp = IsHandledByRP(request);
-            var tenantId = request.Resolve<IApiContext>().TenantId;
+            var context = request.Resolve<IApiContext>();
+            var tenantId = context.TenantId == -1 ? (int?)null : context.TenantId;
 
             var loginAppRouter = new LoginAppRouteHelper(request.Resolve<ISettings>().LoginPath);
             var postback = !handledByRp ? "http://" + request.Headers.GetValues("host").First() + "/admin/auth/pants" : "";
