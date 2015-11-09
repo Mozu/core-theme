9121561e6248304dd91bb0ac4173c05928bef3a7
Ross Libby <ross_libby@volusion.com>
2015-11-09 15:03:43 -0600

    #62236 fixed image uploading preview image

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/website/widgetEditors/v2/Image.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/website/widgetEditors/v2/Image.js
@@ -556,7 +556,10 @@ Ext.define('Taco.view.website.widgetEditors.v2.Image', {
 
     handleUploadLink: function (files) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         this.linkStore.removeAll();
+=======
+>>>>>>> b1c2b68... #62236 enabled image uploading for click-through link
 =======
 >>>>>>> b1c2b68... #62236 enabled image uploading for click-through link
         this.down('#linkField').onUploadFile(files);
