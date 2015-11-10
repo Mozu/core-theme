d17b29a90a163909fa6ee7e42c6d01a1fa253fe3
anup ponnarassery <anup_ponnarassery@volusion.com>
2015-11-10 17:06:30 -0600

    #72367 tests for gosite

--- Mozu.SiteBuilder.UnitTests/Admin/Api/GoSiteUrlTests.cs
+++ Mozu.SiteBuilder.UnitTests/Admin/Api/GoSiteUrlTests.cs
@@ -29,6 +29,8 @@ namespace Mozu.SiteBuilder.UnitTests.Admin.Api
             yield return new object[] { null, "laughs.com", false, "~/" };
             yield return new object[] { "foo/bar", "laughs.com", false, "~/foo/bar" };
             yield return new object[] { "foo/bar?t=%2f", "laughs.com", false, "~/foo/bar?t=%2f" };
+            yield return new object[] { "/foo/bar?t=%2fblah%2b", "laughs.com", true, "http://laughs.com/foo/bar?t=%2fblah%2b" };
+            //yield return new object[] { "%2fback-office%2forders%2f07427f37157c2821b82431570000179a%3ft%3dNx9lavdBfmLnQCcE1PNGt8252PD7fZavoEFZkDycZODTzwTSeQ3cPoODggStSpQEM6PIH3KQFcMJsj6XGZu0NfdDo3c3EGOeiUpbX3wW4p%252fmC3OGqDBRo4DyzHAN1IhAun2DetuVPpivDcIl%252buWlNMNRG4vDqVl%252fYSCO84AjTR09cvyFCHh7vhPfJpSJupy%252b5gqsxi9JTO%252ba4W2u8sqjUfPloDeEcqrqo1nYHuVMXEMe510IMbk09F3BkcbpHqapzeu0I3C%252bSfyJD2kGvH1b%252bg%253d%253d", "laughs.com", true, "http://laughs.com/back-office/orders/07427f37157c2821b82431570000179a?t=Nx9lavdBfmLnQCcE1PNGt8252PD7fZavoEFZkDycZODTzwTSeQ3cPoODggStSpQEM6PIH3KQFcMJsj6XGZu0NfdDo3c3EGOeiUpbX3wW4p%252fmC3OGqDBRo4DyzHAN1IhAun2DetuVPpivDcIl%252buWlNMNRG4vDqVl%252fYSCO84AjTR09cvyFCHh7vhPfJpSJupy%252b5gqsxi9JTO%252ba4W2u8sqjUfPloDeEcqrqo1nYHuVMXEMe510IMbk09F3BkcbpHqapzeu0I3C%252bSfyJD2kGvH1b%252bg%253d%253d" };
         }
     }
 }
