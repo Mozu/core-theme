c37a355107ace5eed8f576257cc0060ee753a370
Ben <ben_cripps@volusion.com>
2015-11-11 10:51:47 -0600

    Removing dataset attribute from all the codes #72746

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Index.js
@@ -214,6 +214,7 @@ Ext.define('Taco.view.category.Index', {
     },
 
     onCellClick: function (view, td, cellIndex, record, tr, rowIndex, e, eOpts) {
+
         var target= Ext.fly(e.getTarget()),
             metaData = { id: record.getId() },
             header = view.getHeaderAtIndex(cellIndex);
@@ -224,10 +225,6 @@ Ext.define('Taco.view.category.Index', {
 
         if ((header.dataIndex || header.allowNavigation === true) && header.allowNavigation !== false && this.allowNavigation !== false) {
             e.preventDefault();
-
-            if (e.target) {
-                metaData = Ext.apply(metaData, e.target.dataset);
-            }
 
             this.launchEditor(record, metaData);
         }
