8e2cc2135e2ee808f4adb3e81df42ec38355ad21
greg_murray <greg_murray@volusion.com>
2015-11-12 12:27:57 -0600

    #72766 modified fix based on Simeon's code review to use model load.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/General.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/product/subform/General.js
@@ -1037,27 +1037,22 @@ Ext.define('Taco.view.product.subform.General', {
 
     onProductTypeChange: function (selectField, val) {
         var me = this;
-        Taco.core.data.StoreManager.getOrCreate({
-            type: 'Taco.store.ProductTypes',
-            pageSize: 1,
-            autoLoad: true,
-            createOnly:true,
-            filterOnLoad: true,
-            remoteFilter: true,
-            filters: [{
-                property: 'id',
-                value: val
-            }],
-            listeners: {
-                load: me.onProductTypeLoad,
-                scope: this
+        Taco.model.ProductType.load(val, {
+            scope: this,
+            success: me.onProductTypeLoad,
+            failure: function(record, operation) {
+                if (operation.error) {
+                    Taco.core.util.ExceptionWhiner.handleRemoteFailure(operation.error);
+                } else {
+                    Taco.app.fireEvent('setmessage', 'Unexpected error: could not find product type', 'error');
+                }
             }
         });
     },
 
-    onProductTypeLoad: function (scope, records) {
+    onProductTypeLoad: function (record) {
         var me = this,
-            productTypeRecord = records[0],
+            productTypeRecord = record,
             productTypeId = productTypeRecord.get('id'),
             productUsages = productTypeRecord.get("productUsages"),
             isDigitalCreditProductType = (productTypeRecord.get("goodsType") === 'DigitalCredit'),
