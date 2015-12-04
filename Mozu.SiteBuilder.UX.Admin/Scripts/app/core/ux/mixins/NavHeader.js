77fda1fe19433045a81c32a883eb4aa5f984a72a
Greg Ritter <greg@lolay.com>
2015-12-04 13:01:21 -0800

    Add back link ui to cancel button

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/mixins/NavHeader.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/mixins/NavHeader.js
@@ -388,7 +388,7 @@ Ext.define('Taco.core.ux.mixins.NavHeader', {
                     height: 40,
                     text: me.cancelText,
                     margin: "0 0 0 10",
-                    ui: 'action',
+                    ui: 'link',
                     scale: 'medium',
                     hidden: !me.cancelButtonVisible || this.cancelHidden || !this.allowCreate(),
                     itemId: 'cancelActionButton',
