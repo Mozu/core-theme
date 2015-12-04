8669b36f5633a592fdedb608e0f148f861d75592
Greg Ritter <greg@lolay.com>
2015-12-04 12:58:15 -0800

    Fix for missing class for filter container

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/mixins/NavHeader.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/mixins/NavHeader.js
@@ -352,6 +352,7 @@ Ext.define('Taco.core.ux.mixins.NavHeader', {
         if (me.enableSearchBarInHeader) {
             me.searchBox = Ext.widget({
                 xtype: 'taco-filtercontainer',
+                cls: 'taco-filtercontainer',
                 searchType: 'navigation',
                 width: '100%',
                 flex: 1,
