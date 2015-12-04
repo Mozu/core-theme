350d7242ff3868fb4e84a488f7cf4d272a72935e
Ross Libby <ross_libby@volusion.com>
2015-12-04 13:47:41 -0600

    Added class to fix the toolbar nav

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/mixins/NavHeader.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/mixins/NavHeader.js
@@ -499,6 +499,7 @@ Ext.define('Taco.core.ux.mixins.NavHeader', {
         // need to create container for buttons so that they can force the titleCmp to have elipsis
         var actionToolbar = {
             xtype: 'toolbar',
+            cls: 'navheader-action-toolbar',
             itemHeader: 'navHeaderActionContainer',            
             items: me.actions
         }
