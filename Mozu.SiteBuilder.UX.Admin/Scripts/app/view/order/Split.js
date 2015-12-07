34c963d23f1ab1c8a62f2e6da4a0a44e5099e5f8
Brandon Jernigan <brandon_jernigan@volusion.com>
2015-12-07 16:37:18 -0600

    fixed order grid selection triggering select of item

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/Split.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/Split.js
@@ -88,6 +88,9 @@ Ext.define('Taco.view.order.Split', {
                 itemclick: {
                     scope: this,
                     fn: function (grid, record, row, index, e, opts) {
+                        if (window.getSelection().toString()) { // if the user has highlighted text, do not launch editor
+                            return;
+                        }
                         if (!e.getTarget('.' + Taco.core.ux.grid.MenuColumn.prototype.iconCls) &&
                             !e.getTarget('.' + Taco.core.ux.grid.MenuColumn.prototype.tdCls) &&
                             !e.getTarget('.x-grid-cell-row-checker')) {
