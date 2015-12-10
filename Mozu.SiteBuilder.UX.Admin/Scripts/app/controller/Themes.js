3144e0bb9304c9ace3d1c66137066d278321be70
Ben <ben_cripps@volusion.com>
2015-12-10 12:49:16 -0600

    rename and remove old theme view files

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/controller/Themes.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/controller/Themes.js
@@ -5,6 +5,6 @@
 Ext.define('Taco.controller.Themes', {
     extend: 'Taco.core.Controller',
     modelName: 'Taco.model.ThemeListing',
-    requires: ['Taco.view.theme.newIndex'],
-    indexView: 'Taco.view.theme.newIndex'
+    requires: ['Taco.view.theme.Index'],
+    indexView: 'Taco.view.theme.Index'
 });
