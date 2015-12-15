aded5be134dd10bc91f8ff74fa4c1acbd951ee0f
Greg Ritter <greg@lolay.com>
2015-12-15 15:45:03 -0800

    Adjust the position of the more header menu

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/grid/Header.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/grid/Header.js
@@ -48,7 +48,26 @@ Ext.define('Taco.core.ux.grid.Header', {
             if (me.menu && me.menu.isVisible()) {
                 me.menu.hide();
             } else {
-                me.showMenuBy(t, header);
+                // inlined from Container.js showMenuBy to allow us to adjust the showBy offset
+                var menu = this.getMenu(),
+                    ascItem  = menu.down('#ascItem'),
+                    descItem = menu.down('#descItem'),
+                    sortableMth;
+
+                // Use ownerButton as the upward link. Menus *must have no ownerCt* - they are global floaters.
+                // Upward navigation is done using the up() method.
+                menu.activeHeader = menu.ownerButton = header;
+                header.setMenuActive(true);
+
+                // enable or disable asc & desc menu items based on header being sortable
+                sortableMth = header.sortable ? 'enable' : 'disable';
+                if (ascItem) {
+                    ascItem[sortableMth]();
+                }
+                if (descItem) {
+                    descItem[sortableMth]();
+                }
+                menu.showBy(t, 'tr-br', [0, 5]);
             }
         }
     }
