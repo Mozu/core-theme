0d8460c9a27c1000dc73a74e798e98a96a5992bc
Ross Libby <ross_libby@volusion.com>
2015-12-17 09:46:42 -0600

    #75360 fixed this bug in /locationInventory

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/mixins/LaunchEditor.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/mixins/LaunchEditor.js
@@ -91,7 +91,7 @@ Ext.define('Taco.core.ux.mixins.LaunchEditor', {
             delete complexMetaData.record;
         }
 
-        if (Taco.core.StateManager.getCurrentState().metaData.controller != 'redirects') {
+        if (Taco.core.StateManager.getCurrentState().metaData.controller != 'redirects' && Taco.core.StateManager.getCurrentState().metaData.controller != 'locationInventory') {
             Ext.defer(function () {
                 Taco.core.StateManager.attemptNavigate(Taco.core.StateManager.getCurrentState().metaData.controller + '/edit/' + record.getId(), complexMetaData);
             }, 1, this);
