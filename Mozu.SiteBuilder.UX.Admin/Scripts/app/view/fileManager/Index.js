10c614c6e5919ce0c2dd38026f89ae2496c22e4a
Ross Libby <ross_libby@volusion.com>
2016-01-06 11:03:38 -0600

    Fixed positioning of save/cancel buttons on file manager inline editor

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/fileManager/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/fileManager/Index.js
@@ -16,6 +16,34 @@ Ext.define('Taco.view.fileManager.Index', {
         'Taco.view.fileManager.AdvancedSearchForm'
     ],
 
+    listeners: {
+        click: {
+            element: 'el',
+            fn: function (e) {
+                var parentNode          = e.target.parentNode.nodeName,
+                    grandparent         = e.target.parentNode.parentNode.nodeName,
+                    lastRow             = e.target.parentNode.parentNode.parentNode.parentNode.nodeName,
+                    parentHeight        = e.target.parentNode.offsetHeight,
+                    grandparentHeight   = e.target.parentNode.parentNode.offsetHeight;
+
+                console.log(parentNode, parentHeight);
+
+                if (parentNode == 'TD' || parentNode == 'TR') {
+                    if ((grandparentHeight > 38 && grandparent == 'TR') || parentHeight > 38) {
+                        if (document.querySelector('[id^="roweditorbuttons"]').style.bottom == (78 + 'px')) {
+                            document.querySelector('[id^="roweditorbuttons"]').className = document.querySelector('[id^="roweditorbuttons"]').className + ' top-buttons'
+                        } else {
+                            document.querySelector('[id^="roweditorbuttons"]').className = document.querySelector('[id^="roweditorbuttons"]').className.split(' top-buttons')[0];
+                        }
+                        document.querySelector('[id^="roweditorbuttons"]').style.top = 77 + 'px';
+                    } else if (grandparent == 'TD' || parentHeight == 38) {
+                        document.querySelector('[id^="roweditorbuttons"]').style.top = 44 + 'px';
+                    }
+                }
+            }
+        }
+    },
+
     mixins: {
         uploadable: 'Taco.shared.util.Uploadable'
     },
