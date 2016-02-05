6ca43e1d12ba87ff15ceb30d42dde2c7b04d3c78
Brandon Jernigan <brandon_jernigan@volusion.com>
2016-02-05 11:26:00 -0600

    Revert "actually fix logic due to naming inconsistency"
    This reverts commit 5bf1fd1d676faf394f56c5c55ce929e123735f4e.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/discount/CriteriaForm.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/discount/CriteriaForm.js
@@ -145,7 +145,7 @@ Ext.define('Taco.view.discount.CriteriaForm', {
 
         this.applyDiscountTo = Ext.widget({
                 xtype: 'checkbox',
-                name: 'appliesToMostExpensiveProductsFirst',
+                name: 'appliesToLeastExpensiveProductsFirst',
                 value: !this.record.get('appliesToLeastExpensiveProductsFirst'),
                 itemId: 'apply-to-highest-priced-product',
                 boxLabel: 'Apply discount to highest-priced qualifying product(s) first',
@@ -155,13 +155,7 @@ Ext.define('Taco.view.discount.CriteriaForm', {
                     messageKey: 'discount.criteria.applyDiscountToHighestPricedProduct',
                     offsetLeft: 20,
                     offsetTop: 15
-                }),
-                listeners: {
-                    change: function(self, newValue, oldValue, eOpts) {
-                        this.record.set('appliesToLeastExpensiveProductsFirst', !newValue)
-                    },
-                    scope: me
-                }
+                })
             }
         );
 
