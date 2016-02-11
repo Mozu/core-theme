ed2141b3ed5a1bbfe7917aa8208931d4635a4db6
Brandon Jernigan <brandon_jernigan@volusion.com>
2016-02-11 11:30:07 -0600

    updated tooltips to work with elements using boxLabel instead of fieldLabel

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/content/Tooltip.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/core/ux/content/Tooltip.js
@@ -259,6 +259,9 @@ Ext.define('Taco.core.ux.content.Tooltip', {
 		this.component = component[0];
 
 		switch (this.hoverTarget) {
+            case 'boxLabelEl':
+                this.instantiateToolTipEvents(this.component.boxLabelEl);
+                break;
 			case 'label':
 				this.instantiateToolTipEvents(this.component.labelEl);
 				break;
