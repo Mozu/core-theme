508b7365df6a41177f9eb9d2d115973005a5a14a
Paul Theron <paul_theron@volusion.com>
2016-04-14 11:47:29 -0500

    Adding more information to the Catalog- and Site-listing pages #84204 #79495 #76461

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/provisioning/SiteProvisionerModal.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/provisioning/SiteProvisionerModal.js
@@ -43,7 +43,7 @@ Ext.define('Taco.view.provisioning.SiteProvisionerModal', {
                     name: 'catalogId',
                     store: this.catalogStore,
                     valueField: 'id',
-                    displayField: 'name',
+                    displayField: 'nameWithId',
                     fieldLabel: 'Catalog',
                     queryMode:'local'
                 },
