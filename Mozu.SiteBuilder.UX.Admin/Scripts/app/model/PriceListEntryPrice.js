69101f7ea7a1b6e084cdc96a59b3ee13e752abe2
greg_murray <greg_murray@volusion.com>
2016-04-21 16:12:19 -0500

    #84495 fixed entry grid display for blank & default.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/model/PriceListEntryPrice.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/model/PriceListEntryPrice.js
@@ -35,38 +35,7 @@ Ext.define('Taco.model.PriceListEntryPrice', {
             useNull: true
         }
     ],
-
-    //getDeletePromptMessage: function() {
-    //    var msg = 'Are you sure you want to delete "' + this.get('name') + '"?';
-    //    //if (this.get('pricingEntries').length > 1) {
-    //    //    msg += '<br/>It will affect these categories: ' + this.get('categoryNamesJoined');
-    //    //}
-    //    return msg;
-    //},
-
+    
     belongsTo: 'Taco.model.PriceListEntry'
-
-    //validations: [
-    //    { field: 'currencyCode', type: 'length', max: 3 }
-    //]
-
-    //proxy: {
-    //    type: 'ajaxproxy',
-    //    api: {
-    //        read: '/admin/app/priceList/entry/price/list',
-    //        create: '/admin/app/priceList/create',
-    //        update: '/admin/app/priceList/edit',
-    //        destroy: '/admin/app/priceList/delete'
-    //    },
-    //    reader: {
-    //        type: 'json',
-    //        root: 'items',
-    //        successProperty: 'success',
-    //        messageProperty: 'message'
-    //    },
-    //    writer: {
-    //        allowSingle: false,
-    //        type: 'json'
-    //    }
-    //}
+    
 });
