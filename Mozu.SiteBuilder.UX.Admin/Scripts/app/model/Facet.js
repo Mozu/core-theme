20ea676dfc2cd7ab2af8e40885838406ff9f4001
greg_murray <greg_murray@volusion.com>
2016-05-06 18:00:40 -0500

    #85170 attribute defintion only available for attribute type facets and not elements.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/model/Facet.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/model/Facet.js
@@ -74,18 +74,26 @@ Ext.define('Taco.model.Facet', {
     }, {
         name: 'valueSortType',
         type: 'string',
-        useNull: true,
-        defaultValue: 'AttributeDefinition'
+        useNull: true
     }],
 
     isInherited: function() {
         return this.get('categoryId') !== this.get('categoryId2');
     },
+    
+    getDefaultSortValue: function () {
+        if (this.get('facetType') === 'RangeQuery') {
+            return 'ValuesAscending';
+        }
+        return (this.get('sourceType') === 'Attribute') ? 'AttributeDefinition' : 'CountDescending';
+    },
 
     getFacetSortingStore: function () {
         var valAscDisplay,
             valDescDisplay,
-            dataTypeLower = this.get('sourceDataType') ? this.get('sourceDataType').toLowerCase() : 'string';
+            dataTypeLower = this.get('sourceDataType') ? this.get('sourceDataType').toLowerCase() : 'string',
+            isAttributeType = this.get('sourceType') === 'Attribute',
+            sortData = [];
 
         switch (dataTypeLower) {
             case 'number':
@@ -101,30 +109,38 @@ Ext.define('Taco.model.Facet', {
                 valDescDisplay = 'Alphabetical: Z to A';
         }
 
-        return Ext.create('Ext.data.Store', {
-            fields: ['id', "name"],
-            data: [
+        if (isAttributeType) {
+            sortData = [
                 {
                     id: "AttributeDefinition",
                     name: "Attribute Definition"
                 }, {
                     id: "AttributeDefinitionDescending",
                     name: "Attribute Definition (Reverse)"
-                }, {
-                    id: "CountDescending",
-                    name: "Facet Count: High to Low"
-                },
-                {
-                    id: "CountAscending",
-                    name: "Facet Count: Low to High"
-                },  {
-                    id: "ValuesAscending",
-                    name: valAscDisplay
-                }, {
-                    id: "ValuesDescending",
-                    name: valDescDisplay
                 }
-            ]
+            ];
+        } 
+
+        sortData = sortData.concat([
+            {
+                id: "CountDescending",
+                name: "Facet Count: High to Low"
+            },
+            {
+                id: "CountAscending",
+                name: "Facet Count: Low to High"
+            },  {
+                id: "ValuesAscending",
+                name: valAscDisplay
+            }, {
+                id: "ValuesDescending",
+                name: valDescDisplay
+            }
+        ]);
+
+        return Ext.create('Ext.data.Store', {
+            fields: ['id', "name"],
+            data: sortData
         });
     }
 
