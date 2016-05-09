0968fb48645241f2148ec2c8ce665687c8182bf1
Ben <ben_cripps@volusion.com>
2016-05-09 09:35:45 -0500

    #85804 more work to make link editor better in sitebuilder

--- Mozu.SiteBuilder.UX.Admin/Scripts/chorizo/caliente/src/ui-components/Block/index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/chorizo/caliente/src/ui-components/Block/index.js
@@ -102,10 +102,6 @@ export default class Block {
                 }
             });
         }
-
-        else if (Chorizo.contentWidget && Chorizo.contentWidget.revealEditor) {
-            Chorizo.contentWidget.revealEditor(block);
-        }
     }
 
     update(html, cfg) {
@@ -121,6 +117,14 @@ export default class Block {
     }
 
     onDoubleClick() {
+
+        // if this is a content widget, do nothing
+        if (this 
+            && this.widgetData
+            && this.widgetData.definitionId === 'content') {
+            return false;
+        }
+
         this.doEdit();
     }
 
