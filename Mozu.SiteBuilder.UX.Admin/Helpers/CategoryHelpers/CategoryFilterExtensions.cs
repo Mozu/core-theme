e708c8bfdeb66ba19f6ea7d6c6ba0860a163ec64
Ben <ben_cripps@volusion.com>
2016-05-17 16:56:39 -0500

    Adding slug to category search

--- Mozu.SiteBuilder.UX.Admin/Helpers/CategoryHelpers/CategoryFilterExtensions.cs
+++ Mozu.SiteBuilder.UX.Admin/Helpers/CategoryHelpers/CategoryFilterExtensions.cs
@@ -41,7 +41,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Helpers.CategoryHelpers
                     return String.Format("{2} {1} {0}", filter.value, filter.comparison, ID);
 
                 case "all":
-                    return String.Format("( content.name cont \"{0}\" or categorycode eq \"{0}\")", filter.value);
+                    return String.Format("( content.name cont \"{0}\" or content.slug cont \"{0}\" or categorycode eq \"{0}\")", filter.value);
 
                 case "categorycode":
                     return String.Format("{2} {1} {0}", filter.value, filter.comparison, CATEGORY_CODE);
