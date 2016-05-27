0e9f023d5cd9cb8a8e768a55f243d45f67b474f1
Jonathan Kuchar <Jonathan_Kuchar.volusion.com>
2016-05-27 13:47:53 -0500

    Feature #64504 PBI #77310 Task #85814 - MyAccount middle tier work.

--- Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/MyAccountController.cs
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/MyAccountController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using AutoMapper;
 using Mozu.CommerceRuntime.Contracts.Clients;
 using Mozu.Core;
 using Mozu.Core.Api.Client;
@@ -21,6 +23,7 @@ using Mozu.SiteBuilder.Mvc.Extensions;
 using Mozu.SiteBuilder.UX.Filters;
 using Mozu.Core.Actions;
 using Mozu.SiteBuilder.Mvc.OAF;
+using StorefrontModel = Mozu.SiteBuilder.UX.Areas.StoreFront.Models;
 
 namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
 {
@@ -112,6 +115,10 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
             var reasonList = _returnApiClient.GetReasons();
             var storeCreditsTask = _creditApiClient.GetCredits(0, 25, "activationDate DESC", String.Format("CustomerId eq \"{0}\" and activationdate le \"{1}\" and expirationdate ge \"{1}\"", account.Id, DateTime.UtcNow.ToString("o")));
             var wishlistTask = _wishlistApiClient.GetWishlistByName(account.Id, DEFAULT_WISHLIST_NAME);
+            var purchaseOrderAccount =
+                (await _customerAccountWebApiClient.GetCustomerPurchaseOrderAccount(this.PageContext.User.AccountId))
+                    .ReadAsSync();
+            var customerPurchaseOrder = Mapper.Map<Mozu.SiteBuilder.UX.Models.Customers.CustomerPurchaseOrderAccount>(purchaseOrderAccount);
 
             var shipTask = GetShippableCountries();
             var billTask = GetBillingCountries();
@@ -159,6 +166,24 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
             jAccount.Add("hasSavedCards", cards.Items.Count > 0);
             jAccount.Add("hasSavedContacts", account.Contacts.Count > 0);
             jAccount.Add("cards", cards.Items.ToJArray());
+
+            if (SiteContext.CheckoutSettings.PurchaseOrder != null && SiteContext.CheckoutSettings.PurchaseOrder.IsEnabled && customerPurchaseOrder != null)
+            {
+                var paymentTermOptions = this.SiteContext.CheckoutSettings.PurchaseOrder.PaymentTerms;
+                // helper object that inherits from contract and add description, then create new payment array and apply it to accountPurchaseOrder before doing .toJObject()
+                foreach (var term in customerPurchaseOrder.PaymentTerms)
+                {
+                    var siteSettingsPaymentTerm = paymentTermOptions.Find(paymentTerm => paymentTerm.Code == term.Code);
+                    if (siteSettingsPaymentTerm != null)
+                    {
+                        term.Description = siteSettingsPaymentTerm.Description;
+                    }
+                }
+                var purchaseOrderJObject = customerPurchaseOrder.ToJObject();
+
+                jAccount.Add("purchaseOrder", purchaseOrderJObject);
+            }
+
             if (credits.Items.Count > 0)
             {
                 jAccount.Add("credits", credits.Items.ToJArray());
