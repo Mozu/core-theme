3ad31e73b8b88ab077c76e207c1aff5c20095efc
Ross Libby <ross_libby@volusion.com>
2016-06-17 15:46:40 -0500

    #87574 made ti show the correct amount

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/modal/CapturePayment.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/modal/CapturePayment.js
@@ -38,7 +38,7 @@ Ext.define('Taco.view.order.modal.CapturePayment', {
                     currencyCode: this.order.getCurrencyCode(),
                     selectOnFocus: true,
                     width: 170,
-                    value: this.record.get('paymentType') === 'PurchaseOrder' ? this.record.get('amountRequested') : Math.min(this.record.data.amountAuthorized, this.order.getCaptureAmountHint())
+                    value: this.record.get('paymentType') === 'PurchaseOrder' ? Math.min(this.order.getCaptureAmountHint(), this.record.get('amountRequested')) : Math.min(this.record.data.amountAuthorized, this.order.getCaptureAmountHint())
                 },
                 notes
             ]
