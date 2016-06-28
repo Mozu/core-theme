08cc00cb6777c058659b914c8e7e9e365be49e86
Ben <ben_cripps@volusion.com>
2016-06-28 14:16:55 -0500

    undoing some unfinished changes
    Former-commit-id: cbccf70bf0ab493f97c29abcaa5f50b84940959b

--- Mozu.SiteBuilder.UX.Admin/Api/ThemeSettingController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ThemeSettingController.cs
@@ -11,7 +11,6 @@ using Mozu.SiteBuilder.Mvc.Themes;
 using Mozu.SiteBuilder.UX.Admin.Api.Models;
 using Mozu.SiteBuilder.UX.Models.Settings;
 using Newtonsoft.Json.Linq;
-using Mozu.Content.Contracts.Clients;
 
 namespace Mozu.SiteBuilder.UX.Admin.Api
 {
@@ -22,9 +21,8 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
         private readonly IThemeSettingsRepository _themeSettingsRepository;
         private readonly IThemeRepository _themeRepository;
         private readonly IThemeContentRetriever _contentRetriever;
-        private readonly IDocumentListWebApiClient _documentListWebApiClient;
 
-        public ThemeSettingController( IThemeSettingsRepository themeSettingsRepository, IThemeRepository themeRepository, IThemeContentRetriever contentRetriever, IDocumentListWebApiClient documentListRepository)
+        public ThemeSettingController( IThemeSettingsRepository themeSettingsRepository, IThemeRepository themeRepository, IThemeContentRetriever contentRetriever)
         {
             if(themeSettingsRepository == null)
             {
@@ -35,7 +33,6 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
             _themeSettingsRepository = themeSettingsRepository;
             _themeRepository = themeRepository;
             _contentRetriever = contentRetriever;
-            _documentListWebApiClient = documentListRepository;
         }
 
         /// <summary>
@@ -58,18 +55,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
         public async Task<HttpResponseMessage> ReadInstance(string themeId)
         {
             var values = await _themeSettingsRepository.GetInstanceValues(themeId);
-            var docId = String.Join("", "theme_settings_", themeId); 
-            var docList = (await _documentListWebApiClient.GetDocuments(documentListName: "siteSettings@mozu")).ReadAsSync();
-            var theme = _themeRepository.GetThemeOrDefault(new ThemeSelection() { Id = themeId });
-
             var configSettings = this.ReadConfiguration(themeId);
-            bool isPublishable = false;
-
-            if (docList != null)
-            {
-                var doc = docList.Items.Find(x => x.Id == docId);
-            }
-
             foreach (var setting in configSettings.Items)
             {
                 JToken o = null;
