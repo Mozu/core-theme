b022503971d8002a5160d4e51266b26833a0b9b3
Paul Theron <paul_theron@volusion.com>
2016-08-22 15:53:49 -0500

    Change the product-runtime-preview to use POST instead of GET #90051
    Former-commit-id: 87f8734ec0ae3a31e02c845ce8bd66295650f7f7

--- Mozu.SiteBuilder.UX.Admin/Helpers/ProductRuntimeHelpers/ProductRuntimeSortExpressionBuilder.cs
+++ Mozu.SiteBuilder.UX.Admin/Helpers/ProductRuntimeHelpers/ProductRuntimeSortExpressionBuilder.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
 using Mozu.SiteBuilder.UX.Admin.Api.Models;
 
 namespace Mozu.SiteBuilder.UX.Admin.Helpers.ProductRuntimeHelpers
 {
     public interface IProductRuntimeSortExpressionBuilder : ISortExpressionBuilder
     {
+        string GetFromSortString(string input);
     }
 
     /// <summary>
@@ -40,6 +43,22 @@ namespace Mozu.SiteBuilder.UX.Admin.Helpers.ProductRuntimeHelpers
         protected override string GetDefaultSort()
         {
             return string.Empty;
+        }
+
+        public string GetFromSortString(string input)
+        {
+            var returnString = "";
+
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(SortingCollection));
+                var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(input)) { Position = 0 };
+                var sortObj = (SortingCollection)serializer.ReadObject(stream);
+                if (sortObj.Count > 0)
+                    returnString = this.ToSortString(sortObj);
+            }
+
+            return returnString;
         }
     }
 }
