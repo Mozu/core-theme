9e59cdd2f5282d4d7ae12f9a8dab4f2b5892253c
Sparkel Phipps <thomas_phipps@volusion.com>
2016-09-21 18:54:44 -0500

    #92109:Add Arc context functions to storefront Search and Category routes
    Former-commit-id: df17e7d7c9eb0def2ef4cb1b1381a008025ff51d

--- Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/SearchController.cs
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/SearchController.cs
@@ -20,6 +20,9 @@ using System;
 using System.Net.Http;
 using Mozu.SiteBuilder.Mvc.ViewEngine;
 using Mozu.SiteBuilder.Mvc.Helpers;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.Controllers;
+using Mozu.SiteBuilder.Mvc.Catalog;
 
 namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
 {
@@ -47,83 +50,58 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
         [SbActionExtensionFilter(actionId: ActionFilterConstants.SearchIndexBeforeAction, executionType: ActionExtensionExecutionTypes.BeforeController)]
         [SbActionExtensionFilter(actionId: ActionFilterConstants.SearchIndexAfterAction, executionType: ActionExtensionExecutionTypes.AfterController)]
         [HttpGet]
-        public async Task<HttpResponseMessage> Index(string query = null, int? categoryId = null, int? page = null,
+        public async Task<HttpResponseMessage> Index(
+            string query = null, 
+            int? categoryId = null, 
+            string categoryCode = null , 
+            int? page = null,
             //adding w as an extra param for jelly belly.  Plan to remove in r6.1
-            string w = null)
+            string w = null,
+            [FromUri]AdvancedSearchParamaters searchParams = null)
         {
-   
-            query = query ?? w;
-
-            if ( categoryId == null )
+            //nulled out in asp model binder  ... should only be set if done in arcjs
+            if (query != null)
             {
-                int tmp;
-                if ( int.TryParse(PageContext.Search.Facets["categoryId"], out tmp))
-                {
-                    PageContext.Search.CategoryId = categoryId = tmp;
-                }
+                searchParams.query = query;
             }
-            
-
            
 
             PageContext.PageType = "search";
-            PageContext.CategoryId = categoryId;
-            page = page.GetValueOrDefault(1);
 
-            var themeSettings = SiteContext.ThemeSettings;
-            var includeFacets = ((bool?)(JToken)themeSettings["showCategoryFacets"]);
-            var searchQuery = new StringBuilder();
-            string facetTemplate = null;
-            string facetHierValue = null;
-            string facetHierDepth = null;
-            string searchTuningRuleContext = null;
+            var _ = searchParams;
+            var searchResponse = (await _searchClient.Search(
+                query: _.query,
+                filter: _.filter,
+                facetTemplate: _.facetTemplate,
+                facetTemplateSubset: _.facetTemplateSubset,
+                facet: _.facet,
+                facetFieldRangeQuery: _.facetFieldRangeQuery,
+                facetHierPrefix: _.facetHierPrefix,
+                facetHierValue: _.facetHierValue,
+                facetStartIndex: _.facetStartIndex,
+                facetPageSize: _.facetPageSize,
+                cursorMark: _.cursorMark,
+                enableSearchTuningRules: _.enableSearchTuningRules,
+                facetHierDepth: _.facetHierDepth,
+                facetPrefix: _.facetPrefix,
+                facetSettings: _.facetSettings,
+                facetTemplateExclude: _.facetTemplateExclude,
+                facetValueFilter: _.facetValueFilter,
+                pageSize: _.pageSize,
+                responseFields: _.responseFields,
+                responseGroups: _.responseGroups,
+                responseOptions: _.responseOptions,
+                searchSettings: _.searchSettings,
+                searchTuningRuleCode: _.searchTuningRuleCode,
+                searchTuningRuleContext: _.searchTuningRuleContext,
+                sortBy: _.sortBy,
+                startIndex: _.startIndex,
+                targetContextLevel: _.targetContextLevel)).ReadAsSync();
 
-            var isVolumePricingBandsEnabled = ((bool?)(JToken)themeSettings["listVolumePricing"]);
-            string responseOptions = isVolumePricingBandsEnabled.GetValueOrDefault() ? "volumePriceBands" : null;
-            string facets = null;
-            int? pageSize = PageContext.Search.PageSize;
-            int? startIndex = PageContext.Search.StartIndex;
-
-            if (categoryId.HasValue)
-            {
-                searchQuery.Append("categoryId req ");
-                searchQuery.Append(categoryId);
-                searchTuningRuleContext = "categoryId:" + categoryId;
-            }
-
-            if (pageSize == null)
-            {
-                pageSize = ((int?)(JToken)themeSettings["defaultPageSize"]) ?? 20;
-
-            }
-
-            if (startIndex == null && page != null)
-            {
-                startIndex = (page.Value - 1) * pageSize;
-            }
-            else
-            {
-                startIndex = startIndex.GetValueOrDefault(0);
-            }
-
-            if (includeFacets.GetValueOrDefault(true))
-            {
-                facets = "categoryId";
-                facetHierDepth = "categoryId:2";
-                if (categoryId.HasValue)
-                {
-                    facetTemplate = "categoryId:" + categoryId;
-                    facetHierValue = "categoryId:" + categoryId;
-                }
-            }
-
-            
-
-            var searchResponse = (await _searchClient.Search(query, searchQuery.ToString(), facetHierValue: facetHierValue, facetTemplate: facetTemplate, facetHierDepth: facetHierDepth, facetValueFilter: PageContext.Search.ToFacetValueFilter(), startIndex: startIndex.Value, sortBy: PageContext.Search.SortBy, pageSize: pageSize.Value, facet: facets, searchTuningRuleContext: searchTuningRuleContext, responseOptions:responseOptions)).ReadAsSync();
             var pc = Mapper.Map<ProductSearchResult>(searchResponse);
-           
+
             pc.Init(true, this.PageContext.Search);
-            pc.UrlBase = "/search?query=" + query;
+            pc.UrlBase = "/search?query=" + _.query;
 
             var searchPageType = pc.TotalCount > 0 ? "search-results" : "no-search-results";
 
@@ -138,6 +116,127 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
             };
 
             return Request.CreateResponse(System.Net.HttpStatusCode.OK, View(searchPageType, pc));
+        }
+
+        class AdvancdSearchParamterModelBinder : IModelBinder
+        {
+            public bool BindModel(HttpActionContext actionContext, ModelBindingContext bindingContext)
+            {
+                //null out action arguments so that we can detect if they were modified in arcjs
+                actionContext.ActionArguments["query"] = null;
+                actionContext.ActionArguments["categoryId"] = null;
+                actionContext.ActionArguments["categoryCode"] = null;
+                actionContext.ActionArguments["page"] = null;
+                actionContext.ActionArguments["w"] = null;
+                
+                AdvancedSearchParamaters avp = new AdvancedSearchParamaters();
+             
+                var sc = actionContext.Request.Resolve<ISiteContext>();
+                var pc = actionContext.Request.Resolve<IPageContext>();
+                var includeFacets = ((bool?)(JToken)sc.ThemeSettings["showCategoryFacets"]);
+                var isVolumePricingBandsEnabled = ((bool?)(JToken)sc.ThemeSettings["listVolumePricing"]);
+
+                
+                avp.pageSize = pc.Search.PageSize;
+                avp.startIndex = pc.Search.StartIndex;
+
+                object tmp;
+                var query = (string)null; ;
+                var res = bindingContext.ValueProvider.GetValue("query");
+                if (res != null)
+                {
+                    query = res.RawValue?.ToString();
+                }
+               
+                //need to support for jellybelly
+                if (query == null )
+                {
+                    res = bindingContext.ValueProvider.GetValue("w");
+                    query = res?.RawValue?.ToString();
+                }
+
+                avp.query = query;
+
+                int? categoryId = null;
+                res = bindingContext.ValueProvider.GetValue("categoryId");
+                int tmpInt;
+                if (res != null && int.TryParse(res.RawValue?.ToString(), out tmpInt))
+                {
+                    categoryId = tmpInt;
+                }
+                res = bindingContext.ValueProvider.GetValue("categoryCode");
+                if ( categoryId == null && res != null )
+                {
+                    var categoryCode = res.RawValue.ToString();
+                    categoryId = actionContext.Request.Resolve<ICategoryTreeProvider>().GetAllCategories().Result.FindByCode(categoryCode)?.Id;
+                }
+                if (categoryId == null)
+                {
+                    int tempInt;
+                    if (int.TryParse(pc.Search.Facets["categoryId"], out tempInt))
+                    {
+                        pc.Search.CategoryId = categoryId = tempInt;
+                    }
+                }
+
+                if ( categoryId != null)
+                {
+                    avp.filter = $"categoryId req {categoryId}";
+                    avp.searchTuningRuleCode = $"categoryId:{categoryId}";
+                }
+
+                pc.Search.CategoryId = categoryId;
+                pc.CategoryId = categoryId;
+
+                avp.responseOptions =  isVolumePricingBandsEnabled.GetValueOrDefault() ? "volumePriceBands" : null;
+
+                if ( includeFacets.GetValueOrDefault(true) )
+                {
+                    avp.facet = "categoryId";
+                    avp.facetHierDepth = "categoryId:2";
+                    if (categoryId.HasValue)
+                    {
+                        avp.facetTemplate = "categoryId:" + categoryId;
+                        avp.facetHierValue = "categoryId:" + categoryId;
+                    }
+
+                }
+                bindingContext.Model = avp;
+                return true;
+            }
+        }
+
+        [ModelBinder(typeof(AdvancdSearchParamterModelBinder))]
+        public class AdvancedSearchParamaters
+        {
+
+            public string query { get; set; }
+            public string filter { get; set; }
+            public string facetTemplate { get; set; }
+            public string facetTemplateSubset { get; set; }
+            public string facet { get; set; }
+            public string facetFieldRangeQuery { get; set; }
+            public string facetHierPrefix { get; set; }
+            public string facetHierValue { get; set; }
+            public string facetHierDepth { get; set; }
+            public string facetStartIndex { get; set; }
+            public string facetPageSize { get; set; }
+            public string facetSettings { get; set; }
+            public string facetValueFilter { get; set; }
+            public string sortBy { get; set; }
+            public int? pageSize { get; set; }
+            public int? startIndex { get; set; }
+            public string searchSettings { get; set; }
+            public bool? enableSearchTuningRules { get; set; }
+            public string searchTuningRuleContext { get; set; }
+            public string searchTuningRuleCode { get; set; }
+            public string responseGroups { get; set; }
+            public string facetTemplateExclude { get; set; }
+            public string facetPrefix { get; set; }
+            public string responseOptions { get; set; }
+            public string cursorMark { get; set; }
+            public string responseFields { get; set; }
+            public Core.Api.Contracts.TargetContextLevelType targetContextLevel { get; set; }
         }
 
         private IDictionary<string, object> MakeSearchDict(string query, int? categoryId, int? page, string w)
