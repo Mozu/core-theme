f1d5ced4251c510329ef376eb26037308280548b
greg_murray <greg_murray@volusion.com>
2016-09-27 18:32:40 -0500

    switched edit page to point to new (RX) categories controller
    Former-commit-id: 40472aa573cdcc39494d8209dcb2f81b3a4e9c0b

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Edit.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/category/Edit.js
@@ -11,7 +11,7 @@ Ext.define('Taco.view.category.Edit', {
     enableSearchBarInHeader: false,
     parentTitleCfg: {
         title: 'Categories',
-        controller: 'category'
+        controller: 'categories'
     },
     doCreate: function () {
         var controller = "category",
