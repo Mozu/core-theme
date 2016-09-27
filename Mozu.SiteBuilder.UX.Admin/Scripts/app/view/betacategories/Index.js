ab11eda2725f5ecc0bb6374cd87a5181db9a86b5
Brandon Jernigan <brandon_jernigan@volusion.com>
2016-09-27 10:48:52 -0500

    updated required contexts for categories
    Former-commit-id: f9fd13d6ba47cc65a321d9e2ab592288eeb6f0ef

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/betacategories/Index.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/betacategories/Index.js
@@ -14,6 +14,11 @@ Ext.define('Taco.view.betacategories.Index', {
         advancedFormCls: 'Taco.view.category.AdvancedSearchForm',
         emptySearchText: 'Search'
     },
+
+    contextConfig: {
+        supportedLevels: ['c'],
+        requiresContextOfType: ['c']
+    },
     
     allowCreate: function() {
         return false;
