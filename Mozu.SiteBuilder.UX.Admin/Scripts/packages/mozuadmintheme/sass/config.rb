06d85f6c7eefa1a35a7c4e23074828882c6a3a5f
Ben <ben_cripps@volusion.com>
2016-10-04 12:58:40 -0500

    adding random func to config.rb to bust mozicon cache
    Former-commit-id: bc8e0f37f23216d31520246e4aadce4a991a13f8

--- Mozu.SiteBuilder.UX.Admin/Scripts/packages/mozuadmintheme/sass/config.rb
+++ Mozu.SiteBuilder.UX.Admin/Scripts/packages/mozuadmintheme/sass/config.rb
@@ -1,1 +1,7 @@
 output_style = :expanded
+
+module Sass::Script::Functions
+  def random(max = Sass::Script::Number.new(1000))
+    Sass::Script::Number.new(rand(max.value), max.numerator_units, max.denominator_units)
+  end
+end
