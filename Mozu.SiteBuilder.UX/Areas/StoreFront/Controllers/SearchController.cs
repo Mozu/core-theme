cd0c691e2de5a5fb83b199b9ee69d4a99328f5e9
greg_murray <greg_murray@volusion.com>
2016-10-11 17:23:47 -0500

    #93795 added sortBy to search controller as missed for search arc action support.
    Former-commit-id: 34b553e0f589562e625eb1d75e533e5a9e9ca3aa

--- Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/SearchController.cs
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/SearchController.cs
@@ -139,9 +139,9 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
                 
                 avp.pageSize = pc.Search.PageSize;
                 avp.startIndex = pc.Search.StartIndex;
+                avp.sortBy = pc.Search.SortBy;
 
-                object tmp;
-                var query = (string)null; ;
+                var query = (string)null;
                 var res = bindingContext.ValueProvider.GetValue("query");
                 if (res != null)
                 {
