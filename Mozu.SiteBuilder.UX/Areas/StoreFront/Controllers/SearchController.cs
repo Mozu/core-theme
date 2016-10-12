d5556faa0f2c6492d6374f345690e59b78e3555b
greg_murray <greg_murray@volusion.com>
2016-10-12 10:10:42 -0500

    #93476 added sort and facetValueFilter
    Former-commit-id: 078bad397642aee10b9d8e26a4dcb92aac4c7ccd

--- Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/SearchController.cs
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/SearchController.cs
@@ -139,9 +139,9 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
                 
                 avp.pageSize = pc.Search.PageSize;
                 avp.startIndex = pc.Search.StartIndex;
+                avp.sortBy = pc.Search.SortBy;
 
-                object tmp;
-                var query = (string)null; ;
+                var query = (string)null;
                 var res = bindingContext.ValueProvider.GetValue("query");
                 if (res != null)
                 {
@@ -189,7 +189,7 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
                 pc.CategoryId = categoryId;
 
                 avp.responseOptions =  isVolumePricingBandsEnabled.GetValueOrDefault() ? "volumePriceBands" : null;
-
+                avp.facetValueFilter = pc.Search.ToFacetValueFilter();
                 if ( includeFacets.GetValueOrDefault(true) )
                 {
                     avp.facet = "categoryId";
