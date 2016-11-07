fde29d787faf99c1542555ad1dcc7449eb2a146b
Thomas Phipps <Thomas_Phipps@volusion.com>
2016-11-07 13:05:01 -0600

    adding cache setting for compressed cache of contextbuilder
    Former-commit-id: fb6a0cfbdedef20ced3ccb8d2f0456ea72a0c629

--- Mozu.SiteBuilder.Mvc/ContextBuilder/Builder.cs
+++ Mozu.SiteBuilder.Mvc/ContextBuilder/Builder.cs
@@ -55,7 +55,7 @@ namespace Mozu.SiteBuilder.Mvc.Context
         ISettings _settings;
         string _machineId;
         Autofac.ILifetimeScope _globalScope;
-        const string CacheName = "Sitebuilder.ContextBuilder";
+        const string CacheName = "Sitebuilder.ContextBuilder.Compressed";
         const int TimerInterval = 15 * 1000;
         public const string CacheVersion = "1";
         const string EnableCleanJobConfigKey = "sitebuilder:context.enableCleanJob";
