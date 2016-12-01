1c910a0060d1a8336e296fe6d779533e2d96eda0
Ben <ben_cripps@volusion.com>
2016-12-01 14:52:20 -0600

    more fixes for ##93809
    Former-commit-id: 37f0043d352e8091a43385bd2d6b4490aa9fe7c0

--- Mozu.SiteBuilder.Mvc/Contexts/SearchContext.cs
+++ Mozu.SiteBuilder.Mvc/Contexts/SearchContext.cs
@@ -215,9 +215,14 @@ namespace Mozu.SiteBuilder.Mvc.Contexts
         public string ToClearUrl(ICustomRouteHandler routeHandler)
         {
             var routeData = this._request.GetRouteData();
-            int catId = -1; ;
-        
+            int catId = -1;
             object tmp;
+
+            if (routeData.Values.TryGetValue("categoryId", out tmp))
+            {
+                int.TryParse((tmp ?? new object()).ToString(), out catId);
+            }
+
             bool isSearchRoute = routeData.Values.TryGetValue("controller", out tmp) && string.Equals(tmp as string, "search", StringComparison.OrdinalIgnoreCase);
             var routeType = isSearchRoute ? FancyRoute.Search : FancyRoute.Category;
 
@@ -238,10 +243,6 @@ namespace Mozu.SiteBuilder.Mvc.Contexts
             if (baseUrl == null)
             {
                 string categorySlug = null;
-                if ( routeData.Values.TryGetValue("categoryId", out tmp) ) 
-                {
-                    int.TryParse((tmp ?? new object()).ToString(), out catId);
-                }
                 if (routeData.Values.TryGetValue("categorySlug", out tmp))
                 {
                     categorySlug = tmp as string;
@@ -265,12 +266,13 @@ namespace Mozu.SiteBuilder.Mvc.Contexts
                 }
                 
             }
-            if( isSearchRoute && catId > 0 )
+            if( isSearchRoute)
             {
                 return ToUrl(new SearchContextOverrides()
                 {
                     UrlBase = baseUrl,
-                    CategoryId = catId
+                    ClearFacets = true,
+                    //CategoryId = catId > 0 ? catId : (int?)null
                 });
             }
             return baseUrl;
