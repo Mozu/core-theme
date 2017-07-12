87c3495424812170e237c76534c6a2028300aa51
MONOLITH\Arya <arya.mohades@kibocommerce.com>
2017-07-12 15:17:19 -0500

    Adding field for save prompt to mvc layer

--- Mozu.SiteBuilder.Mvc/Contexts/TenantAdminSettingsContext.cs
+++ Mozu.SiteBuilder.Mvc/Contexts/TenantAdminSettingsContext.cs
@@ -127,5 +127,12 @@ namespace Mozu.SiteBuilder.Mvc.Contexts
             }
         }
 
+        public bool IsSavePromptEnabled
+        {
+            get
+            {
+                return ((bool?)_state.Value.GetValue("isSavePromptEnabled")).GetValueOrDefault(false);
+            }
+        }
     }
 }
