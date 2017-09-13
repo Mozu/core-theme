0af0729b3dde94d692a991e9cb891647d05af6d8
Jason Vanderslice <jason_vanderslice@volusion.com>
2017-09-13 11:11:01 -0500

    Added error messaging for includeUserClaims and cacheResults

--- Mozu.SiteBuilder.UX/Hypr/Tags/IncludeProductsTag.cs
+++ Mozu.SiteBuilder.UX/Hypr/Tags/IncludeProductsTag.cs
@@ -104,7 +104,11 @@ namespace Mozu.SiteBuilder.UX.Hypr.Tags
             
             var productSearchWebApiClient = context.Resolve<IProductSearchWebApiClient>();
           
-            
+            if (includeUserClaims == true && cacheResults == true)
+            {
+                throw new NDjango.Interfaces.RenderingError("If includeUserClaims is true then cacheResults must be set to false", Microsoft.FSharp.Core.FSharpOption<Exception>.None);
+            }
+
             string facetTemplate = null;
 
             string facetValueFilter = null;
