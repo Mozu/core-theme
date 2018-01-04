2ce7b0c8c3b82424b1517c80b0d59ae9a420255c
Adam Turner <Adam.Turner@kibocommerce.com>
2018-01-04 13:48:04 -0600

    Fix automapper issues. Fully test mapping of CustomerContact.

--- Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/CustomerContactMapping.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/ModelMapping/CustomerContactMapping.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using AutoMapper;
 using Mozu.SiteBuilder.UX.Admin.Api.Models;
 using DC = Mozu.Customer.Contracts;
@@ -14,41 +12,22 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
         {
             CreateMap<DC.CustomerContact, CustomerContact>()
                 // explicitly perform base class mapping Mozu.Core.Api.Contracts.Contact <=> Contact
-                //.BeforeMap((dc, x) => { Mapper.Map<Mozu.Core.Api.Contracts.Contact, Contact>(dc, x); })
-                .IncludeBase<Mozu.Core.Api.Contracts.Contact, Contact>()
+                .IncludeBase<Core.Api.Contracts.Contact, Contact>()
                 .ForMember(x => x.AccountId, op => op.ResolveUsing(dc => dc.AccountId))
                 .ForMember(x => x.FaxNumber, op => op.ResolveUsing(dc => dc.FaxNumber))
                 .ForMember(x => x.IsShipping, op => op.ResolveUsing(dc => dc.Types.Any(dct => dct.Name == DC.ContactTypeConst.SHIPPING)))
                 .ForMember(x => x.IsPrimaryShipping, op => op.ResolveUsing(dc => dc.Types.Any(dct => dct.Name == DC.ContactTypeConst.SHIPPING && dct.IsPrimary)))
                 .ForMember(x => x.IsBilling, op => op.ResolveUsing(dc => dc.Types.Any(dct => dct.Name == DC.ContactTypeConst.BILLING)))
                 .ForMember(x => x.IsPrimaryBilling, op => op.ResolveUsing(dc => dc.Types.Any(dct => dct.Name == DC.ContactTypeConst.BILLING && dct.IsPrimary)))
-
-                // base class maps AddressType. DO NOT let automapper overwrite it.
-                .ForMember(x => x.AddressType, op => op.Ignore())
-
-                // ignore a bunch of other members that base class maps, but unit test fails on
-                .ForMember(x => x.MiddleName, op => op.Ignore())
-                .ForMember(x => x.LastName, op => op.Ignore())
-                .ForMember(x => x.Address1, op => op.Ignore())
-                .ForMember(x => x.Address2, op => op.Ignore())
-                .ForMember(x => x.Address3, op => op.Ignore())
-                .ForMember(x => x.Address4, op => op.Ignore())
-                .ForMember(x => x.CityOrTown, op => op.Ignore())
-                .ForMember(x => x.StateOrProvince, op => op.Ignore())
-                .ForMember(x => x.CountryCode, op => op.Ignore())
-                .ForMember(x => x.PostalOrZipCode, op => op.Ignore())
-                .ForMember(x => x.HomePhone, op => op.Ignore())
-                .ForMember(x => x.MobilePhone, op => op.Ignore())
-                .ForMember(x => x.WorkPhone, op => op.Ignore())
                 ;
 
             CreateMap<CustomerContact, DC.CustomerContact>()
                 // explicitly perform base class mapping Contact <=> Mozu.Core.Api.Contracts.Contact
-                .IncludeBase<Contact, Mozu.Core.Api.Contracts.Contact>()                
-                //.BeforeMap((x, dc) => { Mapper.Map<Contact, Mozu.Core.Api.Contracts.Contact>(x, dc); })
+                .IncludeBase<Contact, Core.Api.Contracts.Contact>()
                 .ForMember(dc => dc.AccountId, op => op.ResolveUsing(x => x.AccountId))
                 .ForMember(dc => dc.FaxNumber, op => op.ResolveUsing(x => x.FaxNumber))
-                .ForMember(dc => dc.Types, op => op.ResolveUsing(x => {
+                .ForMember(dc => dc.Types, op => op.ResolveUsing(x =>
+                {
                     var types = new List<DC.ContactType>();
                     if (x.IsPrimaryBilling)
                         types.Add(new DC.ContactType { Name = DC.ContactTypeConst.BILLING, IsPrimary = x.IsPrimaryBilling });
@@ -57,16 +36,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.ModelMapping
 
                     return types;
                 }))
-
-                // have to ignore the following members for unit tests to pass even though they are mapped by .BeforeMap
-                .ForMember(dc => dc.PhoneNumbers, op => op.Ignore())
-                .ForMember(dc => dc.MiddleNameOrInitial, op => op.Ignore())
-                .ForMember(dc => dc.LastNameOrSurname, op => op.Ignore())
-                .ForMember(dc => dc.Address, op => op.Ignore())
-                .ForMember(dc => dc.AuditInfo, op => op.Ignore())
-                .ForMember(dc => dc.Label, op => op.Ignore()) // todo: xverify - Greg Murray on 2014-08-26
                 ;
-
         }
     }
 }
--- Mozu.SiteBuilder.UnitTests/Admin/Api/ModelMapping/CustomerContactMappingTest.cs
+++ Mozu.SiteBuilder.UnitTests/Admin/Api/ModelMapping/CustomerContactMappingTest.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using AutoMapper;
 using Mozu.SiteBuilder.UX.Admin.Api.ModelMapping;
 using NUnit.Framework;
 using DC = Mozu.Customer.Contracts;
 using Mozu.SiteBuilder.UX.Admin.Api.Models;
+using Should;
 
 namespace Mozu.SiteBuilder.UnitTests.Admin.Api.ModelMapping
 {
@@ -15,47 +14,6 @@ namespace Mozu.SiteBuilder.UnitTests.Admin.Api.ModelMapping
     [Category("Mapping")]
     public class CustomerContactMappingTest
     {
-        private static DC.CustomerContact dcTestObj = new DC.CustomerContact
-        {
-            AccountId = 1100,
-            Address = new Core.Api.Contracts.Address
-            {
-                Address1 = "1835 Kramer Lane",
-                CityOrTown = "Austin",
-                StateOrProvince = "TX",
-                CountryCode = "US",
-                PostalOrZipCode = "78758",
-                AddressType = Core.Api.Contracts.Address.AddressTypes.Residential
-            },
-            CompanyOrOrganization = "",
-            Email = "joeblow@volusion.com",
-            FaxNumber = null,
-            FirstName = "AAA",
-            MiddleNameOrInitial = "",
-            LastNameOrSurname = "BB",
-            PhoneNumbers = new Core.Api.Contracts.Phone
-            {
-                Home = "512-555-0000",
-                Mobile = "512-555-0000",
-                Work = ""
-            },
-            Types = new List<DC.ContactType> {
-                new DC.ContactType { Name = DC.ContactTypeConst.SHIPPING, IsPrimary = true },
-                new DC.ContactType { Name = DC.ContactTypeConst.BILLING, IsPrimary = true },
-            }
-        };
-        private static CustomerContact testObj = new CustomerContact
-        {
-            AccountId = 1100,
-            CompanyOrOrganization = "",
-            Email = "joeblow@volusion.com",
-            FaxNumber = null,
-            FirstName = "AAA",
-            IsBilling = true
-
-        };
-
-
         [TestFixtureSetUp]
         public void FixtureSetup()
         {
@@ -65,7 +23,6 @@ namespace Mozu.SiteBuilder.UnitTests.Admin.Api.ModelMapping
                 cfg.AddProfile<ContactMapping>();
                 cfg.AddProfile<CustomerContactMapping>();
             });
-            
         }
 
         [TestFixtureTearDown]
@@ -75,41 +32,107 @@ namespace Mozu.SiteBuilder.UnitTests.Admin.Api.ModelMapping
         }
 
         [Test]
-        public void AddressType_should_map_to_sb_contract() {
-            var mapped = Mapper.Map<DC.CustomerContact, CustomerContact>(dcTestObj);
-            Assert.AreEqual(Core.Api.Contracts.Address.AddressTypes.Residential, mapped.AddressType);
-        }
-        
-        [Test]
-        public void AddressType_should_map_to_dc_contract()
+        public void Should_map_from_contract_to_domain()
         {
-            var custContact = new CustomerContact()
+            var contractContact = new DC.CustomerContact
             {
-                IsPrimaryShipping = true,
-                IsShipping = true
+                AccountId = 1100,
+                Address = new Core.Api.Contracts.Address
+                {
+                    Address1 = "1835 Kramer Lane",
+                    CityOrTown = "Austin",
+                    StateOrProvince = "TX",
+                    CountryCode = "US",
+                    PostalOrZipCode = "78758",
+                    AddressType = Core.Api.Contracts.Address.AddressTypes.Residential
+                },
+                CompanyOrOrganization = Guid.NewGuid().ToString(),
+                Email = "joeblow@volusion.com",
+                FirstName = "AAA",
+                MiddleNameOrInitial = "BBB",
+                LastNameOrSurname = "CCC",
+                FaxNumber = "555-444-3333",
+                PhoneNumbers = new Core.Api.Contracts.Phone
+                {
+                    Home = "512-555-0000",
+                    Mobile = "512-555-0000",
+                    Work = "555-123-1234"
+                },
+                Types = new List<DC.ContactType> {
+                    new DC.ContactType { Name = DC.ContactTypeConst.SHIPPING, IsPrimary = true },
+                    new DC.ContactType { Name = DC.ContactTypeConst.BILLING, IsPrimary = true }
+                }
             };
-            var mapped = Mapper.Map<DC.CustomerContact>(custContact);
-            Assert.IsTrue(mapped.Types[0].IsPrimary);
-            Assert.AreEqual("Shipping", mapped.Types[0].Name);
+
+            var domainContact = Mapper.Map<DC.CustomerContact, CustomerContact>(contractContact);
+            domainContact.AccountId.ShouldEqual(contractContact.AccountId);
+            domainContact.Address1.ShouldEqual(contractContact.Address.Address1);
+            domainContact.CityOrTown.ShouldEqual(contractContact.Address.CityOrTown);
+            domainContact.StateOrProvince.ShouldEqual(contractContact.Address.StateOrProvince);
+            domainContact.CountryCode.ShouldEqual(contractContact.Address.CountryCode);
+            domainContact.PostalOrZipCode.ShouldEqual(contractContact.Address.PostalOrZipCode);
+            domainContact.AddressType.ShouldEqual("Residential");
+            domainContact.CompanyOrOrganization.ShouldEqual(contractContact.CompanyOrOrganization);
+            domainContact.Email.ShouldEqual(contractContact.Email);
+            domainContact.FirstName.ShouldEqual(contractContact.FirstName);
+            domainContact.MiddleName.ShouldEqual(contractContact.MiddleNameOrInitial);
+            domainContact.LastName.ShouldEqual(contractContact.LastNameOrSurname);
+            domainContact.FaxNumber.ShouldEqual(contractContact.FaxNumber);
+            domainContact.HomePhone.ShouldEqual(contractContact.PhoneNumbers.Home);
+            domainContact.MobilePhone.ShouldEqual(contractContact.PhoneNumbers.Mobile);
+            domainContact.WorkPhone.ShouldEqual(contractContact.PhoneNumbers.Work);
+            domainContact.IsBilling.ShouldBeTrue();
+            domainContact.IsPrimaryBilling.ShouldBeTrue();
+            domainContact.IsShipping.ShouldBeTrue();
+            domainContact.IsPrimaryShipping.ShouldBeTrue();
         }
 
         [Test]
-        public void Should_map_CustomerContact_base()
+        public void Should_map_from_domain_to_contract()
         {
-            var mapped = Mapper.Map<CustomerContact>(dcTestObj);
+            var domainContact = new CustomerContact
+            {
+                AccountId = 1100,
+                Address1 = "1835 Kramer Lane",
+                CityOrTown = "Austin",
+                StateOrProvince = "TX",
+                CountryCode = "US",
+                PostalOrZipCode = "78758",
+                AddressType = "Residential",
+                CompanyOrOrganization = Guid.NewGuid().ToString(),
+                Email = "joeblow@volusion.com",
+                FirstName = "AAA",
+                MiddleName = "BBB",
+                LastName = "CCC",
+                FaxNumber = "555-444-3333",
+                HomePhone = "512-555-0000",
+                MobilePhone = "512-555-0000",
+                WorkPhone = "555-123-1234",
+                IsBilling = true,
+                IsPrimaryBilling = true,
+                IsShipping = true,
+                IsPrimaryShipping = true
+            };
 
-            Assert.AreEqual(mapped.Email, dcTestObj.Email);
-            Assert.IsTrue(mapped.Email == dcTestObj.Email);
+            var contractContact = Mapper.Map<CustomerContact, DC.CustomerContact>(domainContact);
+            contractContact.AccountId.ShouldEqual(domainContact.AccountId.Value);
+            contractContact.Address.Address1.ShouldEqual(domainContact.Address1);
+            contractContact.Address.CityOrTown.ShouldEqual(domainContact.CityOrTown);
+            contractContact.Address.StateOrProvince.ShouldEqual(domainContact.StateOrProvince);
+            contractContact.Address.CountryCode.ShouldEqual(domainContact.CountryCode);
+            contractContact.Address.PostalOrZipCode.ShouldEqual(domainContact.PostalOrZipCode);
+            contractContact.Address.AddressType.ShouldEqual(Core.Api.Contracts.Address.AddressTypes.Residential);
+            contractContact.CompanyOrOrganization.ShouldEqual(domainContact.CompanyOrOrganization);
+            contractContact.Email.ShouldEqual(domainContact.Email);
+            contractContact.FirstName.ShouldEqual(domainContact.FirstName);
+            contractContact.MiddleNameOrInitial.ShouldEqual(domainContact.MiddleName);
+            contractContact.LastNameOrSurname.ShouldEqual(domainContact.LastName);
+            contractContact.FaxNumber.ShouldEqual(domainContact.FaxNumber);
+            contractContact.PhoneNumbers.Home.ShouldEqual(domainContact.HomePhone);
+            contractContact.PhoneNumbers.Mobile.ShouldEqual(domainContact.MobilePhone);
+            contractContact.PhoneNumbers.Work.ShouldEqual(domainContact.WorkPhone);
+            contractContact.Types.FirstOrDefault(x => x.Name == DC.ContactTypeConst.BILLING && x.IsPrimary).ShouldNotBeNull();
+            contractContact.Types.FirstOrDefault(x => x.Name == DC.ContactTypeConst.SHIPPING && x.IsPrimary).ShouldNotBeNull();
         }
-
-        [Test]
-        public void Should_map_CustomerContact_base_reverse()
-        {
-            var mapped = Mapper.Map<DC.CustomerContact>(testObj);
-
-            Assert.AreEqual(mapped.Email, dcTestObj.Email);
-            Assert.IsTrue(mapped.Email == dcTestObj.Email);
-        }
-
     }
 }
