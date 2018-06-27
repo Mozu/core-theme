d8b3a62170b34159f095b1326571e8e5c5c2f8eb
Paul Theron <Paul.Theron@KiboCommerce.com>
2018-06-27 14:18:24 -0500

    #NGDX-450 adding support to hypr tag set_header

---
+++ Mozu.SiteBuilder.Mvc/MessageHandler/ResponseHeaderAppenderMessagHandler.cs
@@ -0,0 +1,65 @@
+﻿using Mozu.Core.Logging;
+using Mozu.SiteBuilder.Mvc.Logging;
+using Mozu.SiteBuilder.Mvc.ViewEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mozu.SiteBuilder.Mvc.MessageHandler
+{
+    public class ResponseHeaderAppenderMessagHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return base.SendAsync(request, cancellationToken)
+                .ContinueWith(task => AddAdditionalHeaders(request, task.Result));
+        }
+        
+        public static HttpResponseMessage AddAdditionalHeaders (HttpRequestMessage request, HttpResponseMessage resp)
+        {
+            if(!request.HasAdditionalRespnoseHeaders())
+            {
+                return resp;
+            }
+            ILogger logger = null;
+            var additionalHeaders = request.GetAdditionalRespnoseHeaders();
+            foreach( var nvHeader in additionalHeaders)
+            {
+                if (!resp.Headers.TryAddWithoutValidation(nvHeader.Name, nvHeader.Value))
+                {
+                    if ( logger == null)
+                    {
+                        var exceptionLogWrapper = request.Resolve<ExceptionContextLogWrapper>();
+                        logger = exceptionLogWrapper.GetLogger();
+                    }
+                    logger.Warn($"unable to write header {nvHeader.Name} {nvHeader.Value}");
+                }
+            }
+            return resp;
+        }
+    }
+}
+public static class ResponseHeaderExtensions
+{
+    const  string GetRespnoseHeadersKey = "Mozu.SiteBuilder.Mvc.MessageHandler.GetRespnoseHeaders";
+    public static List<NameValueHeaderValue> GetAdditionalRespnoseHeaders ( this HttpRequestMessage req )
+    {
+    
+        object tmp;
+        if (!req.Properties.TryGetValue(GetRespnoseHeadersKey, out tmp))
+        {
+            tmp = new List<NameValueHeaderValue>();
+            req.Properties.Add(GetRespnoseHeadersKey, tmp);
+        }
+        return (List<NameValueHeaderValue>)tmp;
+    }
+    public static bool HasAdditionalRespnoseHeaders(this HttpRequestMessage req)
+    {
+        return req.Properties.ContainsKey(GetRespnoseHeadersKey);
+    }
+}
---
+++ Mozu.SiteBuilder.UnitTests/Mvc/Tags/SetHeaderTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Routing;
+using NUnit.Framework;
+using Autofac;
+using Mozu.SiteBuilder.Mvc;
+using Mozu.SiteBuilder.Mvc.Catalog;
+using Mozu.SiteBuilder.Mvc.Contexts;
+using Mozu.SiteBuilder.Mvc.SEO;
+using Mozu.SiteBuilder.UX.Models.StoreFront.Catalog;
+using NSubstitute;
+using UrlHelper = Mozu.SiteBuilder.Mvc.Helpers.UrlHelper;
+using System.Web;
+using Mozu.SiteBuilder.Mvc.ViewEngine;
+
+namespace Mozu.SiteBuilder.UnitTests.Mvc.Tags
+{
+    [Category("Hypr")]
+    [Category("MakeUrl")]
+    [TestFixture]
+    public class SetHeaderTests : TemplateTestBase
+    {
+        [Test, TestCaseSource("GetTests")]
+        public void SetHeader(TestDescriptor desc)
+        {
+            RunTemplate(desc);
+        }
+
+        private static List<TestDescriptor> GetTests()
+        {
+           
+            
+            
+            
+            Action<ContainerBuilder> containerMods = cb =>
+            {
+                var hcb = Substitute.For<HttpContextBase>();
+                var hrb = Substitute.For<HttpResponseBase>();
+                hcb.Response.Returns(hrb);
+                var headers = new System.Collections.Specialized.NameValueCollection();
+                headers.Add("a", "c");
+                hrb.Headers.Returns(headers);
+
+                cb.Register(c => hcb).As<HttpContextBase>();
+            };
+
+
+
+            return new List<TestDescriptor>
+            {
+                new TestDescriptor
+                {
+                    Name = "custom valueonly w replace ",
+                    Template = @"{% set_header ""a:b"" %}",
+                    ContainerModifier = containerMods,
+                    ExpectedFunc = TestDescriptor.CompareLiteral(""),
+                    AssertFn = (s,d)=>{
+                        var httpContext = (d.Context["_vc"] as HyprViewContext).HttpContext;
+                        httpContext.Response.Received().AddHeader("a","b");
+
+                        Assert.AreEqual(httpContext.Response.Headers["a"] ,null);
+                    }
+                },
+                 new TestDescriptor
+                {
+                    Name = "custom valueonly no replace ",
+                    Template = @"{% set_header ""a:b"" replace=false %}",
+                    ContainerModifier = containerMods,
+                    ExpectedFunc = TestDescriptor.CompareLiteral(""),
+                    AssertFn = (s,d)=>{
+                        var httpContext = (d.Context["_vc"] as HyprViewContext).HttpContext;
+                        httpContext.Response.Received().AddHeader("a","b");
+
+                        Assert.AreEqual(httpContext.Response.Headers["a"] ,"c");
+                    }
+                },
+
+                 new TestDescriptor
+                {
+                    Name = "custom kvp w replace ",
+                    Template = @"{% set_header name=""a"" value=""b"" %}",
+                    ContainerModifier = containerMods,
+                    ExpectedFunc = TestDescriptor.CompareLiteral(""),
+                    AssertFn = (s,d)=>{
+                        var httpContext = (d.Context["_vc"] as HyprViewContext).HttpContext;
+                        httpContext.Response.Received().AddHeader("a","b");
+
+                        Assert.AreEqual(httpContext.Response.Headers["a"] ,null);
+                    }
+                },
+
+            };
+        }
+
+    }
+}
