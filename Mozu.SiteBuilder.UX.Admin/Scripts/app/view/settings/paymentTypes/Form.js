8cd84e6de8a1318020294ac3ea8da3aca221ee3a
Aaron Jones <Amjcraft@gmail.com>
2018-06-29 10:03:30 -0500

    NGCOM-1433 Switching context on payment types now creates new externalpayments store

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/settings/paymentTypes/Form.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/settings/paymentTypes/Form.js
@@ -25,7 +25,10 @@ Ext.define('Taco.view.settings.paymentTypes.Form', {
             me.purchaseOrder
         ];
 
-        me.externalGateWayDefinitionsStore = Taco.core.data.StoreManager.getOrCreate('Taco.store.ExternalGatewayDefinitions');
+        me.externalGateWayDefinitionsStore = Ext.create('Taco.store.ExternalGatewayDefinitions', {
+            autoLoad: true
+        });
+
         me.buildExternalGateway();
 
         me.mon(Taco.core.StateManager, {
