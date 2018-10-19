28f072244a0cecc057407e31120cc703e9a6c4e1
Adam Turner <Adam.Turner@kibocommerce.com>
2018-10-19 16:09:19 -0500

    Revert "Change to external def store to update on site context change"
    This reverts commit bcc71e70ac2a8a4fce179acf175920ecc6414f69.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/store/ExternalGatewayDefinitions.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/store/ExternalGatewayDefinitions.js
@@ -4,7 +4,6 @@ Ext.define('Taco.store.ExternalGatewayDefinitions', {
     storeManagerConfig: {
         clearFilters: true,
         clearSort: true,
-        autoLoad: true,
-        contextLevel: 's'
+        autoLoad: true
     }
 });
