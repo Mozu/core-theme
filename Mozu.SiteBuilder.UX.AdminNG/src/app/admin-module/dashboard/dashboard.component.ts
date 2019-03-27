b4a235f57590059794e9f4be79c99129cadb1e13
xpmadhura <madhurad@xpanxion.co.in>
2019-03-27 19:31:55 +0530

    dashboard component mappingfunction moved to service file

--- Mozu.SiteBuilder.UX.AdminNG/src/app/admin-module/dashboard/dashboard.component.ts
+++ Mozu.SiteBuilder.UX.AdminNG/src/app/admin-module/dashboard/dashboard.component.ts
@@ -14,8 +14,6 @@ import {
 
 import { NotificationService } from '@global'
 import {
-  AccessTileModel,
-  AccessTileLink,
   Constants
 } from '@shared/index';
 
@@ -74,37 +72,13 @@ export class AdminDashboardComponent implements OnInit, OnDestroy {
 
     this._dashboardService.fetchAllDashboardTiles().subscribe((successResponse) => {
       this._loggerService.info("AdminDashboardComponent : _dashboardService.fetchAllDashboardTiles_successResponse");;
-      this.model.systemTiles = this.MapDasasboardCategoryToTiles(successResponse.filter(function (eachCategory) { return eachCategory.navParent == Constants.systemTileJsonNavParentPrefix; }));
-      this.model.mainTiles = this.MapDasasboardCategoryToTiles(successResponse.filter(function (eachCategory) { return eachCategory.navParent == Constants.mainTileJsonNavParentPrefix; }));
+      this.model.systemTiles = this._dashboardService.MapDasasboardCategoryToTiles(successResponse.filter(function (eachCategory) { return eachCategory.navParent == Constants.systemTileJsonNavParentPrefix; }));
+      this.model.mainTiles = this._dashboardService.MapDasasboardCategoryToTiles(successResponse.filter(function (eachCategory) { return eachCategory.navParent == Constants.mainTileJsonNavParentPrefix; }));
       this._changeDetectionRef.detectChanges();
 
     }, (errResponse) => {
       this._loggerService.info("AdminDashboardComponent : _dashboardService.fetchAllDashboardTiles_errResponse");;
       throw new HttpError(ErrorCode.DashboardTilesGetFailed,ErroNotificationType.Toaster);
     });
-  }
-
-  private MapDasasboardCategoryToTiles(dashboardCategories: any): AccessTileModel[] {
-    this._loggerService.info("AdminDashboardComponent : MapDasasboardCategoryToTiles");
-    let allAccessTiles: AccessTileModel[];
-    if (dashboardCategories != null && dashboardCategories != undefined && dashboardCategories.length > 0) {
-      allAccessTiles = [];
-      dashboardCategories.forEach(eachDasboardCategory => {
-        let accessTileModel = new AccessTileModel();
-        accessTileModel.sectionText = eachDasboardCategory.label;
-        accessTileModel.sectionImageURL = eachDasboardCategory.imageURL;
-        if (eachDasboardCategory.items != null && eachDasboardCategory.items != undefined && eachDasboardCategory.items.length > 0) {
-          accessTileModel.sectionLinks = [];
-          eachDasboardCategory.items.forEach(eachDasbhboardCategoryItem => {
-            let sectionLink = new AccessTileLink();
-            sectionLink.linkDataText = eachDasbhboardCategoryItem.label;
-            sectionLink.linkDataURL = eachDasbhboardCategoryItem.navUrl;//Constants.voidNavigationLink; 
-            accessTileModel.sectionLinks.push(sectionLink);
-          });
-        }
-        allAccessTiles.push(accessTileModel);
-      });
-    }
-    return allAccessTiles;
   }
 }
--- Mozu.SiteBuilder.UX.AdminNG/src/app/admin-module/dashboard/dashboard.service.ts
+++ Mozu.SiteBuilder.UX.AdminNG/src/app/admin-module/dashboard/dashboard.service.ts
@@ -1,19 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Constants } from '@shared';
 import { HttpClient } from '@angular/common/http';
+import { LoggerService } from '@core'
 
 import {
     HttpService
 } from '@core';
 import { Observable } from 'rxjs';
 
+import { AccessTileModel, AccessTileLink } from '@shared/index';
+
 @Injectable()
 export class DashbaordService {
 
-    constructor(private _http: HttpClient) { }
+    constructor(private _http: HttpClient,   
+                private _loggerService: LoggerService) {}
 
     public fetchAllDashboardTiles(): Observable<any> {
         return this._http.get(Constants.JsonResources.dasbhoardTiles);
     }
+
+    public MapDasasboardCategoryToTiles(dashboardCategories: any): AccessTileModel[] {
+        this._loggerService.info("AdminDashboardComponent : MapDasasboardCategoryToTiles");
+        let allAccessTiles: AccessTileModel[];
+        if (dashboardCategories != null && dashboardCategories != undefined && dashboardCategories.length > 0) {
+          allAccessTiles = [];
+          dashboardCategories.forEach(eachDasboardCategory => {
+            let accessTileModel = new AccessTileModel();
+            accessTileModel.sectionText = eachDasboardCategory.label;
+            accessTileModel.sectionImageURL = eachDasboardCategory.imageURL;
+            if (eachDasboardCategory.items != null && eachDasboardCategory.items != undefined && eachDasboardCategory.items.length > 0) {
+              accessTileModel.sectionLinks = [];
+              eachDasboardCategory.items.forEach(eachDasbhboardCategoryItem => {
+                let sectionLink = new AccessTileLink();
+                sectionLink.linkDataText = eachDasbhboardCategoryItem.label;
+                sectionLink.linkDataURL = eachDasbhboardCategoryItem.navUrl;//Constants.voidNavigationLink; 
+                accessTileModel.sectionLinks.push(sectionLink);
+              });
+            }
+            allAccessTiles.push(accessTileModel);
+          });
+        }
+        return allAccessTiles;
+      }
 }
 
