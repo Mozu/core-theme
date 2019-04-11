3b0f499a4412df40d24a9c5c059741c8811b547b
Paul Theron <Paul.Theron@KiboCommerce.com>
2019-04-11 14:36:10 -0500

    NGCOM-2564 - Publish robots file when remains in it pending.

--- Mozu.SiteBuilder.UX.Admin/Api/WebToolsController.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/WebToolsController.cs
@@ -1,13 +1,9 @@
-using System;
-using System.IO;
-using System.Linq;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.InteropServices;
-using System.ServiceModel;
-using System.ServiceModel.Web;
 using System.Threading.Tasks;
-using System.Web.Http;
+using Mozu.Content.Contracts;
+using Mozu.Content.Contracts.Clients;
 using Mozu.Core.Api.Routing;
 using Mozu.SiteBuilder.Mvc.CMS;
 using Mozu.SiteBuilder.UX.Admin.Api.Models;
@@ -19,10 +15,18 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
     public class WebToolsController : BaseController
     {
         private readonly IWebToolsRepository _webToolsRepository;
-
-        public WebToolsController(IWebToolsRepository webToolsRepository)
+        private readonly IDocumentPublishingWebApiClient _documentPublishingWebApiClient;
+        private readonly IDocumentListWebApiClient _documentListWebApiClient;
+        private const string ContentCollection = "siteSettings@mozu";
+        private const string RobotsFileName = "robots.txt";
+        
+        public WebToolsController(IWebToolsRepository webToolsRepository,
+            IDocumentListWebApiClient documentListWebApiClient,
+            IDocumentPublishingWebApiClient documentPublishingWebApiClient)
         {
             _webToolsRepository = webToolsRepository;
+            _documentListWebApiClient = documentListWebApiClient;
+            _documentPublishingWebApiClient = documentPublishingWebApiClient;
         }
 
         public class WmtReq 
@@ -34,21 +38,24 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
 		[HttpPostRoute(UriTemplate = "webmasterTools")]
         public async Task<Response<string>> UpdateWebmasterTools(WmtReq req)
 		{
-
-
 		    await _webToolsRepository.SaveWebmasterToolsFile(req.Content, req.Name);
-
-                return Message3<string>(true, "File uploaded");
-            
-
-         
+            return Message3<string>(true, "File uploaded");
         }
 
-		[HttpPostRoute(UriTemplate = "robotsTxt")]
+        [HttpPostRoute(UriTemplate = "robotsTxt")]
         public async Task<HttpResponseMessage> UpdateRobotsTxt(RobotsTxtSettings robots)
         {
-            
-            await _webToolsRepository.SaveRobotsContent(robots);
+            await _webToolsRepository.SaveRobotsContent(robots).ConfigureAwait(false);
+
+            //Publish the document if it is not active.
+            var docResult = (await _documentListWebApiClient.GetTreeDocument(ContentCollection, RobotsFileName))
+                .ReadAsSync();
+
+            if (docResult.PublishState == PublishStates.Draft)
+            {
+                var docIds = new List<string> {docResult.Id};
+                await _documentPublishingWebApiClient.PublishDocuments(docIds).ConfigureAwait(false);
+            }
 
 		    return Request.CreateResponse(HttpStatusCode.OK);
         }
