9aecd5e035496d3e0a9c9492dd7f1e9c0d690682
Sneha Nerekar <sneha.nerekar@kibocommerce.com>
2019-05-10 14:01:58 +0530

    Sticky Navbar

--- Mozu.SiteBuilder.UX.AdminNG/src/app/app.component.ts
+++ Mozu.SiteBuilder.UX.AdminNG/src/app/app.component.ts
@@ -30,7 +30,8 @@ import {
 @Component({
     moduleId: module.id,
     selector: 'app-root',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
 
--- Mozu.SiteBuilder.UX.AdminNG/src/app/location-groups-module/create/create.component.ts
+++ Mozu.SiteBuilder.UX.AdminNG/src/app/location-groups-module/create/create.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoggerService } from '@core'
 import {TreeNode} from 'primeng/components/common/api';
+import { trigger, state, transition, style, animate } from '@angular/animations';  
+import { DOCUMENT } from '@angular/common';
 
 @Component({
     selector: 'location-group-create',
@@ -23,4 +25,22 @@ export class LocationGroupCreateComponent implements OnInit {
         this._loggerService.info("LocationGroupCreateComponent : onPhysicalLocationSelect"+ JSON.stringify(physicalLocation));
         this.physicalLocation = physicalLocation;
     }
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll(e) {
+       if (window.pageYOffset > 20) {
+         let element = document.getElementById('stickynav');
+         element.classList.add('sticky');
+         let element2 = document.getElementById('main-container');
+         element2.classList.add('forextratoppadding');
+         //
+       } else {
+        let element = document.getElementById('stickynav');
+          element.classList.remove('sticky'); 
+            let element2 = document.getElementById('main-container');
+              element2.classList.remove('forextratoppadding');
+       }
+    }
+    scroll(el: HTMLElement) {
+        el.scrollIntoView({behavior: 'smooth'});
+      }
 }
