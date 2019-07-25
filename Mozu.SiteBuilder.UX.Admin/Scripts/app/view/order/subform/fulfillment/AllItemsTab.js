265cf0dc97742b0cf11720b972cc6ec97e082eb3
Madhura Todkar <madhura.todkar@kibocommerce.com>
2019-07-25 18:13:43 +0530

    allitemstab changes, removed buttons and related functions

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/subform/fulfillment/AllItemsTab.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/subform/fulfillment/AllItemsTab.js
@@ -18,241 +18,26 @@ Ext.define('Taco.view.order.subform.fulfillment.AllItemsTab', {
     },
 
     initUI: function () {
-        this.packageItems = Ext.create('Taco.view.order.widget.ShippingPackagesGrid', {
+        var me = this;
+        // itemId: this.packageRecord.code + 'shipment';
+        isEditable = me.isEditable;
+        this.shipmentItems = Ext.create('Taco.view.order.widget.ShippingPackagesGrid', {
             shipmentRecord: this.shipmentRecord,
             record: this.record,
             hidden: this.shipmentRecord.shipmentStatus == 'Cancelled',
             margin: '10px 0 10px 0',
-            padding: '0 1px 0 0'
+            padding: '0 1px 0 0',
+            minHeight: '300',
         });
         
 
         this.items = [
-            this.buildAllItemsActions(),
-            this.packageItems
+            this.shipmentItems
         ];
-    },
-
-    buildAllItemsActions: function () {
-        this.items = [];
-        var me = this;
-        isEditable = me.isEditable;
-        this.totalsContainer = Ext.widget({
-            xtype: 'container',
-            cls: 'taco-order-fulfillment-package-body',
-            padding: '0 0 10 0',
-            layout: {
-                type: 'hbox',
-                align: 'left'
-            },
-            defaults: {
-                xtype: 'component',
-            },
-            items: [
-                {
-                    margin: {
-                        left: 500,
-                        top: 50,
-                    },
-                    hidden: this.shipmentRecord.shipmentStatus != 'Cancelled',
-                    flex: 1,
-                    html: '<h3 class="">Shipment is cancelled, see all items on the cancellation tab.</h3>',
-                },
-                {
-                    flex: 1,
-                    html: '',
-                },
-                {
-                    xtype: 'container',
-                    padding: '8 0 0 0',
-                    items: [
-                        Ext.widget('button', {
-                            itemId: 'cancelShipmentTotals',
-                            ui: 'action',
-                            scale: 'medium',
-                            text: 'Cancel',
-                            hidden: !me.isEditable,
-                            handler: function (evt) {
-                                me.toggleEdit();
-                            }
-                        }),
-                        Ext.widget('button', {
-                            itemId: 'saveShipmentTotals',
-                            ui: 'action-primary',
-                            scale: 'medium',
-                            text: 'Save',
-                            hidden: !me.isEditable,
-                            margin: {
-                                left: 10,
-                                right: 70
-                            },
-                            handler: function (evt) {
-                                me.toggleEdit();
-                                var grid = me.packageItems.getView();
-                                console.log(grid);
-                                me.doSave(me.shipmentRecord.items);
-                            }
-                        }),
-                        Ext.widget('button', {
-                            itemId: 'editShipmentTotals',
-                            ui: 'action',
-                            scale: 'medium',
-                            text: 'Edit',
-                            margin: '0 20 0 0',
-                            hidden: me.isEditable,//me.isShipmentAction(),
-                            handler: function (evt) {
-                                me.toggleEdit();
-                                var grid = me.packageItems.getView();
-                                grid.getSelectionModel().checkOnly = true;
-                                var selected = grid.getSelectionModel().getSelection();
-                                grid.editingPlugin.startEditByPosition({ row: selected[0].$extCollectionIndex, column: 5 });
-                            }
-                        }),
-                        Ext.widget('splitbutton', {
-                            menuAlign: 'tr-br?',
-                            margin: '0 20 0 0',
-                            text: 'Reassign Item',
-                            itemId: 'reassignItemSplitButton',
-                            hidden: me.isShipmentAction(),
-                            menu: [
-                                {
-                                    text: 'Manual Reassign',
-                                    handler: function () {
-                                                me.record.getCandidateSuggestions({
-                                                    jsonData: "1",
-                                                    success: function (response) {
-                                                        me.isRecordSaved = true;
-                                                        me.setLoading(false, me.body);
-                                                        var json = Ext.decode(response.responseText, true);
-
-
-                                                        Ext.create('Taco.view.order.modal.fulfillment.ItemsReassign', {
-                                                            layout: 'hbox',
-                                                            width: 1080,
-                                                            height: 450,
-                                                            record: me.record,
-                                                            inventoryData: json,
-                                                            listeners: {
-                                                                saveSuccess: {
-                                                                    fn: function (json) {
-                                                                        //me.fireEvent('orderCancelled', json);
-                                                                    },
-                                                                    //scope: me
-                                                                }
-                                                            }
-                                                        });
-                                                    },
-                                                    failure: function (response) {
-                                                        me.setLoading(false, me.body);
-                                                        // close the dialog
-                                                        me.close();
-                                                    }
-                                                })
-                                            }
-                                    
-                                },
-                                {
-                                    text: 'Auto Reassign',
-                                    handler: function () { }
-                                }
-                            ],
-
-                            ui: 'action',
-                            scale: 'medium',
-                        }),
-                        Ext.widget('splitbutton', {
-                            menuAlign: 'tr-br?',
-                            margin: '0 20 0 0',
-                            text: 'Update Item Status',
-                            itemId: 'updateItemSplitButton',
-                            handler: function () {
-
-                            },
-                            hidden: me.isShipmentAction(),
-                            menu: me.getShipmentLevelSplitMenu(),
-                            ui: 'action',
-                            scale: 'medium',
-                        })
-                    ]
-                }
-            ],
-            listeners: {
-                afterrender: function (obj) { 
-                    me.updateUI();
-                }
-            }
-        });
-        return this.totalsContainer;
-        //this.items.push(this.totalsContainer);
-    },
-
-    updateUI: function () {
-        isEditable = this.isEditable;
-    },
-
-    getShipmentLevelSplitMenu: function () {
-
-        var actionCancelItem = {
-            text: 'Cancel Item',
-            handler: function () { }
-        };
-
-        var actionMoveToBackorder = {
-            text: 'Move To backorder',
-            handler: function () { }
-        };
-
-        var actionEditItem = {
-            text: 'Edit Item',
-            handler: function () { }
-        };
-
-        if (this.shipmentRecord.shipmentStatus.toLowerCase() == 'ready') {
-            return [
-                actionMoveToBackorder,
-                actionEditItem,
-                actionCancelItem
-            ];
-        }
-        else if (this.shipmentRecord.shipmentStatus.toLowerCase() == 'backorder') {
-            return [
-                actionEditItem,
-                actionCancelItem
-            ];
-        }
-        else if (this.shipmentRecord.shipmentStatus.toLowerCase() == 'customer_care') {
-            return [
-                actionMoveToBackorder,
-                actionEditItem,
-                actionCancelItem
-            ];
-        }
-    },
-
-    isShipmentAction: function () {
-        if (this.shipmentRecord.shipmentStatus.toLowerCase() == 'fulfilled' || this.shipmentRecord.shipmentStatus.toLowerCase() == 'canceled')
-            return true;
-        return false;
     },
 
     onDestroy: function () {
         this.callParent(arguments);
-    },
-
-    toggleEdit: function () {
-        if (this.isEditable) {
-            this.isEditable = false;
-            this.totalsContainer.items.items[2].items.get('saveShipmentTotals').hide();
-            this.totalsContainer.items.items[2].items.get('cancelShipmentTotals').hide();
-            this.totalsContainer.items.items[2].items.get('editShipmentTotals').show();
-        }
-        else {
-            this.isEditable = true;
-            this.totalsContainer.items.items[2].items.get("saveShipmentTotals").show();
-            this.totalsContainer.items.items[2].items.get('cancelShipmentTotals').show();
-            this.totalsContainer.items.items[2].items.get('editShipmentTotals').hide();
-        }
-        this.updateUI();
     },
 
     doSave: function (shipmentRecord) {
