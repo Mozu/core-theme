889f35559e462908bca2b88c8bbb379ea2df2fa8
Madhura Todkar <madhura.todkar@kibocommerce.com>
2019-08-06 15:22:45 +0530

    undo mock data for candidate api

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/model/Order.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/model/Order.js
@@ -2960,10 +2960,10 @@ Ext.define('Taco.model.Order', {
         //}
 
         Ext.apply(config, {
-            url: '/admin/Scripts/app/mocks/candidateSuggestions.json',
-            method: 'GET'
-             //url: '/admin/app/order/shipping/candidates',
-             //method: 'POST'
+            //url: '/admin/scripts/app/mocks/candidatesuggestions.json',
+            //method: 'get'
+             url: '/admin/app/order/shipping/candidates',
+             method: 'POST'
             
         });
 
