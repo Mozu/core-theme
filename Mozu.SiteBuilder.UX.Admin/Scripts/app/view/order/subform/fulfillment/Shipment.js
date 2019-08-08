d9f81b5e4acc9c119c7a15c02a20ef2836f281f2
SagarRaut <sagar.raut@kibocommerce.com>
2019-08-08 11:01:45 +0530

    COM-469 Fixed
    https://jiracloudkibo.atlassian.net/browse/COM-469

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/subform/fulfillment/Shipment.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/subform/fulfillment/Shipment.js
@@ -125,7 +125,7 @@ Ext.define('Taco.view.order.subform.fulfillment.Shipment', {
                         items: [
                             Ext.widget('splitbutton', {
                                 menuAlign: 'tr-br?',
-                                text: 'Update Shipment Status',
+                                text: 'Update Shipment',
                                 itemId: 'updateShipmentSplitButton',
                                 handler: function () {
 
