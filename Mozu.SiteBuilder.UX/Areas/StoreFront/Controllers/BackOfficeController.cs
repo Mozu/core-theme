fa2f311588ea10851811c6635b259cd25c1cf1e4
Gabriela Gonzalez <Gabriela.Gonzalez@kibocommerce.com>
2019-08-16 16:36:47 -0500

    Fulfiller Hypr Template test data files and changes

--- Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/BackOfficeController.cs
+++ Mozu.SiteBuilder.UX/Areas/StoreFront/Controllers/BackOfficeController.cs
@@ -42,6 +42,8 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
         private const string CMS_LIST_NAME = "emailTemplateContent@mozu";
         private const string ORDER_PREVIEW_RESOURCE_NAME = "backoffice.order1";
         private const string PACKAGE_PREVIEW_RESOURCE_NAME = "backoffice.package1";
+        private const string PICKWAVE_PREVIEW_RESOURCE_NAME = "backoffice.pickwave1";
+        private const string SHIPMENTS_PREVIEW_RESOURCE_NAME = "backoffice.shipments1";
 
         /// <summary>
         /// Public constructor.
@@ -314,6 +316,13 @@ namespace Mozu.SiteBuilder.UX.Areas.StoreFront.Controllers
                 object order = TestDataBroker.GetFileContents(ORDER_PREVIEW_RESOURCE_NAME).FirstOrDefault();
                 object model = TestDataBroker.GetFileContents(PACKAGE_PREVIEW_RESOURCE_NAME).FirstOrDefault();
                 ViewData["order"] = order;
+                return await RenderWithContext(template, model);
+            }
+            else if (templateid == "pick-list")
+            {
+                object shipment = TestDataBroker.GetFileContents(SHIPMENTS_PREVIEW_RESOURCE_NAME).FirstOrDefault();
+                object model = TestDataBroker.GetFileContents(PICKWAVE_PREVIEW_RESOURCE_NAME).FirstOrDefault();
+                ViewData["shipment"] = shipment;
                 return await RenderWithContext(template, model);
             }
             else
