36e810346ac471f39b79c53345706577cc614e93
ShaneMontgomery <Shane.Montgomery@kibocommerce.com>
2019-08-23 16:38:16 -0500

    missing pollyfill for Array.prototype.includes. Array.prototype.includes
     There was a missing pollyfill for Array.prototype.includes. Added a file for the pollyfill in the

--- Mozu.SiteBuilder.UX/BuiltinScripts/RequireJS/dist/mozu-require.compiler.js
+++ Mozu.SiteBuilder.UX/BuiltinScripts/RequireJS/dist/mozu-require.compiler.js
@@ -1,5 +1,5 @@
 /*! 
- * Mozu Require - v0.2.0 - 2019-03-29
+ * Mozu Require - v0.2.0 - 2019-04-18
  *
  * Copyright (c) 2019 Volusion, Inc.
  *
@@ -20,6 +20,57 @@
         };
     }
 })(window)
+//Resource copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes#Polyfill
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function (valueToFind, fromIndex) {
+
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      // 1. Let O be ? ToObject(this value).
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      var n = fromIndex | 0;
+
+      // 5. If n = 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(valueToFind, elementK) is true, return true.
+        if (sameValueZero(o[k], valueToFind)) {
+          return true;
+        }
+        // c. Increase k by 1. 
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
 //Resource copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 if (typeof Object.assign != 'function') {
     // Must be writable: true, enumerable: false, configurable: true
