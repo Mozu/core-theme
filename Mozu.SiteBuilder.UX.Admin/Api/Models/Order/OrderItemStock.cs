4a6c5779394877798772ab6aecd68f65f4111712
SagarRaut <sagar.raut@kibocommerce.com>
2019-09-24 14:02:00 -0500

    COM-724
    [UI][Backorder] - UI not showing appropriate date when backorder 'Available days' not configured on product/site level

--- Mozu.SiteBuilder.UX.Admin/Api/Models/Order/OrderItemStock.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/Models/Order/OrderItemStock.cs
@@ -9,7 +9,7 @@ namespace Mozu.SiteBuilder.UX.Admin.Api.Models.Order
     {
         public bool ManageStock { get; set; }
         public bool IsOnBackOrder { get; set; }
-        public DateTime AvailableDate { get; set; }
+        public DateTime? AvailableDate { get; set; }
         public int? StockAvailable { get; set; }
     }
 }
