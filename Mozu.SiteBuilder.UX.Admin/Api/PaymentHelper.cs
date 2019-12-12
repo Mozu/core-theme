73717fa52654a2241ac20af6895c40ba26b550b2
Aaron Jones <aaron.jones@kibocommerce.com>
2019-12-12 15:37:47 -0600

    COM-1036 check if payment had card type before compare

--- Mozu.SiteBuilder.UX.Admin/Api/PaymentHelper.cs
+++ Mozu.SiteBuilder.UX.Admin/Api/PaymentHelper.cs
@@ -18,6 +18,11 @@ namespace Mozu.SiteBuilder.UX.Admin.Api
 
             private bool hasCardType(string rank, string cardType)
             {
+                if(String.IsNullOrEmpty(cardType) || String.IsNullOrEmpty(rank))
+                {
+                    return false;
+                }
+
                 rank = rank.ToLower().Trim();
                 cardType = cardType.ToLower();
 
