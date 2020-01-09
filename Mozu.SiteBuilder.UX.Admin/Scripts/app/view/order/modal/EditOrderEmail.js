9eb3254b91d7efa1f0bea6d4aea430d1e8230e1e
AndrewMcclellan <andrew.mcclellan@kibocommerce.com>
2020-01-09 13:09:15 -0600

    suse 7- added ability to update order email address (#128)
    * Initial implementation for adding ability to update email address of orders
    
    * Finished adding feature to update order email

---
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/modal/EditOrderEmail.js
@@ -0,0 +1,65 @@
+﻿
+/**
+ * @class Taco.view.order.modal.EditOrderEmail
+ */
+
+Ext.define('Taco.view.order.modal.EditOrderEmail', {
+    extend: 'Taco.core.ux.window.Modal',
+   
+
+    autoShow: true,
+    scale: 'small',
+    title: 'Edit Order Email Address',
+
+    initComponent: function () {
+        this.form = Ext.widget(Ext.apply(Ext.create('Taco.core.ux.form.Form', {
+            layout: {
+                type: 'hbox'
+            },
+            defaults: {
+                margin: '0 20 0 0',
+                width: '100%'
+            },
+            items: [{
+                xtype: 'textfield',
+                name: 'email',
+                fieldLabel: 'Email',
+                value: this.record.getData().email,
+                data: this.record.getData()
+            }]
+        })));
+
+
+        this.items = [this.form];
+
+        this.callParent(arguments);
+
+    },
+    doSave: function () {
+        var me = this,
+            data = this.form.getValues();
+      
+
+        me.setLoading({
+            msg: "Saving"
+        }, me.body);
+
+        this.record.updateEmailAddress({
+            jsonData: this.record.getData(),
+            success: function (response) {
+                me.setLoading(false, me.body);
+                var json = Ext.decode(response.responseText, true),
+                    data;
+
+                if (!json) { return }
+                
+                data = json.items;
+                me.record.set(data);
+                me.saveSuccess(me.record);
+            },
+            failure: function () {
+                me.setLoading(false, me.body);
+            }
+        }, data.email);
+    }
+});
