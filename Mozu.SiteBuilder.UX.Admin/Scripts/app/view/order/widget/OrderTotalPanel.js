93458cfbda4518b14c47e2276df20a9f9cc34700
atulchavan <atul.chavan@kibocommerce.com>
2020-02-27 05:35:09 +0530

    COM-1081 Show shipping toggle without pricing fields for OMS only site (#193)
    * Show the shipping details toggle (but not other toggles) if this is an OMS only site.
    * Hide unsupported shipping-related pricing fields if this is an OMS only site.

--- Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/widget/OrderTotalPanel.js
+++ Mozu.SiteBuilder.UX.Admin/Scripts/app/view/order/widget/OrderTotalPanel.js
@@ -97,12 +97,16 @@ Ext.define('Taco.view.order.widget.OrderTotalPanel', {
 
     initSummaryToggleButtons: function () {
         var me = this;
-        if (!Taco.app.context.getSite().omsOnly) {
-            me.initToggleButton(".adjustment-summary", me.toggleAdjustmentDetails);
+
+        if (Taco.app.context.getSite().omsOnly) {
             me.initToggleButton(".shipping-summary", me.toggleShippingDetails);
-            me.initToggleButton(".tax-summary", me.toggleTaxDetails);
-            me.initToggleButton(".handling-summary", me.toggleHandlingDetails);
+            return;
         }
+
+        me.initToggleButton(".adjustment-summary", me.toggleAdjustmentDetails);
+        me.initToggleButton(".shipping-summary", me.toggleShippingDetails);
+        me.initToggleButton(".tax-summary", me.toggleTaxDetails);
+        me.initToggleButton(".handling-summary", me.toggleHandlingDetails);
     },
 
     initUI: function () {
@@ -518,7 +522,7 @@ Ext.define('Taco.view.order.widget.OrderTotalPanel', {
         '<tr class="shipping-handling-item">',
         '<td class="{tdCls}"></td>',
         '<td class="{tdCls}" colspan=><div class="{tdInnerCls}">Order Shipping Fee<tpl if="values.shippingMethodName">: {shippingMethodName}</tpl></div></td>',
-        '<td class="{tdCls}"><div class="{priceCls} {tdInnerCls}">{[this.getCurrencyFormat(values.shippingAmountBeforeDiscountsAndAdjustments)]}</div></td>',
+        '<td class="{tdCls}"><tpl if="!isOmsOnly"><div class="{priceCls} {tdInnerCls}">{[this.getCurrencyFormat(values.shippingAmountBeforeDiscountsAndAdjustments)]}</div></tpl></td>',
         '<td class="{tdCls}"></td>',
         '<td class="{tdCls}"></td>',
         '</tr>',
@@ -528,7 +532,7 @@ Ext.define('Taco.view.order.widget.OrderTotalPanel', {
         '<tr class="shipping-handling-item lineitemdiscount">',
         '<td class="{tdCls}"></td>',
         '<td class="{parent.tdCls}"><div class="x-grid-cell-inner {parent.tdInnerCls}">Line {lineId} Shipping Discount: {name}</div></td>',
-        '<td class="{parent.tdCls}"><div class="{parent.priceCls} {parent.tdInnerCls} negative-currency">({[this.getCurrencyFormat(values.fee)]})</div></td>',
+        '<td class="{parent.tdCls}"><tpl if="!isOmsOnly"><div class="{parent.priceCls} {parent.tdInnerCls} negative-currency">({[this.getCurrencyFormat(values.fee)]})</div></tpl></td>',
         '<td class="{parent.tdCls}"></td>',
         '<td class="{parent.tdCls}"></td>',
         '</tr>',
@@ -540,7 +544,7 @@ Ext.define('Taco.view.order.widget.OrderTotalPanel', {
         '">',
         '<td class="{tdCls}"></td>',
         '<td class="{parent.tdCls}"><div class="{parent.tdInnerCls}">Shipping Discount: {description}</div></td>',
-        '<td class="{parent.tdCls}"><div class="{parent.priceCls} {parent.tdInnerCls} negative-currency">({[this.getCurrencyFormat(values.total)]})</div></td>',
+        '<td class="{parent.tdCls}"><tpl if="!isOmsOnly"><div class="{parent.priceCls} {parent.tdInnerCls} negative-currency">({[this.getCurrencyFormat(values.total)]})</div></tpl></td>',
         '<td class="x-action-col-cell taco-menu-col-cell x-action-col-celladjustment-cell{parent.tdCls}">',
         '<div unselectable="on" isActive="{isActive}" discountId="{discountId}"  action="processDiscount"',
         'class="order-action-icon discount-', '<tpl if="isActive">suppress<tpl else>activate</tpl>', '">',
@@ -573,10 +577,11 @@ Ext.define('Taco.view.order.widget.OrderTotalPanel', {
             tdCls = "taco-grid-cell ",
             tdInnerCls = "taco-grid-cell-inner ",
             priceCls = "price-detail "
-        isEditable = this.isEditable,
+            isEditable = this.isEditable,
             isEditableCls = (isEditable) ? "taco-editable " : "",
             // need to fake an editable field in the template dom. if editable needs to be an anchor tag to allow for tabbing and div when uneditable
-            fakeInputDom = (isEditable) ? "a " : "div";
+            fakeInputDom = (isEditable) ? "a " : "div",
+            isOmsOnly = Taco.app.context.getSite().omsOnly;
 
         return [
 
