d4ac9a9efed21ab1c9a6c85082547fd67b995500
yogestmatta <yogest.matta@kibocommerce.com>
2020-04-06 15:08:00 +0530

    Removed getCookie function that is not required now.

--- scripts/pages/cart.js
+++ scripts/pages/cart.js
@@ -348,15 +348,7 @@ define(['modules/api',
         ],
         handleEnterKey: function () {
             this.addCoupon();
-        },
-        getCookie: function(cookiename) 
-        {
-          // Get name followed by anything except a semicolon
-          var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
-          // Return everything after the equal sign, or an empty string if the cookie name not found
-          return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
         }
-
     });
 
   function renderVisaCheckout(model) {
