554bd5e4ca0a62d100a58db488bcc65e04b8e30f
sparkle phipps <thomas.phipps@kibocommerce.com>
2020-06-08 18:59:30 +0000

    mozu core prime 1.201.12 [cicd:deploy qa-sb]

--- Mozu.SiteBuilder.Mvc/Tags/CMS/CmsDataAttributesTag.cs
+++ Mozu.SiteBuilder.Mvc/Tags/CMS/CmsDataAttributesTag.cs
@@ -18,94 +18,75 @@ using NDjango.FiltersCS.Compatibility;
 
 namespace Mozu.SiteBuilder.Mvc.Tags.Data
 {
-    
-    //[Obsolete]
-    //[Name("cms_data_attributes")]
-    //public class CmsDataAttributesTag : SimpleTagBase
-    //{
-    //    static string Convert(Dictionary<String,object> mmd)
-    //    {
-    //        var sw = new StringWriter();
-    //        object obj;
-    //        var  attributeName = "data-editing-element";
-    //        if (mmd.TryGetValue("data-attribute-name", out obj))
-    //        {
-    //            attributeName = obj.ToString();
-    //        }
-    //        if (mmd.TryGetValue("data-editing", out obj))
-    //        {
-    //            var value = obj as JsonValue;
-    //            if (value != null)
-    //            {
-    //                value.Save(sw, JsonSaveOptions.None);
-    //            }
-    //            else
-    //            {
-    //                var o = obj as JObject;
-    //                if (o != null)
-    //                {
-    //                    o.WriteTo(new JsonTextWriter(sw) { Formatting = Formatting.None });
-    //                }
-    //                else
-    //                {
-    //                    var s = obj as string;
-    //                    if (s != null)
-    //                    {
-    //                        sw.Write(s);
-    //                    }
-    //                    else
-    //                    {
-    //                        var jv = JsonValueExtensions.CreateFrom(obj);
-    //                        jv.Save(sw, JsonSaveOptions.None);
 
-    //                    }
-    //                }
-    //            }
-    //        }
-    //        else
-    //        {
-    //            var json = new JsonObject();
-    //            JsonPrimitive jPrim = null;
+    [Obsolete]
+    [Name("cms_data_attributes")]
+    public class CmsDataAttributesTag : SimpleTagBase
+    {
+        static string Convert(Dictionary<String, object> mmd)
+        {
+            var sw = new StringWriter();
+            object obj;
+            var attributeName = "data-editing-element";
+            if (mmd.TryGetValue("data-attribute-name", out obj))
+            {
+                attributeName = obj.ToString();
+            }
+            if (mmd.TryGetValue("data-editing", out obj))
+            {
+                var value = obj as JToken;
+                if (value != null)
+                {
+                    value.WriteTo(new JsonTextWriter(sw) { Formatting = Formatting.None });
+                }
+                else if (obj is string || obj?.GetType().IsPrimitive == true)
+                {
+                    sw.Write(obj.ToString());
+                }
+                else
+                {
+                    var j = JToken.FromObject(obj);
+                    j.WriteTo(new JsonTextWriter(sw) { Formatting = Formatting.None });
+                }
+            }
+            else
+            {
+                var j = JToken.FromObject(obj);
+                j.WriteTo(new JsonTextWriter(sw) { Formatting = Formatting.None });
+            }
 
-    //            foreach (var item in mmd.Where(item => JsonPrimitive.TryCreate(item.Value, out jPrim)))
-    //            {
-    //                json[ToJSName(item.Key)] = jPrim;
-    //            }
-    //            json.Save(sw, JsonSaveOptions.None);
-    //        }
+            return attributeName + "=\"" + HttpUtility.HtmlAttributeEncode(sw.GetStringBuilder().ToString()) + "\"";
+        }
 
-    //        return attributeName + "=\"" + HttpUtility.HtmlAttributeEncode(sw.GetStringBuilder().ToString()) + "\"";
-    //    }
-
-    //    static string ToJSName(string name)
-    //    {
-    //        return name;
-    //    }
+        static string ToJSName(string name)
+        {
+            return name;
+        }
 
 
-    //    protected override IEnumerable<WalkResult> ProcessTag(ArgumentCollection arguments, IContext context, Func<string, ITemplate> getTemplateFunc)
-    //    {
-    //        var isEditmode = context.PageContext().IsEditMode;
-    //        if (!isEditmode) return Enumerable.Empty<WalkResult>();
+        protected override IEnumerable<WalkResult> ProcessTag(ArgumentCollection arguments, IContext context, Func<string, ITemplate> getTemplateFunc)
+        {
+            var isEditmode = context.PageContext().IsEditMode;
+            if (!isEditmode) return Enumerable.Empty<WalkResult>();
 
-    //        Dictionary<string, object> mmd = null;
+            Dictionary<string, object> mmd = null;
 
-    //        var exp = arguments[0].TokenValue;
-    //        var dotParts = exp.Split('.');
-    //        var entry = context.tryfind(dotParts[0]);
-    //        if (entry == null || entry.Value == null) return Enumerable.Empty<WalkResult>();
-            
-    //        var extractor = entry.Value as ICmsMetaDataExtrator;
-    //        if (extractor == null) return Enumerable.Empty<WalkResult>();
+            var exp = arguments[0].TokenValue;
+            var dotParts = exp.Split('.');
+            var entry = context.tryfind(dotParts[0]);
+            if (entry == null || entry.Value == null) return Enumerable.Empty<WalkResult>();
 
-    //        mmd = extractor.GetCmsModelMetadata(exp);
-    //        if (arguments.Count > 1)
-    //        {
-    //            mmd["fieldType"] = arguments[1].TokenValue;
-    //        }
+            var extractor = entry.Value as ICmsMetaDataExtrator;
+            if (extractor == null) return Enumerable.Empty<WalkResult>();
 
-    //        if(mmd == null) return Enumerable.Empty<WalkResult>();
-    //        return WalkResultHelpers.Buffer(Convert(mmd)).ToFSharpList();
-    //    }
-    //}
+            mmd = extractor.GetCmsModelMetadata(exp);
+            if (arguments.Count > 1)
+            {
+                mmd["fieldType"] = arguments[1].TokenValue;
+            }
+
+            if (mmd == null) return Enumerable.Empty<WalkResult>();
+            return WalkResultHelpers.Buffer(Convert(mmd)).ToFSharpList();
+        }
+    }
 }
--- Mozu.SiteBuilder.Mvc/Tags/CMS/DataAttributesTag.cs
+++ Mozu.SiteBuilder.Mvc/Tags/CMS/DataAttributesTag.cs
@@ -14,190 +14,178 @@ using System.Web;
 using System.IO;
 using Mozu.SiteBuilder.UX.Models.ModelMetaData;
 using NDjango.FiltersCS.Compatibility;
+using Newtonsoft.Json.Linq;
 
 namespace Mozu.SiteBuilder.Mvc.Tags.Data
 {
-    //[Obsolete]
-    //[NDjango.ParserNodes.Description("wrapper of MVC HTML ActionLink Extension")]
-    //[Name("data_attributes")]
-    //public class DataAttributesTag : SimpleTagBase
-    //{
-    //    string Convert (IReadOnlyDictionary<string, object> mmd )
-    //    {
-    //        var sw = new StringWriter();
-    //        object  obj;
-    //        var attributeName = "data-editing-element";
-    //        if (mmd.TryGetValue("data-attribute-name", out obj))
-    //        {
-    //            attributeName = obj.ToString();
-    //        }
-    //        if ( mmd.TryGetValue ( "data-editing", out obj ))
-    //        {  
-    //            if (obj is JsonValue)
-    //            {
-    //                ((JsonValue)obj).Save(sw, JsonSaveOptions.None);
-    //            }
-    //            if (obj is Newtonsoft.Json.Linq.JObject)
-    //            {
-    //                ((Newtonsoft.Json.Linq.JObject) obj).WriteTo(new JsonTextWriter(sw) {Formatting = Newtonsoft.Json.Formatting.None});
-    //            }
-    //            else if (obj is string)
-    //            {
-    //                sw.Write((string)obj);
-    //            }
-    //            else
-    //            {
-    //                var jv =  JsonValueExtensions.CreateFrom(obj);
-    //                jv.Save(sw, JsonSaveOptions.None);
-    //            }
-    //        }
-    //        else
-    //        {
-    //            var json = new JsonObject();
+    [Obsolete]
+    [NDjango.ParserNodes.Description("wrapper of MVC HTML ActionLink Extension")]
+    [Name("data_attributes")]
+    public class DataAttributesTag : SimpleTagBase
+    {
+        string Convert(IReadOnlyDictionary<string, object> mmd)
+        {
+            var sw = new StringWriter();
+            object obj;
+            var attributeName = "data-editing-element";
+            if (mmd.TryGetValue("data-attribute-name", out obj))
+            {
+                attributeName = obj.ToString();
+            }
+            if (mmd.TryGetValue("data-editing", out obj))
+            {
+                var value = obj as JToken;
+                if (value != null)
+                {
+                    value.WriteTo(new JsonTextWriter(sw) { Formatting = Formatting.None });
+                }
+                else if (obj is string || obj?.GetType().IsPrimitive == true)
+                {
+                    sw.Write(obj.ToString());
+                }
+                else
+                {
+                    var j = JToken.FromObject(obj);
+                    j.WriteTo(new JsonTextWriter(sw) { Formatting = Formatting.None });
+                }
+            }
+            else
+            {
+                var j = JToken.FromObject(obj);
+                j.WriteTo(new JsonTextWriter(sw) { Formatting = Formatting.None });
+            }
 
-    //            foreach (var item in mmd)
-    //            {
-    //                JsonPrimitive jPrim;
-    //                if (JsonPrimitive.TryCreate(item.Value, out jPrim))
-    //                {
-    //                    json[ToJSName(item.Key)] = jPrim;
-    //                }
-    //            }
+            return attributeName + "=\"" + HttpUtility.HtmlAttributeEncode(sw.GetStringBuilder().ToString()) + "\"";
+        }
 
-    //            json.Save(sw, JsonSaveOptions.None);
-    //        }
+        public string ToJSName(string name)
+        {
+            return name;
+        }
 
-    //        return  attributeName +  "=\"" + HttpUtility.HtmlAttributeEncode(sw.GetStringBuilder().ToString()) + "\"";
-    //    }
+        protected override IEnumerable<WalkResult> ProcessTag(ArgumentCollection arguments, IContext context, Func<string, ITemplate> getTemplateFunc)
+        {
+            var isEditmode = context.PageContext().IsEditMode;
+            if (!isEditmode) return Enumerable.Empty<WalkResult>();
 
-    //    public string ToJSName(string name)
-    //    {
-    //        return name;
-    //    }
-        
-    //    protected override IEnumerable<WalkResult> ProcessTag(ArgumentCollection arguments, IContext context, Func<string, ITemplate> getTemplateFunc)
-    //    {
-    //        var isEditmode = context.PageContext().IsEditMode;
-    //        if (!isEditmode) return Enumerable.Empty<WalkResult>();
+            var obj = arguments[0].Value;
+            Dictionary<string, object> mmd = null;
+            if (obj != null)
+            {
+                var container = obj as IModelMetadataContainer;
+                if (container != null)
+                {
+                    var cont = container;
+                    mmd = cont.GetModelMetadata();
 
-    //        var obj = arguments[0].Value;
-    //        Dictionary<string, object> mmd = null;
-    //        if (obj != null)
-    //        {
-    //            var container = obj as IModelMetadataContainer;
-    //            if (container != null)
-    //            {
-    //                var cont = container;
-    //                mmd = cont.GetModelMetadata();
+                }
+            }
+            var exp = arguments[0].TokenValue;
+            var dotParts = exp.Split('.');
+            var dependantItem = obj as IModelMetadataAncestorDependantItem;
+            if (dependantItem != null)
+            {
+                var context2 = context;
+                var item = dependantItem;
+                var ancestors = dotParts.Reverse().Select(context2.tryfind).Where(x => x != null).Select(x => x.Value).ToList();
 
-    //            }
-    //        }
-    //        var exp = arguments[0].TokenValue;
-    //        var dotParts = exp.Split('.');
-    //        var dependantItem = obj as IModelMetadataAncestorDependantItem;
-    //        if (dependantItem != null)
-    //        {
-    //            var context2 = context;
-    //            var item = dependantItem;
-    //            var ancestors = dotParts.Reverse().Select(context2.tryfind).Where(x => x != null).Select(x => x.Value).ToList();
+                mmd = item.GetModelMetadata(ancestors);
+            }
+            if (mmd == null)
+            {
+                //todo:  totally refactor this beatch
+                if (dotParts.Length == 2)
+                {
+                    var entry = context.tryfind(dotParts[0]);
+                    if (entry != null && entry.Value != null)
+                    {
+                        var parentObj = entry.Value;
+                        var container = parentObj as IModelMetadataParentContainer;
+                        if (container != null)
+                        {
+                            var cont = container;
+                            mmd = cont.GetModelMetadata(dotParts[1]);
+                        }
+                    }
+                }
+                else if (dotParts.Length > 2)
+                {
+                    var entry = context.tryfind(dotParts[0]);
+                    if (entry != null && entry.Value != null)
+                    {
+                        var parentObj = entry.Value;
+                        for (int i = 1; i < dotParts.Length; i++)
+                        {
+                            object childObj = null;
+                            var dictionary = parentObj as IDictionary;
+                            if (dictionary != null)
+                            {
+                                try
+                                {
+                                    childObj = dictionary[dotParts[i]];
+                                }
+                                catch
+                                {
+                                    break;
+                                }
+                            }
+                            if (childObj == null && parentObj is IDictionary<string, object>)
+                            {
+                                try
+                                {
+                                    childObj = ((IDictionary<string, object>)parentObj)[dotParts[i]];
+                                }
+                                catch
+                                {
+                                    break;
+                                }
+                            }
+                            if (childObj == null)
+                            {
+                                break;
+                            }
+                            parentObj = childObj;
+                            if (i == dotParts.Length - 1)
+                            {
+                                var container = childObj as IModelMetadataContainer;
+                                if (container != null)
+                                {
+                                    var cont = container;
+                                    mmd = cont.GetModelMetadata();
+                                }
+                                if (mmd == null && childObj is IModelMetadataAncestorDependantItem)
+                                {
+                                    var context2 = context;
+                                    var item = (IModelMetadataAncestorDependantItem)childObj;
+                                    var ancestors = dotParts.Reverse().Select(context2.tryfind).Where(x => x != null).Select(x => x.Value).ToList();
 
-    //            mmd = item.GetModelMetadata(ancestors);
-    //        }
-    //        if (mmd == null)
-    //        {
-    //            //todo:  totally refactor this beatch
-    //            if (dotParts.Length == 2)
-    //            {
-    //                var entry = context.tryfind(dotParts[0]);
-    //                if (entry != null && entry.Value != null)
-    //                {
-    //                    var parentObj = entry.Value;
-    //                    var container = parentObj as IModelMetadataParentContainer;
-    //                    if (container != null)
-    //                    {
-    //                        var cont = container;
-    //                        mmd = cont.GetModelMetadata(dotParts[1]);
-    //                    }
-    //                }
-    //            }
-    //            else if (dotParts.Length > 2)
-    //            {
-    //                var entry = context.tryfind(dotParts[0]);
-    //                if (entry != null && entry.Value != null)
-    //                {
-    //                    var parentObj = entry.Value;
-    //                    for (int i = 1; i < dotParts.Length; i++)
-    //                    {
-    //                        object childObj = null;
-    //                        var dictionary = parentObj as IDictionary;
-    //                        if (dictionary != null)
-    //                        {
-    //                            try
-    //                            {
-    //                                childObj = dictionary[dotParts[i]];
-    //                            }
-    //                            catch
-    //                            {
-    //                                break;
-    //                            }
-    //                        }
-    //                        if (childObj == null && parentObj is IDictionary<string, object>)
-    //                        {
-    //                            try
-    //                            {
-    //                                childObj = ((IDictionary<string, object>)parentObj)[dotParts[i]];
-    //                            }
-    //                            catch
-    //                            {
-    //                                break;
-    //                            }
-    //                        }
-    //                        if (childObj == null)
-    //                        {
-    //                            break;
-    //                        }
-    //                        parentObj = childObj;
-    //                        if (i == dotParts.Length - 1)
-    //                        {
-    //                            var container = childObj as IModelMetadataContainer;
-    //                            if (container != null)
-    //                            {
-    //                                var cont = container;
-    //                                mmd = cont.GetModelMetadata();
-    //                            }
-    //                            if (mmd == null && childObj is IModelMetadataAncestorDependantItem)
-    //                            {
-    //                                var context2 = context;
-    //                                var item = (IModelMetadataAncestorDependantItem)childObj;
-    //                                var ancestors = dotParts.Reverse().Select(context2.tryfind).Where(x => x != null).Select(x => x.Value).ToList();
+                                    mmd = item.GetModelMetadata(ancestors);
+                                }
+                            }
+                            else if (i == dotParts.Length - 2)
+                            {
+                                var container = childObj as IModelMetadataParentContainer;
+                                if (container != null)
+                                {
+                                    var cont = container;
+                                    mmd = cont.GetModelMetadata(dotParts[dotParts.Length - 1]);
+                                }
+                            }
+                            if (mmd != null)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            if (mmd == null)
+            {
+                System.Diagnostics.Debug.WriteLine("refactor away from mmd");
+                return Enumerable.Empty<WalkResult>();
+            }
 
-    //                                mmd = item.GetModelMetadata(ancestors);
-    //                            }
-    //                        }
-    //                        else if (i == dotParts.Length - 2)
-    //                        {
-    //                            var container = childObj as IModelMetadataParentContainer;
-    //                            if (container != null)
-    //                            {
-    //                                var cont = container;
-    //                                mmd = cont.GetModelMetadata(dotParts[dotParts.Length - 1]);
-    //                            }
-    //                        }
-    //                        if (mmd != null)
-    //                        {
-    //                            break;
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //        }
-    //        if (mmd == null)
-    //        {
-    //            System.Diagnostics.Debug.WriteLine("refactor away from mmd");
-    //            return Enumerable.Empty<WalkResult>();
-    //        }
-
-    //        return WalkResultHelpers.Buffer(Convert(mmd)).ToFSharpList();
-    //    }
-    //}
+            return WalkResultHelpers.Buffer(Convert(mmd)).ToFSharpList();
+        }
+    }
 }
