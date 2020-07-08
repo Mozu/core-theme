6d44cafcea4759da365f4898a0ff3a4cfc2e6988
RameshDesai <Ramesh.Desai@kibocommerce.com>
2020-07-08 23:28:47 +0530

    COM-2007:Curbside Shipment Landing Page | LET US KNOW HOW TO DELIVER YOUR ITEMS & WE WILL BE WITH YOU SHORTLY
    updated template as per review comments

--- scripts/pages/crubside.js
+++ scripts/pages/crubside.js
@@ -10,7 +10,6 @@ define([
 var CrubsideCustomerModel = Backbone.MozuModel.extend({
     mozuType: 'crubside', //This is a reference to the JS SDK object
     onDeliveryMethodSubmit: function(payload){
-        console.log("test");
         var self = this;
         return api.action('customer', 'orderCurbsideEvent', payload);
     }
@@ -27,26 +26,48 @@ var CrubsideCustomerView = Backbone.MozuView.extend({
     onDeliveryMethodSubmit: function(e) {
         // Here you could use e.element to search the dom and get any data we need.
         // Since we have hooked to autoUpdate no need.
-
+        
         var self = this,
             $target = $(e.currentTarget);
 
+
             //Our payload is kinda odd for this request. Normally we would not have to manually create the payload 
             // The request would just use the data in the model.
+            e.preventDefault();
+            var isBackofficePreview = $('[data-mz-isBackofficePreview]').val();
             var payload ={
                 orderNumber: self.model.get('orderNumber'),
                 orderId: self.model.get('orderId'),
                 shipmentNumber: self.model.get('shipmentNumber'),
-                CurbsideFormData: {
-                    'vehicleModel': self.model.get('vehicleModel'),
-                    'parkingSpotText': self.model.get('parkingSpotText'),
-                    'deliveryMethod' : self.model.get('deliveryMethod')
-                }
+                CurbsideFormData: [
+                    {
+                
+                        key: $('#parkingspotlabel').text(),
+                        value: $('[data-mz-parkingspotText]').val()
+                    },
+                    {
+                        key: $('#vehiclemodelLabel').text(),
+                        value: $('[data-mz-vehiclemodel]').val()
+                    },
+                    {
+                        key: $('#deliverymethodlabel').text(),
+                        value: $('[data-mz-deliverymethod]').val()
+                    }
+                ]
             };
+             // if it is preview then blocking the acutal call and redirecting to qrcode.
+            if(isBackofficePreview === 'True'){
+                window.location.href = window.location.pathname + '-qrcode';
+              }
+              else {
 
-        this.model.onDeliveryMethodSubmit(payload).then(function(){
-            //Do after request stuff here.
-        });
+                this.model.onDeliveryMethodSubmit(payload).then(function(respnse){
+                    self.model.set('hasCurbsideData', respnse.data.hasCurbsideData);
+                    self.model.set('qrCode',respnse.data.qrCode)
+                    self.render();
+
+                });
+    }
     },
     render: function(){
         //Do things before component render here
@@ -61,10 +82,10 @@ var CrubsideCustomerView = Backbone.MozuView.extend({
 
 $(document).ready(function () {
     var crubsideModel = new CrubsideCustomerModel(require.mozuData("curbside") || {});
-    console.log("test");
     var crubSideView = new CrubsideCustomerView({
         el: $('#mz-customer-at-curbside-content'),
         model: crubsideModel
+
     });
 
     crubSideView.render();
