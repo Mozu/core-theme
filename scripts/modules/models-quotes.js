a75a2e06eee68e64e9a012775291b2551de2735f
Sagar Raut <sagar.raut@kibocommerce.com>
2020-11-18 16:06:48 +0530

    COM-2595: Created a Edit Quote page (UI) for a buyer / seller.
    Note: All of this is just UX to UI. Not any real data.

---
+++ scripts/modules/models-quotes.js
@@ -0,0 +1,61 @@
+define(["modules/api", 'underscore', "modules/backbone-mozu", "hyprlive", "modules/models-product"], function(api, _, Backbone, Hypr, ProductModels, ReturnModels) {
+
+    var QuoteItem = Backbone.MozuModel.extend({
+            relations: {
+                product: ProductModels.Product
+            },
+            helpers: ['uniqueProductCode'],
+            uniqueProductCode: function() {
+                //Takes into account product variation code
+                var self = this,
+                    productCode = self.get('productCode');
+
+                if (!productCode) {
+                    productCode = (self.get('product').get('variationProductCode')) ? self.get('product').get('variationProductCode') : self.get('product').get('productCode');
+                }
+                return productCode;
+            },
+            toJSON: function() {
+                var j = Backbone.MozuModel.prototype.toJSON.apply(this, arguments);
+                if (j.parent) {
+                    j.parent = j.parent.toJSON();
+                }
+                return j;
+            }
+        }),
+
+        QuoteItemsList = Backbone.Collection.extend({
+            model: QuoteItem
+        }),
+
+        Quote = Backbone.MozuModel.extend({
+            mozuType: 'quote',
+            idAttribute: 'id',
+            relations: {
+                items: QuoteItemsList
+            },
+            handlesMessages: true,
+            initialize: function() {
+                var self = this;
+
+            }
+        }),
+        QuoteCollection = Backbone.MozuPagedCollection.extend({
+            mozuType: 'quotes',
+            defaults: {
+                pageSize: 5
+            },
+            relations: {
+                items: Backbone.Collection.extend({
+                    model: Quote
+                })
+            }
+        });
+
+    return {
+        QuoteItem: QuoteItem,
+        Quote: Quote,
+        QuoteCollection: QuoteCollection
+    };
+
+});
