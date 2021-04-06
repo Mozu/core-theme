d880655fab9ac80ab7ae0e0a5e9ffd344316b33e
Rajesh  D <32330565+rajeshdatir@users.noreply.github.com>
2021-04-06 09:26:15 +0530

    COM-3587 B2B P2: Enforce account filter on buyer Quotes grids (#317)
    When using the filters on the buyer Quotes grids the customerAccountId filter was being dropped. Enforce the account filter when searching from the buyer Quotes grid on My Account and the View B2B Account page.
    
    Co-authored-by: RajeshDatir <rajesh.datir@kibocommerce.com>

--- scripts/modules/b2b-account/quotes.js
+++ scripts/modules/b2b-account/quotes.js
@@ -251,14 +251,21 @@ define([
             self.model.syncApiModel();
             self.render();
         },
-        filter: function(collection) {
+        filter: function (collection) {
+            var self = this;
             var filterStr = "";
             var qName = $("#searchName").val();
             var qNumber = $("#searchQuoteNumber").val();
             var status = $("#statusDropdown").val();
             var expDate = $("#expirationdate").val();
-            var accountId = $("#selectb2bAccountGrid > input").data('mz-value');
+            var accountId = "";
 
+            if (isSalesRep) {
+                accountId = $("#selectb2bAccountGrid > input").data('mz-value');
+            } else {
+                var viewB2BAccount = self.model.attributes.viewB2BAccount;
+                accountId = viewB2BAccount ? self.model.attributes.accountToView : require.mozuData("user").accountId;
+            }
             if (accountId) {
                 filterStr += accountNameFilter + accountId;
             }
