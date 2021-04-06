e087f106f69e53860167aaaae93dcd98fc79cbcb
Prakhar Khandelwal <36433443+prakhar308@users.noreply.github.com>
2021-04-06 07:06:24 +0530

    COM-3580 B2B P2: Add View Quote action to the View B2B Account page (#316)
    Add support for the View Quote action on the View B2B Account page quotes grid. This is necessary to allow buyers to view quotes on a child/descendant account as they can not be viewed from the My Account quotes grid.
    Also remove View Account option from the account hierarchy UI if the account is the user's own account.

--- scripts/modules/b2b-account/account-hierarchy.js
+++ scripts/modules/b2b-account/account-hierarchy.js
@@ -161,12 +161,14 @@ define(["modules/jquery-mozu", 'modules/api', "underscore", "hyprlive", "modules
         },
         setAccountOnHierarchy: function (item, accounts, isDescendantOfAssociatedAccount) {
             var self = this;
+            var isOwnAccount = require.mozuData('user').accountId === item.id;
+
             // isDescendantOfAssociatedAccount - This parameter will be false until recursion finds the account associated with the current user. On further recursion it will enable descendant behaviors.
             if (!isDescendantOfAssociatedAccount) {
                 //Check whether this is a current logged in users associated account.
-                isDescendantOfAssociatedAccount = require.mozuData('user').accountId === item.id;
-                //set "View Account" menu on current account.
-                item.canViewAccount = isDescendantOfAssociatedAccount && (self.isUserAdmin() || self.isUserPurchaser());
+                isDescendantOfAssociatedAccount = isOwnAccount;
+                //Can't view an account unless it is a descendant of your own account
+                item.canViewAccount = false;
             }
             else {
                 //set menu's on all current associated accounts descendant accounts.
@@ -177,8 +179,7 @@ define(["modules/jquery-mozu", 'modules/api', "underscore", "hyprlive", "modules
             item.account = self.getAccount(item.id, accounts);
 
             //All accounts that this account can view (self + descendants) are supported as parent accounts when adding a new child account
-            if (item.canViewAccount === true)
-            {
+            if (isOwnAccount || item.canViewAccount === true) {
                 supportedParentAccounts.push(item.account);
             }
 
