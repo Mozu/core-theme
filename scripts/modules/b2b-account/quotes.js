633ca4fc1c69e54220d67f1f8bfcd3c0b666f0bf
BhushanSharma <Bhushan.Sharma@kibocommerce.com>
2021-05-04 17:53:18 +0530

    COM-3573- Added account search configuration field
    configure sales rep user id and sort direction
    
    Combine filter string added remove extra configuration

--- scripts/modules/b2b-account/quotes.js
+++ scripts/modules/b2b-account/quotes.js
@@ -147,30 +147,25 @@ define([
                         model: B2BAccountModels.b2bAccounts,
                         pageSize: 20,
                         textField: 'companyOrOrganization',
+                        filterOption: 'cont',
                         valueField: 'id',
                         placeHolder: $('#selectb2bAccount > .mz-dd-placeholder').text(),
-                        filterOption: 'cont',
-                        filterOption1: 'eq',
                         noRecords: $('#selectb2bAccount > .mz-dd-no-records').text(),
                         pageSelector: 'selectb2bAccount',
-                        optionalFilter: 'and',
-                        filterKey: 'isActive',
-                        filterValue: 'true',
-                        userId: userId
+                        filters: 'isActive eq true and salesrep.userid eq ' + userId,
+                        sortDirection: 'asc'
                     });
                     $("#selectb2bAccountGrid").mozuPaginatedSearchableDropdown({
                         model: B2BAccountModels.b2bAccounts,
                         pageSize: 10,
                         textField: 'companyOrOrganization',
+                        filterOption: 'cont',
                         valueField: 'id',
                         placeHolder: $('#selectb2bAccountGrid > .mz-dd-placeholder').text(),
-                        filterOption: 'cont',
-                        filterOption1: 'eq',
                         noRecords: $('#selectb2bAccountGrid > .mz-dd-no-records').text(),
                         pageSelector: 'selectb2bAccountGrid',
-                        optionalFilter: 'and',
-                        filterKey: 'isActive',
-                        filterValue: 'true'
+                        filters: 'isActive eq true and salesrep.userid eq ' + userId,
+                        sortDirection: 'asc'
                     });
                 }
             }
