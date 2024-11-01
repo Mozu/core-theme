32de17891e06acaa977b4f437afef59c0b7912db
kibo-kevinwatts <kevin.watts@kibocommerce.com>
2024-11-01 18:59:35 -0500

    tecr-311: include sitesubdirectory in auth redirect filter

--- Mozu.SiteBuilder.Mvc/ActionFilters/NoWarmAuthActionFilter.cs
+++ Mozu.SiteBuilder.Mvc/ActionFilters/NoWarmAuthActionFilter.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using Microsoft.Extensions.DependencyInjection;
+using Mozu.SiteBuilder.Mvc.Contexts;
 
 namespace Mozu.SiteBuilder.Mvc.ActionFilters
 {
@@ -13,10 +14,14 @@ namespace Mozu.SiteBuilder.Mvc.ActionFilters
         }
         public override void OnActionExecuting(ActionExecutingContext actionContext)
         {
+            var siteContext = actionContext.HttpContext.RequestServices.GetService<SiteContext>();
             var sbContext = actionContext.HttpContext.RequestServices.GetService<ISiteBuilderApiContext>();
             if (sbContext.UserClaims.IsAnonymous || sbContext.UserClaims.IsAuthenticationHot) return;
-            var returnUrl = ReturnUrl ?? System.Web.HttpUtility.UrlEncode(actionContext.HttpContext.Request.Path) + actionContext.HttpContext.Request.QueryString.Value;
-            actionContext.Result = new RedirectResult(new Uri("/user/login?returnUrl=" + returnUrl, UriKind.Relative).ToString());
+            var returnUrl = string.IsNullOrEmpty(ReturnUrl)
+                ? System.Web.HttpUtility.UrlEncode(actionContext.HttpContext.Request.Path) +
+                  actionContext.HttpContext.Request.QueryString.Value
+                : siteContext.SiteSubdirectory + ReturnUrl;
+            actionContext.Result = new RedirectResult(new Uri( siteContext.SiteSubdirectory + "/user/login?returnUrl=" + returnUrl, UriKind.Relative).ToString());
         }
     }
 }
