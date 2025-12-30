15c4e630a5bcea845c90cb97f19f79df639fdd79
Bhushan Dhage <86051527+Kibo-Bhushan@users.noreply.github.com>
2025-12-30 16:03:51 +0530

    fixed event

--- scripts/pages/amazon-checkout-v2.js
+++ scripts/pages/amazon-checkout-v2.js
@@ -66,6 +66,16 @@ require(["modules/jquery-mozu","modules/backbone-mozu", "modules/eventbus","unde
 		// Use Amazon Pay V2 model
 		var checkoutModel = window.order = new AmazonCheckoutModelsV2.AwsCheckoutPage(checkoutData);
 
+		// Listen for checkout complete event to navigate back to main checkout
+		checkoutModel.on("awscheckoutcomplete", function(id){
+			var checkoutUrl = hyprlivecontext.locals.siteContext.generalSettings.isMultishipEnabled ? "/checkoutv2" : "/checkout";
+
+			if(checkoutModel.attributes.originalQuoteId)
+				window.location = "/checkout/quoteOrder/" + id;
+			else
+				window.location = checkoutUrl + "/" + id;
+		});
+
 		var tableElement = $('#shippingBillingTbl');
 		
 		if (tableElement.length > 0) {
